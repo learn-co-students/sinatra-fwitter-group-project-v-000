ae88c4a3da7b0fbdb8e113f6196c44c4ba4fdbcc
benser1 <briganenser28@gmail.com>
2016-02-06 10:42:21 -0700

    Delete action working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -105,25 +105,32 @@ class ApplicationController < Sinatra::Base
 
   end
 
-  post '/tweets/:id/edit' do 
-    @tweet = Tweet.find_by_id(params[:id])
-    if @tweet.update(content: params[:content])
-      redirect '/tweets'
+  patch '/tweets/:id/edit' do 
+    if logged_in? 
+      @tweet = Tweet.find_by_id(params[:id])
+      @user = current_user
+      if @user.id == @tweet.user_id && params[:content] != ""
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect '/tweets'
+      end
     else
       redirect '/login'
     end
-
-  end 
-
-  post '/tweets/:id/delete' do
-    @tweet = Tweet.find_by_id(params[:id])
-    if session[:id] == @user.id 
-      @tweet.destroy
-      redirect '/tweets'
-    else 
-      redirect '/login'
-    end
   end
+
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+       @tweet = Tweet.find_by_id(params[:id])
+       if session[:id] == @tweet.user_id
+        @tweet.delete 
+        redirect '/tweets'
+     else
+       redirect '/login'
+     end
+   end
+ end
 
   get '/logout' do 
     session.clear
