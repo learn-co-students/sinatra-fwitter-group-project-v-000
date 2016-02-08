1789b7722ef03c1c10045377ac3db970a72c2425
Chris Aporta <christopher.aporta@gmail.com>
2016-02-08 00:39:36 -0500

    build models, migrations, and pass specs for homepage && signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,29 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:user_id].nil?
+      erb :signup
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    params.any? {|k,v| v.blank?} ? redirect('/signup') : user = User.create(params)
+    session[:user_id] = user.id
+    redirect '/tweets'
+  end
+
+  get '/tweets' do
+    erb :'tweets/index'
   end
 
 end
