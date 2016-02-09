8c5db03c84cfa446d31642b85131144263278037
Chris Aporta <christopher.aporta@flatironschool.com>
2016-02-08 20:04:16 -0500

    pass initial series of specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -24,6 +22,29 @@ class ApplicationController < Sinatra::Base
     params.any? {|k,v| v.blank?} ? redirect('/signup') : user = User.create(params)
     session[:user_id] = user.id
     redirect '/tweets'
+  end
+
+  get '/login' do
+    if session[:user_id].nil?
+      erb :login
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    if User.find_by(params).nil?
+      redirect '/login'
+    else
+      user = User.find_by(params)
+      session[:user_id] = user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/logout' do
+    session.clear if !session[:user_id].nil?
+    redirect '/login'
   end
 
   get '/tweets' do
