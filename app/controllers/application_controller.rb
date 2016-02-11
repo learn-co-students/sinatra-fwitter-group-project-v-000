d37da13c085aa643bedf256310580410a4d32791
Karuna <imkaruna@gmail.com>
2016-02-11 10:38:41 -0500

    Initial commit -Controller Model and Views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,50 @@
 require './config/environment'
-
+require 'pry'
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:username].nil?
+      erb :signup
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if (params[:username] == "" || params[:email]=="" || params[:password] =="")
+      redirect '/signup'
+    else
+      session[:username] = params[:username]
+      redirect "/tweets"
+
+    end
+  end
+
+  get '/login' do
+    if session[:username].nil?
+      erb :login
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    session[:username] = params[:username]
+    @username = User.find_by(params[:username])
+    @tweets = @username.tweets
+    redirect "/tweets"
+  end
+
 
 end
