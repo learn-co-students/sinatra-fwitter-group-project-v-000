8ec59bd0e47848dccaf78588b8213acae34363a8
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-02 20:59:20 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,6 +68,67 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/tweets/new' do 
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do 
+    if params[:content].empty?
+      redirect '/tweets/new'
+    else
+      @tweet = Tweet.create(content: params[:content], user: current_user)
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do 
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if current_user.id == @tweet.user_id
+        erb :'tweets/edit_tweet'
+      else
+        redirect '/tweets'
+      end
+    else 
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id/edit' do 
+    if params[:content] == ""
+      redirect "/tweets/#{@tweet.id}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    end
+
+  end
+
+  post '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if current_user.id == @tweet.user_id
+      @tweet.delete
+      redirect '/tweets'
+    else 
+      redirect '/tweets'
+    end
+  end
+
   get '/logout' do 
     session.clear
     redirect '/login'
