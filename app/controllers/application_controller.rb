c16299785dfbb44c025d6dc66c297ef8c144da68
github username <github email address>
2016-03-05 18:19:05 -0500

    created login and signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -7,4 +8,60 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+     redirect to '/tweets' 
+    end
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    user = User.new(params)
+    if user.save
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/tweets' do
+    @user = current_user
+    erb :'tweets/tweets'
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username]) 
+    if !!user && user.authenticate(params[:password])
+      session[:id] = user.id
+    end
+    redirect "/tweets"
+  end
+
+  get '/logout' do
+    session.clear
+  end
+
+  #helper methods
+  helpers do
+
+    def current_user
+      User.find_by(id: session[:id])
+    end
+
+    def logged_in?
+      !!session[:id]
+    end
+  end
 end
