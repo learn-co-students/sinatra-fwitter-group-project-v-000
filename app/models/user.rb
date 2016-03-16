195139e50aef6c2aba4bf4f2544a518549ba77b6
shinyrachel <rachel.kory@gmail.com>
2016-03-16 10:34:17 -0400

    passing user spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def slug
-    self.name.gsub(" ", "-")
+    self.username.gsub(" ", "-")
   end
 
   def self.find_by_slug(slug)
     name = slug.gsub("-", " ")
-    self.find_by(name: name)
+    self.find_by(username: name)
   end
 
 end
