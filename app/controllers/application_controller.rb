397810c7cd679bd81c5764326c5dadc30e19aa8a
Smulligan85 <sean.mulligan85@gmail.com>
2016-03-17 11:58:23 -0700

    Add secure_password, and slug methods to User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :index
+  end
 
+  get 'singup' do
+    erb :signin
+  end
+
+  post 'signup' do
+
+  end
 end
