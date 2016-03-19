7271d334b820bdf46c087a737c09bfaf2e67611a
LKWLaLa <lkwlala@gmail.com>
2016-03-19 15:37:47 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,49 @@
 class UsersController < ApplicationController
 
-get "/users/new" do
-    erb :"users/create_user"
-  end 
+get "/signup" do
+  erb :"users/create_user"
+end
+
+post "/signup" do
+        user = User.new(params[:user])
+        if user.save
+            redirect "/login"
+        else
+            redirect "/failure"
+        end
+    end
+
+
+  get "/login" do
+    erb :"users/login"
+  end
+
+   post "/login" do
+        user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/success"
+        else
+            redirect "/failure"
+        end
+    end
+
+  get "/success" do
+    @user = User.find_by(session[:user_id])
+    if @user.logged_in?
+      erb :success
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect to "/"
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ActiveRecord::Base
   has_many :tweets
+
+  has_secure_password
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def slug
+      self.username.downcase.gsub(" ", "-")
+    end
+
+    def self.find_by_slug(slug)
+      User.all.find{|user| user.slug == slug}
+    end
+
+
 end
