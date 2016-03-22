b4d378451c86ba520e9d28599ea87b74ce000a6d
sameera91 <sameera.bachoti@gmail.com>
2016-03-22 15:11:24 +0000

    Finished homepage and started signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,19 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :layout
+  end
+
+  get '/signup' do
+    erb :index
+  end
+
+  post '/signup' do
+    redirect "/tweets"
+  end
+
+  get '/login' do
+    erb :"tweets/users/login"
+  end
 end
