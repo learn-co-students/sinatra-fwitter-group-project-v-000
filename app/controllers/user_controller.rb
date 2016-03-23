eb47b9af4b0696b932bcdbab5b7b1d01683bebda
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-23 10:31:05 -0500

    Controllers Fixed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,7 +8,17 @@ class UserController < ApplicationController
   end
 
   get '/login' do
-    erb :'/tweets/tweets'
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/signup' do
