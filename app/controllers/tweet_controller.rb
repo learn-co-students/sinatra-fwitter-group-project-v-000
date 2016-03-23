08a1ae19fabd9c0f66b31d2def576851ccc7f4ac
thelucasmoore <lucasedwardmoore@gmail.com>
2016-03-23 12:47:54 -0500

    Passing New Action Tests

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,24 +1,39 @@
 class TweetController < ApplicationController
 
   get '/tweets' do
-    @tweet = Tweet.all
     if User.is_logged_in?(session)
+      @tweet = Tweet.all
       erb :'/tweets/tweets'
     else
       redirect '/login'
     end
   end
 
-  post '/tweets' do
-    @tweet = Tweet.create(params["tweet"])
-    if params["tweet"]["content"].empty?
-      redirect :'/tweets/new', locals: {message: "Your tweet was empty."}
+  get '/tweets/new' do
+    if User.is_logged_in?(session)
+      @user = User.current_user(session)
+      erb :'/tweets/create_tweet'
     else
-      erb :'/tweets/show', locals: {message: "Successfully created tweet."}
+      redirect to "users/login"
     end
   end
 
-  get '/tweets/new' do
-    erb :'/tweets/create_tweet'
+  post '/tweets' do
+    if params[:content].empty?
+      redirect to "/tweets/new", locals: {message: "Your tweet was empty."}
+    else
+      @user = User.find_by_id(session[:user_id])
+      @tweet = Tweet.create(:content => params[:content], :user_id => @user.id)
+      redirect to "/tweets/#{@tweet.id}", locals: {message: "Successfully created tweet."}
+    end
+  end
+
+  get '/tweets/:id' do
+    if User.is_logged_in?(session)
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else 
+      erb :'users/login'
+    end
   end
 end
