1661c28e93a24772dbe40cfbc4fed044f2536295
Lady Codington <myemailforcoding@gmail.com>
2016-03-28 12:14:55 -0400

    Building Login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,40 @@
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
-    # set :views, 'app/views'
-    set :views, Proc.new { File.join(root, "../views/") }
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
   get '/' do
-    binding.pry
-    erb :'index'
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:id]
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if empty_fields?
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets' do
+    @user = User.find_by(id: session[:id])
+    erb :'tweets/index'
+  end
+
+  private
+  def empty_fields?
+    params[:username].empty? || params[:password].empty? || params[:email].empty?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  # has_secure_password
+  has_secure_password
+
+  def slug
+    username.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    User.find_by(username: slug.gsub('-',' '))
+  end
 end
