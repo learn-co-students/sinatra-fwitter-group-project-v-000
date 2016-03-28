054c22cc5b96a2a9bcd9380595f7766159abc2e9
Lady Codington <myemailforcoding@gmail.com>
2016-03-28 12:44:54 -0400

    tweets index after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,20 @@ class ApplicationController < Sinatra::Base
     erb :'tweets/index'
   end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/tweets"
+    else
+        redirect "/failure"
+    end
+  end
+
   private
   def empty_fields?
     params[:username].empty? || params[:password].empty? || params[:email].empty?
