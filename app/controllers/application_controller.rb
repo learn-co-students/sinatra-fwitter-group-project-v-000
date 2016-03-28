bf2dbf10573da3dffbc43f9e7869064d2913ff10
Lady Codington <myemailforcoding@gmail.com>
2016-03-28 11:11:18 -0400

    new fail yay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
-require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    # set :views, 'app/views'
+    set :views, Proc.new { File.join(root, "../views/") }
   end
 
+  get '/' do
+    binding.pry
+    erb :'index'
+  end
 end
