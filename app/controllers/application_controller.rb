b1044132a30829f9c0e7a9dcbc9688e44a937168
cernanb <cernanb@gmail.com>
2016-04-02 17:15:08 -0700

    created fwitter app with user authentication, restful routes, and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    if logged_in?
+      get '/tweets'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
   end
 
 end
