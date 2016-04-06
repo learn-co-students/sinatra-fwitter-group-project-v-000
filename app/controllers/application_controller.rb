458a848cf9c431efeb837f66f7e8ad6698435383
jenjiyi <jenny.yamada@gmail.com>
2016-04-06 09:42:29 -0700

    passed specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,26 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "ftwitter_session_secret"
+  end
+
+  get '/' do 
+    erb :index
+  end
+
+  helpers do
+    def is_logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
