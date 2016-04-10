4e56de4d9c7007957ea4bfc7f8cc51a2a402da9b
tyleresmith <oats000@hotmail.com>
2016-04-10 10:37:00 -0400

    all tests green!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,65 @@
 require './config/environment'
 
+
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/login' do
+    if !session[:user_id]
+      erb :index
+    else
+      redirect '/tweets'
+    end
+  end
+
+  get '/signup' do
+    if !session[:user_id]
+      erb :signup
+    else
+      redirect '/tweets' 
+    end
+
+  end
+
+  post '/signup' do
+
+
+    if !params.any? {|key, value| value.empty?} 
+      @user = User.create(params)
+      session[:user_id] = @user.id
+     
+      redirect '/tweets'
+    end
+
+    redirect '/signup'
+
+  end
+
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect to '/tweets'
+    end
+    redirect '/login'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
   end
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+  has_secure_password
+  
+  include Slugifiable
+  extend Slugifiable
+end
