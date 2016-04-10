5856f00ba8291e7a8b33fe87e1c4308f130142c7
viparthasarathy <viparthasarathy@gmail.com>
2016-04-09 18:59:16 -0700

    complete tweet creation feature

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,29 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      @user = current_user
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    tweet = Tweet.new(params)
+    if tweet.save
+      current_user.tweets << tweet
+      redirect to "/tweets/#{tweet.id}"
+    else
+      redirect to '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+  end
+
+
 end
