61f964771713803607f58bfff21e837ad3b83e4e
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-04-28 18:17:45 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,48 +10,6 @@ class ApplicationController < Sinatra::Base
   end
 
   get('/') {erb :homepage}
-  get('/signup') {if logged_in?; redirect to "/tweets" else erb :signup end}
-  get('/login') {erb :login}
-  # post('login') {@user = User.find_by(params[:username]) ;erb :"/tweets/tweets"}
-  get('/tweets/new') {erb :'/tweets/new'}
-  post('/tweets') {erb :'/tweets/tweets'}
-  get '/tweets' do 
-    if logged_in?
-      @user = current_user
-      erb :'/tweets/tweets'
-    else
-      erb :login
-    end
-  end
-
- post '/signup' do 
-    @user = User.create(params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to "/tweets"
-    else
-      redirect to "/signup"
-    end
-  end
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/tweets/tweets'
-    else
-      erb :login
-    end
-  end
-
-  get '/tweets/:id/edit' do
-    @tweet = Tweet.all.find_by_id(params[:id])
-    erb :"/tweets/edit"
-  end
-
-  post '/tweets/:id' do
-    @tweet = Tweet.all.find_by_id(params[:id]) 
-    erb :'/tweets/show'
-  end
 
   helpers do
 
@@ -61,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       if logged_in?
-      @user = User.find(session[:user_id])
+        @user = User.find(session[:user_id])
       end
     end
 
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,69 @@
 class TweetsController < ApplicationController
 
+  get('/tweets/show') {@user = current_user; erb :"tweets/show"}
+
+  get('/tweets/new') {if logged_in?; erb :'/tweets/new' else redirect to "/login" end}
+
+  post '/tweets' do
+    @tweet = Tweet.create(content: params["content"], user_id: session[:user_id])
+    if @tweet.save
+      @user = current_user
+      erb :'/tweets/index'
+    else
+      redirect to "tweets/new"
+    end
+  end
+
+  get('/tweets/index'){@user = current_user; erb :"/tweets/index"}
+
+  get '/tweets' do 
+    if logged_in?
+      @user = current_user
+      erb :'/tweets/index'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.all.find_by_id(params[:id])
+      erb :"/tweets/edit"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/tweets/:id' do 
+    if logged_in?
+      @user = current_user
+      @tweet = Tweet.all.find_by_id(params[:id])
+      erb :"tweets/show"
+    else
+      redirect to "/login"
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.all.find_by_id(params[:id])
+    @tweet.content = params["content"]
+    if @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @user = current_user
+      @tweet = Tweet.all.find_by_id(params[:id])
+      if @tweet.user == current_user
+        @tweet.destroy
+      end
+    else
+      redirect to "/login"
+    end
+    redirect to "/tweets/index"
+  end
 end
