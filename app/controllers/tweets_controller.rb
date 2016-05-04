8c668c64e3200a9a1516fd8c282c256b84fb1c0d
Heber Sandoval <he3ers@gmail.com>
2016-05-04 08:05:46 -0400

    Show action not redirecting bt can display user's tweet, edit action can make edits and save tweet.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,56 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new'do
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to '/tweets/new'
+    else
+      @user = User.find_by_id(session[:user_id])
+      @tweet = Tweet.create(content: params[:content],user_id: @user.id)
+      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do
+    if session[:user_id]
+      @user = User.find_by_id(session[:user_id])
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @user =  User.find_by_id(session[:user_id])
+    if session[:user_id]
+      if @user == @tweet.user
+        erb :'/tweets/edit_tweet'
+      else
+        redirect to "/tweets/#{@tweet.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if params[:content] == ""
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
 end
