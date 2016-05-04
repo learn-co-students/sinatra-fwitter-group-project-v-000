ebf1e2c12cca73e9e7d2bef880a5954f0ae0f688
Heber Sandoval <he3ers@gmail.com>
2016-05-04 05:44:24 -0400

    Can logout and redirect to login page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,4 +37,13 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/logout' do
+    if session[:user_id] != nil
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
 end
