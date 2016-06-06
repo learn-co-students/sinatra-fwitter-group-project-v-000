426cbecda80e7a0236f18fe8497620993296f063
LonghornRach <LonghornRach@users.noreply.github.com>
2016-06-05 21:47:40 -0500

    finished user index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,9 +2,31 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
+      @user = current_user
+      @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
       redirect '/login'
     end
   end
+
+  get '/tweets/new' do
+    if logged_in?
+      @user = current_user
+      erb :'/tweets/create_tweet'
+    else
+      '/'
+    end
+  end
+
+  post '/tweets' do
+    @user = current_user
+    @tweet = Tweet.new(:content => params[:content], :user_id => @user.id)
+    if @tweet.save
+      redirect "/users/#{@user.slug}"
+    else
+      erb :'/tweets/create_tweet'
+    end
+  end
+
 end
