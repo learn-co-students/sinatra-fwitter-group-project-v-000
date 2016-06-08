03f9272db9566b816b536fe7671e12090b31e7d6
jbok4 <jbok4@aol.com>
2016-06-08 13:55:13 -0400

    create tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,8 +9,56 @@ class TweetsController < ApplicationController
     end   
   end
 
-  get '/tweets/create_tweet/' do
-    "Hello World"
+  get '/tweets/new' do
+    if session[:user_id]
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content].empty?
+      redirect to '/tweets/new'
+    else
+      @tweet = Tweet.new(content: params[:content])
+      @tweet.user_id = current_user.id
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do 
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else 
+      redirect to '/users/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if session[:user_id] 
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet.user_id == session[:user_id]
+       erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do 
+    if !params[:content].empty?
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    else
+     redirect to "/tweets/#{params[:id]}/edit"
+    end
   end
 
 end
