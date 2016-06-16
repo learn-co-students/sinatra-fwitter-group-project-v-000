c2b60a3a025ffc47280bacca97e6aa81e5d848bb
PeteAJ <peter.jacobson@gmail.com>
2016-06-16 15:38:41 -0400

    Done.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+require './config/environment'
+
+class TweetsController < ApplicationController
+
+get '/tweets' do
+    "hello world" 
+    erb :'tweets/tweets'
+  end
+
+  post '/tweets' do
+    "hello world"
+  end
+
+  get '/tweets/create_tweet' do
+    if logged_in?.empty?
+      redirect "/users/login"
+    else
+      erb :'tweets/create_tweet'
+    end
+  end
+
+ get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+end
