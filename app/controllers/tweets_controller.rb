c91be29556c52a6c8abb67e7ba94eb3c04e18dea
aback1227 <aback1227@gmail.com>
2016-06-16 14:01:17 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,4 +32,51 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id' do
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+      if session[:user_id] == @tweet.user_id
+        erb :'/tweets/edit_tweet'
+      else
+        redirect "/tweets"
+      end
+    else
+      redirect "/login"
+    end
+  end
+
+  patch '/tweets/:id' do
+    if params[:content].empty?
+      redirect "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = (params[:content])
+      @tweet.save
+      redirect "/tweets" 
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+      if session[:user_id] == @tweet.user_id 
+        @tweet.delete
+        redirect "/tweets"
+      else
+        redirect "/tweets"
+      end
+    else
+      redirect "/login"
+    end
+  end
+
 end
