80355172c4439224116f0f3a6b795d83f5e71004
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-06-27 21:15:25 +0000

    capybara can't find submit button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"    
   end
 
   get '/' do
@@ -12,20 +14,81 @@ class ApplicationController < Sinatra::Base
   end
   
   get '/signup' do
-    erb :signup
+    if session[:id]
+      redirect '/tweets'
+    else
+      erb :'/users/create_user'
+    end
   end
   
   get '/login' do
-    erb :login
+    if session[:id].nil?
+      erb :'/users/login'
+    else
+      redirect '/tweets'
+    end
+  end
+  
+  get '/logout' do
+    if session[:id]
+      session.clear
+    else
+      redirect '/login'
+    end
   end
   
   get '/tweets' do
-    @tweets = Tweet.all
-    erb '/tweets/index'
+    if session[:id]
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
   
-  post '/users' do
-    @user = User.create(params[:user])
-    redirect '/tweets'
+  get '/tweets/new' do
+    if session[:id]
+      @user = User.find(session[:id])
+      erb :'/tweets/create_tweet'
+    else
+      redirect :'/login'
+    end
+  end
+  
+  get '/tweets/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+  
+  get '/tweets/:slug' do
+    erb :'/tweets/show_tweet'
+  end
+  
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save
+      session[:id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+  
+  post '/login' do
+    @user = User.find_by(username: params[:username], email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+  
+  post '/tweets' do
+    if session[:id].nil?
+      redirect '/login'
+    else
+      @tweet = Tweet.create(content: params[:content], user_id: session[:id])
+      redirect "/tweets"
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+  validates_presence_of :username, :email, :password
 
   def slug
     self.username.split(" ").join("-").downcase
