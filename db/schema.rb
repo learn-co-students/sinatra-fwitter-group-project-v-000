e8f16ef78d33c94a73fdcbb01d33d52cdb2beb74
joshnevius <joshnevius92@gmail.com>
2016-07-02 13:04:22 -0400

    Project completed.

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160612213519) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
   end
