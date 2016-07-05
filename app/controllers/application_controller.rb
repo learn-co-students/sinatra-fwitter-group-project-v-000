e22abbd5301b54cf37f0c62a1933bac3bbb33062
Christian Whitesides <whitesidescr@gmail.com>
2016-07-05 12:36:35 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
   
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "my_application_secret"
+    set :session_secret, "fwitter_secret"
   end
 
   get '/' do 
@@ -14,32 +16,60 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :'/users/create_user'
+    if is_logged_in?
+      redirect to '/tweets'
+    else
+      erb :'/users/create_user'
+    end
   end
 
   post '/signup' do
     @user = User.new(params[:user])
     if @user.save
       session[:id] = @user.id
-      redirect to "/tweets"
+      redirect to '/tweets'
     else
-      redirect to "/signup"
+      flash[:message] = "Error: Please fill in all the fields."
+      redirect to '/signup'
     end
   end
 
   get '/tweets' do
-    @user = User.find(session[:id])
-    erb :'/tweets/tweets'
+    if is_logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/index'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/login' do
+    if is_logged_in?
+      redirect to '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect to '/tweets'
+    else
+      flash[:message] = "It looks like you don't have an account yet. Sign up now to get started."
+      redirect to '/signup'
+    end
   end
 
 
   helpers do
-    def logged_in?
-      !!session[:user_id]
+    def is_logged_in?
+      !!session[:id]
     end
 
     def current_user
-      User.find(session[:user_id])
+      User.find(session[:id])
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ActiveRecord::Base
   include Slugifiable::InstanceMethods
   extend Slugifiable::ClassMethods
 
-  has_secure_password 
+  has_many :tweets
 
-  validates_presence_of :username, :email, :password
+  has_secure_password
   
+  validates_presence_of :username, :email, :password
+
 end
