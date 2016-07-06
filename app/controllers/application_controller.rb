dd3fa57ed2b1cf929ad1dc036cae05dbf454e109
miclp <michaelpeimani@gmail.com>
2016-07-06 09:26:10 -0700

    Add file structure and files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions unless test?
+    set :session_secret, "imnottelling"
   end
 
 end
