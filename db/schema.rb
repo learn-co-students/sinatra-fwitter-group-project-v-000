8c2c219807cb0b13fd76ddbe36d3aecfd49d6616
Nathaniel Miller <dukeoflaser@gmail.com>
2016-07-14 08:31:32 -0700

    Create Tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713232742) do
+ActiveRecord::Schema.define(version: 20160714152902) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.string  "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
