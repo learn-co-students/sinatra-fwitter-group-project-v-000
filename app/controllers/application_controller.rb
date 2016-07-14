f324c23a04fbc9f9d6a2b47793bdaa04e4308a46
Nathaniel Miller <dukeoflaser@gmail.com>
2016-07-14 11:06:23 -0700

    User can signup with valid parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,27 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    redirect to('/tweets') if is_logged_in?
+    erb :signup
+  end
+
+  post '/signup' do
+    a = params[:username]
+    b = params[:email]
+    c = params[:password]
+
+    [a, b, c].each do |param|
+      redirect to('/signup') if param.empty?
+    end
+
+    User.new( username: a, email: b, password: c)
+    redirect to('/tweets')
+  end
 
   helpers do
     def is_logged_in?
-      session[:user_id].nil?
+      !session[:user_id].nil?
     end
 
     def current_user
