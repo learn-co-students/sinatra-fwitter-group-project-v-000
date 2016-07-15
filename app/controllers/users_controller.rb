3f3c3c84a75d2bd7da3474dd9e52f7b755629810
aprietof <adrianprieto00@gmail.com>
2016-07-15 17:11:14 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,16 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
     end
-
     @user = User.create(username:params[:username], email:params[:email], password:params[:password])
     session[:user_id] = @user.id
     redirect to "/tweets/tweets"
-    
+  end
+
+  get '/login' do
+    erb :'users/login'
   end
 
 
