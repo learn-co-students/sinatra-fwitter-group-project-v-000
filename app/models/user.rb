a417decb561d9cb76da536c3109cdf8b12d4fa58
bshap93 <benihana858@gmail.com>
2016-07-23 17:24:58 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   has_many :tweets
+  has_secure_password
   
   def slug
-    self.name.downcase.split(" ").join("-")
+    self.username.downcase.split(" ").join("-")
   end
 
   def self.find_by_slug(slug)
-    self.class.all.find{|item| item.slug == slug}
+    self.all.find{|item| item.slug == slug}
   end
 end
--- db/migrate/20160723212436_create_users.rb.rb
+++ db/migrate/20160723212436_create_users.rb.rb
@@ -1,7 +1,9 @@
-class CreateUsers.rb < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :email
+      t.string :password_digest
+    end
   end
 end
