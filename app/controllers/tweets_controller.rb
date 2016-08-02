ce60c155c4e205cb0514922ec14dcdd0fb5e1d82
Han Lee <hanlee12@gmail.com>
2016-08-02 21:06:28 +0000

    pass all tests

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,65 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect to "/login"
+    end
+  end
+
+  post "/tweets" do
+    tweet = Tweet.new(content: params["content"])
+    if tweet.content != ""
+      tweet.user_id = current_user.id
+      tweet.save
+      redirect to "/tweets"
+    else
+      redirect to "/tweets/new"
+    end
+  end
+
+  get "/tweets/:id" do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get "/tweets/:id/edit" do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+      redirect to "/login"
+    end
+  end
+
+  patch "/tweets/:id" do
+
+    @tweet = Tweet.find(params[:id])
+    if params[:content] != ""
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect "tweets/#{@tweet.id}"
+    else
+      redirect "tweets/#{@tweet.id}/edit"
+    end
+  end
 
 
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    if current_user == @tweet.user
+      @tweet = Tweet.find(params[:id])
+      @tweet.delete
+      redirect to '/tweets'
+    else
+      redirect to "/login"
+    end
+  end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User <ActiveRecord::Base
 
   def self.find_by_slug(slug)
     stop_words = %w{a an and the or for of nor with}
-    name = slug.split("-").each_with_index.map { |word, index| stop_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+    name = slug.split("-").join(" ")
     self.find_by(username: name)
   end
 
