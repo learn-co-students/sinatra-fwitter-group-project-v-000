012d70c8747451bec5f1107841ad92ed1ebead20
ebanh <ebanhernandez@gmail.com>
2016-08-19 16:44:39 +0900

    added controllers and views for tweets CRUD

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,12 +1,52 @@
+require 'pry'
+
 class TweetController < ApplicationController
+
+  get '/tweets' do
+    erb :"/tweets/index"
+  end
 
   get '/tweets/new' do
     erb :"/tweets/new"
   end
 
   post '/tweets' do
-    
-    redirect to "/tweets/#{tweet.id}"
+    @tweet = Tweet.create(params)
+
+    redirect to "/tweets/#{@tweet.id}"
   end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :"/tweets/show"
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :"/tweets/edit"
+  end
+
+  get '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+
+    erb :"/tweets/delete"
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect "/tweets"
+  end
+
 
 end
