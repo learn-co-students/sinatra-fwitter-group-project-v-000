6304ca326dd27105f92a587c269aa54589b0ef88
Chris-Sexton <champlain2013@gmail.com>
2016-08-29 12:33:12 -0400

    Done.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,4 @@
+require 'pry'
 describe 'User' do 
   before do
     @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
@@ -14,7 +15,7 @@ describe 'User' do
   it 'has a secure password' do 
     
     expect(@user.authenticate("dog")).to eq(false)
+
     expect(@user.authenticate("test")).to eq(@user)
-    
   end
 end
