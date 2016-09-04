7ca105bd6716c27b8b4971a80f6672b3aa33395f
ThisIsTyDell <milleb94@gmail.com>
2016-09-04 13:24:41 -0500

    add ClassMethod find_by_slug

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -4,4 +4,10 @@ module Slugifiable
       self.name.gsub(" ","-").downcase
     end
   end
+
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find{|x| x.slug == slug}
+    end
+  end
 end
