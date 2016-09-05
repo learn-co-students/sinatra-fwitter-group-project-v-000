3b113060dba05b63c0a3a8e2b2df4f28e5c028e1
ThisIsTyDell <milleb94@gmail.com>
2016-09-05 13:20:42 -0500

    implement bcrypt to encrypt passwords and protect user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,8 +40,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    user = User.find_by(username: params[:username], password: params[:password])
-    if user != nil
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect '/tweets'
     else
