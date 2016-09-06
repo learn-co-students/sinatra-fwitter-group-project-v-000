385bf3bedf7f8947cc42256cab36a4f0feae46d5
ThisIsTyDell <milleb94@gmail.com>
2016-09-06 10:57:11 -0500

    add validation to only allow unique usernames and email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
 
+  validates :username, uniqueness: { case_sensitive: false }
+  validates :email, uniqueness: { case_sensitive: false }
+
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
 end
