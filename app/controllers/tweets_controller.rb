93e07fcc3358e51b93b94a4e052040f7802347d8
tylertaylor <vikingtyty@gmail.com>
2016-09-06 15:23:18 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -53,7 +53,7 @@ class TweetsController < ApplicationController
     if logged_in? && @tweet.user_id == current_user.id
       erb :'/tweets/edit'
     else
-
+      binding.pry
       redirect '/login'
     end
   end
