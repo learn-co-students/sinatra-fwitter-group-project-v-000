74f47499aac0bc1588490cc4e22b8e779add99e4
Marcel Galang <marcelgalang@gmail.com>
2016-09-16 16:17:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,61 +1,63 @@
 class TweetsController < ApplicationController
 
-  #Create
-    get '/tweets/new' do
-      if session[:user_id]
-        erb :'tweets/create_tweet'
-      else
-        redirect to '/users/login'
-      end
-    end
-
-    post'/tweets' do
-      user = User.find_by_id(session[:user_id])
-      @tweet = Tweet.create(:content=> params[:content], :user_id=> user.id)
-
-      redirect("/tweets/#{@tweet.id}")
-    end
-  #Update/edit
-    get '/tweets/:id/edit' do
-      if session[:user_id]
-        @tweet = Tweet.find_by_id(params[:id])
-        erb :'tweets/edit_tweet'
-      else
-        redirect to '/tweets'
-      end
-    end
-
-    post '/tweets/:id' do
-      @tweet = Tweet.create(params)
-      @tweet.user = User.find(params[:username])
-
-      redirect("/tweets/#{@tweet.id}")
-    end
-    #
-     delete '/tweets/:id/delete' do
-       if logged_in
-         @tweet = Tweet.find_by_id(params[:id])
-         if @tweet.user_id == current_user.id
-           @tweet.delete
-           redirect to '/tweets'
-         else
-           redirect to '/tweets'
-         end
-       else
-         redirect to '/login'
-       end
-     end
-
-  #Read
-
-    get '/tweets/:id' do
-      @tweet = Tweet.find_by_slug(params[:id])
-      erb :'tweets/show_tweet'
-    end
-
-    get '/tweets' do
+  get '/tweets' do
+    if session[:user_id]
       @tweets = Tweet.all
       erb :'tweets/tweets'
+    else
+      redirect to '/login'
     end
+  end
+
+  get '/tweets/new' do
+    if session[:user_id]
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  post'/tweets' do
+    user = User.find_by_id(session[:user_id])
+    @tweet = Tweet.create(:content=> params[:content], :user_id=> user.id)
+
+    redirect("/tweets/#{@tweet.id}")
+  end
+
+  get '/tweets/:id/edit' do
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.create(params)
+    @tweet.user = User.find(params[:username])
+
+    redirect("/tweets/#{@tweet.id}")
+  end
+
+   delete '/tweets/:id/delete' do
+     if logged_in
+       @tweet = Tweet.find_by_id(params[:id])
+       if @tweet.user_id == current_user.id
+         @tweet.delete
+         redirect to '/tweets'
+       else
+         redirect to '/tweets'
+       end
+     else
+       redirect to '/login'
+     end
+   end
+
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_slug(params[:id])
+    erb :'tweets/show_tweet'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,26 @@
 class UsersController < ApplicationController
-
-  get '/signup' do
-    if !session[:user_id]
-      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
-    else
-      redirect to '/tweets'
+require 'pry'
+    get '/signup' do
+      if !session[:user_id]
+        erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+      else
+        redirect to '/tweets'
+      end
     end
-  end
 
-  post '/signup' do
-    if params[:username] == '' || params[:email] =='' || params[:password]= ''
-      redirect to '/signup'
-    else
-      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-      session[:user_id] = @user.id
-      redirect to '/tweets'
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/tweets'
+      end
     end
-  end
+
+
+
 
   get "/login" do
     erb :login
