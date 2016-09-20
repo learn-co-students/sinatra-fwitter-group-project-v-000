5b8264386bc5f15379a0b986e7af81e24e857acd
bmolina-nyc <bruce.d.molina@gmail.com>
2016-09-19 22:15:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,117 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
-  get '/' do
-    erb :'/views/index'
+  # this is an index page 
+  # should expect logic for redirects to login, and signup
+  get '/' do  
+    erb :index
   end
+
+  # the tweets index page
+  # this appears to be the users homepage once they're signed in
+  # can grab a users specific tweets - a user has many tweets
+  get '/tweets' do 
+    @user = User.find_by_id(session[:id])
+
+      if @user 
+        erb :'tweets/tweets'
+      else
+        redirect to '/login'
+      end
+  end
+
+  # Signup with a username, login, password
+  # If a session is logged in - it should redirect to the users page
+  get '/signup' do 
+    @user = User.find_by_id(session[:id])
+    
+    if @user  # if a user session isnt already live, populate the signup
+      redirect to '/tweets'
+    else
+      erb :'/signup'
+    end
+  end
+
+  # catches the signup params - creates a new user - and logs them into the tweets homepage
+  post '/signup' do 
+    if params.values.any? { |el| el.empty? }
+      redirect to '/signup'
+    else 
+      @user = User.create(username: params["username"], email: params["email"], password: params["password"])
+      @user.save
+      session[:id] = @user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/login' do 
+
+    @user = User.find_by_id(session[:id])
+    
+    if @user  # if a user session isnt already live, populate the signup
+      redirect to '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do 
+    @user = User.find_by_id(session[:id])
+  
+    if @user
+      redirect to '/tweets'
+    else
+      @user = User.find_by(username: params["username"], password: params["password"])
+      session[:id] = @user.id
+      redirect to  '/tweets'
+    end
+  end
+
+  get '/logout' do 
+    session.clear
+    redirect to '/login'
+  end
+
+  get '/users/:slug' do 
+    @user = User.find_by_slug(params[:slug])
+    erb :'/tweets/show_tweet'
+  end
+
+  get '/tweets/new' do 
+    @user = User.find_by_id(session[:id])
+
+    if @user
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/new' do
+    @user = User.find_by_id(session[:id])
+
+    if params[:content].empty?
+      redirect '/tweets/new'
+    else
+      @tweet = Tweet.new(content: params[:content], user_id: @user.id)
+      @tweet.save
+      @user.tweets << @tweet 
+      @user.save
+      redirect to '/tweets'
+    end
+  end
+
+
+
+
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base 
   has_many :tweets
+
+
+  def slug 
+    self.username.downcase
+  end
+
+
+  def self.find_by_slug(slug)
+    User.all.detect do |target|
+      target.slug == slug
+    end
+  end
+
 end 
