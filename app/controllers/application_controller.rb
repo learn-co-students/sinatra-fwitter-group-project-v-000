097c2bdbae1e9e46c113547115bca625ac3ffc8d
WilliamMena <williammena1991@gmail.com>
2016-09-27 20:23:01 -0400

    add /signup route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,35 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do 
     erb :index
+  end
+
+  get '/signup' do 
+    if session[:user_id]
+      redirect to 'tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params["username"].empty? || params["password"].empty? || params["email"].empty?
+      redirect to '/signup'
+    else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/tweets' do
+    @user = User.find_by_id(session[:user_id])
+    erb :tweets
   end
 
 
