a654b62cae15b87cc05fdb4ba8832a4b1c9b2fdb
SnowboardTechie <bryan@snowboardteche.com>
2016-09-29 17:13:10 -0600

    Signup page and homepage created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions unless test?
+    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :'application/index'
   end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  get '/signup' do
+    if (session[:id].nil?)
+      erb :'users/signup'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if (params[:username].empty? || params[:password].empty? || params[:email].empty?)
+      redirect '/signup'
+    else
+      user = User.create(params)
+      session[:id] = user.id
+      redirect '/tweets'
+    end
+
+  end
+
+end
