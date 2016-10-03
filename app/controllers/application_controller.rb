6f0fd6153f6ff1fc862b0dd9a754b43c1f081cce
Lani1321 <lani.berry13@gmail.com>
2016-10-03 12:36:13 -0400

    create users and tweets table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,22 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # Password Security
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  helpers do 
+
+    # Finds the user in the database and returns user
+    def current_user(session_info)
+      @user = User.find_by_id(session_info["user_id"])
+    end
+    # Returns true f user id is in session 
+    def logged_in?(session_info)
+      !!session_info["user_id"]
+    end
   end
 
 end
+
