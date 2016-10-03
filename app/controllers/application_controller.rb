0d553966b63790dfa293df49e2408021995699b9
nicolefederici <nicolefederici@yahoo.com>
2016-10-03 00:36:54 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+  erb :index
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do
+    erb :'tweets/tweets'
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+
+  end
+
+  post '/signup' do
+    
+    erb :'tweets/tweets'
+  end
 end
