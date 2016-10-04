963eeabf5f99a2a0759a4ee8036ab6b8bd685326
Lani1321 <lani.berry13@gmail.com>
2016-10-03 20:08:49 -0400

    add get '/users/:id' to show a user's tweets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,16 +19,6 @@ class UsersController < ApplicationController
     end
   end
 
-  #  get '/login' do
-  #   if !session[:user_id]
-  #     erb :'users/login'
-  #   else
-  #     redirect '/tweets'
-  #   end
-  # end
-
-
-
   get '/login' do
     if !logged_in?
        erb :'/users/login'
@@ -36,16 +26,6 @@ class UsersController < ApplicationController
        redirect to '/tweets'
     end
   end
-
-  # post '/login' do
-  #   user = User.find_by(:username => params[:username])
-  #   if user && user.authenticate(params[:password])
-  #     session[:user_id] = user.id
-  #     redirect "/tweets"
-  #   else
-  #     redirect to '/signup'
-  #   end
-  # end
 
   post '/login' do
     user = User.find_by(:username => params[:username])
@@ -57,15 +37,10 @@ class UsersController < ApplicationController
     end
   end
 
-
-  # get '/logout' do
-  #   if session[:user_id] = nil
-  #     redirect to '/'
-  #   else
-  #     session.destroy
-  #     redirect to '/login'
-  #   end
-  # end
+  get '/users/:id' do 
+    @user = User.find(params[:id])
+    erb :"/users/show"
+  end
 
   get '/logout' do
     if logged_in?
@@ -76,11 +51,5 @@ class UsersController < ApplicationController
      end
 
   end
-
-
-
-
-
-
 
 end
