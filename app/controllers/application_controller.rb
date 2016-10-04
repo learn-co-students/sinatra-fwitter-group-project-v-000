68af2d91f2b86e582c9bdc0eb39b19b0ce12febf
Samuel Lair <lair001@gmail.com>
2016-10-04 00:13:59 -0400

    implemented helpers and login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,49 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "password_security"
-  end
+	configure do
+		set :public_folder, 'public'
+		set :views, 'app/views'
+		enable :sessions
+		set :session_secret, "password_security"
+	end
+
+	get '/' do 
+		erb :index
+	end
+
+	get '/tweets' do 
+		erb :'tweets/tweets'
+	end
+
+	get '/signup' do 
+		if self.logged_in
+			redirect '/tweets'
+		else
+			erb :'users/create_user'
+		end
+	end
+
+	post '/users' do 
+		@user = User.new
+		@user.username = params[:user][:username]
+		@user.email = params[:user][:email]
+		@user.password = params[:user][:password]
+		if @user.save
+			redirect '/tweets'
+		else
+			redirect '/signup'
+		end
+	end
+
+	helpers do
+		def logged_in?
+		  !!session[:user_id]
+		end
+
+		def current_user
+		  User.find(session[:user_id])
+		end
+	end
 
 end
