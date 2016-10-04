e2c974820470b21cf314be62037c3cb09b3f876a
Samuel Lair <lair001@gmail.com>
2016-10-04 02:03:49 -0400

    viewing a single tweet now requires login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -96,8 +96,12 @@ class ApplicationController < Sinatra::Base
 	end
 
 	get '/tweets/:id' do
-		@tweet = Tweet.find(params[:id])
-		erb :'tweets/show_tweet'
+		if logged_in?
+			@tweet = Tweet.find(params[:id])
+			erb :'tweets/show_tweet'
+		else
+			redirect '/login'
+		end
 	end
 
 	delete '/tweets/:id/delete' do 
