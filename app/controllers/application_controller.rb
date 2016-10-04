a5ad66ca5974c5572624517eda74fdb0d1141af8
Samuel Lair <lair001@gmail.com>
2016-10-04 00:59:32 -0400

    implemented logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,15 @@ class ApplicationController < Sinatra::Base
 		end
 	end
 
+	get '/logout' do 
+		if logged_in?
+			session.clear
+			redirect '/login'
+		else
+			redirect '/'
+		end
+	end
+
 
 	post '/signup' do 
 		@user = User.new
