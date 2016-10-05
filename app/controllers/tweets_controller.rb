9d4bb48ac00e643c49c82412026a71678273c2d6
Samuel Lair <lair001@gmail.com>
2016-10-04 21:05:42 -0400

    Done.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,60 @@
+class TweetsController < Controller
+
+	get '/tweets' do 
+		if logged_in?
+			erb :'tweets/tweets'
+		else
+			redirect '/login'
+		end
+	end
+
+	get '/tweets/new' do
+		if logged_in?
+			erb :'tweets/create_tweet'
+		else
+			redirect '/login'
+		end
+	end
+
+	post '/tweets' do 
+		@tweet = current_user.tweets.build(content: params[:content])
+		if	@tweet.save
+			redirect "/tweets/#{@tweet.id}"
+		else
+			redirect '/tweets/new'
+		end
+	end
+
+	get '/tweets/:id' do
+		if logged_in?
+			@tweet = Tweet.find(params[:id])
+			erb :'tweets/show_tweet'
+		else
+			redirect '/login'
+		end
+	end
+
+	get '/tweets/:id/edit' do
+		redirect '/login' if !logged_in?
+		@tweet = Tweet.find(params[:id])
+		redirect '/tweets' if @tweet.nil? || @tweet.user.nil? || @tweet.user != current_user
+		erb :'tweets/edit_tweet'
+	end
+
+	delete '/tweets/:id/delete' do 
+		@tweet = Tweet.find(params[:id])
+		@tweet.delete if @tweet.user == current_user
+		redirect '/tweets'
+	end
+
+	patch '/tweets' do 
+			@tweet = Tweet.find(params[:id]) 
+			@tweet.content = params[:content]
+		if	@tweet.save
+			redirect "/tweets/#{@tweet.id}"
+		else
+			redirect "/tweets/#{params[:id]}/edit"
+		end
+	end
+
+end
