03dc0350d150e2199c852f0e04e09312720ee92f
nicolefederici <nicolefederici@yahoo.com>
2016-10-05 15:53:32 -0400

    syncing with Adam

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -16,16 +18,54 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/tweets' do
+    tweet = Tweet.create(params)
+    tweet.save
+    redirect('/tweets')
+  end
+
+  get '/tweets' do
+    @tweets = Tweet.all
     erb :'tweets/tweets'
   end
 
   get '/signup' do
-    erb :'users/create_user'
-
+    if !session
+     erb :'users/create_user'
+    else
+    redirect('/tweets')
+    end
   end
 
   post '/signup' do
-    
-    erb :'tweets/tweets'
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect('/signup')
+    else
+    redirect('/tweets')
   end
+end
+
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+   post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user != nil && @user.password == params[:password]
+     
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else 
+      erb :index
+    end
+  end
+  
+
+
+
+
+
+
+
+
 end
