7197ba9a55a40b425f06b9a36ac082ae32627bad
Brad Watson <brad.watson.orlando@gmail.com>
2016-10-08 16:45:12 +0000

    edit, create and index views done, some controllers done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,12 +21,36 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/tweets/:id/edit' do
+    if is_logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet.user_id == current_user.id
+        erb :'tweets/edit_tweet'
+      else
+        redirect to '/tweets'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
   post '/tweets' do
     if params[:content] == nil || params[:content] == ""
       redirect to '/tweets/new'
     else
       @tweet = current_user.tweets.create(content: params[:content])
       redirect to '/tweets/#{@tweet.id}'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if params[:content] == "" || params[:content] == nil
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
     end
   end
 
