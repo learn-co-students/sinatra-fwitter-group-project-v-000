2a359d72bc4c8630a2e957f03e76a1aebea16d2b
Brad Watson <brad.watson.orlando@gmail.com>
2016-10-08 16:02:12 +0000

    Brad fixed tables and worked on index and create_tweet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,41 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "Fwitter is secure"
   end
 
-  
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+    if is_logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content] == nil || params[:content] == ""
+      redirect to '/tweets/new'
+    else
+      @tweet = current_user.tweets.create(content: params[:content])
+      redirect to '/tweets/#{@tweet.id}'
+    end
+  end
+
+  helpers do
+
+    def is_logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
 
 end
