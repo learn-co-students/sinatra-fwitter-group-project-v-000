408ff5208a34fe8ec7b4a27a5a431c8c44e05833
SnowboardTechie <bryan@snowboardteche.com>
2016-10-15 12:56:57 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
       @user = User.find_by(session[:id])
       erb :'tweets/index'
     else
-      redirect :'login'
+      redirect '/login'
     end
   end
 
@@ -12,13 +12,13 @@ class TweetsController < ApplicationController
     if !session[:id].nil?
       erb :'tweets/new'
     else
-      redirect :'login'
+      redirect '/login'
     end
   end
 
   post '/tweets/new' do
     if params["tweet"]["content"].nil? || params["tweet"]["content"] == ""
-      redirect :'tweets/new'
+      redirect '/tweets/new'
     else
       user = User.find_by(session[:id])
       tweet = Tweet.new(params["tweet"])
@@ -30,6 +30,28 @@ class TweetsController < ApplicationController
   get '/tweets/:id' do
     @tweet =  Tweet.find_by(params[:id])
     erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(params[:id])
+    if !session[:id].nil?
+      @user = User.find_by(session[:id])
+    end
+    if @tweet.user.id == @user.id
+      erb :'/tweets/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id/edit' do
+    if params[:content] == "" || params[:content].nil?
+      tweet = Tweet.find_by(params[:id])
+      tweet.content = params[:content]
+      redirect to "/tweets/#{tweet.id}"
+    else
+      redirect to("/tweets/#{params[:id]}")
+    end
   end
 
 end
