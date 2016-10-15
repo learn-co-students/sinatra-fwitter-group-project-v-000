d2c436c21e7b8e598dd656aa6ac60459d57add32
hilaryml <hilyttle@gmail.com>
2016-10-14 23:45:08 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,42 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+    use Rack::Flash
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def login(username, password)
+      @user = User.find_by(username: username)
+      if @user && @user.authenticate(password)
+        session[:email] = @user.email
+        redirect "/tweets"
+      else
+        redirect "/login"
+      end
+    end
+
+    def current_user
+      @current_user ||= User.find_by(:email => session[:email]) if session[:email]
+    end
+
+    def logout
+      session.clear
+    end
   end
 
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,44 @@
+class UserController < ApplicationController
+
+  get '/signup' do
+    if !logged_in?
+      erb :"/users/signup"
+    else
+      redirect "/tweets"
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if @user.save
+      session[:email] = @user.email
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect "/tweets"
+    end
+  end
+
+  post '/login' do
+    login(params[:username], params[:password])
+    redirect '/tweets'
+  end
+
+  get '/logout' do
+    logout
+    redirect '/login'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"users/show"
+  end
+
+end
