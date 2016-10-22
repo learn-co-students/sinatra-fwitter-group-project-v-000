1b38c4352898a0eb07b367c7ec95879a4175880b
Clare Horton <clare.e.horton@gmail.com>
2016-10-22 00:33:45 -0500

    tweet create/edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -14,11 +15,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/new' do
-    erb :'tweets/new'
+    erb :'/tweets/new'
   end
 
   post '/tweets' do
+    if !params[:content].empty?
+      @tweet = Tweet.create(content: params[:content])
+    else
+      redirect to "/tweets/new"
+    end
+    redirect to "/tweets/#{@tweet.id}"
+  end
 
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'/tweets/index'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'/tweets/edit'
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  post '/tweets/:id/delete' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect to "/tweets"
   end
 
 end
