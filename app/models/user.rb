513a6ac8556ce73e42e5a8a6ea978f847c917319
Will Lynch <will.lynch91@gmail.com>
2016-10-26 20:17:53 +0000

    models setup

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+    has_many :tweets
+
+    has_secure_password
+
+    def slug
+        self.username.parameterize
+    end
+
+    def self.find_by_slug(slug)
+        self.all.map do |user|
+            if user.slug == slug
+                return user
+            end
+        end
+    end
+
+end
