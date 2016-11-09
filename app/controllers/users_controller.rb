749c3149422c52962dd143aef4f7f77c58da7c57
Shmully Wolfson <shmullywolfson@gmail.com>
2016-11-09 15:46:08 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,41 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'users/create_user'
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/tweets'
+    end
   end
 
-
   post '/signup' do
-    user = User.create(username: params[:username], email: params[:email], password: params[:password])
-
-    if user
-      session[:user_id] = user.id
-      redirect "/tweets"
+    if params.any?{|key, value| value.empty?}
+      redirect to '/signup'
     else
-      redirect "/login"
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to '/tweets'
     end
   end
 
   get '/login' do
-    erb :'users/login'
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
   end
 
   post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect to '/signup'
+    end
   end
+
 
 end
