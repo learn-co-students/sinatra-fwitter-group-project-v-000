e1eef99c5e3f6246df48193d027d0faa70bd5b83
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-11-13 21:22:17 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,36 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "time again"
   end
 
-
-
   get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :"users/create_user"
+  end
+
+  post "/signup" do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get "/tweets/new" do
+    erb :"tweets/create_tweet"
+  end
+
+  post "/tweets" do
+    @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+    redirect "/tweets"
+  end
+
+  get "/tweets" do
+    erb :"tweets/tweets"
   end
 
 end
