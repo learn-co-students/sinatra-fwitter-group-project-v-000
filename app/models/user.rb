17701b377bb84a704270cbeb762c21992386ac37
Perla Yates <pianistps@yahoo.com>
2016-11-15 02:57:30 +0000

    added relationships and slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :tweets
+
+  def slug
+    self.username.gsub(" ","-")
+  end
+
+  def self.find_by_slug(arg)
+    #test-123 > test 123
+    new_arg = arg.gsub("-"," ")
+    User.find_by(username: new_arg )
+    #binding.pry
+  end
 end
