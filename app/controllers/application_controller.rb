7a77792895f7e75294351c929aff38d01a2b0dc0
Perla Yates <pianistps@yahoo.com>
2016-11-19 21:44:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,17 +36,44 @@ class ApplicationController < Sinatra::Base
     #binding.pry
     u ||= User.new(params)
     if !u.username.empty? && !u.email.empty? && u.password_digest
+      #binding.pry
       u.save
+      session[:id] = u.id
       redirect '/tweets'
     else
       redirect '/signup'
     end
   end
 
+  get '/login' do
+    if is_logged_in?(session)
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    binding.pry
+    redirect '/tweets'
+  end
+
+  get '/tweets' do
+
+    erb :'/tweets/tweets'
+  end
 
   get '/tweets/new' do
 
     erb :'/tweets/create_tweet'
+  end
+
+  get '/logout' do
+    if is_logged_in?(session)
+      #redirect '/tweets'
+    else
+      #erb :'/users/login'
+    end
   end
 
 end
