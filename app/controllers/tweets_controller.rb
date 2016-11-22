904dcdafef446dedc24b99843aecc9eae343fdb3
Perla Yates <pianistps@yahoo.com>
2016-11-22 20:42:23 +0000

    added a lot to tweets controller and actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,6 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if is_logged_in?
       @tweets = Tweet.all
-      @user = current_user
       erb :'/tweets/tweets'
     else
       redirect '/login'
@@ -23,7 +22,34 @@ class TweetsController < ApplicationController
       redirect to "/tweets/new"
     else
       current_user.tweets.create(content: params[:content])
-      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
+  get '/tweets/:id' do
+    if is_logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if is_logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if params[:content] != ""
+      @tweet.update(content: params[:content])
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
     end
   end
 end
