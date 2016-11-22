47530b11d0067aa68d9ac74bc7494285b64ade71
Perla Yates <pianistps@yahoo.com>
2016-11-22 01:51:17 +0000

    separated each action in controllers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,16 @@
+require './config/environment'
 class TweetsController < ApplicationController
+  get '/tweets' do
+    if is_logged_in?
+      @user = current_user
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
+  end
 
+  get '/tweets/new' do
+
+    erb :'/tweets/create_tweet'
+  end
 end
