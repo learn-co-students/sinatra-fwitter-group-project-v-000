0a8b2462ba76a696686b875f035447770dccc20b
Perla Yates <pianistps@yahoo.com>
2016-11-22 02:56:34 +0000

    edited tweets actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,7 @@ require './config/environment'
 class TweetsController < ApplicationController
   get '/tweets' do
     if is_logged_in?
+      @tweets = Tweet.all
       @user = current_user
       erb :'/tweets/tweets'
     else
@@ -10,7 +11,19 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/new' do
+    if is_logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
 
-    erb :'/tweets/create_tweet'
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to "/tweets/new"
+    else
+      current_user.tweets.create(content: params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    end
   end
 end
