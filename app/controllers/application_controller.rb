4afb23820dc4372b447674920e5c796a4eab0d6a
Ray Mulligan <rmullig2@optonline.net>
2016-12-07 00:37:17 -0500

    Start on application controller and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,22 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+  
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
+  
+  post '/signup' do
+    User.create(params)
+    redirect to '/tweets'
+  end
 
 end
