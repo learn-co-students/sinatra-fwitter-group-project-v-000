2039ef96a47ad7ca5723ff82047b92de090b4ad4
Lmilne <lmilne360@gmail.com>
2016-12-10 01:59:12 -0500

    Still getting errors

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,46 +1,73 @@
 class TweetsController < ApplicationController
 
 	get '/tweets' do 
+		if logged_in?
 		@tweets = Tweet.all
 		erb :'tweets/index'
+		else 
+			redirect '/login'
+		end
 	end
 
 	get '/tweets/new' do 
+		if logged_in?
 		erb :'tweets/new'
+		else
+			redirect '/login'
+		end
 	end
 
 	post '/tweets' do
-		@tweet = Tweet.new(content: params[:content])
-		@tweet.save
-
-		redirect '/tweets'
+		if params[:content] == ""
+			redirect "/tweets/new"
+		else
+		tweet = current_user.tweets.create(content: params[:content])
+		redirect "/tweets/#{tweet.id}"
+		end
 	end
 
 	get '/tweets/:id' do 
-
+		if logged_in?
 		@tweet = Tweet.find_by_id(params[:id])
 		erb :'tweets/show'
+		else
+			redirect '/login'
+		end
 	end
 
 	get '/tweets/:id/edit' do
-		@tweet = Tweet.find_by_id(params[:id])
-		erb :'tweets/edit'
+		if logged_in?
+			@tweet = Tweet.find_by_id(params[:id])
+			if current_user.id == @tweet.user_id
+		 		erb :'tweets/edit' 
+		 	else
+		 		redirect to '/tweets'
+		 	end
+		
+		else
+			redirect '/login'
+		end
 	end
 
-	post '/tweets/:id' do
-		binding.pry
-	@tweet = Tweet.find_by_id(params[:id])
-	@tweet.update(content: params[:content]) unless params[:content].blank?
-	@tweet.save	
-
-	redirect "/tweets/#{@tweet.id}"
+	patch '/tweets/:id' do
+		if params[:content].blank?
+			redirect to "/tweets/#{params[:id]}/edit"
+		else
+			@tweet = Tweet.find_by_id(params[:id])
+			@tweet.updatet(content: params[:content])
+			redirect to "/tweets/#{@tweet.id}"
+		end
+		
 	end
 
-	post '/tweets/:id/delete' do 
-		@tweet = Tweet.find(params[:id])
-		@tweet.delete
-
-		redirect "/tweets"
+	delete '/tweets/:id/delete' do 
+		if logged_in
+			@tweet = Tweet.find_by_id(params[:id])
+			@tweet.delete if @tweet.user_id == current_user.id
+			redirect "/tweets"
+		else
+			redirect "/login"
+		end		
 	end
 
 
