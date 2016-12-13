a916e53b5402c425b02d76710de77572e3dac79e
Elyse Klova <elyse.klova@gmail.com>
2016-12-13 11:06:44 -0600

    all tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,6 +58,35 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if is_logged_in?(session) && @tweet.user_id == current_user.id
+      erb :'/tweets/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find_by_id(params[:id])
+    if params[:content] != ""
+      tweet.update(content: params[:content])
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    tweet = Tweet.find_by_id(params[:id])
+    if is_logged_in?(session) && tweet.user_id == current_user.id
+      tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
   get "/users/:slug" do
     @user = User.find_by_slug(params[:slug])
     erb :'/users/show_user'
