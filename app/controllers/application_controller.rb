e504234e49bbe56e235c30443d5454d301a5016e
William Story <wstory@villanova.edu>
2016-12-13 19:28:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,34 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'/tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/create_tweeet'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params["tweet"])
+    @tweet.save
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by(params[:id])
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(params[:id])
+    erb :'tweets/edit_tweet'
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find_by(params[:id])
+    @tweet.save
+    redirect to "tweets/#{@tweet.id}"
+  end
+
 end
