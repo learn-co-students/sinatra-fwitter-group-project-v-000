52966f96712586bda3ff0970320ea25f29b902e7
Elyse Klova <elyse.klova@gmail.com>
2016-12-14 19:29:13 -0600

    corrected order of user page tweets and added success and error flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
+    use Rack::Flash
     set :session_secret, "ek_and_mk_for_lyfe"
   end
 
@@ -28,6 +28,7 @@ class ApplicationController < Sinatra::Base
       @tweets = Tweet.all.sort_by {|tweet| tweet.id}.reverse
       erb :'/tweets/tweets'
     else
+      flash[:error] = "Please log in to view tweets."
       redirect '/login'
     end
   end
@@ -36,6 +37,7 @@ class ApplicationController < Sinatra::Base
     if is_logged_in?(session)
       erb :'/tweets/create_tweet'
     else
+      flash[:error] = "Please log in or create an account to start tweeting."
       redirect '/login'
     end
   end
@@ -43,8 +45,10 @@ class ApplicationController < Sinatra::Base
   post '/tweets' do
     if params[:content] != ""
       Tweet.create(content: params[:content], user_id: session[:user_id])
+      flash[:success] = "Tweet posted successfully!"
       redirect '/tweets'
     else
+      flash[:error] = "Tweets are short, but not that short. Make sure you have text in your tweet!"
       redirect '/tweets/new'
     end
   end
@@ -54,6 +58,7 @@ class ApplicationController < Sinatra::Base
       @tweet = Tweet.find_by_id(params[:id])
       erb :'/tweets/show_tweet'
     else
+      flash[:error] = "Please log in to view tweets."
       redirect '/login'
     end
   end
@@ -62,7 +67,11 @@ class ApplicationController < Sinatra::Base
     @tweet = Tweet.find_by_id(params[:id])
     if is_logged_in?(session) && @tweet.user_id == current_user.id
       erb :'/tweets/edit'
+    elsif is_logged_in?(session) && @tweet.user_id != current_user.id
+      flash[:error] = "Hands off that tweet! Remember, you can only edit your own tweets."
+      redirect '/tweets'
     else
+      flash[:error] = "Please log in before editing any tweets."
       redirect '/login'
     end
   end
@@ -71,8 +80,10 @@ class ApplicationController < Sinatra::Base
     tweet = Tweet.find_by_id(params[:id])
     if params[:content] != ""
       tweet.update(content: params[:content])
+      flash[:success] = "Tweet updated successfully!"
       redirect "/tweets/#{tweet.id}"
     else
+      flash[:error] = "Tweets are short, but not that short. Make sure you have text in your tweet!"
       redirect "/tweets/#{tweet.id}/edit"
     end
   end
@@ -81,8 +92,13 @@ class ApplicationController < Sinatra::Base
     tweet = Tweet.find_by_id(params[:id])
     if is_logged_in?(session) && tweet.user_id == current_user.id
       tweet.delete
+      flash[:success] = "Tweet deleted."
+      redirect '/tweets'
+    elsif is_logged_in?(session) && tweet.user_id != current_user.id
+      flash[:error] = "Hands off that tweet! Remember, you can only delete your own tweets."
       redirect '/tweets'
     else
+      flash[:error] = "Please log in to delete a tweet."
       redirect '/login'
     end
   end
@@ -106,6 +122,7 @@ class ApplicationController < Sinatra::Base
 			session[:user_id] = user.id
       redirect '/tweets'
     else
+      flash[:error] = "Sorry, your username and/or password is incorrect. Please try again."
       redirect '/login'
     end
   end
@@ -122,14 +139,17 @@ class ApplicationController < Sinatra::Base
     if params.none? {|key, value| value == ""}
       user = User.create(username: params[:username], email: params[:email], password: params[:password])
       session[:user_id] = user.id
+      flash[:success] = "Account created successfully. Welcome to Fwitter!"
       redirect '/tweets'
     else
+      flash[:error] = "Please make sure you have completed all fields before creating your account."
       redirect '/signup'
     end
   end
 
   get '/logout' do
     session.clear
+    flash[:success] = "Buh-bye now!"
     redirect '/login'
   end
 
