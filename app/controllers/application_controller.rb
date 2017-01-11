680d209bee08d8275ef80972f9d70f0d9a797071
Jon Fougner <jon.fougner@gmail.com>
2017-01-11 21:10:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,54 @@ class ApplicationController < Sinatra::Base
     erb :"/tweets/tweet"
   end
 
+  get '/tweets/new' do
+    if !session[:id] then redirect "/login" end
+    @user=User.find(session[:id])
+    erb :"/tweets/new"
+  end
+
+  get "/tweets/:id" do
+    if !session[:id] then redirect "/login" end
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/show_tweet"
+  end
+
+  get '/tweets/:id/edit' do
+    if !session[:id] then redirect "/login" end
+    @tweet = Tweet.find(params[:id].to_i)
+    if session[:id] != @tweet.id then redirect "/tweets" end
+    erb :"/tweets/edit_tweet"
+  end
+
+  post '/tweets/new' do
+    if !session[:id] then redirect "/login" end
+    if params[:content]=="" then redirect "/tweets/new" end
+    @tweet = Tweet.create(content: params[:content], user_id: session[:id])
+    redirect "/tweets"
+  end
+
+  post '/tweets/:id' do
+    if params[:content]=="" then redirect "/tweets/#{params[:id]}/edit" end
+    @tweet=Tweet.find(params[:id])
+    @tweet.update(content: params[:content])
+    @tweet.save
+    redirect "/tweets"
+  end
+
+  post '/tweets/:id/delete' do
+    tweet=Tweet.find(params[:id])
+    if tweet.user.id==session[:id] then tweet.delete end
+    redirect "/tweets"     
+  end
+
   get '/logout' do
     session.clear
     redirect "/login"
+  end
+
+  get '/users/:slug' do
+    @user=User.find_by_slug(params[:slug])
+    erb :"/tweets/tweets"
   end
 
   post '/signup' do
