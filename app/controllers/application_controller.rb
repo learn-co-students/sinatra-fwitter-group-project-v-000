b26ed69d90d2b52f7b4a2cf5f5525572a1c5ce3c
Chenxi Wu <wuchenxi2013@gmail.com>
2017-01-11 23:38:38 +0100

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,37 +14,39 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    if session[:id] then redirect "/tweets" end
+    if logged_in? then redirect "/tweets" end
     erb :"/users/create_user"
   end
 
   get '/tweets' do
-    if !session[:id] then redirect "/login" end
-    @user=User.find(session[:id])
-    erb :"/tweets/tweet"
+    if !logged_in? then redirect "/login" end
+    @user=current_user
+    erb :"/tweets/tweets"
   end
 
   get '/tweets/new' do
-    if !session[:id] then redirect "/login" end
-    @user=User.find(session[:id])
+    if !logged_in? then redirect "/login" end
+    @user=current_user
     erb :"/tweets/new"
   end
 
   get "/tweets/:id" do
-    if !session[:id] then redirect "/login" end
+    if !logged_in? then redirect "/login" end
+   @session=session
     @tweet = Tweet.find(params[:id])
     erb :"/tweets/show_tweet"
   end
 
   get '/tweets/:id/edit' do
-    if !session[:id] then redirect "/login" end
+    if !logged_in? then redirect "/login" end
     @tweet = Tweet.find(params[:id].to_i)
-    if session[:id] != @tweet.id then redirect "/tweets" end
+    @user=@tweet.user
+    if session[:id]!= @user.id then redirect "/tweets" end
     erb :"/tweets/edit_tweet"
   end
 
   post '/tweets/new' do
-    if !session[:id] then redirect "/login" end
+    if !logged_in? then redirect "/login" end
     if params[:content]=="" then redirect "/tweets/new" end
     @tweet = Tweet.create(content: params[:content], user_id: session[:id])
     redirect "/tweets"
@@ -71,7 +73,7 @@ class ApplicationController < Sinatra::Base
 
   get '/users/:slug' do
     @user=User.find_by_slug(params[:slug])
-    erb :"/tweets/tweets"
+    erb :"/tweets/user_profile"
   end
 
   post '/signup' do
@@ -98,4 +100,15 @@ class ApplicationController < Sinatra::Base
       redirect "/login"
     end
   end
+
+   helpers do
+    def logged_in?
+      !!session[:id]
+    end
+
+    def current_user
+      User.find(session[:id])
+    end
+   end
+   
 end
