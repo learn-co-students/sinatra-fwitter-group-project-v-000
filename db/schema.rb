6f0c44da864d15ba45b0317b853e2b62b0216287
Senad Hadrovic <github email address>
2017-01-16 11:17:12 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,8 @@
 ActiveRecord::Schema.define(version: 20170115233741) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string "content"
+    t.string  "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
