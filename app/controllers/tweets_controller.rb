394b76196dba1d25332e258f9cda5713f7897376
Christina L. Cole <christinalcole@gmail.com>
2017-01-17 12:38:34 -0500

    initial signup routes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,5 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    erb :'/tweets/index'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
-  get '/' do
-    "Welcome to Fwitter"
+  get '/signup' do  # signup
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do #user creation
+    user = User.create(params[:user])
+    redirect to '/tweets'
   end
 end
+
+# {"user"=>
+#   {"user_name"=>"picasso", "email"=>"picasso@artist.es", "password"=>"pablo"},
+#  "Sign Up"=>"Submit"}
+
+# SIGN UP
+# You'll need to create two controller actions, one to display the user signup and one to process the form submission.
+#  The controller action that processes the form submission should create the user and save it to the database.
+# The form to sign up should be loaded via a GET request to /signup and submitted via a POST request to /signup.
+# The signup action should also log the user in and add the user_id to the sessions hash.
+# Make sure you add the Signup link to the home page.
