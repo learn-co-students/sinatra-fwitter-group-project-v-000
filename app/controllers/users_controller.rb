bc0daca141562d293dd838348d952439aa3eca4a
Christina L. Cole <christinalcole@gmail.com>
2017-01-18 15:30:25 -0500

    blocking logged-in user from signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,20 @@
 class UsersController < ApplicationController
   get '/signup' do  # signup
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
     erb :'/users/create_user'
+  end
   end
 
   post '/signup' do #user creation
-    binding.pry
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
     else
       @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-      binding.pry
-    #binding.pry
       session[:user_id] = @user.id
       redirect to '/tweets'
 
     end
   end
 end
-
-# {"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"}
-# The signup action should also log the user in and add the user_id to the sessions hash.
