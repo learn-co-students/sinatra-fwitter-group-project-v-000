f63d781e1149357670a9912690a2cd95f8292851
Christina L. Cole <christinalcole@gmail.com>
2017-01-17 22:55:09 -0500

    revising view, signup form to match test specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,17 @@ class UsersController < ApplicationController
 
   post '/signup' do #user creation
     binding.pry
-    # if !params[:user].empty
-    #   binding.pry
-      user = User.create(params[:user])
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      user = User.create(params[:user_name => "username", :email => "email", :password => "password"])
       redirect to '/tweets'
-    # else
-    #   redirect to '/signup'
-    # end
+
+    end
   end
 end
 
+# {"username"=>"skittles123", "email"=>"skittles@aol.com", "password"=>"rainbows"}
 # {"user"=>
 #   {"user_name"=>"picasso", "email"=>"picasso@artist.es", "password"=>"pablo"},
 #  "Sign Up"=>"Submit"}
