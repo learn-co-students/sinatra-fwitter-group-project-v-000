fc13d09825afad1f05e6e2bd50c8d57bfbc2a6cb
Christina L. Cole <christinalcole@gmail.com>
2017-01-19 22:01:28 -0500

    adding tweet index actions

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,9 @@ class TweetsController < ApplicationController
     if !Helpers.is_logged_in?(session)
       redirect to '/login'
     else
+      @user = User.find_by_id(session[:user_id])
+      @tweets = Tweet.all
+      #binding.pry
       erb :'/tweets/index'
     end
   end
