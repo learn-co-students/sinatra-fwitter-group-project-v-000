ac50d71953ecc6a7a9aa54412363dbf61d7a1827
Michael Burak <michaelrburak@gmail.com>
2017-01-27 17:12:19 +0000

    Done.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,7 @@
+class TweetsController < ApplicationController
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in? then
+     redirect '/tweets'
+  else
+   erb :'users/create_user'
+  end
+end
+
+  post "/signup" do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+        redirect '/tweets'
+    else
+        redirect '/signup'
+  end
+end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+  @user = User.find_by(:username => params[:username])
+  if @user && @user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect '/tweets'
+      else
+          redirect '/login'
+      end
+end
+
+get "/logout" do
+  session.clear
+  redirect "/"
+end
+
+end
