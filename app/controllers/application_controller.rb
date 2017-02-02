c475abc4707eb954d7eba5f67972ee74ff0b303c
mmelmer <mikemelmer@gmail.com>
2017-02-01 22:33:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,84 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+
+  helpers do
+    def logged_in_user
+      User.find_by(:id => session[:user_id])
+    end 
+
+    def logged_in
+      !!session[:user_id]
+    end
+
+  end
+
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if !logged_in
+      erb :'/users/create_user'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if (params[:username] == "") || (params[:email] == "") || (params[:password] == "")
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    if !logged_in
+      erb :"/users/login"
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user !=nil && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in
+      session.clear
+      redirect '/login'
+    else
+      redirect '/' 
+    end
+  end
+
+  get '/tweets' do
+    if logged_in
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 
 end
