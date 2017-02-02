007969fa0dce92d1a7858794921aa60343e99101
Brie Coyle <brie.coyle@gmail.com>
2017-02-02 00:32:52 +0000

    Initial commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,51 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+
+  end
+
+  post '/tweets' do
+
+  end
+
+  get '/tweet/:id' do
+
+  end
+
+  get '/tweet/:id/edit' do
+
+  end
+
+  patch '/tweet/:id' do
+
+  end
+
+  post '/tweet/:id/delete' do
+
+  end
+
+  get '/signup' do
+
+  end
+
+  post '/signup' do
+
+  end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+  post '/logout' do
+    
+  end
 end
