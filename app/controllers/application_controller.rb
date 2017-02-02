2e77a938d7e9d271346823c5e83e90d452e41e1e
Brie Coyle <brie.coyle@gmail.com>
2017-02-02 01:24:32 +0000

    Add signup and login functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -15,8 +18,9 @@ class ApplicationController < Sinatra::Base
 
   end
 
-  post '/tweets' do
-
+  get '/tweets' do
+    @user = User.find(session[:id])
+    erb :'tweets/tweets'
   end
 
   get '/tweet/:id' do
@@ -36,22 +40,42 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-
+    if !session[:id] == nil
+      redirect to '/tweets'
+    else
+      erb :'/users/create_user'
+    end
   end
 
   post '/signup' do
-
+    @user = User.create(params)
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      session[:id] = @user.id
+      redirect to '/tweets'
+    end
   end
 
   get '/login' do
-
+    if !session[:id] == nil
+      redirect to '/tweets'
+    else
+      erb :'/users/login'
+    end
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
   post '/logout' do
-    
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+  validate :username
+  validate :email
 
   def slug
     username.downcase.gsub(" ", "-")
