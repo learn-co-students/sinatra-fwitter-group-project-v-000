49edc79280ef5cd415a25cc3f0a9e785e4815e56
Gordon Siu <gs2@GSMB2.local>
2017-02-04 04:51:06 -0800

    separated tweet concerns into transfer controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,101 +13,13 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get "/tweets" do
-    if logged_in?
-      @user = User.find(session[:user_id])
-      @tweets = Tweet.all
-      # binding.pry
-      erb :"/tweets/tweets"
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/users/:slug' do
-      @user = User.find(session[:user_id])
-      @slug = params[:slug]
-      @show_user = User.find_by_slug(@slug)
-      erb :'/users/show_user'
-  end
-
-  get '/tweets/new' do
-     if logged_in?
-         erb :'/tweets/create_tweet'
-    else
-      redirect "/login"
-    end
-  end
-
-  post '/tweets' do
-    @user = User.find(session[:user_id])
-    if !params["content"].empty?
-        @user.tweets << Tweet.create(content: params["content"])
-        redirect to "/tweets"
-    else
-        redirect to "/tweets/new"
-    end
-  end
-
-    get '/tweets/:tweet_id' do
-     if logged_in?
-        @tweet_id = params[:tweet_id]
-        @tweet = Tweet.find_by(id: @tweet_id)
-         erb :'/tweets/show_tweet'
-    else
-      redirect "/login"
-    end
-  end
-
-    get '/tweets/:tweet_id/edit' do
-        if logged_in?
-            @user = User.find(session[:user_id])
-            @tweet_id = params[:tweet_id]
-            @tweet = Tweet.find_by(id: @tweet_id)
-            if @tweet.user_id == @user.id
-                erb :'/tweets/edit_tweet'
-            else
-                redirect "/tweets"
-            end
-        # binding.pry
-        else
-          redirect "/login"
-        end
-    end
-
-    patch '/tweets/:tweet_id' do
-        @tweet_id = params[:tweet_id]
-        @tweet = Tweet.find_by(id: @tweet_id)
-        if !params["content"].empty?
-            @tweet.update(content: params["content"])
-            redirect to '/tweets'
-        else
-            redirect to "/tweets/#{@tweet_id}/edit"
-        end
-    end
-
-    delete '/tweets/:tweet_id/delete' do
-        if logged_in?
-            @user = User.find(session[:user_id])
-            @tweet_id = params[:tweet_id]
-            @tweet = Tweet.find_by(id: @tweet_id)
-            if @tweet.user_id == @user.id
-                @tweet.delete
-            else
-                redirect "/tweets"
-            end
-        else
-          redirect "/login"
-        end
-    end
-
   helpers do
     def logged_in?
       !!session[:user_id]
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
   end #helpers
 
