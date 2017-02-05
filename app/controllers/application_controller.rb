c179edf0b31ca992f3fbf6b7273c8cea56553e52
github username <github email address>
2017-02-04 21:34:23 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -16,6 +18,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
+    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    session[:id] = @user.id
+    redirect to "/tweets"
+  end
+
+  get '/tweets' do
+    erb :"tweets/tweets"
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/login' do
     
   end
+
+  private
+  def self.current_user(session)
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    !!(session[:user_id])
+  end
+
 end
