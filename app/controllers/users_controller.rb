bd716fcdda586d63d72a1586a0e4a56b65fdcc26
github username <github email address>
2017-02-10 15:02:27 -0500

    building /signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     erb :'/users/create_user'
   end
 
+  post '/signup' do
+    if params.has_value?("")
+      redirect to '/signup'
+    else
+      User.create(params)
+    end
+    redirect to '/tweets'
+  end
 end
