5de19c5615eff54e08f560b50b997058b4e385f7
M. Simon Borg <msimonborg@gmail.com>
2017-02-19 16:01:27 -0500

    create tweet form, validate tweet length

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,16 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
     erb :'tweets/create_tweet'
   end
+
+  post '/tweets' do
+    @user = current_user
+    @tweet = Tweet.new params
+    if @tweet.save
+      @user.tweets << @tweet
+      redirect "/users/#{@user.slug}"
+    else
+      flash[:message] = @tweet.errors.full_messages.uniq
+      redirect '/tweets/new'
+    end
+  end
 end
