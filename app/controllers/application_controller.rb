d8936c1ffa608430a27b5768f8745b2db20d3bd0
John Pfingst <jpfingst@outlook.com>
2017-03-03 19:26:30 +0000

    create and edit tweets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,17 +38,64 @@ class ApplicationController < Sinatra::Base
     end
 	end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+	    redirect '/login'
+    end
+	end
+
+  get '/tweets/:id/delete' do
+    if logged_in?
+      tweet = Tweet.find(params[:id])
+      tweet.delete
+    else
+	    redirect '/login'
+    end
+	end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+	    redirect '/login'
+    end
+	end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+	    redirect '/login'
+    end
+	end
+
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
-	    redirect '/users/login'
+	    redirect '/login'
     end
 	end
 
   get '/users/:slug' do
+    user = User.find_by(name: params[:slug])
+    @tweets = Tweet.find_by(user_id: user.id)
+    erb :'/tweets/show'
+  end
 
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/failure'
+    end
   end
 
 	post '/signup' do
@@ -61,14 +108,18 @@ class ApplicationController < Sinatra::Base
     end
 	end
 
-	post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect '/tweets'
+  post '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    if tweet.update(content: params[:content])
+      redirect "/tweets/#{tweet.id}"
     else
-      redirect '/failure'
+      redirect "/tweets/#{tweet.id}/edit"
     end
+  end
+
+  post '/tweets' do
+    tweet = Tweet.create(user_id: session[:user_id], content: params[:content])
+    redirect '/tweets/new'
   end
 
   def self.slug(name)
