b80dbdca06b37d7c15f49e983dbb7e5da4acc687
BrunoBoehm <bruno.boehm@gmail.com>
2017-03-11 12:05:09 +0100

    added a signup page and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,19 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "azerty"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+  get "/" do
+  	"Welcome to Fwitter"
+  end
+
+  get "/signup" do
+  	erb :"users/new.html"
+  end
+
 
 end
