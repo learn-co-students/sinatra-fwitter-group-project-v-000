72fa7f92b279ca829b9a6696f5a2dabfb3ed835a
Joseph Surla <josephsurla@gmail.com>
2017-03-23 17:00:41 -0700

    Tweet belong to users and users has many tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,9 +2,28 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if !!session[:id]
+      @tweets = Tweet.all
       erb :tweets
     else
       redirect '/login'
     end
+  end
+
+  get '/tweets/new' do
+    if !!session[:id]
+
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    tweet = Tweet.new(content: params[:content])
+    if tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170323183945) do
+ActiveRecord::Schema.define(version: 20170323231700) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
