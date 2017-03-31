7e1c17c9530ab6ad93d14e6678bb63eb97423df6
vicision <duncan.j.mclachlan@gmail.com>
2017-03-31 17:15:06 -0400

    Created migrations and migrated, created MVC, started building out app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,26 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/users/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/users/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    user.save ? redirect to "/users/#{@user.slug}/tweets"
+  end
+
+  get '/users/login' do
+    erb :'/users/login'
+  end
+
+  post '/users/login' do
+    erb :'/users/tweets'
+  end
+
+
 end
---
+++ app/models/tweet.rb
@@ -0,0 +1,5 @@
+class Tweet < ActiveRecord::Base
+  has_many :tweets
+
+
+end
