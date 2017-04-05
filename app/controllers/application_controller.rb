2d284339346bbf6686920d307a4cf2e9e742a070
github username <github email address>
2017-04-04 21:02:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,49 +12,5 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
-
-  get "/signup" do
-  	if !!session[:id]
-  		redirect "/tweets"
-  	else
-  		erb :"users/create_user"
-  	end
-  end
-
-  post "/signup" do
-  	if params[:username] == "" || params[:email] == "" || params[:password] == ""
-  		redirect "/signup"
-  	else
-  		user = User.create(params)
-  		session[:id] = user.id
-  	end
-  	redirect "/tweets"
-  end
-
-  get "/login" do 
-  	if !!session[:id]
-  		redirect "/tweets"
-  	else
-  		erb :"users/login"
-  	end
-  end
-
-  post "/login" do
-  	user = User.find_by(params)
-  	session[:id] = user.id
-  	redirect "/tweets"
-  end
-
-  get "/tweets" do 
-  	if !!session[:id]
-  		erb :"tweets/tweets"
-  	else
-  		redirect "/login"
-  	end
-  end
-
-  get "/logout" do 
-  	session.clear
-  	redirect "/login"
-  end
+ 
 end
