38f7ac6751de0ccfae83deb4e0b6cf63fb87c827
Daniel Lempesis <danlrcn@hotmail.com>
2017-04-11 17:50:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 require './config/environment'
 
-class ApplicationController < Sinatra::Base
+class ApplicationController < Sinatra::Base #probably over-refactored this lab
 
   configure do
-    set :public_folder, 'public'
+    # set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions unless test?
     set :session_secret, "steal3th*s1pa ssword4secret1"
@@ -17,10 +17,10 @@ class ApplicationController < Sinatra::Base
     logged_in? ? (redirect '/tweets') : (erb :'/users/create_user')
   end
 
-  post '/signup' do #signs up a user and redirects to index (/tweets) if successful, else loops back to signup page.
-    redirect '/signup' if params.any? {|param| param[1].empty?} #not convinced this is more readable, but it did save space.
+  post '/signup' do #signs up a user, creates and sets session to user.id, and redirects to index (/tweets) if successful, else loops back to signup page.
+    redirect '/signup' if params.any? {|param| param[1].empty?} #Ensures no fields are blank. Not convinced this is more readable, but it did save space.
     @user = User.create(params)
-    session[:user_id] = @user.id
+    session[:user_id] = @user.id #creates session attribute :user_id and sets to @user.id until logout
     redirect '/tweets'
   end
 
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     redirect '/login' if params[:username].empty? || params[:password].empty?
     @user = User.find_by(username: params[:username]) #find and set user (if possible)
-    if @user.authenticate(params[:password])
+    if @user.authenticate(params[:password]) #create session (log user in) if supplied credentials are valid
       session[:user_id] = @user.id
       redirect '/tweets'
     else
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   end
 end
 
-  get '/logout' do
+  get '/logout' do #logs user out and redirects to login page, otherwise redirects to homepage.
     if logged_in?
       session.clear
       redirect '/login'
@@ -48,30 +48,20 @@ end
     end
   end
 
-  get '/tweets' do #loads tweet page
-    if !logged_in?
-      redirect '/login'
-    else
-      @user = current_user
-      @tweets = @user.tweets
-      erb :'/tweets/tweets'
-    end
+  get '/tweets' do #sends user to login page if not logged in, else loads tweet page
+    redirect '/login' if !logged_in?
+    @user = current_user #provides erb access to user and user's tweets
+    @tweets = @user.tweets
+    erb :'/tweets/tweets'
   end
 
-  get '/tweets/new' do #try a lambda instead of unless (and definitely instead of else/if)
-    if !logged_in?
-      redirect '/login'
-    else
-      erb :'/tweets/create_tweet'
-    end
+  get '/tweets/new' do #Presents user with new tweet form, or sends them away if not logged in
+    logged_in? ? (erb :'/tweets/create_tweet') : (redirect '/login')
   end
 
-  post '/tweets/new' do
+  post '/tweets/new' do #creates tweet from new tweet form post request unless it's blank.
     unless params[:content].empty?
-      @user = current_user
-      @tweet = Tweet.create(params) #later change to .new
-      @tweet.user_id = @user.id
-      @tweet.save
+      @tweet = Tweet.create(user_id: current_user.id, content: params[:content]) #creates new tweet using current_user object id and params
       redirect '/tweets'
     end
   end
@@ -81,40 +71,26 @@ end
     erb :"/tweets/show_tweet"
   end
 
-  get "/tweets/:id" do
-    #binding.pry
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :'/tweets/edit_tweet'
-    else
-      redirect '/login'
-    end
+  get "/tweets/:id" do #redirects to tweet edit URL, made necessary because I misunderstood that the spec didn't want edit/delete on the same page.
+    logged_in? ? (redirect "/tweets/#{params[:id]}/edit") : (redirect "/login") #login check/redirect login is just to satisfy spec at this juncture
   end
 
-  get "/tweets/:id/edit" do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :'/tweets/edit_tweet'
-    else
-      redirect '/login'
-    end
-  end
-
-  post "/tweets/:id" do
+  get "/tweets/:id/edit" do #finds tweet and renders edit page if the user is logged in, otherwise sends packing
+    redirect '/login' unless logged_in?
     @tweet = Tweet.find(params[:id])
-    if params[:content].empty?
-      redirect "/tweets/#{@tweet.id}/edit"
-    else
-      @tweet.content = params[:content]
-      @tweet.save
-      redirect '/tweets'
-    end
+    erb :'/tweets/edit_tweet'
   end
 
-  delete '/tweets/:id/delete' do
-    @user = current_user
+  post "/tweets/:id" do #Handles POST from tweet edit form and catches no-content exception
     @tweet = Tweet.find(params[:id])
-    if @user.id == @tweet.user_id
+    redirect "/tweets/#{@tweet.id}/edit" if params[:content].empty? #reloads page instead of saving tweet if tweet blank
+    @tweet.update(content: params[:content]) #otherwise modifies and saves it as expected, then redirects to /tweets
+    redirect '/tweets'
+  end
+
+  delete '/tweets/:id/delete' do #deletes tweet if if belongs to user, otherwise sends them away.
+    @tweet = Tweet.find(params[:id])
+    if current_user.id == @tweet.user_id
       @tweet.delete
       redirect '/tweets'
     else
