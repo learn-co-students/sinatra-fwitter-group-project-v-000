f033e1bf1265901ddf808c84f197931a1666f34a
bryanhou1 <Hou@Bryans-MacBook-Pro.local>
2017-04-27 18:36:52 -0400

    controller skeleton created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,49 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
+
+  get '/' do
+  	erb :index
+  	#have links to login/sign up?
+  end
+
+  get '/signup' do
+  end
+
+  post '/signup' do
+  end
+
+  get '/login' do
+  end
+
+  post '/login' do
+  end
+
+  get '/logout' do
+  	session.clear
+  end
+
+  get '' do
+  end
+
+  get '/tweets' do
+
+  end
+
+
+  get '/tweets/:id/edit' do
+  end
+
+  post '/tweets/:id' do
+  	#update to tweets/id
+  end
+
+  post '/tweets/:id/delete' do
+  	#deletion of tweet 
+  end
+
 
 end
