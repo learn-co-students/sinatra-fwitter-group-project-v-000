a3f864fc7019ee8bc4b3e5740e774db289daa1f2
Omar Mason <mr.omarmason@gmail.com>
2017-04-29 04:14:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,48 +4,77 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    set :views, 'app/'
     enable :sessions
     set :session_secret, "password_security"
   end
 
   get '/' do
-    erb :index
+    erb :'views/index'
   end
 
+  get '/tweets' do
+    if logged_in?
+       @tweets = Tweet.all
+       erb :"views/tweets/tweets"
+     else
+       redirect to "/login"
+     end    
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :"views/users/user_tweets"
+  end
+  
   get "/signup" do
-    erb :"/users/create_user"
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :"views/users/create_user"
+    end  
   end
 
   post "/signup" do
+    if !params[:username].blank? && !params[:email].blank? && !params[:password].blank?
       @user = User.create(username: params[:username], email: params[:email], password_digest: params[:password])
-      redirect "/users/login"
-  end
-
-  get '/account' do
-    @user = User.find(session[:user_id])
-    erb :account
-  end
-
-
-  get "/login" do
-    erb :'users/login'
-  end
-
-  post "/login" do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      @user = user
-      redirect "/user/index"
+      redirect to '/tweets'
     else
-      redirect "/failure"
+      redirect to '/signup'
     end
   end
 
+  get "/login" do
+    if logged_in?
+     redirect to "/tweets"
+    else
+      erb :'views/users/login'
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "users/#{user.slug}"
+    else
+      redirect "views/failure"
+    end
+  end
 
   get "/logout" do
     session.clear
     redirect "/"
   end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      User.find(session[:user_id]) if session[:user_id]
+    end
+  end
+  
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.find('5').destroy
+User.find('3').destroy
+User.find('4').destroy
