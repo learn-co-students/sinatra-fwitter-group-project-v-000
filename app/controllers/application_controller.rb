ea197030c1be85aae59ccb21aaeca89614d8d65d
Erika Hughes <ehughes@knights.ucf.edu>
2017-05-02 22:36:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,25 +5,69 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-  post '/signup' do
-    @user = User.create(params)
-    # binding.pry
-    redirect "/tweets"
-  end
-
   get '/tweets' do
     @tweets = Tweet.all
     erb :'tweets/tweets'
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect "tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    session[:user_id] = @user.id
+    if @user.save && @user.username != "" && @user.email != "" && @user.password != ""
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    # binding.pry
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
