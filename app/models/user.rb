6843f00f171410b359817b69f49cb9e9f6e1791d
Hima Chitalia <hima_chhag@yahoo.com>
2017-05-02 19:02:47 +0000

    Added validation and slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
-class Tweet < ActiveRecord::Base
+class User < ActiveRecord::Base
+
+  has_secure_password
   has_many :tweets
+
+  validates :username, :email, :password_digest, :presence => true
+
+  def slug
+    username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find {|instance| instance.slug == slug}
+  end
+
 end
