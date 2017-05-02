331e069a54026852473d2ad3d6eec09b2daa78b1
Hima Chitalia <hima_chhag@yahoo.com>
2017-05-02 20:48:52 +0000

    Added signup, login, logout controllers and necessary views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,62 @@ require 'rack-flash'
 
 class UserController < ApplicationController
   use Rack::Flash
-  
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    # binding.pry
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect to '/signup'
+      # binding.pry
+    else
+      user = User.create(username: params[:username], password: params[:password], email: params[:email])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/login' do
+    # binding.pry
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to '/tweets'
+    else
+      redirect to 'signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      # binding.pry
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/tweets' do
+    "Welcome, You are now logged in and can see all of your tweets"
+  end
 end
