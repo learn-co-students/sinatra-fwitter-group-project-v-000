030314b3068d0f53f2a56afb5d44b43cdaa9fbf2
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-03 16:38:04 -0400

    add helper methods logged_in? and current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
+  set :session_secret, "fwitter_security"
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
