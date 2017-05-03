fd918a0924e8c4c7b705d102d7f04623ea70c702
JingruZhang <zhang.jingru.93@gmail.com>
2017-05-03 13:56:11 -0500

    Added show, edit and delete actions; able to pass all specs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
     get '/tweets' do
         if logged_in?
             @user = User.find_by_id(session[:user_id])
-            erb :'users/index'
+            erb :'/tweets/tweets'
         else
             redirect to "/login"
         end
@@ -23,6 +23,45 @@ class TweetsController < ApplicationController
             @tweet.save
         else
             redirect "/tweets/new"
+        end
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'tweets/show_tweet'
+        else
+            redirect "/login"
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if logged_in?
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'tweets/edit_tweet'
+        else
+            redirect "/login"
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if !params[:content].empty?
+            @tweet.update(:content => params[:content])
+        else
+            redirect "/tweets/#{@tweet.id}/edit"
+        end
+    end
+
+    get '/tweets/:id/delete' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if logged_in?
+            if current_user == @tweet.user
+                @tweet.delete
+                redirect '/tweets'
+            end
+        else
+            redirect "/login"
         end
     end
 end
