fc7dbb8b867ffaa7f0ca9978b9d069304038854b
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-03 21:00:48 -0400

    add '/' controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,19 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
+  set :views, "app/views"
   enable :sessions
   set :session_secret, "fwitter_security"
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
   end
 
   helpers do
