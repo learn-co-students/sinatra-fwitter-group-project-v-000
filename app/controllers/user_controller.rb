6b90a4fe3dc2b73aa9be16cf1f764b2575b4be23
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-04 12:50:13 -0400

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -37,13 +37,17 @@ class UserController < ApplicationController
     end
   end
 
-  get '/logout' do
-    session.clear
-    redirect '/login'
-  end
-
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'/users/show'
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect to '/'
+    end
   end
 end
