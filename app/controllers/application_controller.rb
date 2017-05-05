1d32bcc6d82f2067b569e7022b1964e9c917daca
Scott Atkins <satkins16@gmail.com>
2017-05-05 13:29:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,9 +27,25 @@ class ApplicationController < Sinatra::Base
   post '/singup' do
     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
     if @user.save
-      redirect to ""
+      redirect to "/tweets"
     else
       redirect to "/failure"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/failure"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to "/"
   end
 
 end
