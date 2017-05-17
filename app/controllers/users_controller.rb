99a30e95debde9e6bb3416ed4893b129be2cd98c
Tracy P Holmes <tracypholmes@gmail.com>
2017-05-17 22:01:29 +0000

    signup directs user to twitter index, requires un, em, pswd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+
   get '/signup' do
-    erb :'/users/create_users'
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    if params.has_value?("") #if parameters have a value of ""
+      redirect to '/signup' # redirect them to signup
+    else # otherwise
+      User.create(params) # have them create a new account
+    end
+    redirect to '/tweets' # no matter what, redirect them all to teh tweets
   end
 
 end
