ed66b729b1637dc4d385fae8754ce00da710acdb
Nick Bair <nickbair@golfingbuddha.com>
2017-05-29 17:25:56 -0400

    User spec passing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+
+  def slug
+    self.username.gsub(/\ |[$.+'()&]/, '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect do |i|
+      i.slug == slug
+    end
+  end
 end
