cc861a419c39e09319f473c83ad8e08f9aef9b07
Mendel Hornbacher <me@mendel.tech>
2017-06-02 09:48:05 -0400

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 require './config/environment'
+require "sinatra/reloader"
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    register Sinatra::Reloader
+    enable :reloader
+  end
+
+  get '/' do
+    erb :homepage
   end
 
 end
