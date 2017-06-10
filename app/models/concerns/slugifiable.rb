f7c51e455b9cc4b59f67358fdce8f98e89a418ac
WilliamBarela <william.barela.dev@gmail.com>
2017-06-10 19:08:35 +0900

    corrected slugify module, User tests all green

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,13 +1,13 @@
 module Parser
   module ClassMethods
     def find_by_slug(slug)
-      self.all.find{|instance| instance.slug == slug}
+      self.all.find{|user| user.slug == slug}
     end
   end
 
   module InstanceMethods
     def slug
-      self.name.downcase.gsub(" ", "-")
+      self.username.downcase.gsub(" ", "-")
     end
   end
 end
