cdd2bf3e179e30d8dd7c00ab886e250fe37b7561
Matteo Piccini <gnappo.1@live.it>
2017-06-10 11:05:01 +0000

    latest commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/users/:username' do
+    @user = User.find_by(username: params[:username])
+    erb :"users/show"
+  end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
