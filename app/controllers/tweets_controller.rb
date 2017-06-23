a9b88bce3e9b1b22bab4a5a51aabfe5ffecfd476
szeidman <sam.zeidman@gmail.com>
2017-06-23 15:43:14 -0400

    Begin populating tweets controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/edit_tweet'
+  end
+
+  post 'tweets/:id'
+    @tweet = Tweet.find(params[:id])
+    @tweet.name 
+    redirect '/tweets/#{@tweet.id}'
+end
