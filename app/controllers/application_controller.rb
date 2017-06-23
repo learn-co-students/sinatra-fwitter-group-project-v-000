a02580da555d9e35d0ed4dc259396f437c0c53fa
Dave Schlauderaff <dschlaud@gmail.com>
2017-06-23 17:23:14 -0500

    signup and login forms tested and working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,17 +20,59 @@ class ApplicationController < Sinatra::Base
   post '/signup' do
     user = User.new(params[:user])
     if user.save
-      redirect '/login'  #change this to redirect directly to index page with user logged in
+      user_log_in(user)
+      redirect '/success'  #change this to redirect directly to index page with user logged in
     else
       redirect '/signup'
-      #todo: implement a rack flash message saying date not valid or somesuch
     end
   end
 
   get '/login' do
-    erb: 'users/login'
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:user][:username])
+
+    # binding.pry
+    user_log_in(user)
+    redirect '/success'
+  end
+
+  get '/success' do
+    @user = current_user
+    # binding.pry
+    erb :'users/success'
   end
 
 
+  get '/failure' do
+    erb :'users/failure'
+  end
 
+  get '/logout' do
+    session.clear
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    # helper to avoid requiring a user to log in after creating account
+    def user_log_in(user)
+      # binding.pry
+      if user && user.authenticate(params[:user][:password])
+        session[:user_id] = user.id
+        # redirect '/account' #redirect to a user's list of tweets
+      else
+        redirect '/failure'
+      end
+    end
+  end
 end
