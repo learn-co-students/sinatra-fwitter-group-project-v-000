7318a6415ec60bff9ec2aa03c48a71745131cbc6
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-25 15:21:51 -0600

    Made two users in seeds

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+user1args = {:email => "test1@test.com", :username => "test1", :password => "RockAndRoll"}
+user2args = {:email => "test2@test.com", :username => "test2", :password => "Love2Code"}
+
+User.create(user1args)
+User.create(user2args)
