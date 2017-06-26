1b1d7daabd1ff13cc55a8cd082cba737542ed67f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-26 11:52:46 -0600

    Working on the main app controller
    *added sign up and working on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,37 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end 
 
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else 
+      erb :'users/create_user'
+    end 
+  end 
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end 
+  end 
+
+  get '/login' do
+    erb :'users/login'
+  end 
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,3 @@
 class UserController < ApplicationController
 
-	get '/users/new' do
-		erb :'users/create_user'
-	end 
-
-	post 'users/new' do
-
-	end 
-
-	get 'user/:slug' do
-
-	end 
-
 end 
