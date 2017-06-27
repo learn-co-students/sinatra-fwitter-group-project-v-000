6d614b29c99d2771765a64db8d39b4affd67482a
Amanda George <github email address>
2017-06-27 10:49:07 -0500

    Done.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -309,6 +309,7 @@ describe ApplicationController do
         fill_in(:password, :with => "kittens")
         click_button 'submit'
         visit '/tweets/1/edit'
+
         expect(page.status_code).to eq(200)
         expect(page.body).to include(tweet.content)
       end
