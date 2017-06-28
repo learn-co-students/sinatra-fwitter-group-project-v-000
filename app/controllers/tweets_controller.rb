7d542a43855e16f4e825a7e4c7e07a46f4e99d51
Luisa Scavo <luisascavo@gmail.com>
2017-06-28 15:21:13 -0400

    stub out RESTful routes for tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,27 @@ class TweetsController < ApplicationController
     erb :"/tweets/tweets"
   end
 
+  get '/tweets/new' do
+
+  end
+
+  post '/tweets' do
+
+  end
+
+  get '/tweets/:id' do
+    erb :"/tweets/show"
+  end
+
+  get '/tweets/:id/edit' do
+
+  end
+
+  patch '/tweets/:id' do
+
+  end
+
+  delete '/tweets/:id' do
+    
+  end
 end
