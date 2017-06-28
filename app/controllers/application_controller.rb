3c686cae241b65a6d920e4ef59dd7daf21ea6c57
Luisa Scavo <luisascavo@gmail.com>
2017-06-28 10:22:23 -0400

    stub out controller actions for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,25 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/signup' do
+    erb :"/users/create_user"
+  end
+
+  post '/users' do
+    redirect("/tweets")
+  end
+
+  get '/login' do
+    erb :"/users/login"
+  end
+
+  get '/tweets' do
+    erb :"/tweets/tweets"
+  end
+
+  get '/logout' do
+    session.clear
+    redirect("/")
+  end
 end
