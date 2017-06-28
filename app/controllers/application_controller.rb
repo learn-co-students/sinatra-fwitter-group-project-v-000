95715bb7a890fd0b5d1c7433d76ea71e6a86d535
Luisa Scavo <luisascavo@gmail.com>
2017-06-28 10:27:03 -0400

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "ho7do9h4m6urg3r"
   end
 
   get '/' do
