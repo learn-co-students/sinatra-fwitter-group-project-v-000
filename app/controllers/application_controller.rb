b408543f247051880b9791326a3fdf533e1bd5f4
streamedLine <dovidlwtn@gmail.com>
2017-07-03 23:57:51 -0400

    adds most routes views and password security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,89 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
+
+  get '/' do 
+  	erb :index
+  end
+
+  get '/tweets' do 
+  	@tweets = Tweet.all 
+
+  	erb :'tweets/tweets'
+  end
+
+  get '/tweets/new' do 
+  	protect_data
+  	erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do 
+  	protect_data
+  	tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+
+  	redirect :"/tweets/#{tweet.id}"
+  end
+
+  get '/tweets/:id' do 
+  	protect_data
+  	@tweet = Tweet.find(params[:id])
+  	@user = User.find(@tweet.user_id)
+
+  	erb :'tweets/show_tweet'
+  end
+
+  get '/signup' do 
+  	erb :'users/create_user'
+  end
+
+  post '/signup' do 
+  	User.create(username: params[:username], password: params[:password])
+
+  	redirect :'/login'
+  end
+
+  get '/login' do 
+  	erb :'users/login'
+  end
+
+  post '/login' do 
+  	user = User.find_by(username: params[:username])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect :"/tweets"
+    else 
+    	redirect :"/login"
+    end
+  end
+
+  get '/logout' do 
+  	session.clear
+  	redirect :'/login'
+  end
+
+  get '/users' do 
+  	protect_data
+  	@user = current_user
+  	erb :'users/show'
+  end 
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def protect_data
+      redirect :'/login' if !logged_in?
+    end
+
+  end
 end
