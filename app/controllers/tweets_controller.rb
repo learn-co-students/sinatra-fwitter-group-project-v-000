6808f1857973c03113ef0962e2311fd31832e548
sarjumulmi <mulmi.sarju@gmail.com>
2017-07-08 21:53:56 -0400

    new tweet page and controller action created

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,24 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      flash[:message] = "Please log in to create tweets."
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    # binding.pry
+    if !params[:content].empty?
+      current_user.tweets.create(params)
+      redirect '/tweets'
+    else
+      flash[:message] = "Cannot create empty tweet!!"
+      redirect '/tweets/new'
+    end
+  end
+
 end
