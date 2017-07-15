3239284f65ab65bc0fde98b862e939f1bdd6563b
Andrew <github email address>
2017-07-15 17:06:22 -0400

    migrated, setup associations, homepage and signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,26 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+
+    erb :"/homepage"
+  end
+  get '/signup' do
+    @user = User.all
+    erb :"/signup"
+  end
+
+  post '/signup' do
+    puts params
+    @users = User.create(params[:user])
+    @users.save
+    session[:id]
+    redirect to "/tweets/index"
+  end
+
+  get '/sessions/login' do
+    puts "hello world"
+
+    erb :"/sessions/login"
+    end
 end
