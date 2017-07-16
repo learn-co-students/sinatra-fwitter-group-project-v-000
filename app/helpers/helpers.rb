0a15927c67a507fb29993f520cbf57f8ba690cd4
Terry Blue <terryblue@comcast.net>
2017-07-16 05:41:33 +0000

    updated views

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
 	def self.current_user(session)
-    	   @user = User.find_by(id: session[:id])
+    	@user = User.find_by(id: session[:id])
 	end
 
 	def self.is_logged_in?(session)
-    	   !!session[:id]
+    	!!session[:id]
 	end
 
 end
