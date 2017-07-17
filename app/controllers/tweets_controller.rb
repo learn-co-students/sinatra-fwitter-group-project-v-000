ec205206f6efb1effb4931757ec4e8bbc3e9b264
Terry Blue <terryblue@comcast.net>
2017-07-17 20:49:53 +0000

    updated controllers and views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,6 +20,7 @@ class TweetsController < ApplicationController
   get '/tweets/:id' do
     if Helpers.is_logged_in?(session)
       @user = User.find_by_id(session[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       erb :'tweets/show_tweet'
     else
       redirect '/login'
@@ -29,6 +30,7 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if Helpers.is_logged_in?(session)
       @user = User.find_by_id(session[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       erb :'tweets/edit_tweet'
     else
       redirect '/login'
@@ -47,15 +49,24 @@ class TweetsController < ApplicationController
   end
 
   post '/tweets/:id' do
-    binding.pry
-    redirect "tweets/#{session[:id]}"
+    if params[:content].size > 0
+      @user = User.find_by_id(session[:id])
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      erb :'tweets/show_tweet'
+    else
+      redirect "/tweets/#{params[:id]}/edit"
+    end
   end
 
-  post '/tweets/:id/delete' do
-    binding.pry
-    @tweet = Tweet.find_by_id(params[:id])
-    @tweet.delete
-    redirect to "tweets/#{session[:id]}"
+  delete '/tweets/:id/delete' do
+    if Helpers.is_logged_in?(session) && Tweet.find_by(id: params[:id], user_id: session[:id])
+      @tweet = Tweet.destroy(params[:id])
+      erb :'tweets/delete_tweet'
+    else
+      redirect '/login'
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
 
-  get '/users' do
-
-  end
-
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/users_show'
   end
-
 
 end
