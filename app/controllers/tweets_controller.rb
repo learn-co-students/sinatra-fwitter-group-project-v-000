c8bef1998d6f535cce1a6541810e64d11fc9a084
Terry Blue <terryblue@comcast.net>
2017-07-17 17:32:07 +0000

    updated views

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,6 +9,14 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/tweets/:id' do
     if Helpers.is_logged_in?(session)
       @user = User.find_by_id(session[:id])
@@ -18,17 +26,36 @@ class TweetsController < ApplicationController
     end
   end
 
-  get '/tweets/new' do
+  get '/tweets/:id/edit' do
     if Helpers.is_logged_in?(session)
-      erb :'tweets/create_tweet'
+      @user = User.find_by_id(session[:id])
+      erb :'tweets/edit_tweet'
     else
       redirect '/login'
     end
   end
 
   post '/tweets' do
+    if params[:content].size > 0
+      @user = User.find_by_id(session[:id])
+      @tweet = Tweet.create(content: params[:content])
+      @user.tweets << @tweet
+      erb :'tweets/show_tweet'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  post '/tweets/:id' do
     binding.pry
-    redirect 'tweets/show_tweet'
+    redirect "tweets/#{session[:id]}"
+  end
+
+  post '/tweets/:id/delete' do
+    binding.pry
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect to "tweets/#{session[:id]}"
   end
 
 end
