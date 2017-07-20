62c991ebefd24d6eacd36d717f299948e9d26ff1
Susanna Klosko <sdklos@gmail.com>
2017-07-20 20:14:34 +0000

    trying to fix permissions issue

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+  has_secure_password
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    @@username = slug.gsub("-", " ")
+    User.all.detect {|user| user.username.downcase == @@username}
+  end
+
+end
