a96cbfcc7b602a1aa8a9972e1054b0e8ede092ad
Susanna Klosko <sdklos@gmail.com>
2017-07-20 22:02:59 +0000

    signup page working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,37 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    if logged_in?
+      redirect("/tweets")
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params["username"].empty? && !params["email"].empty? && !params["password"].empty?
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect("/tweets")
+    else
+     redirect("/signup")
+   end
+  end
+
+  get '/tweets' do
+
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
