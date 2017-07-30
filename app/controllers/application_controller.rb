be913fbc5f03d3a3a00ec2a100057a4c9fbdd4d0
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-07-30 23:54:24 +0100

    Adds '/' route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "fwitter_secret"
+    
   end
+
+  enable :method_override
+  enable :sessions
+
+ get '/' do
+  erb :index
+ end
 
 end
