f9afdbac3c7e24f72af4434eacaf09c23defbdb6
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-07-30 23:24:33 +0100

    Adds slug and find_by_slug methods to User class.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
     has_many :tweets
+    has_secure_password
+
+    def slug
+        self.username.downcase.gsub(" ", "-")
+    end
+
+    def self.find_by_slug(slug)
+        @@user = slug.gsub("-", " ")
+        User.find_by(:username => @@user)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730210902) do
+ActiveRecord::Schema.define(version: 20170730222250) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
