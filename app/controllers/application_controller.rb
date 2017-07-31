a038bf73124082922d77e532bb3dda2a3209960f
Margarita Khodzka <mkhodzka@gmail.com>
2017-07-31 18:40:11 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < Sinatra::Base
     else
       user = User.create(params)
       session[:user_id] = user.id
-      redirect "/tweets"
+      redirect '/tweets'
     end
   end
 
@@ -66,7 +66,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets' do
-    if logged_in?
+    if logged_in? && current_user
       @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
@@ -74,6 +74,64 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/tweets/new' do
+    if logged_in? && current_user
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    @tweet = current_user.tweets.create(content: params[:content])
+    if @tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in? && current_user
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in? && current_user
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.user == current_user
+      if !params[:content].empty?
+        @tweet.update(content: params[:content])
+        redirect "/tweets/#{@tweet.id}"
+      else
+        redirect "/tweets/#{@tweet.id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user_id == current_user.id
+      @tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
 
   helpers do
     def current_user
