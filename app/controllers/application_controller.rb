c655d1cf9564c444e0f9641d769c7d69afd39f5f
Margarita Khodzka <mkhodzka@gmail.com>
2017-07-31 16:22:12 +0000

    created login and logout routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    if session[:user_id]
+    if logged_in?
       redirect '/tweets'
     else
       erb :'/users/create_users'
@@ -29,6 +29,33 @@ class ApplicationController < Sinatra::Base
       user = User.create(params)
       session[:user_id] = user.id
       redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
     end
   end
 
