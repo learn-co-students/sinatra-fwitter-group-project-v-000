e1621bd2f45727e126e97d4579f7e8cfb82be562
Margarita Khodzka <mkhodzka@gmail.com>
2017-07-31 15:55:29 +0000

    completed SignUp, added views/tweets forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,45 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, "my_application_secret"
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "secured_password"
+  end
+
+  get '/' do
+    erb :'/index'
+  end
+
+  get '/signup' do
+    if session[:user_id]
+      redirect '/tweets'
+    else
+      erb :'/users/create_users'
+    end
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect '/signup'
+    else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect "/tweets"
+    end
+  end
+
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
   end
 
 end
