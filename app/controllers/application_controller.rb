afaf8573dcf2018dd2ef792d79e280fb4face786
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-08-01 23:34:19 +0100

    Adds logic for homepage, signup page, login and logout pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,8 +12,69 @@ class ApplicationController < Sinatra::Base
   enable :method_override
   enable :sessions
 
- get '/' do
-  erb :index
- end
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:id] == nil
+      erb :'users/create_user'
+    else
+      @user = User.find(session[:id])
+      redirect to "/tweets"
+    end
+  end 
+
+  post '/signup' do
+    @user = User.new(params)
+
+    if @user.username == "" || @user.email == "" || @user.password == ""
+      redirect to "/signup"
+    elsif @user.save
+      session[:id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/signup"
+    end
+  end 
+
+  get '/tweets' do
+    if session[:id] == nil
+      redirect to "/login"
+    else
+      @user = User.find(session[:id])
+      #erb :'tweets/tweets'
+    end
+      erb :'tweets/tweets'
+  end
+
+  get '/login' do
+    if session[:id] == nil
+      erb :'users/login'
+    else
+      @user = User.find(session[:id])
+      redirect to "/tweets"
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to "/login"
+  end
+
+  get '/users/#{user.slug}' do
+    erb :'users/show'
+  end
 
 end
