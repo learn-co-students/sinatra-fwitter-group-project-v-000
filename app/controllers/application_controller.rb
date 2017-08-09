be4b763b071174055ac34109ecaac9a583268d30
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-08-09 20:35:59 +0000

    Add signup controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -16,6 +18,17 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
+    if !params.any?{|k, v| v == "" || v == " " || v == nil}
+      @user = User.create(params)
+      session[:id] = @user.id
+
+    else
+       redirect to '/signup'
+    end
+      redirect to '/tweets'
+  end
+
+  get '/tweets' do
 
   end
 
