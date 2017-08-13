db6a6bc283d12e93b4dea7af3f9dd5a5f71976b2
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-08-13 05:37:37 +0000

    all tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :tweets
 
   def slug
@@ -11,7 +12,4 @@ class User < ActiveRecord::Base
     end
   end
 
-  def authenticate
-
-  end
 end
