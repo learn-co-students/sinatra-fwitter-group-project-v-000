af34a3fdf670362a328f29e084ba3e0839939475
charlie lee <ctlee9096@gmail.com>
2017-08-20 23:32:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require './config/environment'
 require 'sinatra/base'
 require 'rack-flash'
 class ApplicationController < Sinatra::Base
-
+#set :sessions, true
   configure do
     set :public_folder, 'public'
      set :views, 'app/views'
@@ -49,7 +49,9 @@ class ApplicationController < Sinatra::Base
 
     post '/login' do
       @user = User.find_by(:username => params[:username])
+      #user = User.find_by(:username => params[:username])
       if @user && @user.authenticate(params[:password])
+    # if user && user.authenticate(params[:password])
         session[:user_id] = @user.id
         flash[:message] = "You have signed in successfully."
         redirect to '/tweets'
@@ -58,20 +60,27 @@ class ApplicationController < Sinatra::Base
         redirect to '/login'
       end
     end
-    
+
     get '/logout' do
       session.clear
       redirect to '/login'
     end
 
-  get '/tweets' do
-     if !session[:user_id]
-       redirect to '/login'
-     else
-       @user = User.find(session[:user_id])
-       erb :'/tweets/tweets'
+    get '/tweets' do
+       if !session[:user_id]
+         redirect to '/login'
+       else
+         @user = User.find(session[:user_id])
+         @tweets = Tweet.all
+         erb :'/tweets/tweets'
+       end
      end
-   end
+
+    get '/users/:slug' do
+         @user = User.find_by_slug(params[:slug])
+         @tweets = @user.tweets
+         erb :'users/show'
+    end
 
    get '/tweets/new' do
      if !session[:user_id]
@@ -81,6 +90,46 @@ class ApplicationController < Sinatra::Base
        erb :'/tweets/create_tweet'
      end
    end
+
+    post '/tweets' do
+          if params["content"].empty?
+            #binding.pry
+            redirect to "/tweets/new"
+          else
+              @tweet = Tweet.create(:content => params["content"], :user_id =>session[:user_id])
+              @tweet.save
+              flash[:message] = "Successfully created tweet."
+          end
+    end
+
+    get '/tweets/:id'do
+      if session[:user_id]
+        #binding.pry #find a single tweet from :id
+        @tweet = Tweet.find_by(params[:id])
+        erb :'/tweets/show_tweet'
+      else
+        flash[:message] = "You must be logged in to view a tweet."
+        redirect to '/login'
+      end
+    end
+
+    get '/tweets/:id/edit' do  #load edit form
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/edit_tweet'
+    end
+
+    patch '/tweets/:id' do #edit action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.content = params[:content]
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+    end
+
+    delete '/tweets/:id/delete' do #delete action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect to '/tweets'
+    end
 
   helpers do
     def logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
 end
