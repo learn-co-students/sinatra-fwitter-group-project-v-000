a9c24f8cd5842dd8923e69c8e5732ea38e9e6e85
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-08-20 20:22:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < Sinatra::Base
      @user = User.find_by(username: params[:username])
      if @user != nil && @user.password == params[:password]
        session[:user_id] = @user.id
-       redirect '/tweets'
+       erb :'/tweets/tweets'
      else
        redirect '/login'
      end
