2775d12c6b0817e3829cca923a58c34083514c2b
Timothy Cevallos <timcev@gmail.com>
2017-08-25 22:50:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "fwitter_secret"
+    set :session_secret, "secret_password"
   end
 
   get '/' do
     erb :index
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id]) if logged_in?
+   end
+ end
 end
