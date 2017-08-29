5edb4fa66e6add97369946c3146711835c537377
psnider30 <pbsnider@gmail.com>
2017-08-28 21:49:46 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ActiveRecord::Base
   extend Concerns::Slugifiable::ClassMethods
   include Concerns::Slugifiable::InstanceMethods
 
+  has_many :tweets
+
+  validates_presence_of :username, :email, :password
+
   def authenticate(password)
     password == self.password ? self : false
   end
-  has_many :tweets
-
-
 
 end
