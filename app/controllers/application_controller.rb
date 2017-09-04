aade6614ae9ab55cd5e2a78052bcbf5077e7542d
Tyler <tbuchhei@alumni.nd.edu>
2017-09-03 20:08:30 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,26 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, "tweety bird"
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
   end
 
 end
