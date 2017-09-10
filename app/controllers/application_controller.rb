fadbc299149f539bc04fea462c3c6686f7aba604
Alec Alba <alecgalba@gmail.com>
2017-09-10 22:12:33 +0000

    built helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,24 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "unlockable"
   end
 
+  get '/' do
+    if logged_in?
+      get '/tweets'
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+
 end
