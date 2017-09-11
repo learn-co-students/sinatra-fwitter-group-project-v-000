71a96ad08ffb3732d8fe4c71cb65c4b6a148d281
Alec Alba <alecgalba@gmail.com>
2017-09-11 14:59:23 +0000

    users controller functional as per specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
 
-  # get '/signup' do
-  #   if logged_in?
-  #     redirect to '/tweets'
-  #   else
-  #     erb :"users/create"
-  #   end
-  # end
-
   get '/signup' do
     if !session[:user_id]
       erb :"users/create"
@@ -21,18 +13,6 @@ class UsersController < ApplicationController
     erb :"/users/show"
   end
 
-  # post '/signup' do
-  #   @user = User.new(params)
-  #
-  #   if params[:username].empty? || params[:email].empty? || params[:password].empty?
-  #     redirect to '/signup'
-  #   else
-  #     @user.save
-  #     session[:user_id] = @user.id
-  #     redirect '/tweets'
-  #   end
-  # end
-
   post '/signup' do
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/signup'
@@ -44,14 +24,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # get '/login' do
-  #   if logged_in?
-  #     redirect to '/tweets'
-  #   else
-  #     erb :"users/login"
-  #   end
-  # end
-
   get '/login' do
     if !session[:user_id]
       erb :"users/login"
@@ -60,42 +32,22 @@ class UsersController < ApplicationController
     end
   end
 
-  # post '/login' do
-  #   @user = User.find_by(username: params[:username])
-  #   if @user && @user.authenticate(params[:password])
-  #     session[:user_id] == @user.id
-  #     redirect to '/tweets'
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
-
   post '/login' do
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect to '/tweets'
     else
       redirect to '/signup'
     end
   end
 
-  # get '/logout' do
-  #   if logged_in?
-  #     session.clear
-  #     redirect to '/login'
-  #   else
-  #     redirect to '/'
-  #   end
-  # end
-
   get '/logout' do
-    if session[:user_id] != nil
+    if session[:user_id] != nil && logged_in?
       session.destroy
       redirect to '/login'
-    else
-      redirect to '/'
     end
+    redirect to '/'
   end
 
 end
