801a9dbe8a5b8a5d55b27b4ab4f4ab854a3918ec
Alec Alba <alecgalba@gmail.com>
2017-09-11 14:58:43 +0000

    tweets controller functional per specs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,5 @@
 class TweetsController < ApplicationController
 
-  # get '/tweets' do
-  #   if logged_in?
-  #     erb :"tweets/tweets"
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
-
   get '/tweets' do
     if session[:user_id]
       @tweets = Tweet.all
@@ -17,14 +9,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # get '/tweets/new' do
-  #   if logged_in?
-  #     erb :"tweets/create"
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
-
   get '/tweets/new' do
     if session[:user_id]
       erb :"tweets/create"
@@ -32,17 +16,6 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
-
-  # post '/tweets' do
-  #   @user = current_user
-  #   if params[:content].empty?
-  #     redirect to '/tweets/new'
-  #   else
-  #     @tweet = @user.tweets.build(params)
-  #     @user.save
-  #     redirect to "/tweets/#{@tweet.id}"
-  #   end
-  # end
 
   post '/tweets' do
     if params[:content] == ""
@@ -54,15 +27,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # get '/tweets/:id' do
-    #@tweet = Tweet.find(params[:id])
-  #   if logged_in?
-  #     erb :"tweets/show"
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
-
   get '/tweets/:id' do
     if session[:user_id]
       @tweet = Tweet.find_by_id(params[:id])
@@ -71,18 +35,6 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
-
-  # get '/tweets/:id/edit' do
-  #   if logged_in?
-  #     @tweet = Tweet.find(params[:id])
-  #     if session[:user_id] == @tweet.user_id
-  #       erb :"tweets/edit"
-  #     else redirect to '/tweets'
-  #     end
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
 
   get '/tweets/:id/edit' do
     if session[:user_id]
@@ -97,15 +49,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # patch '/tweets/:id' do
-  #   @tweet = Tweet.find_by_id(params[:id])
-  #   if params[:tweet][:content].empty?
-  #     redirect to "/tweets/#{@tweet.id}/edit"
-  #   else
-  #     @tweet.update(params[:tweet])
-  #   end
-  # end
-
   patch '/tweets/:id' do
     if params[:content] == ""
       redirect to "/tweets/#{params[:id]}/edit"
@@ -116,16 +59,6 @@ class TweetsController < ApplicationController
       redirect to "/tweets/#{@tweet.id}"
     end
   end
-
-  # delete '/tweets/:id/delete' do
-  #   @tweet = Tweet.find_by_id(params[:id])
-  #   if session[:user_id] == @tweet.user_id
-  #     @tweet.delete
-  #     redirect to '/tweets'
-  #   else
-  #     redirect to '/tweets'
-  #   end
-  # end
 
   delete '/tweets/:id/delete' do
     @tweet = Tweet.find_by_id(params[:id])
