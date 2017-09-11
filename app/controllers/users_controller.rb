2573e8c0fa13844122e5dbb7f6a3acb816b68e3a
Alec Alba <alecgalba@gmail.com>
2017-09-11 14:32:01 +0000

    refactored the tweets views and added proper button names to pass tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
+  # get '/signup' do
+  #   if logged_in?
+  #     redirect to '/tweets'
+  #   else
+  #     erb :"users/create"
+  #   end
+  # end
+
   get '/signup' do
-    if logged_in?
-      redirect to '/tweets'
-    else
+    if !session[:user_id]
       erb :"users/create"
+    else
+      redirect to "/tweets"
     end
   end
 
@@ -13,42 +21,80 @@ class UsersController < ApplicationController
     erb :"/users/show"
   end
 
-  post '/signup' do
-    @user = User.new(params)
+  # post '/signup' do
+  #   @user = User.new(params)
+  #
+  #   if params[:username].empty? || params[:email].empty? || params[:password].empty?
+  #     redirect to '/signup'
+  #   else
+  #     @user.save
+  #     session[:user_id] = @user.id
+  #     redirect '/tweets'
+  #   end
+  # end
 
-    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to '/signup'
     else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-      redirect '/tweets'
+      redirect to '/tweets'
     end
   end
+
+  # get '/login' do
+  #   if logged_in?
+  #     redirect to '/tweets'
+  #   else
+  #     erb :"users/login"
+  #   end
+  # end
 
   get '/login' do
-    if logged_in?
-      redirect to '/tweets'
-    else
+    if !session[:user_id]
       erb :"users/login"
+    else
+      redirect to '/tweets'
     end
   end
 
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect to '/login'
-    else
-      redirect to '/'
-    end
-  end
+  # post '/login' do
+  #   @user = User.find_by(username: params[:username])
+  #   if @user && @user.authenticate(params[:password])
+  #     session[:user_id] == @user.id
+  #     redirect to '/tweets'
+  #   else
+  #     redirect to '/login'
+  #   end
+  # end
 
   post '/login' do
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] == @user.id
+      session[:user_id] = user.id
       redirect to '/tweets'
     else
+      redirect to '/signup'
+    end
+  end
+
+  # get '/logout' do
+  #   if logged_in?
+  #     session.clear
+  #     redirect to '/login'
+  #   else
+  #     redirect to '/'
+  #   end
+  # end
+
+  get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
       redirect to '/login'
+    else
+      redirect to '/'
     end
   end
 
