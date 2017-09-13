3af39b4b63344bf86105908b0f9febcc5c89908e
github username <github email address>
2017-09-12 20:15:55 -0500

    started homepage and signup features

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get "/" do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    if params[:username].empty? || params[:password].empty? || params[:email].empty?
+      redirect "/signup"
+    else
+      @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
+      @user.save
+    end
+  end
+
 end
