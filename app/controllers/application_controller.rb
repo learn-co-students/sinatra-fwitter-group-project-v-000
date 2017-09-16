53300bc7880a908707315809e11728adcd3c1c96
github username <12ivazquez@gmail.com>
2017-09-16 15:16:17 -0500

    homepage, signup, login are mostly done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :session unless test?
+    set :session_secret, "secret"
   end
 
   get "/" do
@@ -12,7 +14,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/signup" do
-    erb :signup
+    if !Helpers.logged_in?(session)
+      erb :signup
+    else
+      redirect "/tweets"
+    end
   end
 
   post "/signup" do
@@ -21,7 +27,35 @@ class ApplicationController < Sinatra::Base
     else
       @user = User.new(:username => params[:username], :password => params[:password], :email => params[:email])
       @user.save
+      session[:user_id] = @user.id
+      redirect "/tweets"
     end
+  end
+
+  get "/tweets" do
+    @user = User.find(session[:user_id])
+    erb :'tweets/index' #twitter index page where user must be logged in
+  end
+
+  get "/login" do
+    if !Helpers.logged_in?(session)
+      erb :login
+    end
+  end
+
+  post "/login" do
+    @user = User.find_by_username(params[:username])
+    if @user.nil?
+      erb :error
+    else
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
   end
 
 end
