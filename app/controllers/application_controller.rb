dfccf040f408ea5571b896b64d5756335dfb0ecd
github username <12ivazquez@gmail.com>
2017-09-18 09:49:12 -0500

    started CRUD for Tweets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,27 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
+
   get "/" do
     erb :index
   end
 
   get "/signup" do
-    erb :signup
+    if !logged_in?
+      erb :signup
+    else
+      redirect "/tweets"
+    end
   end
 
   post "/signup" do
@@ -29,27 +44,71 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/login" do
-    erb :login
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :login
+    end
   end
 
   post "/login" do
     @user = User.find_by_username(params[:username])
-    if @user.nil?
-      erb :error
-    else
+    if logged_in?
       session[:user_id] = @user.id
       redirect "/tweets"
+    else
+      redirect "/signup"
     end
   end
 
   get "/logout" do
-    session.clear
-    redirect "/login"
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
   end
 
   get "/tweets" do
-    @user = User.find(session[:user_id])
-    erb :'tweets/index' #twitter index page where user must be logged in
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/index'
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/tweets/new" do
+    erb :'tweets/new'
+  end
+
+  post "/tweets" do
+    @tweet = Tweet.create(content: params[:content], user_id: params[:user_id])
+    redirect "/tweets"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:user_id])
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:user_id])
+    erb :'tweets/edit'
+  end
+
+  patch '/posts/:id' do
+    @tweet = Tweet.find_by_id(params[:user_id])
+    @tweet.content = params[:content]
+    @tweet.save
+    erb :'tweets/show'
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:user_id])
+    Post.delete(params[:id])
+    erb :'tweets/delete'
   end
 
 end
