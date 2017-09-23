5e7f1b20191c2a23b0081360e405cfc1e50399e9
Mariel <marielbeth@gmail.com>
2017-09-23 15:32:06 +0000

    Add #slug & .slugify to User, add password security

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   has_many :tweets
+  has_secure_password
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect do |user|
+      user.slug == slug
+    end
+  end
 end
