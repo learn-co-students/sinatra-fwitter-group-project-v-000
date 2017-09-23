bded605c7f9eeeae4b92e7b2884a2a002628270b
Mariel <marielbeth@gmail.com>
2017-09-23 17:12:42 +0000

    Add signup, fix password auth, add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,32 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  get '/tweets' do
+    @user = current_user
+    erb :'/tweets/index'
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
+
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 end
