b853b40cf784619b638c2ae45a7ec0836aa15bae
Parv Kumar <parvathikumar92@gmail.com>
2017-09-26 23:24:08 +0000

    added methods for User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,28 @@
 class User < ActiveRecord::Base
 
   has_many :tweets
+
+  def slug
+    name = self.username.downcase
+    split = name.split(" ")
+    new_name = split.join("-")
+    return new_name
+  end
+
+  def self.find_by_slug(slug)
+    self.all.each do |user|
+      if user.slug == slug
+        return user
+      end
+    end
+  end
+
+  def authenticate(password)
+    if self.password == password
+      return self
+    else
+      return false
+    end
+  end
+
 end
