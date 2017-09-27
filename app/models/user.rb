81aaca1b068ddbd6b66b6d77525212eb2993dc2d
Z. Drake <zeedrake3000@gmail.com>
2017-09-26 21:54:51 -0500

    Created 'slug' methods for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
 
+  def slug
+    username.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slugified)
+    self.all.find {|username| username.slug == slugified}
+  end
 end
