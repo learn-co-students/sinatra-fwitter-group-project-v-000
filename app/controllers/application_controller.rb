4854e934f47e4165e8dc91ccda0ba39d11dc35f4
Alexandra <superbiscuit@gmail.com>
2017-09-29 09:13:36 -0700

    front page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,22 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+  	enable :sessions
+  	set :session_secret, "Tony-b4G-a-dOoOoonUt$$$$"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+  	erb :index
+  end
+
+  get '/signup' do
+  	erb :'/users/create_user'
+  end
+
+  get '/login' do
+  	erb :'/users/login'
   end
 
 end
