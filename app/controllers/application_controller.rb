2aded1401b7fbd7e66588eb3b7135e8be130a2a1
Alexandra <github email address>
2017-09-29 15:51:19 -0700

    work in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect '/tweets' if logged_in?
   	erb :index
   end
 
@@ -17,12 +18,49 @@ class ApplicationController < Sinatra::Base
   	erb :'/users/create_user'
   end
 
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email] , password: params[:password])
+    if user.save
+      #flash message: account succesfully created
+      redirect '/login'
+    else
+      redirect '/signup'
+      #flash message: error
+    end
+    
+  end
+
   get '/login' do
   	erb :'/users/login'
   end
 
+  post '/login' do
+    redirect '/tweets' if logged_in?
+    binding.pry
+    user = User.find_by(username: params[:user_name])
+    if user && user.authenticate(params[:user_pass])
+      session[:current_user] = user.id
+      redirect '/tweets'
+      #set up flash message to alert user they logged in
+    else
+      erb :'/users/login'
+      #maybe unhide an element that alerts user they failed??
+    end
+
+  end
+
   get '/tweets' do
+    redirect '/login' if !logged_in?
+    @user = current_user
     erb :'/tweets/tweets'
+  end
+
+  def logged_in?
+    !!session[:current_user]
+  end
+
+  def current_user
+    User.find(session[:current_user])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+	has_secure_password
+	has_many :tweets
+
+	def slug
+		input = self.username.downcase.split.collect{|string|string.scan(/[a-z0-9]/)}
+		input.collect {|arr|arr.join("")}.join('-')
+	end
+
+	def self.find_by_slug(slug)
+		collection = self.all
+		collection.find {|instance| instance.slug == slug}
+	end
+
 end
