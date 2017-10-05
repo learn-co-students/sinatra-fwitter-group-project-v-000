51a1d691ac7bb8e37b0bf7c606550a72dcf8a5ee
joanna <github email address>
2017-10-05 01:07:07 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,16 +15,16 @@ class ApplicationController < Sinatra::Base
 
   get '/signup' do
     if !logged_in?
-      # erb :'/users/create_user'
+      erb :'/users/create_user'
     else
       redirect to "/tweets"
     end
   end
 #
   post '/signup' do
-#   # #   #line 26 & 27 not crucial
-#   #   if logged_in?
-#   #     redirect to "/tweets"
+  #   line 26 & 27 not crucial
+  #   if logged_in?
+  #     redirect to "/tweets"
     if params[:username] == "" || params[:email] == "" || params[:password] == ""
       redirect to "/signup"
     else
@@ -34,61 +34,57 @@ class ApplicationController < Sinatra::Base
       redirect to "/tweets"
     end
   end
-#
-#   get '/tweets' do
-#     @tweets = Tweet.all
-#     @user = User.find_by(:username => params[:username])
-#     if !@current_user
-#       redirect to "/login"
-#     else
-#       erb :'/tweets/tweets'
-#     end
-#
-#     @user = User.find_by(session[:user_id])
-#
-#   end
-#
-  # get '/login' do
-#     #INVESTIGATE
-#     # if logged_in? #prevents logged in user from seeing login page
-#     #   # binding.pry
-#       # redirect to "/tweets"
-#     #   # redirect to "/users/#{@user.username}"
-#     # end
-#     erb :'/users/login'
-#   end
-#   #
-#   post '/login' do
-#   #   @session = session
-#     @user = User.find_by(:username => params[:username])
-#   #
-#     if @user
-#       # binding.pry
-#     # if @user && @user.authenticate(params[:password])
-#       session[:user_id] = @user.id
-#   #     # redirect to "/users/#{@user.username}"
-#       redirect to "/tweets"
-#     # else
-#     #   redirect to "/login"
-#     end
-#   end
-#
-#   get '/users/:username' do
-#     @user = User.find_by(:username => params[:username])
-#     erb :'/users/show'
-#   end
-#
-#   get '/logout' do
-#     session.clear
-#     current_user = nil
-#     redirect to "/login"
-#   end
-#
-#
-#
-#   get '/tweets/:id' do
-#     erb :"/tweets/show_tweet"
-#   end
+
+  get '/login' do
+    if logged_in? #prevents logged in user from seeing login page
+      redirect to "/tweets"
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user
+    # if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by(:username => params[:username])
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    current_user = nil
+    redirect to "/login"
+  end
+
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/create_tweet'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(:content => params[:content], :user_id => params[:user_id])
+  end
+
+  # get '/tweets/:id' do
+  #   erb :"/tweets/show_tweet"
+  # end
 #
 #   get '/tweets/:id/edit' do
 #     erb :"/tweets/edit_tweet"
@@ -104,7 +100,7 @@ class ApplicationController < Sinatra::Base
 #
 #Helpers
   def current_user
-    @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
+    User.find_by(session[:user_id]) if session[:user_id]
     #default is nil if session[:user_id] doesn't exist
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tweets
+
+  def slug
+    self.username.downcase.gsub(" ","-")
+  end
 end
