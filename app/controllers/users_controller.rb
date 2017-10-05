70f9a99a925383249bdcf73bd55cc48abf9b2482
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-10-05 01:55:24 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
 
-
 get '/signup' do
   erb :'users/create_user'
 end
 
 post '/signup' do
   if params[:username] == "" || params[:email] == "" || params[:password] == ""
-    redirect to '/signup'
+    redirect '/signup'
   else
-    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
     @user.save
     session[:user_id] = @user.id
-    redirect to '/tweets'
+    redirect '/tweets'
   end
 end
 
