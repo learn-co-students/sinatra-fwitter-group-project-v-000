78ebf96351869eb5e0be8550c3a557d3397bd9f8
joanna <github email address>
2017-10-05 18:56:45 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,8 +45,8 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username => params[:username])
-    if @user
-    # if @user && @user.authenticate(params[:password]) #won't work? something off with bcrypt?
+    # if @user
+    if @user && @user.authenticate(params[:password]) #won't work? something off with bcrypt?
       session[:user_id] = @user.id
       redirect to "/tweets"
     else
