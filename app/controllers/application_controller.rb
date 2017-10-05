1ea351bf534c86b22c41a1b211c39405d6d08226
Dalia Sawaya <sawaya.dalia@gmail.com>
2017-10-05 02:30:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
+    # enable :sessions
   end
 
   get '/' do
