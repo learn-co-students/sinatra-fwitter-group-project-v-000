be8442f0ee8e03671ceb3057e83edc9f8a2b7759
joanna <github email address>
2017-10-05 14:19:00 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -75,30 +75,62 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/new' do
-    erb :'/tweets/create_tweet'
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect to "/login"
+    end
   end
 
   post '/tweets' do
-    @tweet = Tweet.create(:content => params[:content], :user_id => params[:user_id])
-    @tweet.save
+    @user = User.find_by(:username => params[:username])
+    if params[:content] != ""
+      @tweet = current_user.tweets.create(:content => params[:content], :user_id => [:user_id])
+    else
+      redirect to "/tweets/new"
+    end
+
   end
 
-  # get '/tweets/:id' do
-  #   erb :"/tweets/show_tweet"
-  # end
-#
-#   get '/tweets/:id/edit' do
-#     erb :"/tweets/edit_tweet"
-#   end
-#
-#   post '/tweets/:id' do
-#
-#   end
-#
-#   post '/tweets/:id/delete' do
-#
-#   end
-#
+  get '/tweets/:id' do
+    # @user = User.find_by(:username => params[:username])
+    if logged_in?
+      # binding.pry
+      @tweet = Tweet.find_by_id(params[:id]) #not found by content, but specifically by id
+      erb :"/tweets/show_tweet"
+    else
+      redirect to "/login"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+      redirect to "/login"
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+
+    if logged_in? && params[:content] != ""
+      @tweet.update(:content => params[:content])
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect to "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  post '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user_id == session[:user_id]
+      @tweet.delete
+    end
+  end
+
 #Helpers
   def current_user
     User.find_by(session[:user_id]) if session[:user_id]
