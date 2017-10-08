a48ef51376271247818a4a258142c5f579a72d27
Donna <donnamcla@gmail.com>
2017-10-08 14:45:29 -0400

    cleaned up helper method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -5,6 +5,6 @@ class Helpers
     end
 
     def self.is_logged_in?(session)
-        !!User.find_by(id: session[:user_id])  ? true : false
+        !!User.find_by(id: session[:user_id])
     end
 end
