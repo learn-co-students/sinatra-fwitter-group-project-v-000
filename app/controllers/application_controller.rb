86b1ca9cd0b0c2e0ffdc550114c47bbcb539899b
Lara Matyushenko <larabrightside@gmail.com>
2017-10-09 13:10:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,13 +13,20 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-
+    if logged_in?
+      redirect "/tweets"
+    else
     erb :signup
+    end
   end
 
   get '/tweets' do
-
-    erb :'/tweets/index'
+    if logged_in?
+      @user = current_user
+      erb :'/tweets/index'
+    else
+      redirect "/login"
+    end
   end
 
   post '/signup' do
@@ -32,10 +39,48 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/tweets"
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(:slug)
+
+    erb :'users/show'
+  end
 
 
+  helpers do
+    def current_user
+      User.find(session[:user_id])
+    end
 
-
-
+    def logged_in?
+      !!session[:user_id]
+    end
+  end
 
 end
