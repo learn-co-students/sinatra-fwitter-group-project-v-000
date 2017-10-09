921abf59ae9f11d6d47f394e5435ae4313d72912
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-09 01:52:02 +0000

    login and signup views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,42 +9,8 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-  get "/" do
+  get '/' do
     erb :index
-  end
-
-  get "/signup" do
-    erb :signup
-  end
-
-  post "/signup" do
-    redirect "/failure" if params[:username] == ""
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect '/users/login'
-    else
-      redirect '/failure'
-    end
-  end
-
-  get "/login" do
-    erb :'users/login'
-  end
-
-  post "/login" do
-    user = User.find_by(:username => params[:username])
-      if user && user.authenticate(params[:password])
-       session[:user_id] = user.id
-       redirect "/users/show"
-      else
-       redirect "/failure"
-      end
-  end
-
-
-  get "/logout" do
-    session.clear
-    redirect "/"
   end
 
   helpers do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,36 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    erb :'users/create_user'
+  end
+
+  post "/signup" do
+    redirect "/failure" if params[:username] == ""
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect '/users/login'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get "/login" do
+    erb :'users/login'
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect "/users/show"
+      else
+       redirect "/failure"
+      end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
 end
