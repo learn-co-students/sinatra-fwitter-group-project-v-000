9abc66424765e34717371d7864bc7cf39e9383aa
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-09 02:14:39 +0000

    signup done working on login to pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,49 @@
 class UsersController < ApplicationController
 
   get "/signup" do
+    if logged_in?
+      redirect to '/tweets'
+    else
     erb :'users/create_user'
+  end
   end
 
   post "/signup" do
-    redirect "/failure" if params[:username] == ""
-    user = User.new(:username => params[:username], :password => params[:password])
-    if user.save
-      redirect '/users/login'
+    redirect "/signup" if params[:username] == ""
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/tweets'
     else
-      redirect '/failure'
+      redirect '/signup'
     end
   end
 
   get "/login" do
-    erb :'users/login'
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'users/login'
+    end
   end
 
   post "/login" do
     user = User.find_by(:username => params[:username])
       if user && user.authenticate(params[:password])
        session[:user_id] = user.id
-       redirect "/users/show"
+       redirect to "/tweets"
       else
-       redirect "/failure"
+       redirect to "/login"
       end
   end
 
   get "/logout" do
-    session.clear
-    redirect "/"
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+    redirect to "/tweets"
+  end
   end
 
 end
