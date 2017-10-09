0a53268e018a582c70fa9e1b1b48a7eb0ab97731
Lara Matyushenko <larabrightside@gmail.com>
2017-10-09 17:59:34 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,16 +21,6 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-  get '/tweets' do
-    if logged_in?
-      @user = current_user
-      @tweets = Tweet.all
-      erb :'/tweets/index'
-    else
-      redirect "/login"
-    end
-  end
-
   post '/signup' do
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect "/signup"
@@ -67,69 +57,6 @@ class ApplicationController < Sinatra::Base
       redirect "/tweets"
     end
   end
-
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-
-    erb :'users/show'
-  end
-
-  get '/tweets/new' do
-    if logged_in?
-      erb :'tweets/create_tweet'
-    else
-      redirect "/login"
-    end
-  end
-
-  get '/tweets/:id' do
-    if logged_in?
-      @tweet = Tweet.find(params[:id])
-      erb :'tweets/show_tweet'
-    else
-      redirect "/login"
-    end
-  end
-
-  post '/tweets/new' do
-      @user = current_user
-      if !params[:content].empty?
-        @tweet = Tweet.create(content: params[:content], user_id: @user.id)
-        redirect "/tweets/#{@tweet.id}"
-      else
-        redirect "/tweets/new"
-      end
-  end
-
-  get '/tweets/:id/edit' do
-    @tweet = Tweet.find(params[:id])
-    if logged_in? && current_user == @tweet.user
-      erb :'tweets/edit_tweet'
-    else
-      redirect "/login"
-    end
-  end
-
-  patch '/tweets/:id/edit' do
-    if params[:content].empty?
-      redirect "/tweets/#{@tweet.id}/edit"
-    else
-      @tweet = Tweet.find(params[:id])
-      @tweet.update(content: params[:content])
-      redirect "/tweets"
-    end
-  end
-
-  delete '/tweets/:id/delete' do
-    @tweet = Tweet.find(params[:id])
-    if logged_in? && current_user == @tweet.user
-      @tweet.destroy
-      redirect "/tweets"
-    else
-      redirect "/login"
-    end
-  end
-
 
   helpers do
     def current_user
