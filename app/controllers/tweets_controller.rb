09553d60f2b6b288fa642857c47ee14352ac5ca2
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-09 03:37:21 +0000

    testing the tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,32 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] != ""
+        @tweet = current_user.tweets.create(:content => params[:content])
+        redirect to '/tweets'
+      else
+        redirect to '/tweets/create_tweet'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 end
