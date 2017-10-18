9a83c862d1f0f66114e73fb3f8461407dc114944
daniellecroxton <danikrist@gmail.com>
2017-10-18 11:35:27 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :'/index'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
   end
 
 end
