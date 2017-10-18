88ccc052cb925bcb7455d3ddc1a03f870cfab4ce
aisayo <aisayo@icloud.com>
2017-10-18 00:02:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,22 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :sessions, 'fwitter'
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/new'
+  end
+
+  post '/signup' do
+    
   end
 
 end
