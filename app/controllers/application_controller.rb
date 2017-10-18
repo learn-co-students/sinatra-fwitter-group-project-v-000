7059c6e4e24e4a38590758a55c206892445c84d1
aisayo <aisayo@icloud.com>
2017-10-18 00:10:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    
+    if params[:username] = "" && params[:email] = "" && params[:password] = ""
+    @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+    redirect '/tweets'
+    else
+    redirect '/signup'
+    end
   end
 
 end
