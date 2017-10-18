2bc790d862045dc53cc3b2faa4312700a73007e7
aisayo <aisayo@icloud.com>
2017-10-18 01:15:28 +0000

    Done.

--- app/models/helpers.rb
+++ app/models/helpers.rb
@@ -1,14 +1,11 @@
 class Helpers
 
-  def self.current_user(session)
-    @current_user = User.find_by(session[:user_id])
-    binding.pry
-    @current_user
-    binding.pry
-  end
+  def self.logged_in?
+  !!current_user
+end
 
-  def self.logged_in?(session)
-    !current_user(session[:user_id])
-  end
+def self.current_user
+  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+end
 
 end
