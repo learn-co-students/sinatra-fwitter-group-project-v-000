6d7a0330dc18deb64dd055f0a76a968d736400c0
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-18 19:45:46 -0600

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,58 @@
+require 'pry'
+
 class TweetsController < ApplicationController
+
+#Tweet Index
+
+  get "/tweets" do
+    if logged_in?
+      @user = current_user
+      @tweets = Tweet.all
+      erb :"/tweets/tweets"
+    else
+      redirect '/login'
+    end
+  end
+
+#Create Tweet
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :"/tweets/show_tweet"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to "/tweets/new"
+    else
+      @tweet = Tweet.create(:content => params[:content])
+      current_user.tweets << @tweet
+
+      edirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
+#Delete Tweet
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user_id == current_user.id
+      @tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
 end
