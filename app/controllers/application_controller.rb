7d7d97bfcf2e35ceae83ade64e00875cdeade995
Cole Martin <mar08049@gmail.com>
2017-10-19 20:21:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,33 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
+configure do
+    enable :sessions
+    set :session_secret, 'fwitter_secret'
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  delete 'tweets/:id/delete' do
+    @tweet = Tweet.find_by(id: params[:id])
+    @tweet.delete
+    redirect to '/tweets'
+  end
+
+
+  helpers do
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
   end
 
 end
