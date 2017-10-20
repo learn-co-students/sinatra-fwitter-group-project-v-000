ea235c24e82200d0da462b4833cb5bd0db5c2db7
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-10-20 19:47:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    set :sessions
-    enable :session_secret, 'secret'
+    enable :sessions
+    set :session_secret, "topsecret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect "/tweets"
+    end
+    erb :"/users/signup"
+  end
+
+  post "/signup" do
+    if params.has_value?("")
+      redirect "/signup"
+    end
+    @user = User.create(:username => params[:username], email: params[:email], :password => params[:password])
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
   end
 
 end
