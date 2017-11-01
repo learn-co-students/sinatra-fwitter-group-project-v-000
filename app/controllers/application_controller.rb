3a5375a1e9277e88546435feb09c06f4fba3492d
chrisjamesr <chrisjames.r@gmail.com>
2017-10-31 23:03:36 -0400

    add user/id route, modify post/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,24 +15,28 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :'/users/create_user'
+    if logged_in?
+      redirect '/tweets'
+    else  
+     erb :'/users/create_user'
+    end
   end
 
   post '/signup' do
     user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-    if user.username != "" && user.email != "" && user.password != "" && user.save
+    if user.save
       session[:user_id] = user.id
-      redirect "/tweets"
+      redirect '/tweets'
     else
-      redirect "/signup"
+      redirect '/signup'
     end
   end
 
   get '/login' do
-    if !session[:user_id]
-    erb :'/users/login'
+    if logged_in?
+      redirect '/tweets'
     else
-    redirect "/tweets"
+      erb :'/users/login'
     end
   end
 
@@ -40,18 +44,23 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/tweets"
+      redirect '/tweets'
     else
       redirect "/login"
     end
   end
 
+  get '/users/:id' do
+    @user = User.find_by_slug(params[:id])
+    erb :'users/show'
+  end
+
   get '/tweets' do
-    if !!session[:user_id]
+    if logged_in?
       @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
-      redirect "/login"
+      redirect '/login'
     end
   end
 
@@ -96,15 +105,15 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-    if !!session[:id]
+    if logged_in?
       session.clear
-      redirect "/login"
+      redirect '/login'
     else
       redirect "/"
     end
   end
 
-    helpers do
+  helpers do
     def logged_in?
       !!session[:user_id]
     end
@@ -112,7 +121,8 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
+
   end
-  
+
 
 end
