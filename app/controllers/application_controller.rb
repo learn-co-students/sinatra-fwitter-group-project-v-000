41fc3193a510dd6925487151b446f6fcc7171a89
Deborah Seow <deborahseow9@gmail.com>
2017-11-02 19:53:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+
+    erb :index
+  end
+
+#Create The Tweet - C
+  get '/tweets/new' do
+
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do
+
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+#Show The Tweet - R
+
+  get '/tweets' do
+
+    erb :'tweets/tweets'
+  end
+
+  get '/tweets/:id' do
+
+    erb :'tweets/show_tweet'
+  end
+
+  #Edit the Tweet - U
+
+  get '/tweets/:id/edit' do
+
+    erb :'tweets/edit_tweet'
+  end
+
+  post '/tweets/:id' do
+
+    erb :'tweets/show_tweet'
+  end
+
+  #Delete Tweet - D
+
+  post '/tweets/:id/delete' do
+
+    redirect to "/tweets"
+  end
 end
