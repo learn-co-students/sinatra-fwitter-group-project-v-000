6f0b8a96f0e764da568e02b14c561f5b46d7caec
Deborah Seow <deborahseow9@gmail.com>
2017-11-02 21:20:14 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -31,6 +31,12 @@ class TweetsController < Sinatra::Base
 
   #Show The Tweet - R
 
+    get '/tweets' do
+      @user = User.find_by(id: current_user.id)
+
+      erb :'/users/show'
+    end
+
     get '/tweets/:id' do
       #the spec says to not let someone see a single tweet if not logged in, although that makes no sense in a real twitter situation
       #This is only needed for the spec because our ':id' is @tweet.id not @user.id
