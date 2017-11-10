4a697635a8e2af37e5b16a0ab5258e4c2a5d3422
GusVilla303 <gusvilla303@gmail.com>
2017-11-10 14:51:00 -0700

    Add action, route, and view to create a User. Still need the correct redirect in POST action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username],
+                     email: params[:email],
+                     password_digest: params[:password]
+                  )
+    @user.save
+
+    erb :'tweets/tweets'
+  end
+
 end
