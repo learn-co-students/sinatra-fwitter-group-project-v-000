f0e692be4bc571734c64efa872d0cb13947b46f3
Ary Baldioceda <abaldioceda@gmail.com>
2017-11-10 02:10:09 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -2,6 +2,7 @@ class TweetController < ApplicationController
 
   get '/tweets' do
     if logged_in?
+      @user = User.find(session[:user_id])
       erb :'tweets/tweets'
     else
       redirect to '/login'
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -10,9 +10,9 @@ class UserController < ApplicationController
 
   post '/signup' do
     if validate_signup
-      @user = User.new(params)
-      if @user.save
-        session[:user_id] = @user.id
+      user = User.new(params)
+      if user.save
+        session[:user_id] = user.id
         redirect "/tweets"
       else
         redirect "/signup"
@@ -23,18 +23,39 @@ class UserController < ApplicationController
   end
 
   get '/login' do
-    erb :"users/login"
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :"users/login"
+    end
   end
 
   post "/login" do
-	  user = User.find_by(:username => params[:username])
+    if logged_in?
+      redirect "/tweets"
+    else
+	    user = User.find_by(:username => params[:username])
 
-	  if user && user.authenticate(params[:password])
-		 session[:user_id] = user.id
-		 redirect "/tweets"
-	  else
-		 redirect "/failure"
-	  end
+	    if user && user.authenticate(params[:password])
+		    session[:user_id] = user.id
+		    redirect "/tweets"
+	    else
+		    redirect "/login"
+	    end
+    end
 	end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+		  redirect "/login"
+    else
+      redirect "/"
+    end
+  end
+
+  # get '/users/:slug' do
+  #
+  # end
 
 end
