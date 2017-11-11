87e777f261edbf532b54c8893d4d54c634481448
Denis Bondin <dsbondin@gmail.com>
2017-11-11 19:58:27 +0000

    working on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,35 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "spicy pickle"
   end
 
   get '/' do
-    redirect :'index.html'
+    erb :index
   end
 
   get '/signup' do
     erb :'/signup'
   end
 
+  post '/signup' do
+    if !params[:username].empty? and !params[:email].empty? and !params[:password].empty?
+      user = User.create(params)
+    else
+      redirect '/signup'
+    end
+
+    redirect '/tweets'
+  end
+
   get '/login' do
     erb :'/login'
+  end
+
+  get '/tweets' do
+
+    erb :tweets
   end
 
 end
