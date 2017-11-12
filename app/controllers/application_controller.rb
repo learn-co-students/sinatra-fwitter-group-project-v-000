01f79675873c921b10f3ce042bfe2cf0a0a66704
Denis Bondin <dsbondin@gmail.com>
2017-11-12 02:40:29 +0000

    2 fails left

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -99,8 +99,23 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/:id/edit' do
-    @tweet = Tweet.find(params[:id])
-    erb :edit
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :edit
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    if !params[:content].empty?
+      tweet.update(content: params[:content])
+      tweet.save
+      redirect "/tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
   end
 
   post '/tweets/:id/delete' do
