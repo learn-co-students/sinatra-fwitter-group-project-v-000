ec2d4fadce692d167902045283fc67e9a120a8be
GusVilla303 <gusvilla303@gmail.com>
2017-11-14 14:32:23 -0700

    Add functionality for users to log in, create a new tweet.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,27 +13,89 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/create_user'
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
   end
 
-  get 'tweets/tweets' do
-    #binding.pry
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      @current_user = current_user
+      redirect "/tweets"
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+
+    erb :"users/show"
+  end
+
+  get '/tweets' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+      @tweets = Tweet.all
       erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 
   post '/signup' do
     @user = User.new(username: params[:username],
                      email: params[:email],
                      password: params[:password]
-                  )
+                    )
 
     if @user.save
       session[:user_id] = @user.id
-      redirect "tweets/tweets"
+      redirect "/tweets"
     else
       redirect '/signup'
     end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(content: params[:content])
+    current_user.tweets << @tweet
+
+    redirect '/tweets'
+
   end
 
   helpers do
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
+
+  def slug
+    username
+  end
 
 end
