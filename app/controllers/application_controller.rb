64542c921caec41a6148c41d58c9f19be4f8ee38
GusVilla303 <gusvilla303@gmail.com>
2017-11-15 14:58:17 -0700

    Add ability for logged in user to edit their tweet. Spec is passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
 
-    erb :'tweets/show_tweet'
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
   end
 
 
@@ -106,6 +110,30 @@ class ApplicationController < Sinatra::Base
     else
       redirect '/tweets/new'
     end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(content: params[:content])
+
+    redirect "tweets/#{@tweet.id}"
+  end
+
+  post '/tweets/:id/delete' do
+    tweet = Tweet.find(params[:id])
+    tweet.delete
+
+    redirect '/tweets'
   end
 
   helpers do
