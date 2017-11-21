872a659e11434f8c4eac93b7edd969e170a46d85
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-21 19:07:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,5 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-  post '/signup' do
-    if !params[:username].empty? || !params[:email].empty? || !params[:password].empty?
-      redirect 'users/create_user'
-    else
-      redirect 'tweets/tweets'
-    end
-  end
 
 end
