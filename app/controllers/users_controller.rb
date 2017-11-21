945651a25b96b4d4838aaaed2245a8cbb84412a2
alpbourne <alpbourne@gmail.com>
2017-11-21 17:27:42 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,9 +30,18 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to '/tweets'
+      redirect '/tweets'
     else
-      redirect to '/login'
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
     end
   end
 
