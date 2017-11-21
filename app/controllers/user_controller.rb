9b3ea516b4f63223f4367f7f7e69dbe7d366f69d
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-21 23:15:15 +0000

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -34,7 +34,7 @@ end
 
   post '/login' do
     user = User.find_by(:username => params[:username])
-    if user && user.authentication(params[:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "/tweets"
     else
