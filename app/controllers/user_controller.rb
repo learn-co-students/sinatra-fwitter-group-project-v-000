f6ce114bb80e3eb7c65fd9d0b2214013a7c7bf89
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-21 19:19:39 +0000

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,15 +1,3 @@
 class UserController < ApplicationController
 
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-  post '/signup' do
-    if !params[:username].empty? || !params[:email].empty? || !params[:password].empty?
-      redirect 'users/create_user'
-    else
-      redirect 'tweets/tweets'
-    end
-  end
-
 end
