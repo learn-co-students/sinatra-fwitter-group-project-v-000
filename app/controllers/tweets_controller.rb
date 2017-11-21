26309ce06cfabc0e1f413952016d62959e154d3a
alpbourne <alpbourne@gmail.com>
2017-11-21 19:18:39 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,23 @@ class TweetsController < ApplicationController
       erb :'tweets/tweets'
     else
       redirect '/login'
-    end 
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content] != ""
+      current_user.tweets.create(content: params[:content])
+      redirect '/users/#(current_user.slug)'
+    else
+      redirect '/tweets/new'
+    end
   end
 end
