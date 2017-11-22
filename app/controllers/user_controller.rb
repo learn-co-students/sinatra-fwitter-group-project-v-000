e7020e238575f255455dcb56a13a30a03ab9be88
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-22 17:18:54 +0000

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -18,5 +18,33 @@ class UserController < ApplicationController
     end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      logout!
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
 
 end
