bc542336fc355a051ac67c44c6474dac2761c61a
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-11-22 00:41:06 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,49 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    if logged_in?
+    @tweets = Tweets.all
     erb :'/tweets/tweets'
+  else
+    redirect to '/login'
   end
 end
+
+  get '/tweets/new' do
+    if logged_in?
+    erb: '/tweets/create_tweet'
+  else
+    redirect to '/login'
+  end
+end
+
+  post '/tweets' do
+    if params[:content] == ""
+        redirect to '/tweets/new'
+      else
+        @tweet = current_user.tweets.create (:content => params[:content])
+        redirect to '/tweets/#{@tweet.id}'
+      end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'edit_tweet'
+  else
+    redirect to '/login'
+  end
+end
+
+  
+
+ end
