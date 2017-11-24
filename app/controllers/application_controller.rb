e60c7f053953c2982b516f4ae6992a5ff0e13d78
Justin Tooley <jstooley823@gmail.com>
2017-11-24 21:03:51 +0000

    basic outline of controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,48 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    erb :'user/create_user'
+  end
+
+  get '/login' do
+    erb :'user/login'
+  end
+
+  get '/logout' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+
+    erb :"tweet/create_tweet"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by(id: params['id'])
+    erb :"tweets/show_tweet"
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by(id: params['id'])
+    erb :"tweet/edit_tweet"
+  end
+
+  post '/signup' do
+    redirect to
+  end
+
+  post '/login' do
+    redirect to
+  end
+
+  post '/tweets' do
+    redirect to "tweets/show"
+  end
+
+  post '/tweets/:id'do
+    @tweet = Tweet.find_by(id: params['id'])
+    redirect to "tweets/show"
+  end
+
 end
