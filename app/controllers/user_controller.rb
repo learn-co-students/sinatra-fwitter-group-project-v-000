7d3368c3f7167ae258cf47d84252809d94ce436d
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-27 17:47:42 +0000

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -37,6 +37,11 @@ class UserController < ApplicationController
     end
   end
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
   get '/logout' do
     if logged_in?
       logout!
