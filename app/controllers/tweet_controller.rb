4ae36a6d2f8b26379a17e0411b2d3f306cd6f00b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-11-27 18:45:16 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -37,4 +37,24 @@ class TweetController < ApplicationController
     end
   end
 
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in? && !params[:content].empty?
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{params[:id]}/edit"
+    end
+  end
+
 end
