aa2f770cb010cf62a9a9250be0f981e4dbd57823
Vicky Lau <vlau.nyc@gmail.com>
2017-11-29 18:20:37 +0000

    Signup Page tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,49 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "encription_key_that_you_wont_guess"
   end
 
-end
+  get '/' do
+    erb :index
+  end
+
+  get '/login' do
+    if session.has_key?(:user_id)
+      redirect to '/tweets/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  get '/signup' do
+    if session.has_key?(:user_id)
+      redirect to '/tweets/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'  #how can you raise an error here??? ex: you must fill in the all parts of the form to create an account...
+    else
+       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+       session[:user_id]=@user.id
+       redirect to '/tweets'
+     end
+  end
+
+  get '/tweets' do
+    erb :'tweets/tweets'
+  end
+
+
+
+end #of class ApplicationController
