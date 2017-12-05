6862abf2a609b6605a25eb0d470a5d61c9a5d298
Jessica Bensch <jessica.bensch11@gmail.com>
2017-12-04 19:38:20 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    redirect to '/tweets'
+  end
+
+  get '/tweets' do
+
+  end
+
+  def current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
