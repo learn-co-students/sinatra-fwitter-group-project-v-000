353d3a2b57dd1fb55efcffd091335a096969a60b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-10 16:40:11 -0500

    tests passing up to and including edit action - logged out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -67,8 +67,59 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+  get '/tweets/new' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      erb :'tweets/create_tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params[:content] != ""
+      user = User.find(session[:user_id])
+      user.tweets << Tweet.create(content: params[:content])
+      #  TO FOLLOW UP -- WHAT DO I DISPLAY ONCE THIS IS DONE?
+    else
+      redirect to '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if session[:user_id]
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if params[:content] != ""
+      tweet = Tweet.find(params[:id])
+      tweet.content = params[:content]
+      tweet.save
+      redirect to "/tweets/#{tweet.id}"
+    else
+      redirect to "/tweets/#{params[:id]}/edit"
+    end
+  end
+
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
+
+
 end
