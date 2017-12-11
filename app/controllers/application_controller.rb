1c4bf35372f8ecc02fc3ca2bd1ab50accc5bf41a
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-11 14:28:26 -0500

    in the middle of refactoring; added helper methods; this had /edit variations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    if session[:user_id]
+    if logged_in?
       redirect to "/tweets"
     else
       erb :'users/create_user'
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    if session[:user_id]
+    if logged_in?
       redirect to "/tweets"
     else
       erb :"users/login"
@@ -50,7 +50,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/logout' do
-    if session[:user_id]
+    if logged_in?
       session.clear
       redirect to '/login'
     else
@@ -59,7 +59,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets' do
-    if session[:user_id]
+    if logged_in?
       @user = User.find(session[:user_id])
       erb :'tweets/tweets'
     else
@@ -68,7 +68,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/new' do
-    if session[:user_id]
+    if logged_in?
       @user = User.find(session[:user_id])
       erb :'tweets/create_tweets'
     else
@@ -79,35 +79,36 @@ class ApplicationController < Sinatra::Base
   post '/tweets' do
     if params[:content] != ""
       user = User.find(session[:user_id])
-      user.tweets << Tweet.create(content: params[:content])
-      #  TO FOLLOW UP -- WHAT DO I DISPLAY ONCE THIS IS DONE?
+      tweet = Tweet.create(content: params[:content])
+      user.tweets << tweet
+      redirect to "tweets/#{tweet.id}"
     else
       redirect to '/tweets/new'
     end
   end
 
+# # This works but causes a later error in the "delete", because the tests expect a delete button even
+# # if a different user is logged in:
+#   get '/tweets/:id' do
+#     @tweet = Tweet.find(params[:id])
+#     if logged_in? && current_user.id == @tweet.user.id
+#       erb :'tweets/show_tweet'
+#     else
+#       redirect to '/login'
+#     end
+#   end
+
   get '/tweets/:id' do
-    # if @tweet.user.id == session[:user_id] - This is too much security for the test, other tests
-    # won't pass under "delete" if this in place
-    if session[:user_id]
+    if logged_in?
       @tweet = Tweet.find(params[:id])
       erb :'tweets/show_tweet'
     else
       redirect to '/login'
     end
   end
-  # This passes
-  # get '/tweets/:id' do
-  #   if session[:user_id]
-  #     @tweet = Tweet.find(params[:id])
-  #     erb :'tweets/show_tweet'
-  #   else
-  #     redirect to '/login'
-  #   end
-  # end
 
   get '/tweets/:id/edit' do
-    if session[:user_id]
+    if logged_in?
       @user = User.find(session[:user_id])
       @tweet = Tweet.find(params[:id])
       erb :'tweets/edit_tweet'
@@ -117,19 +118,44 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/tweets/:id' do
-    if params[:content] != ""
-      tweet = Tweet.find(params[:id])
+    tweet = Tweet.find(params[:id])
+    if tweet.user.id != current_user.id # If non-current user hits edit button, go to /tweets
+      redirect to "/tweets"
+    elsif params[:content] != "" # If current user hits edit button and content not empty, then edit
       tweet.content = params[:content]
       tweet.save
       redirect to "/tweets/#{tweet.id}"
-    else
+    else # Otherwise refresh the edit page
       redirect to "/tweets/#{params[:id]}/edit"
     end
   end
 
+  # patch '/tweets/:id' do
+  #   tweet = Tweet.find(params[:id])
+  #   if params[:content] != "" && tweet.user.id == current_user.id
+  #     tweet.content = params[:content]
+  #     tweet.save
+  #     redirect to "/tweets/#{tweet.id}"
+  #   else
+  #     redirect to "/tweets/#{params[:id]}/edit"
+  #   end
+  # end
+
+  # patch '/tweets/:id' do
+  #   # This is the edit post
+  #   if params[:content] != ""
+  #     tweet = Tweet.find(params[:id])
+  #     tweet.content = params[:content]
+  #     tweet.save
+  #     redirect to "/tweets/#{tweet.id}"
+  #   else
+  #     redirect to "/tweets/#{params[:id]}/edit"
+  #   end
+  # end
+
   post '/tweets/:id/delete' do
     tweet = Tweet.find(params[:id])
-    if tweet.user.id == session[:user_id]
+    if tweet.user.id == current_user.id
       tweet.delete
       tweet.save
       redirect to '/tweets'
@@ -137,24 +163,22 @@ class ApplicationController < Sinatra::Base
       redirect to '/tweets'
     end
   end
-  # delete '/tweets/:id/delete' do
-  #   binding.pry
-  #   if session[:id]
-  #     tweet = Tweet.find(params[:id])
-  #     if tweet.user.id == session[:id]
-  #       tweet.delete
-  #       redirect to '/tweets'
-  #     end
-  #   else
-  #     redirect to '/tweets'
-  #   end
-  # end
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :'users/show'
   end
 
+  helpers do
 
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
 
 end
