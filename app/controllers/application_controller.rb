be51bfefa1d846c65ab15f972bea18361ae9419a
Emma Feng <ff18qd@gmail.com>
2017-12-12 04:05:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,18 +14,56 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/signup" do
-    erb :signup
+    if logged_in?
+      redirect :"tweets/tweets"
+    else
+      erb :signup
+    end
   end
 
   post "/signup" do
     # binding.pry
     user = User.new(username: params["username"], email: params["email"], password: params["password"])
-    if params["username"] != "" && params["email"] != ""
-      if user.save
+    if params["username"] == "" || params["username"] == nil || params["email"] == "" || params["email"] == nil || params["password"] == "" || params["password"] == nil
+      redirect "/signup"
+    else
+        user.save
         session[:user_id] = user.id
   			redirect :"tweets/tweets"
-      end
     end
+  end
+
+  get "/login" do
+    if logged_in?
+      redirect :"tweets/tweets"
+    else
+      erb :login
+    end
+  end
+
+  post "/login" do
+    # binding.pry
+    user = User.find_by(username: params["username"])
+    if user && user.authenticate(params["password"])
+      session[:user_id] = user.id
+        redirect :"tweets/tweets"
+    end
+
+  end
+
+  # get "/logout" do
+	# 	session.clear
+	# 	redirect "/"
+	# end
+
+  helpers do
+  		def logged_in?
+  			!!session[:user_id]
+  		end
+
+  		def current_user
+  			User.find(session[:user_id])
+  		end
   end
 
 end
