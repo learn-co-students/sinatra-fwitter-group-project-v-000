c20c380c8671054820fe1fb2e54125ac9492de1e
Emma Feng <ff18qd@gmail.com>
2017-12-12 03:09:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,31 @@
 require './config/environment'
+require "./app/models/user"
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    # binding.pry
+    user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    if params["username"] != "" && params["email"] != ""
+      if user.save
+        session[:user_id] = user.id
+  			redirect :"tweets/tweets"
+      end
+    end
   end
 
 end
