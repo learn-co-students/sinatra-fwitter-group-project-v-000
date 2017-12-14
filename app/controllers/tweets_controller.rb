68cc5e20545ad6ad71dc03a1f6e12a8ad9d84190
Emma Feng <ff18qd@gmail.com>
2017-12-14 03:19:22 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -42,4 +42,41 @@ class TweetsController < ApplicationController
     end
   end
 
+  get "/tweets/:id/edit" do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+      if current_user.tweets.include?(@tweet)
+        erb :"tweets/edit_tweet"
+      end
+    else
+      redirect :"/login"
+    end
+  end
+
+  patch "/tweets/:id" do
+    # binding.pry
+    if params[:content] != "" && params[:content] != ""
+      @tweet = Tweet.find_by(params[:id])
+      @tweet.update(content: params["content"])
+      redirect :"/tweets"
+    else
+      redirect :"/tweets/#{params[:id]}/edit"
+    end
+  end
+
+  delete "/tweets/:id/delete" do
+    # binding.pry
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+      if current_user.tweets.include?(@tweet)
+        @tweet.delete
+        redirect :"/tweets"
+      else
+        redirect :"/tweets"
+      end
+    else
+      redirect :"/login"
+    end
+  end
+
 end
