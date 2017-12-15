49810631c7f79a8993f472456f3f7cd1b51557fc
Omar Donnadieu Mercado <donnadieu.86@gmail.com>
2017-12-15 03:00:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,45 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    redirect '/tweets'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    redirect '/tweets'
+  end
+
+
+
+  helpers do
+
+    def logged_in?
+
+    end
+
+    def current_user
+
+    end
+
+    def logout
+      session.clear
+    end
+
+  end
 end
