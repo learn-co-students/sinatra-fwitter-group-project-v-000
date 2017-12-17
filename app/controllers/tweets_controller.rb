f5f803458a6c64b0474b6c2e412b4704d30d5383
miriam <miriam5000@gmail.com>
2017-12-16 23:33:44 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ require "rack-flash"
 class TweetsController < ApplicationController
   use Rack::Flash
 
-
+# read index
   get '/tweets' do
      if logged_in?
         @tweets = Tweet.all
@@ -12,85 +12,88 @@ class TweetsController < ApplicationController
      else
         redirect to '/login' # or /login
      end
-   end
+  end
+#problem: how to code that you only get your tweets? 
 
-   get '/tweets/new' do
-     if logged_in?
-       erb :'tweets/new'
-     else 
-       flash[:notice] = "You must new logged in to tweet!"
-       redirect to "/login"
-     end 
-   end
-
-   post '/tweets' do
-      if logged_in?
-          if current_user && params[:content] != ""
-             @user = User.find_by(:username => params[:username])
-             @tweet = current_user.tweets.create(:content => params[:content], :user_id => current_user.id)
-             redirect to "users/#{current_user.username}"
-          else
-            flash[:notice] = "Tweets need content"
-            redirect to "/tweets/new"
-           end  
-      else 
-        flash[:notice] = "Login to tweet."
-        redirect to "/login"
-      end
-   end
-
-   get '/tweets/:id' do
+#create
+ get '/tweets/new' do
+   if logged_in?
+     erb :'tweets/new'
+   else 
+     flash[:notice] = "You must new logged in to tweet!"
+     redirect to "/login"
+   end 
+ end
+# create/get data 
+ post '/tweets' do
     if logged_in?
-        if @tweet = Tweet.find(params[:id])
-          erb :'tweets/show'
+    
+        if current_user && params[:content] != ""
+           @user = User.find_by(username: params[:username])
+           @tweet = current_user.tweets.create(:content => params[:content], :user_id => current_user.id)
+           redirect to "users/#{current_user.username}"
         else
-          redirect to "/tweets"
-        end
-      else
-        flash[:notice] + "Log in to view a tweet!"
-        redirect to "/login"
-      end 
-    end 
+          flash[:notice] = "Tweets need content"
+          redirect to "/tweets/new"
+         end  
+    else 
+      flash[:notice] = "Login to tweet."
+      redirect to "/login"
+    end
+ end
 
-  get '/tweets/:id/edit' do
-    @tweets = Tweet.all.find_by(:id => params[:id]) # or params[:user_id]
-     if @tweet && logged_in?
-        if current_user.id = @tweet.user_id
+ get '/tweets/:id' do
+    if !logged_in?
+      flash[:notice] = "Log in to view a tweet!"
+        redirect to "/login"
+    else
+     
+      @tweet = Tweet.find_by(params[:user_id])
+          erb :'tweets/show'
+     end 
+  end 
+#update/form
+ get '/tweets/:id/edit' do
+
+    @tweets = Tweet.all.find_by(params[:user_id])
+     if  logged_in? && @tweets
+        if current_user.id = @tweets.user_id
             erb :"tweets/edit"
           # @tweets = Tweet.all.find_by(params[:user_id])  
         else
           redirect to :'/tweets'
         end 
-      else
-        redirect to "/login"
-      end 
-   end
-
-   patch '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
-#if no content
-    if logged_in? and params[:content] == ""
-        redirect to "/tweets/#{@tweet.id}/edit" 
-#if content and current user 
-    elsif
-        @tweet && current_user.id = @tweet.user_id
-        redirect to "/tweets/#{@tweet.id}"
-    else 
+     else
         redirect to "/login"
      end 
-   end
+  end
 
-   delete '/tweets/:id/' do
+#update/data
+  patch '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+#if no content
+     if logged_in? and params[:content] == ""
+        redirect to "/tweets/#{@tweet.id}/edit" 
+#if content and current user 
+     elsif
+        @tweet && current_user.id = @tweet.user_id
+        redirect to "/tweets/#{@tweet.id}"
+     else 
+        redirect to "/login"
+      end 
+  end
+
+  delete '/tweets/:id/' do
      #can't do loggedin and current user in one line...
      if logged_in?
         @tweet = Tweet.find_by(:id => params[:user_id]) # or find_by_id
         
         if @tweet && @tweet.user_id == current_user.id
             @tweet.delete
-            redirect to '/tweets'
+            redirect to '/'
         end 
      else
-       redirect to "/login"
+       redirect to "/"
      end 
    end
 end
