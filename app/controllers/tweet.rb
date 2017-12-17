d93e657bc72401dafcdf3ec0fd250e1c11950991
Elvin Garcia <dimix181@gmail.com>
2017-12-16 21:31:12 -0500

    tweet index spec passsing

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -3,6 +3,8 @@ class TweetController < ApplicationController
 
   get '/tweets' do
     if Helpers.logged_in?(session)
+      @current_user = Helpers.current_user(session)
+      @users = User.all
       erb :"/tweets"
     else
       redirect to '/login'
