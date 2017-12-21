43e287f966a5c62d8cd10b48364851cd5ba35cd5
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-21 11:23:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secre, "password_security"
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
 		def current_user
 			User.find(session[:user_id])
 		end
-    
+
 	end
 
 end
