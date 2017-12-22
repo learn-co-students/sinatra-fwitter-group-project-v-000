8a53943eadd05e4ae3b763fc78ebad6b0d22edc1
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-12-22 15:18:59 -0500

    Stupid Fedex truck ripped the fiperoptic internet cable from our house... no more tests for the night FML.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,63 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+   # set :session_secret, "password_security"
   end
 
-  get '/index' do
+  get '/' do
     erb :index
   end
+
+ get '/signup' do
+    if logged_in?
+
+      redirect '/tweets'
+     else
+        erb :index
+      end
+  end
+
+  post '/signup' do
+
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
+      redirect '/signup'
+     else
+       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+
+       session[:user_id] = @user.id
+      redirect '/tweets'
+      end
+  end
+
+  get "/tweets" do
+    erb '/tweets'
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb 'users/login'
+    end
+  end
+
+
+
+
+
+
+
+
+
+  helpers do
+     def logged_in?
+       !!session[:user_id]
+     end
+
+     def current_user
+       User.find(session[:user_id])
+     end
+   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
-class Tweet < ActiveRecord::Base
-    belongs_to :user
+class User < ActiveRecord::Base
+  has_many :tweets
+   has_secure_password
+
+   def slug
+      username.downcase.gsub(" ", "-")
+   end
+
+   def self.find_by_slug(slug)
+      User.all.find do |user|
+        user.slug == slug
+      end
+   end
+
 end
