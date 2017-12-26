1d986b0ed2c140f4ecb35bd1f5ba3935a94b6850
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-12-26 15:19:34 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,62 +6,28 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-   # set :session_secret, "password_security"
+   set :session_secret, "password_security"
   end
 
   get '/' do
     erb :index
   end
 
- get '/signup' do
-    if logged_in?
-
-      redirect '/tweets'
-     else
-        erb :index
-      end
+  get '/index' do
+    erb :index
   end
-
-  post '/signup' do
-
-    if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect '/signup'
-     else
-       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-
-       session[:user_id] = @user.id
-      redirect '/tweets'
-      end
-  end
-
-  get '/tweets' do
-     if logged_in?
-       @tweets = Tweet.all
-       erb :'tweets/tweets'
-     else
-       redirect to '/login'
-     end
-   end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb 'users/login'
-    end
-
- end
-
-
 
   helpers do
      def logged_in?
-       !!session[:user_id]
+
+      !!session[:user_id]
      end
 
      def current_user
-       User.find(session[:user_id])
+       User.find_by(id: session[:user_id]) if session[:user_id]
+
      end
+
    end
 
 end
