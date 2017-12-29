4bd69e2fd5402ee5fdb13be0e7b7aaa5cde78206
Olena Ageyeva <eageeva@hotmail.com>
2017-12-29 03:12:28 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -22,12 +22,14 @@ class TweetController<ApplicationController
     if params[:content].empty?
       redirect to '/tweets/new'
     else
+      #binding.pry
       @tweet=Tweet.create(content: params[:content], user_id: current_user.id)
       redirect to "/tweets/#{@tweet.id}"
     end
   end
 
-  get 'tweets/:id' do
+  get '/tweets/:id' do
+
     if logged_in?
        @tweet = Tweet.find_by_id(params[:id])
        erb :'tweets/show_tweet'
@@ -36,8 +38,37 @@ class TweetController<ApplicationController
      end
   end
 
-  get 'tweet/:id/delete' do
+  get '/tweets/:id/edit' do
+    if logged_in?
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.user_id==session[:user_id]
+      erb :'tweets/edit_tweet'
+    end
+    else
+      redirect to '/login'
+    end
 
+  end
+
+  patch '/tweets/:id' do
+    if params[:content].empty?
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.content=params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.delete if @tweet.user_id == session[:user_id]      
+      redirect to '/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
 end
