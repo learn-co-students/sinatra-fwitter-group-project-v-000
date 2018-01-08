3677ccd4643a2a7d61fcbaf6ccce5a702548285c
Alexander Friedland <friedlandaj@gmail.com>
2018-01-08 20:36:19 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,11 +8,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     #mine
     enable :sessions unless test?
-    set :session_secret, "secret"
+    set :session_secret, "password_security"
   end
 
   get '/' do
-
+    erb :index
   end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  helpers do
+
+    def logged_in?
+      
+    end
+  end
+
 
 end
