5923fff2abc484e1139b591a87495e66128d2287
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-01-13 21:05:21 -0500

    Create sign up form and associated route.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
   get '/signup' do
-    erb :'/users/create_user'
+    if !logged_in?
+      erb :'/users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
   end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      @user = User.create(params[:user])
+      session[:user_id]=@user.id
+      redirect to '/tweets'
+    end
+  end
+
 end
