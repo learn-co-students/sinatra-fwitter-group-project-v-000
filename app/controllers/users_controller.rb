3654368032c13e0203041e3c8b4bc095f0b0e8c5
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-01-13 21:20:02 -0500

    Create login form and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
   post '/signup' do
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
@@ -14,6 +22,16 @@ class UsersController < ApplicationController
       @user = User.create(params[:user])
       session[:user_id]=@user.id
       redirect to '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user
+      session[:id]= user.id
+      redirect '/account'
+    else
+      redirect to '/login'
     end
   end
 
