532dfc9148cca6e01ab05ab7411696029eeac50d
Chris Boujoukos <github email address>
2018-01-15 11:13:47 -0500

    built out file structure, started stubbing out MVCs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,52 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    user = User.create(params[:user])
+    if user.save && user.username != nil && user.username != ""
+      session[:user_id] = user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticeate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/tweets' do
+    erb :'/users/show'
+  end
+
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[user_id])
+    end
   end
 
 end
