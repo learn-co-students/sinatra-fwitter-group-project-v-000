d3e5cb3c7ffe3774049b442872bb47f3d9dc9433
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-15 00:51:46 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'/users/create_user', locals: {message: "Please sign up before you sign in"}
+      erb :'/users/create_user'
     else
       redirect to '/tweets'
     end
