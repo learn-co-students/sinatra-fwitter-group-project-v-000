31ff5d870f4adef1a5a70125709ec32350ffb046
Alex <github email address>
2018-01-17 15:41:57 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+
+    enable :sessions                    #sets sessions
+    set :session_secret, "secret"       #sets sessions
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
+    get '/' do
+         erb :index
+    end
+
 
 end
