5ed2c42bd92633bee2ad99dc139dade0b507d842
Hannah Carnes <hecarnes@gmail.com>
2018-01-17 17:27:08 -0500

    add reference to tweet table and reset db, and get associations working in tux

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20180117195707) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
