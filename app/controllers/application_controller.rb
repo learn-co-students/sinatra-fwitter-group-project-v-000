9ca0f17d07e0489fd50fe9b5b02ed38e94b0f328
Hannah Carnes <hecarnes@gmail.com>
2018-01-18 16:22:35 -0500

    split into multiple controllers and use helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  helpers ApplicationHelpers
 
   configure do
     set :public_folder, 'public'
@@ -21,51 +22,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-  post '/signup' do
-		user = User.new(username: params[:username], email: params[:email], password: params[:password])
-
-    if user.save
-      sign_in(user)
-			redirect "/tweets"
-		else
-			redirect "/signup"
-		end
-  end
-  
   get '/tweets' do
     erb :'tweets/tweets'
-  end
-
-  get '/login' do
-    erb :'users/login'
-  end
-
-  post '/login' do
-    user = User.find_by(username: params[:username])
-
-    if user && user.authenticate(params[:password])
-      sign_in(user)
-      redirect "/tweets"
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/logout' do
-    session.clear
-    redirect '/login'
-  end
-
-  def sign_in(user)
-    session[:user_id] = user.id
-    @current_user = user
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
