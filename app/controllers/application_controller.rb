676194e464fd698f860a50b19160c97c7a18f279
Hannah Carnes <hecarnes@gmail.com>
2018-01-18 15:21:33 -0500

    progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  before '/signup' do
+    redirect '/tweets' if current_user
+  end
+
   get '/' do
     erb :index
   end
@@ -21,6 +25,7 @@ class ApplicationController < Sinatra::Base
 		user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if user.save
+      sign_in(user)
 			redirect "/tweets"
 		else
 			redirect "/signup"
@@ -35,4 +40,28 @@ class ApplicationController < Sinatra::Base
     erb :'users/login'
   end
 
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      sign_in(user)
+      redirect "/tweets"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  def sign_in(user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
