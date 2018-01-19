658b4be2391fc7ddeee299f968a5e605d5b544ec
Alex <github email address>
2018-01-19 12:40:38 -0500

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,15 +1,94 @@
 require './config/environment'
+require 'rack-flash'
 
 class TweetController < ApplicationController
-
+  use Rack::Flash
     get '/tweets' do
 #binding.pry
-         erb :'/tweets/tweets'
+        if logged_in?
+        #  binding.pry
+            @user=current_user
+            @alltweets = Tweet.all
+            erb :'/tweets/tweets'
+#binding.pry
+        else
+            redirect to "/login"
+        end
+    end
+    get '/tweets/new' do
+        if logged_in?
+            erb :'/tweets/create_tweet'
+        else
+            redirect '/login'
+        end
 
     end
+    post '/tweets' do
+
+            if params[:content] == ""
+                  flash[:message] = "Tweets may not be left blank."
+                  redirect "/tweets/new"
+            else
+
+                  @tweet = Tweet.create(content: params[:content], user_id: current_user[:id])
+                  current_user.tweets << @tweet
+                  @user = current_user
+        #    binding.pry
+                  redirect "/tweets/#{@tweet.id}"
+              end
+    end
+    get '/tweets/:id' do
+#binding.pry
+          if logged_in?
+                  @user = current_user
+                  @tweet = Tweet.find_by(id: params[:id])
+  #      binding.pry
+                  erb :'tweets/show_tweet'
+          else
+                  redirect '/login'
+          end
+    end
+    delete '/tweets/:id' do
+#binding.pry
+          @user = current_user
+          @tweet = Tweet.find_by(id: params[:id])
+          if @user.id == @tweet.user_id
+            Tweet.delete(params[:id])
+            redirect to("/users/#{@user.slug}")
+          else
+            flash[:message] = "You cannot delete a tweet that you did not create."
+            redirect "/tweets/#{@tweet.id}"
+          end
+
+
+
+    end
+    get '/tweets/:id/edit' do
+#binding.pry
+          if logged_in?
+                  @user = current_user
+                  @tweet = Tweet.find_by(id: params[:id])
+                  erb :'/tweets/edit_tweet'
+          else
+                  redirect '/login'
+          end
+    end
+
+  patch '/tweets/:id' do
+#binding.pry
+
+    @tweet = Tweet.find_by(id: params[:id])
+    @tweet.update(content: params[:content])
+    @tweet.save
+
+    flash[:message] = "Successfully updated tweet."
+    redirect "/tweets/#{@tweet.id}/edit"
+  end
+
 
 
 end
 
 
 # =>        rspec spec/controllers/application_controller_spec.rb
+# =>        rspec spec/models/user_spec.rb
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
     get '/signup' do
     #  @session = session
 #binding.pry
-          if !!logged_in?
+          if logged_in?
               redirect '/tweets'
           else
               erb :"/users/create_user"
@@ -14,42 +14,62 @@ class UserController < ApplicationController
 
     end
 
-
     post '/signup' do
-      #:username => "skittles123",
-      #:email => "skittles@aol.com",
-      #:password => "rainbows"
+                  if params[:username]=="" || params[:email]=="" || params[:password]==""
+                       flash[:message] = "Do not leave Username/Email/Password sections blank."
+                        redirect to '/signup'
+                          #GREAT CODE BELOW FOR ADDITIONAL VALIDATIONS, BUT MESSES UP RSPEC TCS
+                                  #  elsif  !!User.find_by(username: params[:username])
+                                  #        flash[:message] = "This username has already been taken. Please makeup a new username."
+                                  #         redirect to '/signup'
+                                  # elsif  !!User.find_by(email: params[:email])
+                                  #         flash[:message] = "This email has already been taken. Please use a new email."
+                                  #        redirect to '/signup'
+                  else
 
-
-          if params[:username]=="" || params[:email]=="" || params[:password]==""
-               flash[:message] = "Do not leave Username/Email/Password sections blank."
-                redirect to '/signup'
-          elsif  !!User.find_by(username: params[:username])
-                flash[:message] = "This username has already been taken. Please makeup a new username."
-                 redirect to '/signup'
-         elsif  !!User.find_by(email: params[:email])
-                 flash[:message] = "This email has already been taken. Please use a new email."
-                redirect to '/signup'
-          else
-
-              @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-              session[:user_id] = @user.id
+                      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+                      session[:user_id] = @user.id
 #binding.pry
-              redirect '/tweets'
-          end
+                      redirect '/tweets'
+                  end
+
+    end
+    get '/login' do
+#binding.pry
+      if logged_in?
+          redirect '/tweets'
+      else
+          erb :"/users/login"
+      end
+    end
+    post '/login' do
+
+        @user = User.find_by(username: params[:username])
+#binding.pry
+        if @user && @user.authenticate(params[:password])
+              session[:user_id] = @user.id
+              redirect to '/tweets'
+        else
+
+              redirect '/login'
+              flash[:alert] = "Your username/password was Incorrect. If you need to register a new account please visit the Signup page."
+              #flash currently not working
+        end
+    end
+    get '/logout' do
+      session.clear
+      redirect to "/login"
+    end
+    get '/users/:slug' do
+
+          @user = current_user
+#binding.pry
+          erb :'/users/show'
 
     end
 
 
 end
 
-
-
-
-#SIGN UP
-#      You'll need to create two controller actions, one to display the user signup and one to process the form submission. The controller action that processes the form submission should create the user and save it to the database.
-#      The form to sign up should be loaded via a GET request to /signup and submitted via a POST request to /signup.
-#  #   The signup action should also log the user in and add the user_id to the sessions hash.
-#      Make sure you add the Signup link to the home page.
 
 # =>        rspec spec/controllers/application_controller_spec.rb
