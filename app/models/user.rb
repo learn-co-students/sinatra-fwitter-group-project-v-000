79d7f10c3589394f76e461c8508b4f16686b2527
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-01-23 15:23:13 +0000

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
     has_many :tweets
     has_secure_password
+    validates_presence_of :username, :email, :password
     
     def slug
-        self.username.gsub(/[^a-zA-Z0-9]/,"-")
+        self.username.gsub(/\s+/, '-').downcase
     end
     
     def self.find_by_slug(slug)
-        self.all.find do |user|
+        self.find do |user|
           user.slug == slug
         end
     end
