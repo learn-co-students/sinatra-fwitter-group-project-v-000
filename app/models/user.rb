a8963b5c10386948a2664b5bb5bb9841bf96beaf
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-24 02:58:05 +0000

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :tweets
+  extend Slugable::ClassMethods
+  include Slugable::InstanceMethods
+  
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+end
