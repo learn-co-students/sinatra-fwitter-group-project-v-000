e788b2b5188c29519230baa49ea2a5f0c7d05bd6
Joshua Needham <joshuabneedham@gmail.com>
2018-01-26 10:08:11 -0700

    Added initial layout and user login and helper code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:id]) if session[:id]
+    end
+  end
+
 end
