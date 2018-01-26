5a7dcb372e1a4b929759e105439416de701370b3
Caroline Shields <caroline.shields@amkor.com>
2018-01-26 22:20:36 +0000

    new,login routes/views, secure password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
-end
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.create(params[:user])
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'/tweets/tweets'
+  end
+
+
+  helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
+
+end #ends AppController class
