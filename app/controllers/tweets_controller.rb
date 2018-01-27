eca9f1957405f1fc427bd77bb68252dbcb4790c1
Aellon Krider <aellonkrider@gmail.com>
2018-01-27 09:52:25 -0500

    Update tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,11 @@
 class TweetsController < ApplicationController
+	get '/tweets' do
+		@tweets = Tweet.all
+		erb :'tweets'
+	end
 
+	get '/tweets/:slug' do
+    @tweet = Tweet.find_by_slug(params[:slug])
+    erb :'tweets/show'
+  end
 end
