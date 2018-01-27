e81a479ca5bf647d7afaa1c8a841ca1210d9b6ec
Aellon Krider <aellonkrider@gmail.com>
2018-01-27 10:00:39 -0500

    add edit route

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,20 @@
 class TweetsController < ApplicationController
 	get '/tweets' do
 		@tweets = Tweet.all
-		erb :'tweets'
+		erb :'/tweets/tweets'
 	end
 
 	get '/tweets/:slug' do
-    @tweet = Tweet.find_by_slug(params[:slug])
-    erb :'tweets/show'
+    	@tweet = Tweet.find_by_slug(params[:slug])
+    erb :'/tweets/tweets/show'
+  end
+
+  get '/tweets/:slug/edit' do
+  	if !logged_in?
+	    @tweet = Tweet.find_by_slug(params[:slug])
+	    erb :'/tweets/tweets/edit'
+	else
+		redirect to '/login'
+	end
   end
 end
