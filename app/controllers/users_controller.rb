8c3b93c4d5223cc211299d997d79e2ee321bd723
Aellon Krider <aellonkrider@gmail.com>
2018-01-27 09:08:13 -0500

    Update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UserController < ApplicationController
 
 	
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post 'signup' do
+  	@user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    erb :'users/show'
+  end
 end
