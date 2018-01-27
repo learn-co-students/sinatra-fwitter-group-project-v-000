b0dd8bacd7b82d7b2c01b62d72da8eade3135344
Aellon Krider <aellonkrider@gmail.com>
2018-01-27 11:24:52 -0500

    update tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,22 +5,31 @@ class TweetsController < ApplicationController
 	end
 
 	get '/tweets/new' do
+		if logged_in?
 		erb :'tweets/create_tweet'
+		else
+		redirect to '/login'
+	end
 	end
 
 	post '/tweets' do
 		@tweet = Tweet.new(content: params[:content])
-		erb :'tweets/show_tweet'
+		redirect to "/tweets/#{@tweet.id}"
 	end
 
-	get '/tweets/:slug' do
-    	@tweet = Tweet.find_by_slug(params[:slug])
+	get '/tweets/:id' do
+		if logged_in?
+    	@tweet = Tweet.find_by(params[:id])
+    	else
+		redirect to '/login'
+    	end
     erb :'tweets/show_tweet'
+
   end
 
-  get '/tweets/:slug/edit' do
+  get '/tweets/:id/edit' do
   	if logged_in?
-	    @tweet = Tweet.find_by_slug(params[:slug])
+	    @tweet = Tweet.find_by(params[:id])
 	    erb :'tweets/edit'
 	else
 		redirect to '/login'
