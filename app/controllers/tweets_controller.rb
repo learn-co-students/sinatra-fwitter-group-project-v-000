1a0ed5b9f901cf81a445f7b763d8641458a65798
Joshua Needham <joshuabneedham@gmail.com>
2018-01-27 09:23:54 -0700

    Pushing up fixes and additional functionality

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,29 +1,41 @@
 class TweetsController < ApplicationController
-	get '/tweets' do
-		@tweets = Tweet.all
-		erb :'tweets/tweets'
-	end
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
 
-	get '/tweets/new' do
-		erb :'tweets/create_tweet'
-	end
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
 
-	post '/tweets' do
-		@tweet = Tweet.new(content: params[:content])
-		erb :'tweets/show_tweet'
-	end
+  post '/tweets' do
+    if params[:content] == ""
+      redirect to "/tweets/new"
+    else
+      @tweet = current_user.tweets.create(content: params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
 
-	get '/tweets/:slug' do
-    	@tweet = Tweet.find_by_slug(params[:slug])
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
     erb :'tweets/show_tweet'
   end
 
-  get '/tweets/:slug/edit' do
-  	if logged_in?
-	    @tweet = Tweet.find_by_slug(params[:slug])
-	    erb :'tweets/edit'
-	else
-		redirect to '/login'
-	end
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_idg(params[:id])
+      erb :'tweets/edit'
+    else
+      redirect to '/login'
+    end
   end
 end
