2d29c0e2c24c6c018f0a3773d6a176347ca6900a
Aellon Krider <aellonkrider@gmail.com>
2018-01-27 08:48:03 -0500

    Add signup controller and form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
   helpers do
     def logged_in?
       !!current_user
