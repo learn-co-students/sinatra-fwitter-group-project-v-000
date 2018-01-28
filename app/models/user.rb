de4d5f84f12257c1873ad009f83b1da82cba3326
Sergio Correa <correa.sergio@gmail.com>
2018-01-28 21:14:56 +0000

    Add password column to Users table, remove secure passowrd for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  # has_secure_password
   has_many :tweets
+
+  def slug
+
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128200705) do
+ActiveRecord::Schema.define(version: 20180128210400) do
 
   create_table "tweets", force: :cascade do |t|
     t.string  "content"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180128200705) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
+    t.string "password"
   end
 
 end
