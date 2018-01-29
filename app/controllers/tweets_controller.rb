1d1ce01afaa00a9aee7a5bbad09b54050412f4df
Sergio Correa <correa.sergio@gmail.com>
2018-01-29 01:05:35 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,23 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    @user = current_user
+    @tweet = Tweet.new(content: params[:content])
+    @tweet.user = @user
+    @user.save
+    redirect "users/#{@user.slug}"
+
+    #must fix this action, use pry to inspect what is going on
+
+  end
+
 end
