c991af06a9085450a8722f985de64ee3ea3b0c4c
Sergio Correa <correa.sergio@gmail.com>
2018-01-29 00:24:05 +0000

    Add #logged_in? verification to GET /login and /tweets routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,26 @@ class UsersController < ApplicationController
   end
 
   get '/login' do
-    erb :'users/login'
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
   end
 
 
