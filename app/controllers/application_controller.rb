da20539d221c0a3bbb69c95a5062bd3673e7d5ac
Caroline Shields <cbshields@outlook.com>
2018-01-29 00:30:05 +0000

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,108 +11,17 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
     erb :index
-  end
-
-  get '/signup' do
-    if !logged_in?
-      erb :'/users/create_user'
-    else
-      redirect '/tweets'
-    end
-  end
-
-  post '/signup' do
-    user = User.create(params[:user])
-
-    if user.save
-      session[:user_id] = user.id
-      redirect '/tweets'
-    else
-      redirect '/signup'
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
-      erb :'/users/login'
-    end
-  end
-
-  post '/login' do
-    if params[:username] == "" || params[:password] == "" || params[:email] == ""
-      redirect '/signup'
-    else
-      user = User.find_by_slug(params[:username])
-        if user && user.authenticate(params[:password])
-          session[:user_id] = user.id
-          redirect '/tweets'
-        else
-          redirect '/signup'
-        end
-      end
 
   end
 
-
-  get '/tweets' do
-    @tweets = Tweet.all
-    erb :'/tweets/tweets'
-  end
-
-  get '/tweets/create' do
-    erb :'/tweets/create_tweet'
-  end
-
-  post '/tweets' do
-    @tweet = Tweet.create(params[:tweet])
-    binding.pry
-    @tweet.username = current_user.username
-    @tweet.save
-  end
-
-
-  get '/tweets/:id' do
-    @tweet = Tweet.find_by_id(params[:id])
-    binding.pry
-    erb :'/tweets/show_tweet'
-  end
-
-
-  get '/tweets/:id/edit' do
-    @tweet = Tweet.find_by_id(params[:id])
-    erb :'/tweets/edit_tweet'
-  end
-
-  patch '/tweets/:id' do
-      @tweet = Tweet.find_by_id(params[:id])
-      @tweet.update(params[:tweet])
-      @tweet.save
-      redirect "/tweets/#{@tweet.id}"
-  end
-
-post '/tweets/:id/delete' do
-  @tweet = Tweet.find_by_id(params[:id])
-  @tweet.delete
-end
-
-get '/logout' do
-
-  if logged_in
-    session.clear
-  else
-    redirect '/login'
-  end
-end
 
   helpers do
 		def logged_in?
-			!!session[:user_id]
+			!!current_user
 		end
 
 		def current_user
-			User.find(session[:user_id])
+			@current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
 		end
 	end
 
