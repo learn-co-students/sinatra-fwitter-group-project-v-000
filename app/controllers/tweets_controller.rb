8d72d064509554a7ab356a4f64bfa3c4df53bde7
Sergio Correa <correa.sergio@gmail.com>
2018-01-29 20:26:37 +0000

    TweetsController - Add GET '/tweets/:id' route and action.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,5 +27,13 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet  = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
 
 end
