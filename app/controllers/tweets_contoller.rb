6cbc45bc7e30fcaf29d7a4e014f67b997a7d3273
Brittany Morris <brittmmendez@gmail.com>
2018-01-31 21:05:09 +0000

    Done.

--- app/controllers/tweets_contoller.rb
+++ app/controllers/tweets_contoller.rb
@@ -1,4 +1,13 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets' do
+
+    erb :'tweets/create_tweet'
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
 
+  post '/signup' do
+    #get back infor from sign up form and add it here
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+##need to continue on with login steps and rest of user
 end
