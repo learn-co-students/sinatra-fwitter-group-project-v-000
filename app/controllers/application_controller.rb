e5b0c5bff6e8f8683d5793334541045163c17f5f
Nolan Byrnes <nolanehs@gmail.com>
2018-02-01 13:25:41 +0000

    Logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,46 @@
 require './config/environment'
+require 'sinatra'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+		enable :sessions
+		set :session_secret, "password_security"
   end
+
+  get '/' do
+
+    erb :index
+  end
+
+
+    get '/signup' do
+        erb :'/users/create_user'
+    end
+
+
+    post '/signup' do
+
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.username = params[:username]
+      @user.email = params[:email]
+      @user.password = params[:password]
+        if @user.username != "" && @user.password != "" && @user.email != ""
+          @user.save
+          session[:id] = @user.id
+          redirect :"/tweets"
+        else
+          redirect :"/signup"
+    end
+  end
+
+    get '/login' do
+      erb :'/users/login'
+    end
+
+
+
 
 end
