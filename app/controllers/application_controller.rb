58679f002841dfc8ad9f24e3595cfeefb0c7c0df
Emily Howard <howard.emilya@gmail.com>
2018-02-05 23:33:40 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,7 +76,7 @@ class ApplicationController < Sinatra::Base
   get '/tweets' do
 
     if logged_in?
-      @user = User.find_by(id: session[:user_id])
+      @user = User.find_by_id(session[:user_id])
       erb :'/tweets/tweets'
     else
       redirect to '/login'
@@ -104,7 +104,7 @@ class ApplicationController < Sinatra::Base
 
   get '/tweets/:id' do
     if logged_in?
-      @tweet = Tweet.find_by(id: params[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       erb :"/tweets/show_tweet"
     else
       redirect "/login"
@@ -113,7 +113,7 @@ class ApplicationController < Sinatra::Base
 
   get "/tweets/:id/edit" do
     if logged_in?
-      @tweet = Tweet.find_by(id: params[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       erb :"/tweets/edit_tweet"
     else
       redirect "/login"
@@ -122,7 +122,7 @@ class ApplicationController < Sinatra::Base
 
   patch "/tweets/:id" do
     if logged_in? && !params[:content].empty?
-      @tweet = Tweet.find_by(id: params[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       @tweet.content = params[:content]
       @tweet.save
       redirect "/tweets/#{@tweet.id}"
@@ -133,7 +133,7 @@ class ApplicationController < Sinatra::Base
 
   delete "/tweets/:id/delete" do
     if logged_in?
-      @tweet = Tweet.find_by(id: params[:id])
+      @tweet = Tweet.find_by_id(params[:id])
       if @tweet.user_id == current_user.id
         @tweet.delete
         @tweet.save
