d449f6cb23d61789088b3844d09ee0a0f0c4a3a0
Emily Howard <howard.emilya@gmail.com>
2018-02-05 19:03:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,52 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "secret"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'/tweets/tweets'
+  end
+
+  get '/signup' do
+
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    binding.pry
+
+    @user = User.create(params)
+    if @user.save
+      redirect to "/login"
+    else
+      redirect to "/signup"
+    end
+  end
+
+  get '/login' do
+
+  end
+
+
 
 end
