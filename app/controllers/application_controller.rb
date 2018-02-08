444aafdae91b6dae3b30c1769163586aa97c7742
Aly Alidina <aly_alidina@hotmail.com>
2018-02-08 03:59:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,31 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    "Welcome to Fwitter"
+    erb :index
   end
 
   get '/signup' do
     # @user = User.create(params["user"])
-    # binding.pry
-    erb :'/index'
+
   end
 
   post '/signup' do
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
+    @user.save
+    session[:id] = @user.id
     redirect :'/tweets'
+    redirect :'/tweets/signup'
   end
+
+  get '/login' do
+
+  end
+
+  post '/login' do
+
+  end
+
+
 
 
 end
