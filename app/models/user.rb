933697c936a30f31ddb21b331bf5041445710fe1
PJ Frias <peejfrias@gmail.com>
2018-02-10 04:42:46 +0000

    add helper and slug methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true
   has_secure_password
   has_many :tweets
 
@@ -9,5 +11,19 @@ class User < ActiveRecord::Base
   def find_by_slug(search_slug)
     self.all.detect{|i| i.slug == search_slug}
   end
+
+  def self.current_user(session_hash)
+    user = User.find(session_hash[:user_id])
+  end
+
+  def self.is_logged_in?(session_hash)
+    user = User.find(session_hash[:user_id]) if session_hash[:user_id]
+    if user
+      !!user
+    else
+      user
+    end
+  end
+end
 
 end
