b9ee64fac51ba7847b72c9b61dd5fba383aeb193
roitblatari <roitblatari@gmail.com>
2018-02-20 22:13:25 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,47 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :'/index'
+  end
+
+  get '/signup' do
+#  binding.pry
+   if logged_in?
+    redirect '/tweets' 
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    
+    # binding.pry   
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     redirect '/signup'
+    else
+      @user.save
+      session[:user_id] = @user.id 
+      redirect '/tweets'   
+    end
+    
+  end
+
+  get '/tweets' do
+   
+    erb :'/tweets/index'
+  end
+  # helpers do
+  def logged_in?
+    !!current_user
+  end
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+  # end
 end
--- db/migrate/20180220185956_create_tweets.rb
+++ db/migrate/20180220185956_create_tweets.rb
@@ -1,4 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
+    create_table :tweets do |t|
+      t.string :user_id
+      t.string :content
+    end
   end
 end
