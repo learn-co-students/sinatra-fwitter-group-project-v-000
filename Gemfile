cb0aa2be30b52e7a4d72c80c2f6079f2f5954253
Jacilyn Hayden <jacihayden@gmail.com>
2018-02-21 07:19:52 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -1,20 +1,16 @@
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem 'sinatra'
 gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
-gem 'rake'
-gem 'require_all'
 gem 'sqlite3'
+gem 'sinatra'
 gem 'thin'
 gem 'shotgun'
+gem 'tux'
 gem 'pry'
-gem 'bcrypt'
-gem "tux"
-
-group :test do
-  gem 'rspec'
-  gem 'capybara'
-  gem 'rack-test'
-  gem 'database_cleaner', git: 'https://github.com/bmabey/database_cleaner.git'
-end
+gem 'rake'
+gem 'rspec'
+gem 'rack-test'
+gem 'sinatra-twitter-bootstrap', :require => 'sinatra/twitter-bootstrap'
+gem 'capybara'
+gem 'require_all'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -7,4 +8,66 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  # load the home page
+  get '/' do
+    erb :'/index'
+  end
+
+  # load the create tweet form
+  get '/tweets/new' do
+    erb :'/tweets/create_tweet'
+  end
+  # process the form submission (new tweet)
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    redirect to '/tweets/show_tweet'
+  end
+  # displays the information for a single tweet
+  get '/tweets/:id' do
+
+    erb :'/tweets/show_tweet'
+  end
+  # one to load the form to edit a tweet
+  get '/tweets/:id/edit' do
+
+    erb :'/tweets/edit_tweet'
+  end
+  # update the tweet entry in the database
+  patch '/tweets/:id' do
+
+    erb :'/tweets/show_tweet'
+  end
+  # delete a tweet
+  delete '/tweets/:id/delete' do
+
+    redirect to '/tweets'
+  end
+  # display the user signup
+  get '/signup' do
+
+    erb :'/'
+  end
+  # process the form submission of user signup
+  patch '/signup' do
+
+    erb :'/'
+  end
+
+  # display the form to log in
+  get '/login' do
+
+    erb :'/'
+  end
+
+  # log add the `user_id` to the sessions hash
+  post '/login' do
+
+    erb :'/'
+  end
+
+# logout the user
+  get '/logout' do
+
+    erb :'/'
+  end
 end
