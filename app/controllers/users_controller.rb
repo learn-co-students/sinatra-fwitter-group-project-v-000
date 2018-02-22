41029e7dc029cb17bd249bca20c112704c739908
Richard Chang <rchang9999@yahoo.com>
2018-02-22 05:34:33 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user', locals:{message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
+  end
+
+end
