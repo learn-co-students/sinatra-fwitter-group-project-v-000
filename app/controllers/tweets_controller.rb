cfec3ef77e2cdcbfc2f460c68b4c126550c50b8c
Chris Harvey <chharvey@icloud.com>
2018-02-25 22:32:19 +0000

    Saving

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,29 @@
 class TweetsController < ApplicationController
+
+  get '/tweets' do
+  end
+
+
+  get '/tweets/new' do
+  end
+
+
+  get '/tweets/:id' do
+  end
+
+
+  get '/tweets/:id/edit' do
+  end
+
+
+  post '/tweets' do
+  end
+
+
+  patch '/tweets/:id' do
+  end
+
+
+  delete '/tweets/:id/delete' do
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+
+  post '/signup' do
+  end
+
+
+  get '/login' do
+  end
+
+
+  post 'login' do
+  end
+
+
+  get '/logout' do
+  end
+
+
+  get '/users/:slug' do
+  end
 end
