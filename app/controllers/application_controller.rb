1ee7a56cc166a1ffd90ff1c49e33dadbcc71a447
funemployeddad <funemployeddad@gmail.com>
2018-02-26 17:07:13 -0700

    working on does not let logged in user view signup page. need to figure out the password_digest issue again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require './config/environment'
+# require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  # use Rack::Flash
 
   configure do
     set :public_folder, 'public'
@@ -11,9 +13,23 @@ class ApplicationController < Sinatra::Base
     erb :"index"
   end
 
-  get '/users/signup' do
+  get '/signup' do
     erb :"/users/create_users"
   end
 
+  post '/signup' do
+  if params[:username].empty? || params[:password].empty? || params[:email].empty?
+      # flash[:message] = "Please enter all fields."
+      redirect '/signup'
+    else
+    redirect '/tweets'
+    end
+  end
+
+  get '/tweets' do
+    # binding.pry
+    @tweets = Tweet.all
+    erb :"/tweets/index"
+  end
 
 end
