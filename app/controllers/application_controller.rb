5c2525e2f2c27eee2331b20d8877140b32919185
github username <github email address>
2018-02-27 15:33:17 -0700

    edit action logged in lets a user edit their own tweet if they are logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -112,6 +112,23 @@ class ApplicationController < Sinatra::Base
      erb :"/tweets/show_tweet"
    end
 
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @user = current_user
+      @tweet = @user.tweets.find_by(:id => params[:id])
+      erb :"/tweets/edit_tweet"
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.update(:content => params[:content])
+    @tweet.save
+
+    redirect '/tweets/:id'
+  end
 
  helpers do
     def logged_in?
