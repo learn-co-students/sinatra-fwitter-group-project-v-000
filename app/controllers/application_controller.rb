c35de78f18eca65c9a584391764fb5cf6ae62256
Eva Navarror <emanao@me.com>
2018-02-28 16:13:39 +0000

    loads home and signup page and directs user to fwitter index after successful signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret_fwitter"
   end
+
+  get '/' do
+    erb :index
+  end
+
 
 end
