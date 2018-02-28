a4a762660d6cbbc337e9c2b1c32639508f36cb5f
Eva Navarror <emanao@me.com>
2018-02-28 20:31:19 +0000

    logout done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,13 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find(session[:user_id]) if !!session[:user_id]
     end
+  get '/logout' do
+      if logged_in?
+        session.clear
+        redirect "/login"
+      else
+        redirect "/"
+      end
+    end
   end
-
-
 end
