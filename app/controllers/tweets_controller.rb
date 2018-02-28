5ca8b07a8197872e83e7c48a6e3fd14acd9a29d2
Chris Harvey <chharvey@icloud.com>
2018-02-28 00:51:36 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,29 +1,50 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    redirect '/login' unless is_logged_in?(session)
+    @user = current_user(session)
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
   end
-
-
-  get '/tweets/new' do
-  end
-
-
-  get '/tweets/:id' do
-  end
-
-
-  get '/tweets/:id/edit' do
-  end
-
 
   post '/tweets' do
+    redirect '/tweets/new' if params[:content].empty?
+    user = User.find(session[:user_id])
+    user.tweets.create(content: params[:content])
+    redirect '/tweets'
   end
 
+  get '/tweets/new' do
+    if is_logged_in?(session)
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    redirect 'login' unless is_logged_in?(session)
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    redirect 'login' unless is_logged_in?(session)
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/edit_tweet'
+  end
 
   patch '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    redirect "/tweets/#{tweet.id}/edit" if params[:content].empty?
+    tweet.update(content: params[:content])
+    redirect "/tweets/#{tweet.id}"
   end
 
-
   delete '/tweets/:id/delete' do
+    tweet = Tweet.find(params[:id])
+    redirect "/tweets/#{tweet.id}" unless current_user(session) == tweet.user
+    tweet.destroy
+    redirect "/tweets"
   end
 end
