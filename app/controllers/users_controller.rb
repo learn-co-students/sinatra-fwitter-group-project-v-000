ae8df1df3196d382dd4a7772c9879ee806674d5a
jacobsilver2 <jacobsilver2@mac.com>
2018-03-03 23:47:23 -0500

    its been awhile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,49 @@
 class UsersController < ApplicationController
-   
-   get '/' do
-      erb :'/index'
+
+   get '/signup' do
+      if !logged_in?
+         erb :'/users/create_user'
+      else         
+         redirect 'tweets'
+      end
    end
 
+   get '/login' do
+      if !logged_in?
+         erb :'/users/login'
+      else
+         redirect '/tweets'
+      end
+   end
+
+   post '/login' do
+      @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect '/tweets'
+      else
+         redirect '/signup'
+      end
+   end
+
+   post '/signup' do
+      if params[:username] == "" || params[:password] == "" || params[:email] == ""
+         redirect '/signup'
+      else
+         @user = User.create(username: params[:username], password: params[:password], email: params[:email])
+         session[:user_id] = @user.id
+         redirect '/tweets'
+      end
+   end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
+    
 
 end
