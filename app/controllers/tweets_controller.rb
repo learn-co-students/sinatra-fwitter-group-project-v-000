0b71f812a31b0b5cef8cefe0cbde8cb65e45f464
James Ferguson <ncjamesferguson@gmail.com>
2018-03-06 20:18:05 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,11 +2,11 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
-    @user = current_user
-    erb :'/tweets/tweets'
-  else
-    redirect '/login'
-  end
+      @user = current_user
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
   end
 
 end
