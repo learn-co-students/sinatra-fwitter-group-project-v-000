2ef08de429fb977cf509ce498110278e0de6c11f
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-06 01:14:40 +0000

    added signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,18 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/registrations/signup'
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      # binding.pry
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      # session[:id] = @user.id
+      redirect '/tweets'
+    end
+  end
+
 end
