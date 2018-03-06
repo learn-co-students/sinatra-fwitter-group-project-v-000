3a8538f0ca2ae93257b41af9db6444a43b6c5c40
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-06 21:22:37 +0000

    added get '/login' and  post '/login' to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,4 +21,16 @@ class UsersController < ApplicationController
   get '/login' do
     erb :'/users/login'
   end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username]).authenticate(params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
 end
