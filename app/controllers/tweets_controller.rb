abca52c787ba3e68cdee5177e7390c99662e4840
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-06 23:08:17 +0000

    let's user view new tweet form if logged in, get '/tweets/new

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,12 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
 end
