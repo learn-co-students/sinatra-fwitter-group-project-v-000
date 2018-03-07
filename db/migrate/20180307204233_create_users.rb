a561f2030c072ad728705fe103843e1737881e99
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-03-07 21:22:14 +0000

    Done.

---
+++ db/migrate/20180307204233_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+#Users should have a username, email, and password, and have many tweets.
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+    end
+  end
+end
