39e8407dbb36e6be6f29480ac2382c83a3f22aad
Katrina Brinson <kbrinso@gmail.com>
2018-03-11 22:34:49 +0000

    login success redirect to tweets not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets' do
+    @user = User.find_by_id(session[:user_id])
+    @tweets = User.tweets
     erb :'/tweets/tweets'
+  end
+
+  get '/login' do
+    erb :'/users/login'
   end
 
   post '/signup' do
@@ -35,10 +41,21 @@ class ApplicationController < Sinatra::Base
       @user = User.new(username: params[:username], email: params[:email], password_digest: params[:password])
       if @user.save
         session[:user_id] = @user.id
-        redirect '/tweets'
+        redirect to '/tweets'
       else
         redirect to '/signup'
       end
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
     end
   end
 
