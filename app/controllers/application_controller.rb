46b30be093880b05ed5684b9a2ad3849bb96fc88
Katrina Brinson <kbrinso@gmail.com>
2018-03-11 23:07:28 +0000

    200 status route - views not displaying html in shotgun bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,12 +26,16 @@ class ApplicationController < Sinatra::Base
 
   get '/tweets' do
     @user = User.find_by_id(session[:user_id])
-    @tweets = User.tweets
+    #@tweets = User.tweets
     erb :'/tweets/tweets'
   end
 
   get '/login' do
-    erb :'/users/login'
+    if session.include?(:user_id)
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
   end
 
   post '/signup' do
@@ -49,10 +53,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
+    user = User.find_by(username: params[:username])
 
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     else
       redirect '/login'
