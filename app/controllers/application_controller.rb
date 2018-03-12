545b8ead817ca028a69a80186c8507a991108db5
Sally Zasloff <szasloff@gmail.com>
2018-03-12 20:11:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 require './config/environment'
+require 'pry'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
 
 
@@ -13,23 +18,37 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    erb :index
+    erb :signup
   end
-  
 
 
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      flash[:message] = "Missing Information- Please Try Sign Up Again"
+      redirect '/signup'
+    else
+      user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      if user.save
 
+      redirect '/tweets/tweets'
+      end
+    end
+  end
 
+    get '/users/login' do
+      erb :'users/login'
+    end
 
-
-
-
-
-
-
-
-
-
+    post '/users/login' do
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/tweets/tweets'
+      else
+        flash[:message] = "Login Failed, Please Try Again"
+        redirect '/users/login'
+      end
+    end
 
 
   helpers do
