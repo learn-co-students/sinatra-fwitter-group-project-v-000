783846bf98bc38ad7436a58f0beb3f245b6e2d6b
github username <github email address>
2018-03-12 21:45:15 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
+  validates_presence_of :username, :email, :password
 
   def slug
     username.downcase.gsub(" ","-")
   end
+
+  # def slug
+  #   self.username.gsub(/[!?,'." "]/, '-').downcase
+  # end
 
   def self.find_by_slug(slug)
     User.all.find{|user| user.slug == slug}
