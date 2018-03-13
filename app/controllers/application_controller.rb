f5314900f3c65352eb22dc1eb1b80363b7c6f80a
ingridwong0715 <ingrid830715@gmail.com>
2018-03-12 20:25:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,13 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
+    # set :views, 'app/views'
+      set :views, 'app/views'
+
+  end
+
+  get '/' do
+    erb :index
   end
 
 end
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,6 @@
+class TweetsController < ApplicationController
+  get '/tweets' do
+    erb :'tweets/tweets'
+  end
+
+end
