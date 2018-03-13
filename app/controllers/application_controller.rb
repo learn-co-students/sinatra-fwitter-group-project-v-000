346e2f0492180d95ddccc1d80b2369a76386f73f
ingridwong0715 <ingrid830715@gmail.com>
2018-03-13 10:37:22 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -59,11 +59,6 @@ class ApplicationController < Sinatra::Base
     redirect "/tweets"
   end
 
-  # get '/users/:id' do
-  #
-  #   @user = User.find(params[:id]) # OR SESSION[:USER_ID]??
-  #   erb :'/users/show'
-  # end
   get "/users/:slug" do
     @user = User.find_by_slug(params[:slug])
     @tweets = @user.tweets
@@ -78,8 +73,52 @@ class ApplicationController < Sinatra::Base
     else
       redirect '/'
     end
-
   end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/new' do
+    if !params[:content].empty?
+      @tweet = current_user.tweets.create(content: params[:content])
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id/edit' do
+
+    @tweet = Tweet.find(params[:id])
+    if !params[:content].empty?
+      @tweet.update(content: params[:content])
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+
+
 
   helpers do # A CONFIRMER: helpers methodes pour VIEW
 
