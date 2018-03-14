32bac6755df64f9931b813f733fe857ca99a00eb
Sally Zasloff <szasloff@gmail.com>
2018-03-14 01:03:31 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class TweetsController < ApplicationController
   use Rack::Flash
 
@@ -11,5 +12,42 @@ class TweetsController < ApplicationController
       end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/create_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == " "
+        redirect '/tweets/new'
+      else
+      @tweet = current_user.tweets.build(content: params[:content])
+      @tweet.save
+      # redirect '/tweets/#{@tweet.id}'
+    end
+  end
+      # else
+      #   redirect '/tweets/new'
+      # end
+
+  # else
+  #   redirect '/login'
+      # binding.pry
+
+
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb  :'/tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+end
 
 end
