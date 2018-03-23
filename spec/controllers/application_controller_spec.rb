0b2f9baf64708ca074a4e4af2e24605846c6be4a
Stephen Decker <sdeckerm@gmail.com>
2018-03-23 08:56:12 +0000

    Done.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -163,7 +163,7 @@ describe ApplicationController do
 
         user2 = User.create(:username => "silverstallion", :email => "silver@aol.com", :password => "horses")
         tweet2 = Tweet.create(:content => "look at this tweet", :user_id => user2.id)
-
+        
         visit '/login'
 
         fill_in(:username, :with => "becky567")
