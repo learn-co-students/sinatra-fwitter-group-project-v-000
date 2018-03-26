de4df436a97f59cbee480375410d569de0b841af
Lindsay <lindsay.c.dennis@gmail.com>
2018-03-25 20:01:40 -0500

    build scaffolding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do 
+  end 
+  
+  get '/signup' do 
+  end 
+
+  get '/login' do 
+  end 
+
+  get '/logout' do 
+  end 
+
+  get '/tweets' do 
+  end 
+
+  get 'tweets/new' do 
+  end 
+
+  get '/tweets/:id' do 
+  end 
+
+  get '/tweets/:id/edit' do 
+  end 
+
+  post '/signup' do 
+  end 
+
+  post '/login' do 
+  end 
+
+  post '/tweets' do 
+  end 
+
+  patch '/tweets/:id' do 
+  end 
+
+  delete '/tweets/:id/delete' do 
+  end 
+
+  module Helpers 
+
+  	def current_user 
+  	end 
+
+  	def logged_in?
+  	end 
+
+  end 
+
 end
