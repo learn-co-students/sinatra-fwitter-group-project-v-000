9f65bc4fd59e3021c1921a631c467b5dcf64e242
Alex Crisan <alexcrisan2005@hotmail.com>
2018-03-26 18:05:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,36 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password]
+      @user = User.create(params)
+    end
+    redirect '/tweets/index'
+  end
+
+
+
+
+
+  #logging in is simply storing the user's ID in the session hash.
+  #logging out is simply clearing the session hash.
+
+
+
+
+
+
+
+
+
+
 end
