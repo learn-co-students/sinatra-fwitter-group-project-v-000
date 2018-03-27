edfe6d2e911b49d80273f75e50d48aac8eb6b63e
Lindsay <lindsay.c.dennis@gmail.com>
2018-03-27 16:09:41 -0500

    add tweets routes and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,19 +14,11 @@ class ApplicationController < Sinatra::Base
   end 
   
   get '/signup' do
-    if logged_in?
-    	redirect to '/tweets'
-    else	
-        erb :'/users/create_user'
-    end 
+    
   end 
 
   get '/login' do
-  	if logged_in?
-  		redirect to '/tweets'
-  	else	
-        erb :'/users/login'
-    end     
+  	 
   end 
 
   get '/logout' do 
@@ -35,8 +27,7 @@ class ApplicationController < Sinatra::Base
   end 
 
   get '/users/:slug' do 
-     @user = User.find(session[:user_id])
-     erb :'/users/show'
+    
   end 
 
   get '/tweets' do 
@@ -76,24 +67,11 @@ class ApplicationController < Sinatra::Base
   end 
 
   post '/signup' do
-    if params[:username] != "" && params[:password] != "" && params[:email] != ""
-	    @user = User.create(username: params[:username], password: params[:password])
-	    session[:user_id] = @user.id 
-	    redirect to '/tweets' 
-	else 
-	    redirect to '/signup'    
-    end
+   
   end 
 
   post '/login' do
-  	@user = User.find_by(username: params[:username])
-  	if @user && @user.authenticate(params[:password])
-  		session[:user_id] = @user.id 
-  		redirect to '/tweets'
-  	else 
-  	    redirect to '/login'
-  	end    	
-
+  	
   end 
 
   post '/tweets' do
