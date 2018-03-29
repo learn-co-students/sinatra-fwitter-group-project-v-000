4e07928a341c547ecb47a1472f420e242cf2f840
shravnk <brendan.ode4@gmail.com>
2018-03-28 23:06:48 -0400

    Basic Tweet controller set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < Sinatra::Base
   	erb :index
   end
 
+  get '/tweets' do
+  	erb :'/tweets/tweets'
+  end
+
   get '/tweets/new' do
   	erb :'/tweets/create_tweet'
   end
@@ -24,5 +28,23 @@ class ApplicationController < Sinatra::Base
   get '/tweets/:id' do
   	@tweet = Tweet.find_by_id(params[:id])
   	erb :'/tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+  	@tweet = Tweet.find_by_id(params[:id])
+  	erb :'tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+  	@tweet = Tweet.find_by_id(params[:id])
+  	@tweet.update(content: params[:content])
+  	@tweet.save
+  	redirect "/tweets/#{@tweet.id}"
+  end
+
+  post '/tweets/:id/delete' do 
+  	@tweet = Tweet.find_by_id(params[:id])
+  	@tweet.delete
+  	redirect "/tweets"
   end
 end
