9250473797205cad91b553d20ac47c7019a8541e
johnwojo <jwojo13@gmail.com>
2018-04-02 16:09:25 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,29 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
   get '/signup' do
-    erb :'users/signup'
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
   end
 
   get '/login' do
     erb :'users/login'
+  end
+
+  post '/login' do
+  end
+
+  get '/logout' do
   end
 
 
