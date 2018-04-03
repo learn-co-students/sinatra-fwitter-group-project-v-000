0391e170413e1627854b4864b3a95af1cbcd53a7
johnwojo <jwojo13@gmail.com>
2018-04-03 12:03:05 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,14 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to "/signup"
+    else
+      @user = User.new(:username => params[:username], password => params[:password])
+      @user.save
+      session[user_id] = @user.id
+      redirect to "/tweets"
+    end
   end
 
   get '/login' do
