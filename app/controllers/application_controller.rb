5e14100118cb603c0fed7168b2c4292709f39353
Kim Gonzales <Kimgo589@gmail.com>
2018-04-03 22:40:16 -0400

    post signup creates a new user and saves based on input from user. If all required input is not provided, redirect to signup page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,30 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
   end
+
+#home page and user registration routes ------------------------
+  get '/' do 
+    erb :index
+  end 
+
+  get '/signup' do 
+    erb :'users/create_user'
+  end 
+
+  post '/signup' do 
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      @user = User.create(params)
+      @user.save 
+      session[:id] = @user.id
+      redirect to '/tweets'
+    else 
+      redirect '/signup'
+    end 
+  end 
+
+
 
 end
