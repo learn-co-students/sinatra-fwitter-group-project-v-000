31d54428a02fdf44b45b373b456d59072f93359a
Kim Gonzales <Kimgo589@gmail.com>
2018-04-04 18:12:13 -0400

    all specs for new action passing

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -9,4 +9,38 @@ class TweetController < ApplicationController
       redirect to '/login'
     end
   end 
+
+  get '/tweets/new' do
+    if User.is_logged_in?(session)
+      erb :'/tweets/create_tweet'
+    else 
+      redirect to '/login'
+    end 
+  end 
+
+  post '/tweets' do
+    if params[:content].empty?
+      flash[:message] ="Provide some content to post a tweet!"
+      redirect to '/tweets/new'
+    elsif User.is_logged_in?(session)
+      @user = User.find_by(id: session[:user_id])
+      @tweet =Tweet.create(params)
+      @user.tweets << @tweet 
+      @user.save 
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect to '/login'
+    end 
+  end
+
+    get '/tweets/:id' do
+      if User.is_logged_in?(session) 
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'/tweets/show_tweet'
+      else 
+        redirect '/login'
+      end 
+    end 
+
+
 end
