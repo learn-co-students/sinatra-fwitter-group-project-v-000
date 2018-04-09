01f7c3b34d5008d1333aba3d7f641df5c6db8c8a
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-04-09 17:22:51 +0000

    serperated controller concerns

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
     has_many :tweets
     has_secure_password
     validates_presence_of :username, :email, :password
-    include Slugger::InstanceMethods
-    extend Slugger::ClassMethods
+    
+    def self.find_by_slug(slug)
+      self.all.find{|x| x.slug == slug}
+    end
+    
+    def slug
+      self.username.downcase.gsub(" ","-")
+    end
 end
