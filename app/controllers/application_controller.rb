b98a9331c076e7bc70c33e6ad6533792bac0e52f
Aubrey Neimeier <neimeier.1@osu.edu>
2018-04-10 14:46:15 +0000

    stubbed out routes up to delete tweet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
+    erb :index
   end
+
+  get '/tweets/new' do
+    #load create tweet form
+    erb :create_tweet
+  end
+
+  get '/tweets/:id' do
+    #individual show tweet page
+    #
+    erb :show_tweet
+  end
+
+  post '/tweets' do
+    #submit and catch the data and create new tweet objects
+    #redirect to individual tweet page
+  end
+
+  get '/tweets/:id/edit' do
+    #load form to edit tweet
+    #find specific tweet from params[:id] to preload form
+    erb :edit_tweet
+  end
+
+  post '/tweets/:id' do
+    #catch the data for an edited tweet
+    #redirect to tweet show page
+  end
+
+  post '/tweets/:id/delete' do
+    #delete tweet by :id
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 end
