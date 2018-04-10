715c86258ab78b6e4299042c47a8122e44c31ac5
github username <github email address>
2018-04-10 10:22:43 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,51 +8,61 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    erb "/index"
   end
 
   get '/tweets/new' do
     #load create tweet form
-    erb :create_tweet
+    erb '/tweets/create_tweet'
   end
 
   get '/tweets/:id' do
     #individual show tweet page
     #
-    erb :show_tweet
+    erb '/tweets/show_tweet'
   end
 
   post '/tweets' do
     #submit and catch the data and create new tweet objects
     #redirect to individual tweet page
+    redirect '/tweets/:id'
   end
 
   get '/tweets/:id/edit' do
     #load form to edit tweet
     #find specific tweet from params[:id] to preload form
-    erb :edit_tweet
+    erb '/tweets/edit_tweet'
   end
 
   post '/tweets/:id' do
     #catch the data for an edited tweet
     #redirect to tweet show page
+    #update
+    redirect '/tweets/show_tweets'
   end
 
   post '/tweets/:id/delete' do
     #delete tweet by :id
+    erb '/index'
   end
 
+  get '/signup' do
+    erb '/users/create_user'
+  end
 
+  post '/signup' do
+    #needs to create new user object
+    #log user in with sessions hash
+    redirect '/login'
+  end
 
+  get '/login' do
+    #render login form
+    erb '/users/login'
+  end
 
-
-
-
-
-
-
-
-
-
-
+  post '/login' do
+    #add user_id to session hash to login
+    erb '/users/show'
+  end
 end
