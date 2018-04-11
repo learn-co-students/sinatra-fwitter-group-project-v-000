b15898f229852f0f94830154bdce9f69c7bf6293
MyMac <mymac@MyMacs-MacBook-Pro.local>
2018-04-11 09:50:14 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,39 +14,68 @@ class ApplicationController < Sinatra::Base
   end 
 
   get '/signup' do 
-  	erb :'users/create_user' 
+  	if logged_in?
+  		redirect to '/tweets'
+  	else
+  		erb :'users/create_user' 
+  	end 
   end 
 
   post '/signup' do 
   	# binding.pry
-  	@user = User.create(params[:user])
-  	session[:id] = @user.id 
-  	redirect to "/users/#{@user.id}"
+  	if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+	  	@user = User.create(username: params[:username], email: params[:email], password: params[:password])
+	  	session[:id] = @user.id 
+	  	redirect to "/tweets"
+	  else 
+	  	redirect to '/signup'
+	  end 
   end 
 
   get '/login' do 
-  	erb :'users/login' 
+  	if !logged_in?
+  		erb :'users/login' 
+  	else
+  		redirect to '/tweets'
+  	end 
   end 
 
   post '/login' do 
-  	@user = User.find_by(email: params[:user][:email])
-  	if @user && @user.authenticate(params[:user][:password])
-  		session[:id] = @user.id 
+  	# binding.pry
+  	if logged_in? 
+  		redirect to '/tweets'
   	else 
-  		redirect to '/login'
-  	end 
-  	redirect to "/users/#{@user.id}"
+	  	@user = User.find_by(username: params[:username])
+	  	if @user && @user.authenticate(params[:password])
+	  		session[:id] = @user.id 
+	  		redirect to "/tweets"
+	  	else 
+	  		redirect to '/login'
+	  	end 
+	  end 
   end 
 
-  get '/users/:id' do 
-  	@user = User.find(params[:id])
+  get '/users/:slug' do 
+  	@user = User.find_by_slug(params[:slug])
   	erb :'/users/show'
   end 
 
-  post '/logout' do 
-  	session.clear
-  	redirect to '/' 
+  get '/logout' do 
+  	if logged_in?
+  		session.clear
+  	end 
+  	redirect to '/login' 
   end 
+
+  get '/tweets' do 
+  	if logged_in?
+  		@user = User.find(session[:id])
+  		erb :'/tweets/tweets' 
+  	else
+  		redirect to '/login'
+  	end 
+  end 
+
 
   get '/tweets/new' do 
   	# binding.pry
@@ -59,39 +88,63 @@ class ApplicationController < Sinatra::Base
   end 
 
   post '/tweets' do 
-  	@tweet = Tweet.create(params[:tweet])
-  	@tweet.user = current_user
-  	@tweet.save
-  	current_user.save 
-  	redirect to "/tweets/#{@tweet.id}"
+  	# binding.pry
+  	if !params[:content].empty?
+	  	@tweet = Tweet.create(content: params[:content])
+	  	@tweet.user = current_user
+	  	@tweet.save
+	  	current_user.save 
+	  	redirect to "/tweets/#{@tweet.id}"
+	  else 
+	  	redirect to '/tweets/new'
+	  end 
   end 
 
   get '/tweets/:id' do 
-  	@tweet = Tweet.find(params[:id])
-  	erb :'/tweets/show_tweet'
+  	if logged_in?
+	  	@tweet = Tweet.find(params[:id])
+	  	erb :'/tweets/show_tweet'
+	  else 
+	  	redirect to '/login'
+	  end 
   end 
 
   get '/tweets/:id/edit' do 
   	if logged_in?
   		@tweet = Tweet.find(params[:id])
-  		erb :'/tweets/edit_tweet'
+  		@user = @tweet.user  
+  		if session[:id] == @user.id
+  			erb :'/tweets/edit_tweet'
+  		else 
+  			redirect to '/tweets'
+  		end  
   	else 
   		redirect to '/login'
   	end 
   end 
 
   post '/tweets/:id' do 
-  	@tweet = Tweet.find(params[:id])
-  	@tweet.update(params[:tweet])
-  	@user = @tweet.user 
-  	redirect to "/tweets/#{@tweet.id}"
+  	if params[:content].empty?
+  		redirect to "/tweets/#{params[:id]}/edit"
+	  else 
+	  	@tweet = Tweet.find(params[:id])
+	  	@tweet.update(content: params[:content])
+	  	redirect to "/tweets/#{@tweet.id}"
+	  end 
   end 
 
   post '/tweets/:id/delete' do 
+  	if !logged_in? 
+  		redirect to '/login'
+  	end 
   	@tweet = Tweet.find(params[:id]) 
   	@user = @tweet.user  
-  	@tweet.delete 
-  	redirect to "/users/#{@user.id}"
+  	if session[:id] == @user.id
+	  	@tweet.delete 
+	  	redirect to "/users/#{@user.slug}"
+	  else 
+	  	redirect to '/tweets'
+	  end 
   end 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base 
 	has_many :tweets
 	has_secure_password 
+
+	def slug 
+		self.username.downcase.gsub(" ", "-")
+	end 
+
+	def self.find_by_slug(slug) 
+		username = slug.gsub("-", " ")
+		self.all.detect{|user| user.username.downcase == username}
+	end 
 end 
