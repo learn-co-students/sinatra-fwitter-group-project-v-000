d54962ce0359dcf7962d7f02537a98d59253d5d2
Antonio Reid <read.musik@gmail.com>
2018-04-15 18:14:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if !session[:user_id].nil?
+      redirect '/tweets/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect '/signup'
+    elsif !session[:user_id].nil?
+      @user = User.find(session[:user_id])
+      redirect '/tweets/tweets'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect '/tweets/tweets'
+    end
+  end
+
 end
