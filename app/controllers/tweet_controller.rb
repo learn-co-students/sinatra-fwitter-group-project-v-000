59a9a8035e10e1ca773a452a1a1a0b1823e3cb7e
Eden Mesfun <emesfun@gmail.com>
2018-04-16 18:18:23 +0000

    created GET route /tweets/new so user can create a new tweet

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -11,5 +11,28 @@ class TweetController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    @user = User.find_by(session["user_id"])
+
+    if @user
+      erb :'tweets/create_tweet'
+    else
+      redirect to "/"
+    end
+  end
+
+  post '/tweets/new' do
+    @user = User.find_by(session["user_id"])
+
+    if params["content"] != ""
+      @tweet = Tweet.new
+      @tweet.content = params["content"]
+      @tweet.user_id = @user.id
+      @tweet.save
+    else
+      redirect to "tweets/new"
+    end
+  end
+
 
 end
