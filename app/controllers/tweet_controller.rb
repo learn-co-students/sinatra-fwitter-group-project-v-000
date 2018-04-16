2f730c66084c259223480151cdf67084088adc3e
Eden Mesfun <emesfun@gmail.com>
2018-04-16 17:09:41 +0000

    moved get /tweets route to Tweet Controller nad updated conditional statement

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,2 +1,15 @@
 class TweetController < ApplicationController
+
+  get '/tweets' do
+
+    @user = User.find_by(id: session["user_id"])
+
+    if logged_in
+      erb :'tweets/tweets'
+    else
+      redirect to "users/login"
+    end
+  end
+
+
 end
