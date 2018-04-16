6e507d72291087d0704e349f264db3d625c1e5fd
Eden Mesfun <emesfun@gmail.com>
2018-04-16 15:28:41 +0000

    created helper method #logged_in to check whether a session includes a user id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+
+  helpers do #block allows you to create helper methods
+    def logged_in
+      session.include?("user_id")
+    end
   end
 
   get '/' do
