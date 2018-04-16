aa548b2fd143385d784501b7062a092c5fe5af76
Eden Mesfun <emesfun@gmail.com>
2018-04-16 15:29:28 +0000

    created if/else statement inside of GET/signup route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,21 @@
 class UserController < ApplicationController
 
     get '/signup' do
-      erb :'users/create_user'
+      if logged_in
+        redirect to "/tweets"
+      else
+        erb :'users/create_user'
+      end
+
     end
 
     post '/signup' do
 
-      if params["username"] == nil || params["username"] == ""
+      if params["username"] == ""
+        redirect to "/signup"
+      elsif params["email"] == ""
+        redirect to "/signup"
+      elsif params["password"] == ""
         redirect to "/signup"
       else
         @user = User.new
@@ -14,9 +23,24 @@ class UserController < ApplicationController
         @user.username = params["username"]
         @user.password_digest = params["password"]
         @user.save
-      end
+        session[:user_id] = @user.id
+        redirect to "/tweets" #goes to GET route '/tweets'
+      end #if statement
+    end #POST route
 
-      redirect to "tweets/tweets"
+    get '/login' do
+      erb :'users/login'
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params["username"])
+      session[:user_id] = @user.id
+      redirect to "/tweets"  #goes to GET route '/tweets'
+    end
+
+    get '/tweets' do
+      @user = User.find_by(id: session["user_id"])
+      erb :'tweets/tweets'
     end
 
 end
