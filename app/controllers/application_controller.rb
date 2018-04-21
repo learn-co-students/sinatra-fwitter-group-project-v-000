ddebc07efeca23ac56b3d00411fd7e721206fbf6
Kelsey Fecho <kfecho30@gmail.com>
2018-04-21 21:46:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,18 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "victorias"
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
   end
 
 end
