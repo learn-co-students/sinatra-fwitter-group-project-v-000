7d07c4151cc29d8f260bd6cc9d8c53e54f5b2078
Araba Taylor <araba.taylor20@gmail.com>
2018-04-21 16:24:07 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,18 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    @tweets = Tweet.all
     erb :'tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+  post '/tweets/new' do
+    if !params[:content] == ""
+      @tweet = Tweet.create(params)
+      @tweet.save
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/' do
-    erb :'/home'
+    erb :'/index'
   end
 
   get '/signup' do
@@ -33,9 +33,11 @@ class UsersController < ApplicationController
 
   post '/login' do
       @user = User.find_by(username: params["username"], password: params["password"])
-      if @user
+      if @user != nil
         session[:user_id] = @user.id
-        redirect '/account'
+        redirect '/tweets'
+      else
+        redirect '/login'
       end
     end
 end
