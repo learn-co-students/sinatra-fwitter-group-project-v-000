d6060748c79ae6fb3229db5d6da40e62c4e16299
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-04-22 21:41:15 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,21 +2,27 @@ class TweetsController < ApplicationController
 
   get "/tweets" do
     if logged_in?
-      @user =User.find_by(username: session[:username])
+      @user = User.find_by(username: session[:username])
       erb :"/tweets/index"
     else
       redirect "/login"
     end
   end
 
-  get '/tweets/:username' do
-    if logged_in?
-      # binding.pry
-      @user = User.find_by(params[:user])
-      erb :"/tweets/show"
-    else
-      redirect "/login"
-    end
+  get '/users/:username' do
+    @user = User.find_by(params[:user])
+    erb :"/tweets/show"
+  end
+
+  get '/tweets/new' do
+    @user = User.find_by(params[:user])
+    erb :"/tweets/new"
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(content: params[:content])
+    @tweet.user = User.find_by(username: session[:username])
+    @tweet.save
   end
 
 
