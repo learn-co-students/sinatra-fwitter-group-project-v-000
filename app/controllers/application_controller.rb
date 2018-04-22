0049b80b91e39d8c39e3e5a8c94b04b5d7244ec2
Bertrand <bertrand.chevalier@gmail.com>
2018-04-22 11:26:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+ 
 
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,31 @@
 require './config/environment'
 
-class SessionsController < Sinatra::Base
+class SessionsController < ApplicationController
 
-    get '/signup' do 
-        @session = session
-        if session[:username] != nil
-          redirect "/tweets/#{session[:username]}"
-        else
-          redirect '/sessions/signup'
-        end
-      end
+  get '/signup' do 
+    @session = session
+    if session[:username] != nil
+      redirect "/tweets/#{session[:username]}"
+    else
+      erb :"/sessions/signup"
+    end
+  end
 
-      # get '/login' do
-      #   erb :'/sessions/login'
-      # end  
+    get '/login' do
+      erb :"/sessions/login"
+    end  
 
-      post '/signup' do
-        @user = User.create(username: params[:username], email:params[:email], password: params[:password])
-        session[:username] = params[:username]
-        redirect "/tweets/#{session[:username]}"
-      end
+  post '/signup' do
+    @user = User.create(username: params[:username], email:params[:email], password: params[:password])
+    session[:username] = params[:username]
+    redirect "/tweets/#{session[:username]}"
+  end
     
+
+  post '/login' do
+    binding.pry
+    @user =User.find()
+  end
 
 
 end
