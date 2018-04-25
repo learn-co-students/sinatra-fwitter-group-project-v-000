07ebcdebd091b96f060edb8938eea6af0fb43fd5
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-04-25 18:38:17 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.idea
+      redirect to '/tweets'
+    end
+  end
+
+end
