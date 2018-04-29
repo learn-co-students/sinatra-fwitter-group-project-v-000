6eed95171279db4ff9bc5574c7f84b38ede2f612
Carol Greene <carolgreene3@gmail.com>
2018-04-29 23:20:39 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,80 @@
 class TweetsController < ApplicationController
 
-  get '/tweets/new' do
-    erb :'tweets/create_tweet'
+  get '/tweets' do
+    if !session[:user_id]
+      redirect '/login'
+    else
+      @user = User.find(session[:user_id])
+      erb :'/tweets/tweets'
+    end
   end
 
+
+  get '/tweets/new' do
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'/tweets/create_tweet'
+    end
+  end
+
+
   post '/tweets' do
-    @tweet = Tweet.create(content: params["content"])
-    @tweet.user_id = session[:user_id]
-    @tweet.save
-    erb :'/tweets/tweets'
+    if !params[:content].empty?
+      @tweet = Tweet.create(content: params["content"])
+      @tweet.user_id = session[:user_id]
+      @tweet.save
+      erb :'/tweets/tweets'
+    else
+      redirect '/tweets/new'
+    end
   end
 
   get '/tweets/:id' do
-    if session[:user_id]
-      @tweet = Tweet.find(params[:id])
-      erb :'/tweets/show'
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
     else
       redirect to '/login'
     end
   end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if !logged_in?
+      redirect '/login'
+    elsif session[:user_id] == @tweet.user_id
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if params[:content].empty?
+      redirect to "/tweets/#{@tweet.id}/edit"
+    else
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    end
+  end
+
+  delete 'tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+
+    if !logged_in?
+      redirect '/login'
+    elsif session[:user_id] == @tweet.user_id
+
+      @tweet.destroy
+      redirect '/tweets'
+    else
+      redirect '/tweets'
+
+    end
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       @user = User.create(username: params["username"], email: params["email"], password: params["password"])
       @user.save
       session[:user_id] = @user.id
-      erb :'tweets/tweets'
+      redirect '/tweets'
     end
   end
 
@@ -28,5 +28,27 @@ class UsersController < ApplicationController
     else
       redirect '/tweets'
     end
+  end
+
+  post '/login' do
+
+    @user = User.find_by(username: params["username"])
+    #binding.pry
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
   end
 end
