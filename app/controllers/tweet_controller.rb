856524a50ed115cd18ea8baae3870b116251254a
Mallory Feuer <mallory.feuer@gmail.com>
2018-05-07 00:34:09 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -3,6 +3,7 @@ class TweetController < ApplicationController
 
   get '/tweets' do
     if logged_in?
+      @user = current_user
       @tweets = Tweet.all
       erb :"/tweets/tweets"
     else
@@ -56,7 +57,7 @@ class TweetController < ApplicationController
       if params[:content] == ""
         redirect "/tweets/#{@tweet.id}/edit"
       else
-  
+
         @tweet.update(content: params[:content])
         redirect "/tweets/#{@tweet.id}/edit"
       end
