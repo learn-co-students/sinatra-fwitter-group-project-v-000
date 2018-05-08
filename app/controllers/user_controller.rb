5779ae658110c6830a462af8bef322db68cd9b15
Joanne Lee <vernapii@gmail.com>
2018-05-08 20:03:14 +0000

    Done.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,15 +4,25 @@ class UserController < ApplicationController
     erb :"users/create_user"
   end
 
+  get '/login' do
+    erb :"users/login"
+  end
+
+  post '/show' do
+
+  end
+  
+  # get '/logout' do
+  #   erb :"users/"
+  # end
+
   post '/signup' do
     if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
       @user = User.create(params)
       session[:user_id] = @user.id
-      binding.pry
-      redirect “/tweets”
+    redirect '/tweets'
     else
-        # binding.pry
-      redirect "/signup"
+    redirect "/signup"
     end
   end
 
