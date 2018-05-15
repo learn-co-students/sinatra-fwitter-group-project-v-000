f41b704e2878eab147305b992240f5ad0cb192b2
Dee Race <deerace12@gmail.com>
2018-05-15 16:07:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,71 +13,15 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/tweets/new' do
-    if !is_logged_in?
-      redirect '/login'
-    else
-      erb :'/tweets/create_tweet'
-    end
-  end
-
-  post '/tweets' do
-    @tweet = Tweet.create(:content => params[:content], :user_id => params[:user_id])
-    redirect to "/tweets/#{@tweet.id}"
-  end
-
-  get '/signup' do
-    erb :'users/create_user'
-  end
-
-  post '/signup' do
-    if params[:username].empty?
-      redirect to :'/signup'
-    elsif params[:email].empty?
-      redirect to :'/signup'
-    elsif params[:password].empty?
-      redirect to :'/signup'
-    else @user = User.create(params)
-    redirect to :'tweets/tweets'
-    end
-  end
-
-  get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    end
-    erb :"users/login"
-  end
-
-  post '/login' do
-    user = User.find_by(:username => params[:username])
-
-    if user && user.authenicate(params[:password])
-      session[:user_id] = user.id
-      redirect '/tweets'
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect '/login'
-    else
-      redirect '/tweets'
-    end
-  end
-
   helpers do
+
     def logged_in?
-      !!current_user
+      !!session[:user_id]
     end
 
     def current_user
-      @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+      User.find_by_id(session[:user_id])
     end
   end
-
 
 end
