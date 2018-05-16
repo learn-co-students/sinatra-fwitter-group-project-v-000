76a5b44317ee248bf824ba9f06ea7a7e0f1ec883
Ally Kadel <akadel01@gmail.com>
2018-05-16 14:06:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+
+    invalid = 0
+    params.each do |param|
+      invalid = 1 if param[1] == ""
+    end
+    redirect '/signup' if invalid == 1
+
+    @user = User.create(params)
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  end
+
 end
