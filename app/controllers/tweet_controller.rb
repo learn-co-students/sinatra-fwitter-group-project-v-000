ae3b3380d70bf7079045bd55d012e8c771ec4afc
britt <britt.cumens@gmail.com>
2018-05-18 17:14:45 -0400

    edited tweet validations and redirects if not logged in. added show tweet route

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -19,6 +19,15 @@ class TweetController < ApplicationController
 
   post '/tweets' do
     @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
-    redirect "/tweets"
+    if !!@tweet.id
+      redirect "/tweets"
+    else
+      redirect "/tweets/new"
+    end
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweet/show_tweet'
   end
 end
