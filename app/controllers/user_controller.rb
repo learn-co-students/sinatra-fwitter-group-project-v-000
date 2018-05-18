d894c555f5976ae3c6fedddf0a49d673f1ddf65f
britt <britt.cumens@gmail.com>
2018-05-18 15:45:08 -0400

    redirects to tweet index after login

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,7 +14,7 @@ class UserController < ApplicationController
     @user = User.find_by(username: params[:username])
     if !!@user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect "/users/#{@user.slug}/tweets"
+      redirect "/tweets"
     else
       redirect to '/login'
       #flash message?
