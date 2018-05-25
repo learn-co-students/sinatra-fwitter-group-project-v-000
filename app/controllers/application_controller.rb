f4986c568ced2de989e6ab5db8f7b4dd5d731aba
amanda johnson <manda.panda91@yahoo.com>
2018-05-25 02:57:00 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    erb :'/tweets/show'
+  end
 end
