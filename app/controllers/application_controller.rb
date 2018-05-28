d86ec44bee876d5dd84015c8d9fbd8c6b334670e
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-28 23:18:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,16 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "ftweet"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  get '/signup' do
+    erb :"users/create_user"
+  end
+
 end
