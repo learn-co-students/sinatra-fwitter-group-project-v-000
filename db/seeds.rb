ceb04eb596b0a81111298af23cf3541cee989175
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-28 22:43:04 +0000

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+lucy = User.create(:user_name => "Lucy Ricardo", :email => "lucy@domain.com", :password => "test")
+t1 = Tweet.create(:content => "I want to be in a show")
+t2 = Tweet.create(:content => "I spend Ricky's money")
+lucy.tweets << t1
+lucy.tweets << t2
+lucy.save
