87e8fe447b536f31d4d02b56e246e6e99eba0a91
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-29 16:43:04 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,21 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if !(params[:username].empty? || params[:password].empty? || params[:email].empty?)
+      User.create(params)
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
 end
