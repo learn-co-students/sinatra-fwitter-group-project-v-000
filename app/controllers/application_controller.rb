7a7cc90e4c66ea20735d2efdbd46346f41f2e1e0
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-30 00:49:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,42 +51,6 @@ class ApplicationController < Sinatra::Base
     redirect '/tweets'
   end
 
-  get '/tweets/:id/edit' do
-    redirect '/failure' if !logged_in?
-    @tweet = Tweet.find(params[:id])
-    redirect '/failure' if @tweet.user_id != session[:user_id]
-    erb :"/tweets/edit_tweet"
-  end
-
-  get '/tweets/new' do
-    redirect "/login" if !logged_in?
-    erb :"/tweets/create_tweet"
-  end
-  post '/tweets' do
-    redirect "/login" if !logged_in?
-    # content required in form but just in case someone bypasses form
-    redirect '/tweets/new' if params[:content].length == 0
-    user = User.find(session[:user_id])
-    tweet = Tweet.create(:content => params[:content])
-    user.tweets << tweet
-    user.save
-    redirect "/tweets"
-  end
-
-  get '/tweets/:id' do
-    redirect "/failure" if !logged_in?
-    @tweet = Tweet.find(params[:id])
-    @user = User.find(@tweet.user_id)
-    redirect "/failure" if current_user.id != @tweet.user_id
-    erb :"/tweets/show_tweet"
-  end
-
-  get '/tweets' do
-    redirect "/login" if !logged_in?
-    @user = current_user
-    @tweets = Tweet.all
-    erb :"/tweets/tweets"
-  end
   get '/users/:slug' do
 #    redirect "/login" if !logged_in?
     @user = User.find_by_slug(params[:slug])
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,51 @@
+require './config/environment'
+
+class TweetsController < ApplicationController
+
+  get '/tweets/:id/edit' do
+    redirect '/login' if !logged_in?
+    @tweet = Tweet.find(params[:id])
+    redirect '/tweets' if @tweet.user_id != session[:user_id]
+    erb :"/tweets/edit_tweet"
+  end
+  patch '/tweets/:id' do
+    redirect '/login' if !logged_in?
+    redirect "/tweets/#{params[:id]}/edit" if params[:content].length == 0
+    @tweet = Tweet.find(params[:id])
+    redirect '/tweets' if @tweet.user_id != current_user.id
+    @tweet.content = params[:content]
+    @tweet.save
+    redirect '/tweets'
+  end
+  get '/tweets/new' do
+    redirect "/login" if !logged_in?
+    erb :"/tweets/create_tweet"
+  end
+
+  post '/tweets' do
+    redirect "/login" if !logged_in?
+    # content required in form but just in case someone bypasses form
+    redirect '/tweets/new' if params[:content].length == 0
+    user = User.find(session[:user_id])
+    tweet = Tweet.create(:content => params[:content])
+    user.tweets << tweet
+    user.save
+    redirect "/tweets"
+  end
+
+  get '/tweets/:id' do
+    redirect "/login" if !logged_in?
+    @tweet = Tweet.find(params[:id])
+    @user = User.find(@tweet.user_id)
+    redirect "/failure" if current_user.id != @tweet.user_id
+    erb :"/tweets/show_tweet"
+  end
+
+  get '/tweets' do
+    redirect "/login" if !logged_in?
+    @user = current_user
+    @tweets = Tweet.all
+    erb :"/tweets/tweets"
+  end
+
+end
