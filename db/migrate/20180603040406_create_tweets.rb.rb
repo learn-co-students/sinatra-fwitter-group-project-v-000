abde8698cddaf226193a5a7c55f648bf197e2a73
Sam. O <samrey2018@gmail.com>
2018-06-03 04:58:12 +0000

    Done.

--- db/migrate/20180603040406_create_tweets.rb.rb
+++ db/migrate/20180603040406_create_tweets.rb.rb
@@ -8,3 +8,8 @@ class CreateTweets.rb < ActiveRecord::Migration
     end
   end
 end
+
+
+# <h1>Index Page</h1>
+#
+# Welcome to Sinatra Security. Please <a href="/signup">Sign Up</a> or <a href="/login">Log In</a> to continue.
---
+++ notes.md
@@ -0,0 +1,3 @@
+ NOTES: all my files are created... But there are not showing in my directory.
+
+ Cheers. 
