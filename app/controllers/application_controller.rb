f0100a9cc9d73e930aa6ae93c16d92eb7e7739b3
JuanGongora <gongora.animations@gmail.com>
2018-06-05 21:48:10 -0400

    Setting up the environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions unless test? #test? refers to base method used for environment selection
+    set :session_secret, "secret"
   end
 
 end
