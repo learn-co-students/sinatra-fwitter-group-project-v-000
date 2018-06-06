228ecda66cd9bbddd2e5c5619b22e5ef9119dd1d
Sam. O <samrey2018@gmail.com>
2018-06-06 05:34:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/figures' do
+  @tweets = Tweet.all
+  @users = User.all
+  # model name followed by a method
+  erb :'/index'
+end
+
 end
