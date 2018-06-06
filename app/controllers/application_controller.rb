f35af1bfa1d765ceede869190e537863a3d7a756
Sam. O <samrey2018@gmail.com>
2018-06-06 17:15:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,11 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/figures' do
-  @tweets = Tweet.all
-  @users = User.all
-  # model name followed by a method
-  erb :'/index'
-end
+  get '/' do
+    @tweets = Tweet.all
+    @users = User.all
+    # model name followed by a method
+    erb :'/index'
+  end
 
 end
--- app/models/tweets.rb
+++ app/models/tweets.rb
@@ -1,4 +1,3 @@
-
 class Tweet < ActiveRecord::Base
   has_secure_password
 
