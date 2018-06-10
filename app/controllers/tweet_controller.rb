2a2a0d9e2e079f6056ce8b9b87e6af24c6ada62c
Trevor Seitz <trevinla@gmail.com>
2018-06-10 19:40:00 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,12 +1,11 @@
 class TweetController < ApplicationController
 
   get '/tweets' do
-    if is_logged_in?
-      # @tweets = current_user.tweets
+    if !is_logged_in?
+      redirect '/login'
+    else
       @tweets = Tweet.all
       erb :'/tweets/index'
-    else
-      redirect '/login'
     end
   end
 
@@ -18,51 +17,49 @@ class TweetController < ApplicationController
     erb :'/errors/signup'
   end
 
+  get '/errors/tweets_show' do
+    erb :'/errors/tweets_show'
+  end
+
   get '/tweets/new' do
-    if is_logged_in?
-      erb :'/tweets/new'
-    else
+    if !is_logged_in?
       redirect '/login'
+    else
+      erb :'/tweets/new'
     end
   end
 
   get '/tweets/show' do
-    if is_logged_in?
-      erb :'/tweets/show'
-    else
+    if !is_logged_in?
       redirect '/login'
+    else
+      erb :'/tweets/show'
     end
   end
 
   get '/tweets/:id' do
-    if is_logged_in?
+    if !is_logged_in?
+      redirect '/login'
+    else
       @tweet = Tweet.find_by_id(params[:id])
       erb :'/tweets/show'
-    else
-      redirect '/login'
     end
   end
 
   get '/tweets/:id/edit' do
-    if is_logged_in?
+    if !is_logged_in?
+      redirect '/login'
+    else
       @tweet = Tweet.find_by_id(params[:id])
-      erb :'/tweets/edit'
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/tweets/:id/delete' do
-    if is_logged_in?
-      destroy Tweet.find_by_id(params[:id])
-      erb :'/tweets/show'
-    else
-      redirect '/login'
+      if @tweet.user_id == current_user.id
+        erb :'/tweets/edit'
+      else
+        erb :'/errors/tweets/show'
+      end
     end
   end
 
   post '/tweets' do
-    # binding.pry
     if params[:content] == ""
       redirect '/tweets/new'
     end
@@ -73,6 +70,44 @@ class TweetController < ApplicationController
       redirect '/user/login'
     end
     @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
-    erb :'/tweets/show'
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  patch '/tweets/:id' do
+    if params[:content] == ""
+          @tweet = Tweet.find_by_id( params[:id])
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+    if !!@user = User.find_by(email: params[:email])
+      redirect '/user/login'
+    end
+    if !is_logged_in?
+      redirect '/user/login'
+    end
+    @tweet = Tweet.find_by_id( params[:id])
+    if @tweet.user_id == current_user.id
+      @tweet.content = params[:content]
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    else
+      erb :'/errors/tweets/show'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if !!@user = User.find_by(email: params[:email])
+      redirect '/user/login'
+    end
+    if !is_logged_in?
+      redirect '/login'
+    else
+      @tweet = Tweet.find_by_id(params[:id])
+      if @tweet.user_id == current_user.id
+        @tweet.delete
+        redirect '/tweets'
+      else
+        erb :'/errors/tweets_show'
+      end
+    end
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet <ActiveRecord::Base
   belongs_to :user
 
+    # validates_presence_of :content
+
   def slug
     self.name.downcase.gsub(" ", "-")
   end
