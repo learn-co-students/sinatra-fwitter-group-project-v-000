3e2918dd89ae8bef9dd23e00a07b506203a80dc4
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-12 14:26:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+    redirect "/tweets"
   end
 
 end
