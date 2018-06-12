ea1dd38fddb3f02d10f5ed25d8523ce346ba2cc1
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-12 15:30:08 -0400

    Rspeclemented new tweet action

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,12 +1,42 @@
 class TweetController < ApplicationController
     get '/tweets' do
-        if logged_in?
-            @tweets = Tweet.all
-            @user = user
-            erb :'/tweets/index'
-
-        else
+        if !logged_in?
             redirect to '/login'
         end
+
+        @tweets = Tweet.all
+
+        erb :'/tweets/index'
+    end
+
+    post '/tweets' do
+        if !logged_in?
+            redirect to '/login'
+        end
+
+        if params[:content].empty?
+            session[:new_tweet_error] = true
+            session[:new_tweet_error_message] = 'Tweets must not be blank!'
+            redirect to '/tweets/new'
+        end
+
+        tweet = Tweet.create(content: params[:content])
+        tweet.user = user
+        tweet.save
+
+
+        redirect to '/tweets'
+    end
+
+    get '/tweets/new' do
+        if !logged_in?
+            redirect to '/login'
+        end
+
+        session.delete(:new_tweet_error) ?
+            @new_tweet_error_message = session.delete(:new_tweet_error_message) :
+            @new_tweet_error_message = ''
+
+        erb :'/tweets/new'
     end
 end
