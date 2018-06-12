a1dfe54b1551c1b6469cd5eb7048a36355dba8cd
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-06-12 15:57:32 -0400

    Rspeclemented edit tweet action

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -57,4 +57,54 @@ class TweetController < ApplicationController
 
         erb :'/tweets/show'
     end
+
+    post '/tweets/:id' do
+        if !logged_in?
+            redirect to '/login'
+        end
+
+        if params[:content].empty?
+            session[:edit_tweet_error] = true
+            session[:edit_tweet_error_message] = "Tweets must not be blank!"
+            redirect to "/tweets/#{params[:id]}/edit"
+        end
+
+        tweet = Tweet.find(params[:id])
+
+        if !tweet
+            redirect to '/tweets'
+        end
+        
+        if !user.tweets.include?(tweet)
+            redirect to '/tweets'
+        end
+
+        tweet.content = params[:content]
+
+        tweet.save
+
+        redirect to "/tweets"
+    end
+
+    get '/tweets/:id/edit' do
+        if !logged_in?
+            redirect to '/login'
+        end
+
+        @tweet = Tweet.find(params[:id])
+
+        if !@tweet
+            redirect to '/tweets'
+        end
+
+        if !user.tweets.include?(@tweet)
+            redirect to '/tweets'
+        end
+
+        session.delete(:edit_tweet_error) ?
+            @edit_tweet_error_message = session.delete(:edit_tweet_error_message) :
+            @edit_tweet_error_message = ''
+
+        erb :'/tweets/edit'
+    end
 end
