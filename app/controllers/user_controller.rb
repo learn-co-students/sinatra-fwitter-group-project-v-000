61663b130db9fc4eb9d5fd6dcc8724f767dc763c
Diego Yanez <diegoyanez@MacBook-Pro-3.fios-router.home>
2018-06-12 19:06:06 -0400

    Done.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,35 @@
+class UserController < ApplicationController
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'user/show'
+  end
+  
+  get '/login' do
+    if !logged_in?
+      erb :'user/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+
+end
