9f0dfd334d7384e12ec8295691786ad4c280be9c
Sam. O <samrey2018@gmail.com>
2018-06-14 19:09:06 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -27,5 +27,34 @@ class TweetsController < ApplicationController
 
     end
 
+    post '/tweets' do
+
+      @tweets= Tweet.create(params[:tweet])   # This is where we set the name for song/ it want us to pass in an hash.
+
+      if logged_in?
+        @tweets.content = Tweet.create(params[:tweet])  # shovel in Title into figure.titles to be used in the views folder
+      end
+
+      @tweets.save
+      redirect to "/tweets/#{@tweets.id}"  # "/tweet/[name of newly created figure]
+    end
+
+    # <this connect here
+
+    patch "/tweets/:id" do
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(params[:tweet])
+
+    if logged_in
+      @tweets.content = Tweet.create(params[:tweet])  # shovel in Title into figure.titles to be used in the views folder
+    end
+
+    # :Note I can explicit with each.. but figures contains everthing already
+
+    @tweets.save
+
+    redirect to "/tweets/#{@tweets.id}"
+
+  end
 
 end
