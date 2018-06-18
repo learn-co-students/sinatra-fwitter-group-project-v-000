266df41f9addb6f112d1cea628aad7eb80ad1937
Emi Ko <emi8emi@gmail.com>
2018-06-18 14:12:50 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618180416) do
+ActiveRecord::Schema.define(version: 20180618210818) do
 
   create_table "tweets", force: :cascade do |t|
     t.string  "content"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20180618180416) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.string "password"
   end
 
 end
