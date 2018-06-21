8df5bb26bb9b27f5a57f11a5c7afbd0b5d81393d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 14:49:24 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,13 @@ class UsersController < ApplicationController
     if @user.save
       redirect :'/tweets'
     else
-      redirect :'/user/signup'
+      redirect :'/signup'
     end
   end
 
   post '/login' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    if @user != nil
+    @user = User.find_by(username: params["username"])
+    if @user && user.authenticate(params[:password])
       session[:id] = @user.id
       redirect '/tweets'
     else
@@ -28,7 +28,23 @@ class UsersController < ApplicationController
   end
 
   get '/tweets' do
-    erb :'/tweets/tweets'
+    if logged_in?
+      erb :'/tweets/tweets'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/create_tweet'
+  end
+
+  post '/tweets' do
+    erb :"/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    erb :'/tweets/show_tweet'
   end
 
 end
