cea4c2515eeb3b4b516d501facc15e5998a4a297
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-21 14:31:16 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/signup' do
+    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    if @user.save
+      redirect :'/tweets'
+    else
+      redirect :'/user/signup'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(email: params["email"], password: params["password"])
+    if @user != nil
+      session[:id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  validates_presence_of :username, :email
+  has_secure_password
+  has_many :tweets
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+end
