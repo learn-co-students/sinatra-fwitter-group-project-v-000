d1a75d5097ab02a533cd6630cbf7032262c3468e
Alex Wilson <fonzmp@gmail.com>
2018-06-21 18:18:03 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,6 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    binding.pry
-
     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
 
     redirect '/tweets'
