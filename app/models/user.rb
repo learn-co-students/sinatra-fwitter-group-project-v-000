3ff181ed69068b6c406d04df81453375aeb3200f
Karl Amoguis <ktamoguis@gmail.com>
2018-06-28 03:25:59 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
 
+  def slug
+    self.name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect{|x|x.slug = slug}
+  end
+
 end
