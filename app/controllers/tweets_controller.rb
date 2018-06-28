775c7b9debda646e067b2ec45cfc11707932274b
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-28 17:47:10 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,7 +6,13 @@ class TweetsController < ApplicationController
   
   post '/tweets' do 
     @tweet = Tweet.create(content: params[:content], user_id: session[:id])
-    
+    redirect to "/tweets/:id"
   end
+  
+  get '/tweets/:id' do 
+    @tweet = Tweet.find_by(user_id: session[:id])
+    erb :'/tweets/show'
+  end
+  
 
 end
