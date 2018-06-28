5a297ff50377aaf5e07f34bc912b0bd17c7d4d93
Suchitra Naik <susanaik@gmail.com>
2018-06-28 19:44:20 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,42 +11,4 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'/users/create_user'
-  end
-
-  get '/login' do
-    erb :'/users/login'
-  end
-
-  post '/signup' do
-    #raise params.inspect
-    username = params[:username]
-    email = params[:email]
-    password = params[:password]
-
-    if username != "" && email != "" && password != ""
-      @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      redirect to '/tweets'
-    else
-      redirect to '/signup'
-    end
-
-  end
-
-  post '/login' do
-    user = User.find_by(:username => params[:username])
-    if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect "/tweets/tweets"
-    else
-        redirect "/failure"
-    end
-  end
-
-  get '/tweets' do
-      erb :'/tweets/tweets'
-  end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,39 @@
 class UsersController < ApplicationController
 
 
+    get '/signup' do
+      erb :'/users/create_user'
+    end
+
+    get '/login' do
+      erb :'/users/login'
+    end
+
+    post '/signup' do
+      #raise params.inspect
+      username = params[:username]
+      email = params[:email]
+      password = params[:password]
+
+      if username != "" && email != "" && password != ""
+        @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        redirect to '/tweets'
+      else
+        redirect to '/signup'
+      end
+
+    end
+
+    post '/login' do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to "/tweets"
+      else
+          redirect to "/failure"
+      end
+    end
+
 
 end
