2e8f9a6140736d1512656f7071f521cfe61609d9
Suchitra Naik <susanaik@gmail.com>
2018-06-29 12:24:12 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,25 @@
 class TweetsController < ApplicationController
 
+    get '/tweets/new' do
+      erb :'/tweets/create_tweet'
+    end
+
     get '/tweets' do
+      @tweets = Tweet.all
         erb :'/tweets/tweets'
+    end
+
+    get '/tweets/:id' do
+    end
+
+    get '/tweets/:id/edit' do
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    end
+
+    get '/tweets/:id/delete' do
+      @tweet = Tweet.find_by_id(params[:id])
+      @tweet.delete
+      reditect to '/tweets'
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,9 +31,33 @@ class UsersController < ApplicationController
           session[:user_id] = user.id
           redirect to "/tweets"
       else
-          redirect to "/failure"
+        if logged_in?
+          redirect to "/tweets"
+        else
+          redirect to '/login'
+        end
       end
     end
+
+    get "/logout" do
+      if logged_in?
+        session.clear
+        redirect "/"
+      else
+          redirect "/login"
+      end
+
+    end
+
+    helpers do
+        def logged_in?
+          !!session[:user_id]
+        end
+
+        def current_user
+          User.find(session[:user_id])
+        end
+   end
 
 
 end
