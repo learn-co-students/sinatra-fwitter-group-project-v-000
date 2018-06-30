3a960a4b8bc53f27060ccb7273bfc80742677c5d
Suchitra Naik <susanaik@gmail.com>
2018-06-30 21:20:53 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,13 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect to "/login"
+    end
+  end
+
   get '/tweets' do
     if logged_in?
       @user = current_user
@@ -9,9 +17,19 @@ class TweetsController < ApplicationController
     end
   end
 
-    get '/tweets/new' do
+    get '/tweets/:id' do
       if logged_in?
-        erb :'/tweets/create_tweet'
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'/tweets/show_tweet'
+      else
+        redirect to "/login"
+      end
+    end
+
+    get '/tweets/:id/edit' do
+      if logged_in?
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'/tweets/edit_tweet'
       else
         redirect to "/login"
       end
@@ -28,17 +46,6 @@ class TweetsController < ApplicationController
        end
      end
 
-
-    get '/tweets/:id' do
-      if logged_in?
-        @tweet = Tweet.find_by_id(params[:id])
-        erb :'/tweets/show_tweet'
-      else
-        redirect to "/login"
-      end
-    end
-
-
     post '/tweets/:id' do
       @tweet = Tweet.find_by_id(params[:id])
       if params[:content] != ""
@@ -50,16 +57,6 @@ class TweetsController < ApplicationController
       end
 
     end
-
-    get '/tweets/:id/edit' do
-      if logged_in?
-        @tweet = Tweet.find_by_id(params[:id])
-        erb :'/tweets/edit_tweet'
-      else
-        redirect to "/login"
-      end
-    end
-
 
     delete '/tweets/:id/delete' do
       @tweet = Tweet.find_by_id(params[:id])
