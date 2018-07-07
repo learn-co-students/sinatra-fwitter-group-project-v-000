507cb23204ae0977557de437a44e177b4067b3e0
Augusto Granitto <augustogranitto@gmail.com>
2018-07-07 23:04:41 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,24 +9,25 @@ class UsersController < ApplicationController
    if !logged_in?
       erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
     else
-      binding.pry
       redirect to "/tweets"
     end
   end
 
   post '/signup' do
-    if validate_signup
+    if params[:username] =="" || params[:password] =="" || params[:email] ==""
+      redirect "/signup"
+    else
       user = User.new(params)
+      redirect "/tweets"
       if user.save
         session[:user_id] = user.id
         redirect "/tweets"
       else
         redirect "/signup"
       end
-    else
-      redirect "/signup"
     end
   end
+
 
   get '/login' do
     if logged_in?
@@ -38,7 +39,6 @@ class UsersController < ApplicationController
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect '/tweets'
