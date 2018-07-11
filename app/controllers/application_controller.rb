5c44c8905ca414e4a238f0149916f8079678b8c6
Gabriel Valle <gvalle91@gmail.com>
2018-07-11 22:38:02 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,33 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "programmingisfun"
   end
 
+  get '/' do
+    erb :index
+  end
+
+
+  helpers do
+
+    def loggin_in?
+      !!session[:id]
+    end
+
+    def login(username, password)
+      user = User.find_by(:username => username)
+      if user && user.authenticate(password)
+        session[:id] = user.id
+        redirect '/users/show'
+      else
+        redirect '/login'
+      end
+    end
+
+    def logout!
+      session.clear
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb
+  end
+
+  get '/login' do
+    erb :"users/login"
+  end
+
+  get '/logout' do
+    logout!
+  end
+
+  post '/login' do
+    login(params[:username], params[:password])
+    end
+  end
+
+  post '/signup' do
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+      login(params[:username], params[:password])
+    else
+      erb :"users/login"
+    end
+  end
 
 end
