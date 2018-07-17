be2b66cd5a054d4d09fcee75c99e30e4d7b51b47
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-07-17 06:37:59 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,78 @@
+require './config/environment'
+
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if Helpers.is_logged_in?(session)
+      @current_user = User.find_by_id(session[:user_id])
+
+      erb :'/tweets/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if Helpers.is_logged_in?(session)
+      @current_user = User.find_by_id(session[:user_id])
+
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by_id(params[:id])
+
+      erb :'/tweets/show'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.find_by_id(params[:id])
+
+      erb :'/tweets/edit'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if !params[:content].empty?
+      @current_user = User.find_by_id(session[:user_id])
+      @current_user.tweets << Tweet.new(params)
+      @current_user.save
+
+      redirect to '/tweets'
+    else
+      redirect to '/tweets/new'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if !params[:content].empty?
+      @tweet.update(content: params[:content])
+      @tweet.save
+
+      redirect to "/tweets/#{@tweet.id}"
+    else
+      redirect to "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @current_user = User.find_by_id(session[:user_id])
+    @tweet = Tweet.find_by_id(params[:id])
+    if @current_user.tweets.find_by_id(@tweet.id)
+      @tweet.delete
+    end
+    redirect to '/tweets'
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,56 @@
+require './config/environment'
+
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @current_user = User.find_by_slug(params[:slug])
+
+    erb :'/users/show'
+  end
+
+  get '/signup' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    end
+    erb :'/registrations/signup'
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      user = User.new(params)
+      user.save
+
+      session[:user_id] = user.id
+
+      redirect to '/tweets'
+    end
+
+    redirect to '/signup'
+  end
+
+  get '/login' do
+    if Helpers.is_logged_in?(session)
+      redirect to '/tweets'
+    else
+      erb :'/sessions/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+
+    redirect '/login'
+  end
 
 end
