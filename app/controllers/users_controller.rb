047dffa35db5c979b0d22d62987e63513ad34ca0
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-17 17:47:11 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,53 @@
 class UsersController < ApplicationController
-  get '/' do
-    erb :index
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/users/signup' do
+    if !@logged_in?
+      erb :'/users/create_user'
+    else
+      redirect '/tweets/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/users/signup'
+    else
+      @user = User.create(params[:id])
+      session[:user_id] == @user.id
+      redirect 'tweets/tweets'
+    end
+  end
+
+  get '/users/login' do
+    if !logged_in?
+      redirect '/users/login'
+    else
+      redirect '/tweets/tweets'
+    end
+  end
+
+  post '/users/login' do
+    user = User.create(params[:id])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets/tweets'
+    else
+      rediret '/users/signup'
+    end
+  end
+
+  get '/users/logout' do
+    if logged_in?
+      session.destroy
+      redirect '/users/login'
+    else
+    redirect '/'
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def slug
-    name.downcase.gsub(" ","-")
+    self.username.gsub(" ", "-").downcase
   end
 
   def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
+    self.all.find{|instance| instance.slug == slug}
   end
 end
