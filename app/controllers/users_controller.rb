286451feaa8c37263fa228f77620959ff89c575e
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-07-17 16:01:43 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,55 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/tweets' #doesn't allow access if already logged in
+    else
+    erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+    if @user.save && !params[:username].empty? && !params[:email].empty? #only allows new user if user name and email are entered
+    session[:user_id] = @user.id
+    redirect to "/tweets"
+    else
+    redirect to "/signup"
+    end
+  end
+
+
+ get '/login' do
+   if logged_in?
+     redirect to "/tweets"
+   else
+    erb :'users/login'
+   end
+ end
+
+ post '/login' do
+   @user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+		    session[:user_id] = @user.id
+        redirect to "/tweets"
+    else
+        redirect to "/signup"
+    end
+ end
+
+ get '/users/:slug' do #go to user page
+   @user = User.find_by_slug(params[:slug])
+   erb :'users/show'
+ end
+
+ get '/logout' do # end session
+   if !logged_in?
+     redirect to "/"
+   else
+     session.clear
+     redirect to "/login"
+   end
+ end
+
 
 end
