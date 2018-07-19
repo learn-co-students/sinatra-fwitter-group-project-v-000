1d38a43965b55a5cfc472e24a8a9e6ad2730cac6
haydenbetts <haydenbetts@gmail.com>
2018-07-18 19:30:24 -0700

    login and logout messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
     end
 
     def logged_in?
-      !!session[:name]
+      !!session[:user_id]
     end
 
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
 
     get '/login' do
+        erb :"users/login"
+    end
+
+    post '/login' do
+        login(params["username"], params["email"], params["password"])
+        redirect "/tweets"
+    end
+
+    get '/logout' do
+        session.clear
+        redirect "/"
     end
 
     get '/signup' do
+
+        if logged_in?
+            redirect "/tweets"
+        end
 
         erb :"users/signup"
     end
 
     post '/signup' do
-
-        # if logged_in?
-        #     redirect "/tweets"
-        # end
 
         @user = User.new(username: params["username"], email: params["email"], password: params["password"])
 
