b330c2489a3107f6dd2b9b8e550946762796f974
haydenbetts <haydenbetts@gmail.com>
2018-07-18 18:47:14 -0700

    redirect from signup to tweets

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,30 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  helpers do 
+
+    def login(username, password)
+      user = User.find_by(username: username)
+
+      if user && user.authenticate(password)
+        session[:name] = user.username
+      else
+        redirect "/login"
+      end
+    end
+
+    def logged_in?
+      !!session[:name]
+    end
+
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,24 @@ class UsersController < ApplicationController
     end
 
     get '/signup' do
+
+        erb :"users/signup"
+    end
+
+    post '/signup' do
+
+        if logged_in?
+            redirect "/tweets"
+        end
+
+        user = User.new(username: params["user"]["name"], password: params["user"]["password"])
+
+        if user.save
+            login(params["user"]["name"], params["user"]["password"])
+            redirect "/tweets"
+        else
+            redirect '/'
+        end
     end
 
 end
