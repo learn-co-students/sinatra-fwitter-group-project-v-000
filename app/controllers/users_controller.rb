fa3109a60703b52244326875a3743d1722dbc116
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-20 14:29:24 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,35 @@
 class UsersController < ApplicationController
 
   get "/signup" do
-    erb :"users/create_user"
+    erb :"/users/create_user"
   end
 
   post "/signup" do
     @user = User.create(params)
   end
 
+  get "/login" do
+    erb :"/users/login"
+  end
 
+  post "/login" do
+    params #save in session hash
+    #session << params
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
+  end
+
+  #helper_methods
+
+  def current_user
+
+  end
+
+  def logged_in?
+
+  end
 
 end
