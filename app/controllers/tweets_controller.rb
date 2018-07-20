16c36ff7f843febd5e0e90cdc2a7a02767c832d2
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-20 14:25:24 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
 
+  get "/tweets/new" do
+    erb :"/tweets/create_tweet"
+  end
+
+  post "/tweets" do
+    @tweet = Tweet.create(params)
+    erb :"/tweets/tweets"
+  end
+
+  get "tweets/:id/edit" do
+    @tweet = Tweet.find_by(id: params[:id])
+    erb :"/tweets/edit_tweet"
+  end
+
+  get "tweets/:id" do
+    @tweet = Tweet.find_by(id: params[:id])
+    erb :"/tweets/show_tweet"
+  end
+
+  patch "tweets/:id" do
+    @tweet = Tweet.find_by(id: params[:id])
+      # change name/whatever according to params
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  delete "tweets/:id/delete" do
+    @tweet = Tweet.find_by(id: params[:id])
+    @tweet.clear
+  end
+
+
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+    erb :"users/create_user"
+  end
+
+  post "/signup" do
+    @user = User.create(params)
+  end
+
+
 
 end
