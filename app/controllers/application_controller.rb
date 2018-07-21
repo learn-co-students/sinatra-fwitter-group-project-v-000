180fefb477ca293e0f21c72d190aa16fd2466e0e
Maverick Tesini <mavericktesini@gmail.com>
2018-07-21 23:34:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/index' do #homepage index request
+    erb :index
+  end
+
+  get '/signup' do #signup page request
+    erb :signup
+  end
+
+  post '/signup' do #signup
+    user = User.create(:username => params[:username], :password => params[:password])
+  end
+
 end
