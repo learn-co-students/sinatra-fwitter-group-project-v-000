2e5cfdab8fa2cbc5e0cfff8cae130f6ace72260e
Elaine Anderson <eparie@bu.edu>
2018-07-24 17:43:52 +0000

    saftey

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,39 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+    erb :signup
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:password].empty? || params[:email].empty?
+      redirect to '/signup'
+    else
+      @user = User.create(params)
+      @user.save
+    session[:user_id] = @user.id
+    redirect to '/tweets'
+    end
+  end
+
+def logged_in?
+!!current_user
+end
+
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,7 @@
 class TweetsController < ApplicationController
 
+get '/tweets' do
+  erb :tweets
+end
 
 end
