743c70e5aac078291d0eb74041999c276f14f80a
Elaine Anderson <eparie@bu.edu>
2018-07-25 18:58:15 +0000

    completes lab

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ has_secure_password
     user = user.split(" ")
     user.join("-")
   end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+      user.slug == slug
+      end
 end
 
 
-def self.find_by_slug(slug)
-  self.all.find do |user|
-    user.slug == slug
-    end
 
     end
