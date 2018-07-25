042146a3fe8e3b0941a5b9d0abb84f1fcdd0a585
Elaine Anderson <eparie@bu.edu>
2018-07-25 13:06:30 +0000

    saftey

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -20,4 +20,24 @@ else
 end
 end
 
+get '/tweets/:id' do
+  @tweet = Tweet.find_by(params[:id])
+  erb :'/tweets/show'
+end
+
+get '/tweets/:id/edit' do
+  @tweet = Tweet.find_by(params[:id])
+  erb :'/tweets/edit'
+end
+
+post '/tweets/:id' do
+  if !params[:content].empty?
+  @tweet = Tweet.find_by(params[:id])
+@tweet.update(params)
+erb :'/tweets/show'
+else
+  redirect :'/tweets/1/edit'
+end
+end
+
 end
