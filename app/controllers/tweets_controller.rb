0087b3283567cf476b62054e674af3daccb87f20
Elaine Anderson <eparie@bu.edu>
2018-07-25 12:34:44 +0000

    saftey

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,4 +6,18 @@ get '/tweets' do
   erb :tweets
 end
 
+get '/tweets/new' do
+  erb :'/tweets/new'
+end
+
+post '/tweets' do
+  @user = User.find_by(params[:id])
+  if !params[:content].empty?
+  @user.tweets << Tweet.create(params)
+  @user.save
+else
+  redirect '/tweets/new'
+end
+end
+
 end
