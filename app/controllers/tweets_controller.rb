1adf933ff68401f49b4f04ffd074d0733f6f9757
Shirley Collins <shirley@m104.us>
2018-07-26 10:22:11 -0700

    updates

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -45,10 +45,41 @@ class TweetController < ApplicationController
     end
   end
 
-  delete '/delete/:tweet_id' do
+  get '/tweets/:tweet_id/edit' do
+    if Helpers.current_user(session)
+      @tweet = Tweet.find_by_id(params[:tweet_id])
+      @tweeter = User.find_by_id(@tweet.user_id)
+
+      if Helpers.current_user(session) == @tweeter
+        erb :'/tweets/edit'
+      else
+        redirect :"/tweets/#{@tweet.id}"
+      end
+    else
+      redirect :'/login'
+    end    
+  end
+
+  patch '/tweets/:tweet_id/edit' do
+    if Helpers.logged_in?(session)
+      if params[:content].empty?
+        redirect :"/tweets/#{@tweet.id}"
+      else
+        @tweet = Tweet.find_by_id(params[:tweet_id])
+        @tweet.content = params[:content]
+        @tweet.save
+
+        redirect :"/tweets/#{@tweet.id}"
+      end
+    else
+      redirect :'/login'
+    end
+  end
+
+  delete '/tweets/:tweet_id/delete' do
     @tweet = Tweet.find_by_id(params[:tweet_id])
     @tweeter = User.find_by_id(@tweet.user_id)
-    
+
     if Helpers.current_user(session) == @tweeter
       @tweet = Tweet.find_by_id(params[:tweet_id])
       @tweet.delete
