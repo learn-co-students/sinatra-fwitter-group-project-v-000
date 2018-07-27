a40b7b92f19bd37a5542508a36f7c0a42b581796
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-27 19:38:05 +0000

    Done.

---
+++ db/migrate/20180727191417_create_users_table.rb
@@ -0,0 +1,12 @@
+
+class CreateUsersTable < ActiveRecord::Migration
+  
+  def change
+    create_table :users do |column|
+      column.string :username
+      column.string :email
+      column.string :password_digest
+    end
+  end
+  
+end
---
+++ db/migrate/20180727192639_add_user_id_to_tweets.rb
@@ -0,0 +1,8 @@
+
+class AddUserIdToTweets < ActiveRecord::Migration
+  
+  def change
+    add_column :tweets, :user_id, :integer
+  end
+  
+end
