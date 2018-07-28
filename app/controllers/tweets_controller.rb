d5fca2155a1a98e52fae667b937147c5f913b898
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-28 21:14:52 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,14 +3,19 @@ class TweetsController < ApplicationController
 
   get "/tweets" do
     if logged_in?
-      @tweets = Tweet.all 
+      @tweets = Tweet.all
       erb :"/tweets/tweets"
     else
       redirect "/login"
     end
   end
 
+  get "/tweets/new" do
+    erb :"/tweets/create_tweet"
+  end
 
-
+  post "/tweets" do
+    binding.pry
+  end
 
 end
