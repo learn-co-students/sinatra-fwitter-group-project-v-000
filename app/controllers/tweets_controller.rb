76614df283e76c8e36c2ab1e67eb33ccebd4a735
Pamela Frieze <pfrieze@gmail.com>
2018-07-28 20:59:14 +0000

    tweet page addition

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
 
   get "/tweets" do
     if logged_in?
+      @tweets = Tweet.all 
       erb :"/tweets/tweets"
     else
       redirect "/login"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,8 +37,8 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    @user = User.find_by(params[:username])
-      if @user && user.authenticate(params[:password])
+    @user = User.find_by(username: params[:username])
+      if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect '/tweets'
       else
@@ -49,6 +49,11 @@ class UsersController < ApplicationController
     get '/logout' do
       session.clear
       redirect "/login"
+    end
+
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :"users/show"
     end
 
 end
