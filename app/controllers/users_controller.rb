0b800d716feca349c51e084c0230b68b4e5752ce
Frank McCallion <fkmccallion@gmail.com>
2018-07-30 04:00:26 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,43 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'/users/signup'
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect "/tweets"
+    else
+      erb :'users/login'
+    end
   end
 
   post '/signup' do
     if params[:username].empty? || params[:email].empty?  || params[:password].empty?
-      redirect '/signup'
+      redirect "/signup"
     else
-      @user = User.new(:username => params[:username], :email => params[:email])
+      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
       @user.save
       session[:user_id] = @user.id
-      binding.pry
-      redirect '/tweets/index'
+      redirect "/tweets"
     end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      #binding.pry
+      redirect "/tweets"
+    else
+      redirect "/signup"
+
+    end
+    #binding.pry
   end
 
 end
