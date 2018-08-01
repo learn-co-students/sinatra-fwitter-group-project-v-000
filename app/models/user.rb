be13154e51a996fde9b421bf5c404a4e4023a4f9
Ryan Manchester <ryanm17@gmail.com>
2018-08-01 19:56:52 +0000

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class User < ActiveRecord::Base
+  has_many :tweets
+  has_secure_password
+
+  def slug
+    self.name.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    binding.pry
+  end
+
+end
--- config.ru
+++ config.ru
@@ -6,3 +6,5 @@ end
 
 use Rack::MethodOverride
 run ApplicationController
+run UsersController
+run TweetsController
