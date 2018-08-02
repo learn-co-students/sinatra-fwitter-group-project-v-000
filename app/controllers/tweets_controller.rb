f432f0dbfda35333e6207d5da36710717046596d
Frank McCallion <fkmccallion@gmail.com>
2018-08-02 04:32:21 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,7 +18,69 @@ class TweetsController < ApplicationController
   end
 
   post '/new' do
-    binding.pry
+    #@tweet = current_user.tweets.build(content: params[:content])
+    if params[:content].empty?
+      redirect '/tweets/new'
+    else
+      @tweet = Tweet.create(content: params[:content], user_id: current_user.id)
+      redirect "/tweets"
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      if @tweet
+        erb :'tweets/show'
+      else
+        redirect "/tweets"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      if @tweet && @tweet.user == current_user
+        erb :'tweets/edit'
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.content = params[:content]
+        @tweet.save
+
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      if @tweet && @tweet.user == current_user
+        @tweet.content = params[:content]
+        @tweet.delete
+
+      else
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
   end
 
 end
