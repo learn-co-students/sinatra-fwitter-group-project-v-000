a1986955bd97be7d1387490d3a76dccd7d29fb32
Kachel Wittig <kjwittig@gmail.com>
2018-08-01 21:36:10 -0500

    making progress

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,37 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
 
-end
+  get '/tweets' do
+    if session["user_id"] == current_user.id
+      @user = User.find(current_user.id)
+      erb :'/tweets/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    #
+    # erb :'/show'
+  end
+
+  get '/tweets/:id' do
+
+  end
+
+  get '/tweets/:id/edit' do
+
+  end
+
+  patch '/tweets/:id' do
+
+  end
+
+  delete 'tweets/:id' do
+
+  end
+
+end # TweetsController
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,47 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params["email"].empty? && !params["username"].empty? && !params["password"].empty?
+      @user = User.create(email: params["email"], username: params["username"], password: params["password"])
+      session["user_id"] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect to '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    if @user = User.find_by(username: params["username"], password: params["password"])
+      session["user_id"] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect to '/'
+    else
+      redirect to '/tweets'
+    end
+  end
 
 end
