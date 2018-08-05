784c41c718a8595926ee006b738ced46fad03dc3
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-05 20:14:45 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,20 @@
 class TweetsController < ApplicationController
 
-  get '/tweets/new' do
-      erb :create_tweet
-    end
+  get '/tweets' do
+     if logged_in?
+       @tweets = Tweet.all
+       erb :'tweets/tweets'
+     else
+       redirect to '/login'
+     end
+   end
+
+   get '/tweets/new' do
+       if logged_in?
+         erb :'tweets/create_tweet'
+       else
+         redirect to '/login'
+       end
+     end
 
 end
