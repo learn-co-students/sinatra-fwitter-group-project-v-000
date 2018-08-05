c1332f7ad7903c5677375bfaedb062829dfeb8d9
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-05 18:51:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
       erb :index
     end
 
+    helpers do
+
+      def current_user
+        @current_user ||= User.find_by_id(session[:user])
+      end
+
+      def logged_in?
+        current_user != nil
+      end
+
+    end
+
 end
