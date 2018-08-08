f732c7817a8ebf1b7ac9986a196457a5a9552963
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-08-08 00:12:58 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/' do
+    erb :'/users/home'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
 
 end
---
+++ app/helpers/helpers.rb
@@ -0,0 +1,9 @@
+class Helpers
+  def current_user(session)
+    @user = User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?(session)
+    !!(session[:user_id])
+  end
+end
