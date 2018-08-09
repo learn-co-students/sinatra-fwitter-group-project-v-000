c97ae170110b119b2a6501b43d184ceef8abcb48
Kevin Chi <kevinchi118@gmail.com>
2018-08-09 04:37:06 +0000

    tweet erbs

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,42 @@
 class TweetsController < ApplicationController
 
 
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'tweets/index'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    redirect "tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/edit'
+  end
+
+  post '/tweets/:id' do
+    @tweet= Tweet.find_by_id(params[:id])
+    @tweet.update(params[:tweet])
+    @tweet.save
+
+    erb :'tweets/show'
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    
+    erb :'tweets/delete'
+  end
 end
