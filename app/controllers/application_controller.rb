ea97f4279fef8272606ee013dd5199d6df215ba2
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-12 20:35:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,29 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :'index'
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+      if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+            user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            user.save
+      end
+
+    redirect to "/"
+  end
+
+  get '/login' do
+    erb :'/users/login'
   end
 
 end
