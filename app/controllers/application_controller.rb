18b7a7b41c6dffbc8f59a51d7831c7da737a108d
Brett Weisberg <weisberg.brett@gmail.com>
2018-08-13 02:05:39 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,22 +9,35 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-  get '/' do
+  get '/' do #loads the homepage
     erb :'index'
   end
 
-  get '/signup' do
-    erb :'/users/create_user'
+  get '/tweets/new' do #creates the create tweet page
+    erb :'/tweets/create_tweet'
   end
 
-  post '/signup' do
-  
-      if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
-            user = User.create(username: params[:username], email: params[:email], password: params[:password])
-            user.save
-      end
+  post '/tweets' do  #post tweet
+    redirect to "/tweets/#{@tweet.id}"
+  end
 
-    redirect to "/tweets"
+  get '/signup' do
+    if session[:user_id] != nil
+      redirect to "/tweets"
+    else
+    erb :'/users/create_user'
+  end
+end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+        user = User.create(username: params[:username], email: params[:email], password: params[:password])
+        session[:user_id] = user.id
+        user.save
+        redirect to "/tweets"
+      else
+        redirect to "/signup"
+    end
   end
 
   get '/login' do #loads login page
@@ -32,8 +45,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do #loads login page
-    user = User.find()
-    redirect to
+    @user = User.find( params[:username])
+    if @user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/tweet/#{@user.id}"
+      else
+        direct "/"
+      end
   end
 
+
+  
 end
