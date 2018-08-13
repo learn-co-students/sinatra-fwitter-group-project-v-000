b92ce7db6dadb7174cfad45793d37f1b75451144
Catherine Bronzo <cbronzo@mac.com>
2018-08-13 04:16:15 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,52 @@
 class UsersController < ApplicationController
+  
+ get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/user/show'
+  end
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/tweets'
+    end
+    erb :'/users/create_user'
+  end
+  
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect :'/tweets'
+    end
+  end
+  
+  get '/login' do
+    if logged_in?
+      redirect to '/tweets'
+    end
+      erb :'/users/login'
+  end
+
+    post '/login' do
+      @user = User.find_by(:username => params[:username])
+       if @user && @user.authenticate(params[:password])
+          session[:id] = @user.id
+          redirect :'/tweets'
+        else
+          redirect to '/signup'
+       end
+    end
+
+    get '/logout' do
+      if logged_in?
+          session.clear
+          redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
+    
 
 end
