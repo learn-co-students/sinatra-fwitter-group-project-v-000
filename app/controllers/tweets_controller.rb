c63cd91477d589eb8d710b2c70d43c5778a3a64e
Nancy Jo <enciie@gmail.com>
2018-08-13 20:59:22 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,8 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    @tweets = Tweet.all
     if logged_in?
-      @user = current_user
       erb :'tweets/tweets'
     else
       redirect to '/login'
@@ -12,6 +12,52 @@ class TweetsController < ApplicationController
   get '/tweets/new' do
     if logged_in?
       erb :'tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/new'
+      else
+        @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+        redirect to "/tweets/#{@tweet.id}"
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && @tweet.user_id == current_user.id
+      if params[:content] == ""
+        redirect to "/tweets/#{@tweet.id}/edit"
+      else
+        @tweet.content = params[:content]
+        @tweet.save
+        redirect to '/tweets/#{@tweet.id}'
+      end
     else
       redirect to '/login'
     end
