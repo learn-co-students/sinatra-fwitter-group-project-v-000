6b64ff805c8156f6ac904b84a6d40efd00b40293
Anu Khambete <anukhambete@gmail.com>
2018-08-14 21:34:37 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,8 +80,54 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/:slug' do
-    binding.pry
+    #binding.pry
+    @user = User.find_by_slug(params[:slug])
+    @tweets = @user.tweets
     erb :'/users/show'
+  end
+
+
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/tweets/:id' do
+    #binding.pry
+    if logged_in?
+      @user = User.find(session[:user_id])
+      @tweet = Tweet.find(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect to "/login"
+    end
+  end
+
+  post '/tweets' do
+    #binding.pry
+    @user = User.find(session[:user_id])
+    if !params[:content].blank?
+      @tweet = Tweet.create(content: params[:content])
+      @user.tweets << @tweet
+      @user.save
+      @tweet.save
+    else
+      redirect "/tweets/new"
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    #binding.pry
+    @tweet = Tweet.find(params[:id])
+     if @tweet.user_id == current_user.id
+       erb :'/tweets/edit_tweet'
+     else
+       redirect to "/tweets/#{@tweet.id}"
+     end
   end
 
   helpers do
