e991d27c17fa6b9e4854d093d587569415490d13
Anu Khambete <anukhambete@gmail.com>
2018-08-14 19:44:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,79 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "password_security"
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    #binding.pry
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :'/users/login'
+    end
+  end
+
+  get '/tweets' do
+  #  binding.pry
+  @tweets = Tweet.all
+  @user = User.find(session[:user_id])
+    erb :'/tweets/tweets'
+  end
+
+  post '/signup' do
+    #binding.pry
+		if !params[:username].blank? && !params[:password].blank? && !params[:email].blank?
+      user = User.new(username: params[:username], password: params[:password], email: params[:email])
+      user.save
+      session[:user_id] = user.id
+      #binding.pry
+      redirect to "/tweets"
+      #binding.pry
+      #redirect "/failure"
+    else
+      #session[:user_id] = user.id
+      redirect to "/signup"
+    end
+  end
+
+  post "/login" do
+    ##your code here
+    user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect "/login"
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,30 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+
+  def slug
+    a = self.username.downcase
+    #binding.pry
+    if a.gsub!(/[!@% &"]/,'-')
+      slug = a
+      #binding.pry
+    else
+      slug = a
+    end
+    slug
+  end
+
+  def self.find_by_slug(slug)
+    value = nil
+    User.all.each do |user|
+      #binding.pry
+      if user.slug == slug
+        #binding.pry
+        #val = artist
+        value = user
+      end
+    end
+    value
+  end
+
 end
