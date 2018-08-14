394f92fbf2b1933ff1ca5ba9ecbac960227febbb
Josh Cesana <joshuacesana@gmail.com>
2018-08-14 10:08:36 -0700

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @user = User.new(params)
+
+    if @user.save
+      binding.pry 
+      session[:id] = @user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/tweets' do
+    erb :tweets
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many :tweets
+  has_secure_password
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+end
