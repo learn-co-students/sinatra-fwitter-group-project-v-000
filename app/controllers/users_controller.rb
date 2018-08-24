7181d0ca7e475f1b2d3b66477947820c16effaf9
pracniscate <lorena.giddens@outlook.com>
2018-08-24 09:22:24 -0400

    mount the controllers & create a signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if !logged_in?
+            erb :'/users/create_user'
+        else
+            redirect '/tweets'
+        end
+    end
 
 end
