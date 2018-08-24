b5b3342a1441160ea7bea2c6ff2ce13e9caab4e2
pracniscate <lorena.giddens@outlook.com>
2018-08-24 09:25:42 -0400

    add comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+    # loads the signup page
+    # if the user is not logged in, show them the sign up form
+    # if the user is logged in, direct them to twitter index page
     get '/signup' do
         if !logged_in?
             erb :'/users/create_user'
