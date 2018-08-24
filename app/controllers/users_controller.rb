407f9f267d87624ab6b4caaa0cd5c4d9a7fa90e5
pracniscate <lorena.giddens@outlook.com>
2018-08-24 10:02:37 -0400

    create post  signup form route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
     # loads the signup page
-    # if the user is not logged in, show them the sign up form
-    # if the user is logged in, direct them to twitter index page
+    # if the user is not logged in, direct them the sign up form
+    # if the user is logged in, redirect them to twitter index page
     get '/signup' do
         if !logged_in?
             erb :'/users/create_user'
@@ -12,12 +12,26 @@ class UsersController < ApplicationController
     end
 
     # loads the login page
-    # if the user is not logged in, show them the login page
-    # if the user is logged in, direct them to twitter index page
+    # if the user is not logged in, direct them the login page
+    # if the user is logged in, redirect them to twitter index page
     get '/login' do
         if !logged_in?
             erb :'/users/login'
         else
+            redirect '/tweets'
+        end
+    end
+
+    # submits the signup form
+    # if there is a blank input, redirect them back to sign up
+    # if user creates their sign in info with three params, redirect to twitter index page
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect '/signup'
+        else
+            @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+            @user.save
+            session[:user_id] = @user.id
             redirect '/tweets'
         end
     end
