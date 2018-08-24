60a7e3709afa10fa1277c49394021967b7fc4152
pracniscate <lorena.giddens@outlook.com>
2018-08-24 11:16:30 -0400

    add post /tweets route

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,4 +21,29 @@ class TweetsController < ApplicationController
         end
     end
 
+    # submits the new tweet form
+    # if the user is logged in only, if not, redirect to log in page
+    # if the tweet text field is empty, redirect back to /tweets/new
+    # if there is content in text area, build the tweet
+    # if the tweet saves, redirect to that tweet page
+    # if it doesn't save, redirect to /tweets/new form page
+    post '/tweets' do
+        if logged_in?
+            if params[:content] == ""
+                redirect '/tweets/new'
+            else
+                # .build method returns a new object of the tweets that has been instantiated with content
+                @tweet = current_user.tweets.build(content: params[:content])
+                if @tweet.save
+                    # use double quotes for correct interpolation
+                    redirect "/tweets/#{@tweet.id}"
+                else
+                    redirect 'tweets/new'
+                end
+            end
+        else
+            redirect '/login'
+        end
+    end
+
 end
