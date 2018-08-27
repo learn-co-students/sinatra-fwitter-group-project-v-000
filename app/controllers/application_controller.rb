746966016d8b93177121404cadf5168b86729fec
asabogal <asabogal.git@gmail.com>
2018-08-26 23:09:26 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    if logged_in?
+    redirect :"/tweets"
+    else
+      erb :index
+    end
   end
 
   helpers do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(slug)
+    if logged_in?
+      erb :"users/show"
+    else
+      redirect :"/login"
+    end
+  end
+
   get '/signup' do
     if logged_in?
       redirect  :"/tweets"
@@ -29,15 +38,21 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:usr_id] = @user.id
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect :"/tweets"
     else
       redirect :"/signup"
     end
   end
 
-
-
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect :"/login"
+      else
+        redirect :"/"
+      end
+    end
 
 end
