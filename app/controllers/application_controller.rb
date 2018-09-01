caba398712f000fd04d3404e63bfcdfae5137c1c
ValerieHMoore <periwinkle17@outlook.com>
2018-08-31 21:24:10 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,53 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "super secret password"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    redirect "/tweets" if Helpers.is_logged_in?(session)
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    redirect "/tweets" if Helpers.is_logged_in?(session)
+    @user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
+    if params[:username].nil? || params[:username].length <= 0 || params[:email].nil? || params[:email].length <= 0 || params[:password].nil? || params[:password].length <= 0
+      redirect '/signup'
+    else
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+  end
+
+  get '/login' do
+    redirect "/tweets" if Helpers.is_logged_in?(session)    
+    erb :"/users/login"
+  end
+  
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password]) 
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'/users/show'
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,81 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        if Helpers.is_logged_in?(session)
+          @user = Helpers.current_user(session)
+          @tweets = Tweet.all
+          erb :'/tweets/tweets'
+        else
+          redirect '/login'
+        end
+    end
+
+    get '/tweets/new' do
+        if Helpers.is_logged_in?(session)
+        @user = Helpers.current_user(session)
+          erb :'/tweets/new'
+        else
+          redirect '/login'
+        end
+    end
+
+    post '/tweets' do
+        if Helpers.is_logged_in?(session) && params[:content] != ""
+            @user = Helpers.current_user(session)
+            @user.tweets.create(:content => params[:content])
+            redirect '/tweets'
+        elsif params[:content]
+            redirect '/tweets/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    #this is the show route
+    get '/tweets/:id' do
+        if Helpers.is_logged_in?(session)
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'tweets/show_tweet'
+        else
+        redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if Helpers.is_logged_in?(session)
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'tweets/edit_tweet'
+        else
+        redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find_by_id(params[:id])
+        if params[:content] != ""
+            @tweet.update(:content => params[:content])
+            @tweet.save
+            redirect "/tweets/#{@tweet.id}"
+        else
+            redirect "/tweets/#{@tweet.id}/edit"
+        end
+    end
+
+    delete '/tweets/:id/delete' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find(params[:id])
+            @user = Helpers.current_user(session)
+            if @tweet.user == @user
+                @tweet.destroy
+            else
+                redirect '/tweets'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+# Delete Tweet
+# You'll only need one controller action to delete a tweet. The form to delete a tweet should be found on the tweet show page. The delete form doesn't need to have any input fields, just a submit button. The form to delete a tweet should be submitted via a POST request to tweets/:id/delete.
 
 end
