319f805a6814ad635e41dd5361ab7e22f6b3ccbb
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-09-07 19:50:34 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+   get "/users" do
+      erb :"users/index"
+   end
 
-
+   get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
+   end
 end
