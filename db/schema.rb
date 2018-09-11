b81e35f54d71f228861bc78b2a50b552bb1786c6
treverandersen <trever.r.andersen@gmail.com>
2018-09-11 11:27:23 -0500

    setup migration to create Tweets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180911161044) do
+ActiveRecord::Schema.define(version: 20180911162214) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
