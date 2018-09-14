d80903d8a2dc65dcb8a6d0b30d6be59bb4fda4bf
vickicello <vicki.cello@gmail.com>
2018-09-14 14:40:59 -0400

    updated file structure, models, migrations, helpers, associations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter secret password"
   end
 
+  get '/' do
+    erb :index
+  end
 end
