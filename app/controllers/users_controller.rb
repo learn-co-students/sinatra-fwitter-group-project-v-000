4fc9236b5b095bf2f2efb5cee9a73fa4955814f4
Judy McGoldrick <jjeanius@verizon.net>
2018-09-16 02:45:51 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,34 +7,39 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do   #signup / POST request / Create action
-    @user = User.create(:username =>params[:username],
+    @user = User.new(:username =>params[:username],
     :email =>params[:email],
     :password =>params[:password])
 
-    if @user.save
-      redirect '/signup'
-    else
-      redirect "/tweets"
+      if  @user.save
+        redirect "/tweets"
+      else
+        redirect "/signup"
     end
   end
 
   get "/login" do   #login = Get action
-    erb: '/login'
+    erb :'/users/login'
   end
 
-  post "login" do   #login - POST action
+  post "/login" do   #login - POST action
     @user = User.find_by(:username => params[:username], :password => params[:password])
-    if user && user.authenticate(params[:password], params[:username])
+    if @user && @user.authenticate(password: params[:password], username: params[:username])
       session[:user_id] = @user.id
       redirect "/tweets"
     else
-      redirect "/users/login"
+      redirect "/login"
     end
   end
 
   get '/users/:id' do    # Get request / show action
     @user = User.find_by(params[:user_id])
     erb :'/users/show'
+  end
+
+  get '/sesion/logout' do
+    session.clear
+    redirect '/index'
   end
 
 end
