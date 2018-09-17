71c259ce37a9ff471a16c912f4a2268a75c1741e
treverandersen <trever.r.andersen@gmail.com>
2018-09-17 09:22:53 -0500

    enabled sessions, started setting up authentication controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
 
+	get '/signup' do 
+		erb :'/users/create_user'
+	end
+
+	post '/signup' do
+		user = User.new(:email => params[:email], :username => params[:username], :password => [:password])
+
+		if user.save
+			redirect "/"
+		else
+			redirect "/failure"
+		end
+	end
+
+
+
+	helpers do 
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
+	end
 
 end
