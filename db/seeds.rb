eef1ce356bc7ce41114c7996c40d6b17265416d3
Brent Bauman <brentbauman86@gmail.com>
2018-09-17 20:15:37 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,27 @@
 tweet = Tweet.new(content: "tweet")
-tweet.build_user(name: "tom")
-tweet.save 
+tweet.user = User.new(user_name: "tom")
+tweet.save
+
+fweet = Tweet.new(content: "fweet")
+fweet.build_user(user_name: "gom")
+fweet.save
 
 
-
-users_list = {
-    "Madison" => { "madison@gmail.com" => {
-      :password => 1
-    },
-    "Grant" => { "grant@gmail.com" => {
-      :password => 2
-      }
-    }
-  }
-
-users_list.each do |name, email, password_hash|
-  p = Landmark.new
-  p.name = name
-  password_hash.each do |attribute, value|
-      p[attribute] = value
-  end
-  p.save
-end
+# users_list = {
+#     "Madison" => { "madison@gmail.com" => {
+#       :password => 1
+#     },
+#     "Grant" => { "grant@gmail.com" => {
+#       :password => 2
+#       }
+#     }
+#   }
+#
+# users_list.each do |name, email, password_hash|
+#   p = Landmark.new
+#   p.name = name
+#   password_hash.each do |attribute, value|
+#       p[attribute] = value
+#   end
+#   p.save
+# end
