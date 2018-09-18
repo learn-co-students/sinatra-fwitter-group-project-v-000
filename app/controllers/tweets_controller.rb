66570b082d4d9427842c446686edf3345db892f4
freeWillee <will.i.lee@outlook.com>
2018-09-17 23:12:41 -0600

    Created migrations, updated and completed models, hom/signup/login-out

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
-
+    get '/tweets' do
+        @user = User.find_by(session[:id])
+        if @user
+            erb :"tweets/tweets"    
+        else
+            redirect to '/login'
+        end
+    end
 
 end
