f37620c9039b9939643f3b2e47266225be6a12c2
Judy McGoldrick <jjeanius@verizon.net>
2018-09-18 04:37:37 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,40 +1,63 @@
+require 'pry'
 class TweetsController < ApplicationController
 
-  get '/tweets' do     # Get Request / Read action
-    @tweets = Tweet.all
-    erb :'tweets/tweets'
+  get '/tweets' do  # tweet / GET request / read action
+    tweets = Tweet.all
+    erb :'/tweets/tweets'
   end
 
   get '/tweets/new' do    #CREATE action - GET request
-    erb :'/new'
-    end
+    erb :'tweets/new'
+  end
 
   post '/tweets' do     #CREAT action - POST request
-      @tweet = Tweet.create(:content =>params[:content], :user_id =>params[:user_id])
-      @tweet.save
-      redirect to "/tweets/#{@tweet.id}"
-    end
+    @tweet = Tweet.new(params)   #@tweet = Tweet.create(:content =>params[:content], :user_id =>params[:user_id])
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+  end
 
-  get '/tweets/:id/edit_tweet' do   # Get action / Update request
+
+  get '/tweets/:id' do   # Get action / Update request
     @tweet = Tweet.find(params[:id])
+    @tweet_id = params[:id]
     erb :'/tweets/edit_tweet'
   end
 
   patch '/tweets/:id' do   # Patch action /update request
     @tweet = Tweet.find(params[:id])
-    @tweet.content = params[:content]
-    @tweet.save
+      @tweet.content = params[:content]
+        @tweet.save
     redirect "/tweets/#{@tweet.id}"
   end
 
-
   post '/tweets/:id/delete' do   # Delete action / Delete request
-    @tweet = find_by(params[:id])
-    if @user.logged_in? && session[:user_id] = @user.id
-      @tweet = Tweet.destory
-  else
-    redirect "/login"
+    if logged_in? && current_user
+        @tweet = find_by(params[:id])
+        @tweet.clear
+    else
+      redirect "/login"
+    end
   end
 
+  get '/tweets' do     # Get request / check if user login
+    if logged_in
+      user = current_user
+      erb :'/tweets'
+    else
+      redirect to '/login'
+    end
   end
+
 end
+
+  helpers do
+
+    def logged_in
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,25 +11,24 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do   #signup / POST request / Create action
-  #binding.pry
-    @user = User.new(:username =>params[:username], :email =>params[:email], :password =>params[:password])
-    if @user.save
-      session[:user_id] = @user.id
-        redirect "/tweets"
-    else
-      redirect "/signup"
-      end
-    end
+  	@user = User.new(params)   #  @user = User.new(:username =>params[:username], :email =>params[:email], :password =>params[:password])
+			if @user.save
+					session[:user_id] = @user.id
+					redirect to '/tweets/tweets'
+			else
+					redirect to '/signup'
+			end
+		end
 
   get "/login" do   #login = Get action
     erb :'/users/login'
   end
 
   post "/login" do   #login - POST action
-    @user = User.find_by(:username => params[:username], :password => params[:password])
+    @user = User.find_by(params)   #@user = User.find_by(:username => params[:username], :password => params[:password])
     if @user && @user.authenticate(password: params[:password], username: params[:username])
       session[:user_id] = @user.id
-      redirect "/tweets"
+      redirect "/tweets/tweets"
     else
       redirect "/login"
     end
@@ -42,19 +41,20 @@ class UsersController < ApplicationController
 
   patch '/users/show' do
     @user = User.find_by(params[:slug])
-    #@user.username = update.(params[:slug])
-    #@user.username = User.find_by(username: params[:username])
+    @user.username = update.(params[:slug])
+    @user.username = User.find_by(username: params[:username])
     slug = @user.slug
      @user.save
     erb :'/users/#{user.slug}'
   end
 
-
-
-  get '/logout' do
-    session.clear
-    redirect '/login'
-  end
+  get '/logout' do   # Get request / logout action
+    if logged_in? && @user = current_user
+        session.clear
+      else
+        redirect to '/login'
+      end
+    end
 
 helpers do
 
