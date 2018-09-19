c36c2df09e1e2fd0556193de17c0dfce1bc4c901
treverandersen <trever.r.andersen@gmail.com>
2018-09-19 10:31:27 -0500

    lets a user create and view their tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,5 +16,30 @@ class TweetsController < ApplicationController
 			redirect "/login"
 		end
 	end
-	
+
+	post '/tweets' do 
+		if logged_in? 
+			if params[:content] == ""
+				redirect "/tweets/new"
+			else
+				@tweet = current_user.tweets.build(content: params[:content])
+				if @tweet.save
+					redirect "/tweets/#{@tweet.id}"
+				else
+					redirect "/tweets/new"
+				end
+			end
+		else
+			redirect "/login"
+		end
+	end
+
+	get '/tweets/:id' do 
+		if logged_in?
+			@tweet = Tweet.find_by_id(params[:id])
+			erb :'/tweets/show_tweet'
+		else
+			redirect "/login"
+		end
+	end
 end
