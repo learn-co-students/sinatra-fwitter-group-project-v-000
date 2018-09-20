594e60aaf20c1fa2f63a4d4c99e0ee13624502f3
Judy McGoldrick <jjeanius@verizon.net>
2018-09-20 03:02:15 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,68 +6,58 @@ class UsersController < ApplicationController
     erb :'/index'
   end
 
+  get '/index' do
+    if "login"
+      redirect "login"
+      if "signup"
+        redirect "signup"
+      end
+    end
+  end
+
   get '/signup' do  # signup / GET request / Create action
-    if logged_in? && @user = current_user
-      redirect "/tweets"
-    if !logged _in? && !@user = current_user
-      redirect "/signup"
-    end
-    end
+      erb :'/users/signup'
   end
 
   post '/signup' do   #signup / POST request / Create action
-  	@user = User.new(params)   #  @user = User.new(:username =>params[:username], :email =>params[:email], :password =>params[:password])
-      if @user.save
-        session[:user_id] = @user.id
-				redirect "/tweets"
-			else
-				redirect "/signup"
-			end
-		end
+    @user = User.new(params)   #  @user = User.new(:username =>params[:username], :email =>params[:email], :password =>params[:password])
+    if @user.save && session[:user_id] = @user.id
+  		redirect "/tweets"
+   	else
+  		redirect "/signup"
+  	end
+	end
 
   get "/login" do   #login = Get action
-    if logged_in? && @user = current_user
-      redirect "/tweets"
-      if !logged_in? && !@user = current_user
-        redirect "/users/login"
-      end
+    erb :'/users/login'
     end
-  end
 
   post "/login" do   #login - POST action
     @user = User.find_by(params)   #@user = User.find_by(:username => params[:username], :password => params[:password])
-      if @user && @user.authenticate(password: params[:password], username: params[:username])
-        session[:user_id] = @user.id
+      if logged_in? #@user && @user.authenticate(password: params[:password], username: params[:username])
         redirect "/tweets"
-      else
-        redirect "/users/login"
-      end
-  end
-
-  get '/users/:id' do    # Get request / show action
-    @user = User.find_by(params)
-    erb :'/users/show'
-  end
+     else
+      redirect "/login"
+    end
+end
 
 #  get '/users/:slug' do    # Get request / show action
 #    @user = User.find_by_slug(params[:slug])
 #    erb :'/users/show'
 #  end
 
-  get '/users/show' do
-    @user = User.find_by(params)   #@user = User.find_by(:username => params[:username], :password => params[:password])
-    erb :'/users/show'
-  end
+#  get '/users/:id' do  # Get request / show action
+#    @user = User.find_by(params)   #@user = User.find_by(:username => params[:username], :password => params[:password])
+#    erb :'/users/show'
+#  end
 
-  patch '/users/show' do
-    @user = User.find_by(params[:slug])
-    @user.username = User.find_by(username: params[:username])
-    @user.username = update.(params[:slug])
-    slug = @user.slug
-     @user.save
-    erb :'/users/#{user.slug}'
-  end
-
+#  patch '/users/show' do
+#    @user = User.find_by(params[:slug])
+#    @user.username = User.find_by(username: params[:username])
+#    @user.username = update.(params[:slug])
+#    slug = @user.slug
+#    erb :'/users/#{user.slug}'
+#
   get '/logout' do   # Get request / logout action
     if logged_in? && @user = current_user
         session.clear
