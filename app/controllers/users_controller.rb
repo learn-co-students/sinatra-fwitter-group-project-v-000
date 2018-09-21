bf8f77a8f63dff7c674a7983cfee1d0cae760f7e
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-21 14:01:41 -0400

    Still not passing /tweets route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    if !logged_in?
-      erb :'users/create_user'
-    else
+    if logged_in?
       redirect to '/tweets'
+    else
+      erb :'users/create_user'
     end
   end
 
   post '/signup' do
-    if params[:username].empty? || params[:password].empty? || params[:email].empty?
+    if params[:username] == "" || params[:password] == "" || params[:email] == ""
       redirect to '/signup'
     else
       user = User.create(username: params[:username], email: params[:email], password: params[:password])
@@ -18,12 +18,22 @@ class UsersController < ApplicationController
     end
 	end
 
-  get 'login' do
-    if !logged_in?
-      erb :'users/login'
-    else
+  get '/login' do
+    if logged_in?
       redirect to '/tweets'
+    else
+      erb :'users/login'
     end
   end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+	end
 
 end
