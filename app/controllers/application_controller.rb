bd1eba865e5e181c761186904b63c98da8cd5773
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-21 11:37:56 -0400

    Attempt password authenticate and build signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,19 +17,19 @@ class ApplicationController < Sinatra::Base
     erb :'users/create_user'
   end
 
+  post '/signup' do
+    user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    if params[:username].blank? || params[:password].blank? || params[:email].blank?
+      redirect '/signup'
+    elsif user.save
+      session[:user_id] = user.id
+      redirect '/tweets'
+    end
+	end
+
   get 'login' do
     erb :'users/login'
   end
-
-  post "/login" do
-		user = User.find_by(:username => params[:username])
-		if user && user.authenticate(params[:password])
-	    session[:user_id] = user.id
-	    erb 'tweets/tweets'
-	  else
-	    erb :'users/login'
-	  end
-	end
 
   helpers do
 		def logged_in?
