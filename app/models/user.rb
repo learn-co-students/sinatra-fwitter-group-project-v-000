7810dbd8238ad84eb46b59d75b172f4a324063c3
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-21 10:57:07 -0400

    Message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   has_many :tweets
+  has_secure_password
 
   def slug
     name.downcase.gsub(" ","-")
   end
 
-  def authenticate
+  def self.find_by_slug(slug)
+    User.all.select do |user|
+      user.slug == slug
+    end[0]
   end
 end
