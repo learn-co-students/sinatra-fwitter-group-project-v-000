257061315bac3813b6945da6d00a3c0756700106
Brent Bauman <brentbauman86@gmail.com>
2018-09-21 19:40:06 +0000

    Done.

--- db/migrate/20180912011911_tweets.rb
+++ db/migrate/20180912011911_tweets.rb
@@ -1,8 +1,10 @@
 class Tweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.string :content
-      t.integer :user_id 
+      t.text :content
+      t.integer :user_id
+
+      t.timestamps null: false
     end
   end
 end
