202b534a25696ac79d3df5f6a07493f8abded356
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-21 17:20:24 -0400

    Try to udpdate branches. 9 tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,9 +28,10 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+    @user = User.find_by(username: params[:username])
+    binding.pry
+    if @user && @user.authenticate(@user.password)
+      session[:user_id] = @user.id
       redirect to "/tweets"
     else
       redirect to '/signup'
