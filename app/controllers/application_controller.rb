15c7c1656804c92eed1aaf8a3ca32425745699ce
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-09-22 16:48:29 +0000

    add home page and models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "top_secret"
+
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :home
+  end
+  
 end
