e407bde2722e493c5959b0b85e658017dd6673f1
emanuelbierman <emanuel.bierman@gmail.com>
2018-09-24 11:37:15 -0400

    Create a new tweet tests passing

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,7 +8,30 @@ class TweetsController < ApplicationController
     end
   end
 
-  get '/tweets/:id' do
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
 
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by(params[:id])
+    if logged_in? && current_user.tweets.include?(@tweet)
+      erb :"tweets/show"
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets/new' do
+    @user = User.find_by(session[:user_id])
+    if !params["tweet"]["content"].empty?
+      @tweet = Tweet.create(content: params["tweet"]["content"])
+      @tweet.user = @user
+      @tweet.save
+    end
+    redirect to "/tweets/#{@tweet.id}"
   end
 end
