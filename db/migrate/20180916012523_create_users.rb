f5361ca37e98f576c5dfa135e9939676fa85d3eb
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-09-25 21:49:10 +0000

    Done.

--- db/migrate/20180916012523_create_users.rb
+++ db/migrate/20180916012523_create_users.rb
@@ -3,11 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
-<<<<<<< HEAD
       t.string :password_digest
-=======
-      t.string :password
->>>>>>> 5703a4a9a2581946db52475e1e9bdcf9c9f7859f
     end
   end
 end
