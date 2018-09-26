9a049c894ffefb78a114a965c5adb3e593e8c088
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-09-26 00:06:35 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,21 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'/tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  post '/tweets' do
+    erb :'/tweets'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweets.find_by(params[:id])
+    erb :'tweets/show_tweet'
+  end
 
 end
