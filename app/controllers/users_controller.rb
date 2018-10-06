a41262682d855c39ce4b87c12e68198a590d1aee
kev112 <werewaswas@live.com>
2018-10-05 21:10:55 -0700

    signup works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
     get '/signup' do 
-        erb :'users/signup'
+        erb :'users/new'
     end
 
     post '/signup' do 
-        params
+        user = User.create(params)
+        session[:user_id] = user.id
         redirect to :tweets
     end
 
