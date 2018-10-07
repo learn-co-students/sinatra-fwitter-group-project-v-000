1a474fd0075201d61f387b986902caffd73340ef
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-10-07 02:11:05 +0000

    fixed database issue and solved tweet index test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,20 @@ class UsersController < ApplicationController
       end
     end
 
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/signup'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-      redirect '/tweets'
-    end
-  end
+   post '/signup' do
+     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+       redirect '/signup'
+     else
+       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+       session[:user_id] = @user.id
+       redirect '/tweets'
+     end
+   end
+
 
   get '/login' do
     if !logged_in?
-      erb :login
+      erb :index
     else
       redirect '/tweets'
     end
@@ -30,9 +31,13 @@ class UsersController < ApplicationController
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to '/tweets'
+      redirect '/tweets'
     else
       redirect '/signup'
     end
   end
+
+
+
+
 end
