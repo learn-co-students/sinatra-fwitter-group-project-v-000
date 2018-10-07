46311d374495d08ed371a9fca0f5282d60a87fdd
RonSala <ron.sala@gmail.com>
2018-10-06 20:50:04 -0400

    Added yield to layout, format changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,30 +4,30 @@ class UsersController < ApplicationController
       erb :signup
   end
 
-  post '/signup' do
-    if params[:username] == "" || params[:email] == "" || params[:password] == ""
-      redirect '/signup'
-    else
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-      redirect '/tweets'
-    end
-  end
+  # post '/signup' do
+  #   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+  #     redirect '/signup'
+  #   else
+  #     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+  #     session[:user_id] = @user.id
+  #     redirect '/tweets'
+  #   end
+  # end
 
   get '/login' do
     erb :index
   end
 
-  post '/login' do
-    @user = User.find_by(:username => params[:username])
-    # if @user != nil && @user.password == params[:password]
-    if logged_in?
-      session[:user_id] = @user.id
-      redirect to '/tweets'
-    else
-      redirect '/signup'
-    end
-  end
+  # post '/login' do
+  #   @user = User.find_by(:username => params[:username])
+  #   # if @user != nil && @user.password == params[:password]
+  #   if logged_in?
+  #     session[:user_id] = @user.id
+  #     redirect to '/tweets'
+  #   else
+  #     redirect '/signup'
+  #   end
+  # end
 
 
 end
