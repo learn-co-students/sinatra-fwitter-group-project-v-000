b3dfa555de71f1abb243f6c521a9c730c2cd5401
Madeline Stark <madeline.stark1@gmail.com>
2018-10-08 19:45:19 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,70 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    if Helpers.logged_in?(session)
+      erb :'tweets/new'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  post '/tweets' do
+    if !params["tweet"]["content"].empty?
+      @tweet = Tweet.create
+      @tweet.content = params["tweet"]["content"]
+      @tweet.save
+      @user = Helpers.current_user(session)
+      @user.tweets << @tweet
+      @user.save
+      @tweets = @user.tweets
+      erb :'tweets/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find_by_id(params[:id])
+      @user = User.find_by_id(@tweet.user_id)
+      erb :'tweets/show_tweet'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    #need to check if logged in? or current user enough?
+    if Helpers.current_user(session).tweets.include?(@tweet)
+      erb :'/tweets/edit_tweet'
+    else
+      redirect '/users/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.content = params["tweet"]["content"]
+    @tweet.save
+    @user = User.find_by_id(@tweet.user_id)
+    erb :'tweets/show_tweet'
+  end
+
+  get '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if Helpers.current_user(session).tweets.include?(@tweet)
+      @tweet.destroy
+      redirect '/tweets'
+    else
+      redirect '/users/login'
+    end
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,52 @@
 class UsersController < ApplicationController
 
+    get '/users/signup' do
+      erb :'/users/create_user'
+    end
+
+    post '/users/signup' do
+      if params[:email] != "" && params[:email].include?("@") && params[:email].include?(".") && params[:username] != "" && params[:password] != ""
+        #use method to validate email?
+          user = User.new(:email => params[:email], :username => params[:username], :password => params[:password])
+  		    user.save
+          session[:user_id] = user.id
+          redirect "/tweets"
+        else
+         	 redirect "/users/signup" #create failure page?...show doesn't work if not logged in
+        end
+    end
+
+    get "/users/login" do
+      if Helpers.logged_in?(session)
+        redirect '/tweets'
+      else
+        erb :'users/login'
+      end
+    end
+
+    post "/users/login" do
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect "/tweets"
+      else
+          redirect "/users/signup"
+      end
+    end
+
+    get "/users/show" do
+      @user = Helpers.current_user(session)
+      erb :'/users/show'
+    end
+
+    get "/users/logout" do
+      if Helpers.logged_in?(session)
+        session.clear
+        redirect "/tweets"
+      else
+        redirect "/users/login"
+      end
+    end
+
 
 end
