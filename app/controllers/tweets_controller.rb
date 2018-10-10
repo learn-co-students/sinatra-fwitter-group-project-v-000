929b3379456909629c6c6f72f0dc4c7a5e7fbf94
kev112 <werewaswas@live.com>
2018-10-10 13:02:56 -0700

    tweets crud controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,28 @@
 class TweetsController < ApplicationController
 
     get '/tweets' do 
-        erb :'tweets/index'
+        if logged_in?
+            @user = current_user
+            @tweets = current_user.tweets
+            erb :'tweets/index'
+        else
+            redirect to '/'
+        end
     end
 
     get '/tweets/new' do
-
+        erb :'tweets/new'
     end
 
     post '/tweets' do 
-
+        tweet = Tweet.create(content: params[:content])
+        tweet.user = current_user
+        tweet.save
+        redirect to '/tweets'
     end
 
     get '/tweets/:id' do
-
+        erb :'tweets/show'
     end
 
     patch '/tweets/:id' do 
