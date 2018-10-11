b00d4fcb44a14f4318038969a67122b234d5890b
Josely Castro <joselycas@gmail.com>
2018-10-11 13:09:41 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,32 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+      if is_logged_in?
+        #@user = current_user
+        @tweets = Tweet.all
+        erb :'tweets/tweets'
+      else
+        redirect to '/login'
+      end
+  end
 
+  get '/tweets/new' do
+    if is_logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+
+  post '/tweets' do
+    if is_logged_in?
+    @tweet = Tweet.create(:content => params[:content])
+    @tweet.user = User.find_by(id: params[:id])
+    @tweet.save
+
+    redirect to "/tweets"
+  else
+    redirect to "/login"
+  end
+ end
 end
