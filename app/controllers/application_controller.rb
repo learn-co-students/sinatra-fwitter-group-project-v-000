af64b1f49a6e24cb0599627fc468e8dcbc80a1ec
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-16 04:49:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,35 +12,14 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'/users/create_user'
-  end
-  get '/login' do
-    erb :'/users/login'
-  end
-
-  post '/signup' do
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    if params[:username] != "" && params[:password] != "" && params[:email] !=""
-      @user.save
-      redirect "/tweets"
-    else
-      redirect "/signup"
+  helpers do
+    def current_user
+      User.find(session[:user_id])
     end
+
+  	def logged_in?
+  		!!session[:user_id]
+  	end
   end
 
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect "/tweets"
-    else
-      redirect "/signup"
-    end
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,85 @@
+require 'rack-flash'
+
 class TweetsController < ApplicationController
+  use Rack::Flash
+
+  get '/tweets' do
+    @tweets = Tweet.all.sort_by { |t| t.title }
+    if logged_in?
+      erb :'/tweets/tweets'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
 
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to 'login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in? && params[:content] != ""
+      @tweet = current_user.tweets.create(title: params[:title], content: params[:content])
+      @tweet.save
+      redirect to "/tweets/#{@tweet.id}"
+    elsif logged_in? && params[:content] == ""
+      flash[:message] = "Must Enter Tweet Content."
+      redirect to '/tweets/new'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && params[:content] != ""
+      @tweet.update(title: params[:title], content: params[:content])
+      redirect to "/tweets/#{@tweet.id}"
+    elsif logged_in? && params[:content] == ""
+      flash[:message] = "Must Enter Tweet Content."
+      redirect to "/tweets/#{@tweet.id}/edit"
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if logged_in? && current_user.id == @tweet.user.id
+      @tweet.delete
+      flash[:message] = "Tweet Deleted Successfully?"
+      redirect to '/tweets'
+    else
+      flash[:message] = "You must be logged in first."
+      redirect to '/login'
+    end
+  end
 end
