ba2165b15242950f21062070208d96fe84381164
Ayana Bando <ayanaflax@yahoo.com>
2018-10-18 19:21:03 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  get '/' do
+    @users = User.all
+    erb :index
+  end
+  
+  # get '/users/index' do
+  #   @users = User.create(username: params[:username], email: params[:email], password: params[:password])
+  #   erb :'/users/index'
+  # end
+  
+  get "users/signup" do
+    erb :signup
+  end
 
-
+  post 'users/new' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/failure'
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect '/login'
+    end
+  end
 end
--- app/model/tweet.rb
+++ app/model/tweet.rb
@@ -1,4 +1,4 @@
-class User <ActiveRecord::Base
+class Tweet <ActiveRecord::Base
   has_secure_password
   belongs_to :user
  
