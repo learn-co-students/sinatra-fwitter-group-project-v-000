ec0a2e7d3325ac03db2d7a7b6caf3ecc35329430
AlwinaO <aoyewole2586@gmail.com>
2018-10-17 21:42:30 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "my_application_secret"
+    enable :sessions
   end
+
+  get '/' do
+    erb :index
+  end
+
+  
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @user = User.new
+    @user.username = params[:username]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.save
+
+    redirect to "/login"
+  end
 
 end
