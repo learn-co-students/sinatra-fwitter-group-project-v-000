e7255a91c922d6576f236bf3469d6993fc8c8c20
Ayana Bando <ayanaflax@yahoo.com>
2018-10-19 00:43:07 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,33 +19,11 @@ class ApplicationController < Sinatra::Base
       redirect '/signup'
     else
       User.create(username: params[:username], email: params[:email], password: params[:password])
-      redirect "/tweets"
+      redirect "/tweets/index"
     end
   end
   
-  get "/login" do
-    erb :login
-  end
-
-  post "/login" do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-     # session[:user_id] = @user.id
-      redirect to "/tweets"
-    else
-      redirect to "/signup"
-    end
-  end
   
-  get "/failure" do
-    erb :failure
-  end
-
-  get "/logout" do
-    session.clear
-    redirect "/"
-  end
-
   helpers do
     def logged_in?
       !!session[:user_id]
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,5 +2,29 @@ class TweetsController < ApplicationController
   get '/tweets' do
     erb :index
   end
+  
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+     # session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/signup"
+    end
+  end
+  
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
 
 end
