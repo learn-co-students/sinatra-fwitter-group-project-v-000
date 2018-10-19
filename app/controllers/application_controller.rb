d78b9e495fab98cec5f2ac76239dc792734958fa
Ayana Bando <ayanaflax@yahoo.com>
2018-10-19 01:18:43 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,40 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    if params[:username] == ""  ||  params[:email] == "" || params[:password] == ""
+   if params[:username] == ""  ||  params[:email] == "" || params[:password] == ""
       redirect '/signup'
     else
       User.create(username: params[:username], email: params[:email], password: params[:password])
-      redirect "/tweets/index"
+      redirect to "/tweets/index"
     end
   end
   
+  get "/login" do
+    erb :login
+  end
+
+  post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+     session[:user_id] = @user.id
+      redirect to "/tweets/index"
+    else
+      redirect to "/signup"
+    end
+  end
+  
+  get '/tweets/index' do
+     erb :'/tweets/index'
+  end
+  
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
+  end
   
   helpers do
     def logged_in?
