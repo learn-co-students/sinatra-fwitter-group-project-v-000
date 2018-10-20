e27eb1fc38052161a67de2f27db1ec0d9755aaef
kev112 <werewaswas@live.com>
2018-10-20 00:49:11 -0700

    refactor tweet new

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,11 @@ class TweetsController < ApplicationController
     end
 
     get '/tweets/new' do
-        erb :'tweets/new'
+        if logged_in?
+            erb :'tweets/new'
+        else
+            redirect to '/login'
+        end  
     end
 
     post '/tweets' do 
