3f7441799089aac575be7af1ae519b455cea90e2
danschoch <daniel.alexander.schoch@gmail.com>
2018-10-22 13:34:52 -0700

    build create_user view and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+    
+    get '/signup' do
+        if !logged_in?
+            erb :'users/create_user'
+        else
+            redirect to '/tweets'
+        end
+    end
 
+    post '/signup' do
+        user_emails = User.all.collect {|user| user.email}
+        usernames = User.all.collect {|user| user.username}
+
+        if user_emails.include?(params[:email]) || usernames.include?(params[:username]) || params[:username] == "" || params[:email] == "" || params[:password] == ""
+            redirect to '/signup'
+        else
+            @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+            session[:user_id] = @user.id
+            redirect to '/tweets'
+        end
+    end
 
 end
+
