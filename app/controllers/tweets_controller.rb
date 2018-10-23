ac242488b68a03a105d9851f33268c5cd5f8d734
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-23 18:03:43 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,26 @@ class TweetsController < ApplicationController
     end 
   end 
   
+  get '/tweets/new' do 
+    if logged_in?
+      erb :'tweets/new'
+      
+    else 
+      redirect to '/login'
+    end 
+  end 
   
+  post '/tweets' do 
+    if params[:content] = ""
+      redirect to '/tweets/new'
+      
+    else 
+      @tweet = Tweet.new(:content => params[:content])
+      @tweet.save 
+      session[:user_id] = @user.id 
+      redirect to '/tweets/show_tweet'
+    end 
+  end 
   
   
 
