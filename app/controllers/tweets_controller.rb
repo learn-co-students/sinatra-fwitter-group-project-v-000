7a4f64455827b36ad12de4aa8e2e624cf9b46b10
Brenden Thornton <thorntonbrenden@gmail.com>
2018-10-24 21:50:43 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,15 +19,31 @@ class TweetsController < ApplicationController
     end 
   end 
   
-  post '/tweets' do 
-    if params[:content] = ""
-      redirect to '/tweets/new'
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/new'
+        
+      else 
+        @tweet = @current_user.tweets.build(:content => params[:content])
+        if @tweet.save 
+          redirect to "/tweets/#{@tweet.id}"
+        else 
+          redirect to '/tweet/new'
+        end 
+      end 
+    else 
+      redirect to '/login'
+    end 
+  end 
+  
+  get '/tweets/:id' do 
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
       
     else 
-      @tweet = Tweet.new(:content => params[:content])
-      @tweet.save 
-      session[:user_id] = @user.id 
-      redirect to '/tweets/show_tweet'
+      redirect to '/login'
     end 
   end 
   
