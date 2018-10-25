89390935a35b82b90ab3a1b5ac2e60a318314683
Adam Barton <adam.barton333@gmail.com>
2018-10-25 02:54:58 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "93160167"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
 
+  get '/' do
+    erb :index
+  end
+
+  get '/users/signup' do
+    erb :'/users/signup'
+  end
 
 end
