be25a3e0f07338d583db18d304b2cf418b4033f7
amitnyc83 <amit_nyc@live.com>
2018-10-25 14:12:31 -0400

    add user & tweet models & create tables for both

---
+++ db/migrate/20181025180135_create_user_table.rb
@@ -0,0 +1,10 @@
+class CreateUserTable < ActiveRecord::Migration
+  def change
+    create_table :user do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.integer :tweet_id
+    end 
+  end
+end
