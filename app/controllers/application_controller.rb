b65620f4bcc657be28e26f36c2da3da8b4c243a7
Adam Barton <adam.barton333@gmail.com>
2018-10-26 04:36:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
       !!session[:user_id]
     end
 
-    ddef current_user
+    def current_user
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
