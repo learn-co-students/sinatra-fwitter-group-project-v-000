1c9e7654db73bef5c76d01b4525826b7d5b30f32
Jenny Kam <jkam17@gmail.com>
2018-10-30 18:27:33 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class UsersController < ApplicationController
 
   get '/' do
@@ -5,17 +6,53 @@ class UsersController < ApplicationController
   end
 
   get '/signup' do
-    erb :'users/create_user'
+    if session[:user_id] != nil
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
   end
 
-  post '/' do
-    if params[:user][:username].empty? or params[:user][:email].empty? or params[:user][:password].empty?
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect '/signup'
     else
-      user = User.create(params[:user])
-      session[:id] = user.id
+      user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = user.id
       redirect '/tweets'
     end
+  end
+
+  get '/login' do
+    if session[:user_id] != nil
+      redirect '/tweets'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if session[:user_id] == nil
+      redirect '/'
+    else
+      session.clear
+      redirect '/login'
+    end
+  end
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
 end
