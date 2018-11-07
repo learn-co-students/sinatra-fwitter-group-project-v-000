b8e4d27641a027a99677ce6f00478fb639d19eb8
Brian Birdwell <bbirdwell@gmail.com>
2018-11-07 23:02:00 +0000

    complete /signup controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,44 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+
+    erb :index
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :signup
+    elsif logged_in?
+      redirect '/tweets'
+    end
+  end
+
+  post '/signup' do
+    # binding.pry
+
+      # binding.pry
+      if params["username"] != "" && params["email"] != "" && params["password"] != ""
+        user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+        session[:user_id] = user.id
+        # binding.pry
+        redirect '/tweets'
+      else redirect '/signup'
+      end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'tweets/tweets_index'
+  end
 
+  get '/tweets/:id' do
+    erb :show_tweets
+  end
 end
