c0bab248372ed2142726b7b67c76d054a4525d96
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-07 16:18:44 +0000

    automatically backed up by learn

--- app/models/helpers.rb
+++ app/models/helpers.rb
@@ -2,9 +2,6 @@ class Helpers
 
 def self.current_user(session)
    @user = User.find_by_id(session[:user_id])
-   if @user.is_a?(User)
-        @user
-   end
  end
 
  def self.is_logged_in?(session)
