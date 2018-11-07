b69e482bdaa7bcbf76c9d817d9d6510becc963c4
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-07 18:06:29 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
-  validates_presence_of :username, :password, :email
 
   def slug
     self.username.downcase.gsub(" ", "-")
