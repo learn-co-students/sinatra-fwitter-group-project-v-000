c0c02dccb89211f66c57b41910269a5c7ac16755
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-07 17:47:28 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -68,7 +68,7 @@ class TweetsController < ApplicationController
     if @tweet.user != Helpers.current_user(session)
       redirect to '/tweets'
     end
-    if  params["content"] != ""
+    if !params["content"] == ""
         @tweet.update(content: params["content"])
         @tweet.save
       else
