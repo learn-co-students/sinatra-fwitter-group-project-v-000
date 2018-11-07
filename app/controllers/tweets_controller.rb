e36dd9ae9ad56531e42972c92d8469933ad2857f
Richard Sanchez <richard.charles.s@gmail.com>
2018-11-07 16:40:39 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,7 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
+    @user = Helpers.current_user(session)
     if Helpers.is_logged_in?(session)
       @tweets = Tweet.all
       erb :'tweets/tweets'
