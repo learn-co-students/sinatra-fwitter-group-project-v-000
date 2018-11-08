4d21ce1f1f64cff4df10b334bc8a97a1a98cdc63
Brian Birdwell <bbirdwell@gmail.com>
2018-11-08 04:47:30 +0000

    correct users/show view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,11 +45,9 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     user = User.find_by(username: params["username"])
-      # binding.pry
-    if user && user.authenticate(params["password"])
-      # binding.pry
-      session[:user_id] = user.id
 
+    if user && user.authenticate(params["password"])
+      session[:user_id] = user.id
       redirect '/tweets'
     else
       redirect '/login'
