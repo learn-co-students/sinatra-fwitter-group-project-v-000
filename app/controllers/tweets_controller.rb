9fb303df12ad3ac6d91b1ce72cd37e849c62daa6
Brian Birdwell <bbirdwell@gmail.com>
2018-11-08 05:34:57 +0000

    complete edit tweet methods

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,12 +2,69 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if logged_in?
+      @tweets = Tweet.all
       erb :'tweets/tweets_index'
     else redirect '/login'
     end
   end
 
-  # get '/tweets/:id' do
-  #   erb :show_tweets
-  # end
+  get '/tweets/new' do
+    if logged_in?
+      erb :'tweets/tweets_new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if params["content"] != ""
+      @tweet = Tweet.create(content: params["content"])
+      @tweet.user_id = current_user.id
+      @tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params["id"])
+      erb :'tweets/tweets_show'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params["id"])
+
+      erb :'tweets/tweets_edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by(id: params["id"])
+
+    if params["content"] != ""
+      @tweet.content = params["content"]
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect "/tweets/#{@tweet.id}/edit"
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params["id"])
+      @tweet.delete
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
 end
