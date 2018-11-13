6dab576a5613f03b37e49d6fbc3e5096c226e072
SMartinSci <sm674@cornell.edu>
2018-11-13 12:37:12 -0800

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -36,10 +36,11 @@ class TweetsController < ApplicationController
     end
 
     get '/tweets/:id' do
+        if logged_in?
         @tweet = Tweet.find_by_id(params[:id])
         erb :"tweets/show"
-    else
-        redirect to '/login'
+        else
+            redirect to '/login'
         end
     end
 
