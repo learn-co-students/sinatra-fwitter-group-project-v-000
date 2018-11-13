77d2df9137eb40342ec0bf68453305ab6021b33a
SMartinSci <sm674@cornell.edu>
2018-11-13 12:58:40 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
 
     enable :sessions
-    set :session_secret, 'password_security'
+    set :session_secret, "secret"
   end
 
   get '/' do
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
     get '/tweets' do
         if logged_in?
-            @tweets = current_user.tweets
+            @tweets = Tweet.all
             erb :'tweets/tweets'
         else
             redirect '/login'
@@ -20,31 +20,31 @@ class TweetsController < ApplicationController
 
     post '/tweets' do
         if logged_in?
-            if Tweet.new(params[:content]).empty?
+            if params[:content].empty?
                 redirect '/tweets/new'
             else
-                @tweet = current_user.tweets.build(params[:content])
+                @tweet = current_user.tweets.build(content: params[:content])
                 if @tweet.save
-                redirect "/tweets/#{@tweet.slug}"
+                redirect "/tweets/#{@tweet.id}"
         else
             redirect '/tweets/new'
             end
         end
             else
-                redirect to '/login'
+                redirect '/login'
         end
     end
 
     get '/tweets/:id' do
         if logged_in?
         @tweet = Tweet.find_by_id(params[:id])
-        erb :"tweets/show"
+        erb :"tweets/show_tweet"
         else
-            redirect to '/login'
+            redirect '/login'
         end
     end
 
-    get '/tweets/:slug/edit' do
+    get '/tweets/:id/edit' do
         if logged_in?
         @tweet = Tweet.find_by_id(params[:id])
         if @tweet && @tweet.user == current_user
@@ -63,9 +63,9 @@ class TweetsController < ApplicationController
         if @tweet && @tweet.user == current_user
             @tweet.update(params[:content])
             if !params[:content].empty?
-            redirect "/tweets/#{@tweet.id}/edit"
-            end
             redirect "/tweets/#{@tweet.id}"
+            end
+            redirect "/tweets/#{@tweet.id}/edit"
         else
             redirect '/tweets'
         end
@@ -80,10 +80,9 @@ class TweetsController < ApplicationController
         if @tweet && @tweet.user == current_user
             @tweet.delete
             redirect to '/tweets'
-        end
-        redirect to '/tweets'
         else
         redirect to '/login'
         end
     end
+end
 end
