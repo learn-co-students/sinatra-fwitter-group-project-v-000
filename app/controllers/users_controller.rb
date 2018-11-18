3eabfebc7fde55158021a47f850282d9e1e9b560
github username <github email address>
2018-11-17 17:09:45 -0700

    Initial Sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :"users/signup"
+  end
+
+  post "/signup" do
+		user = User.create(params)
+
+    if user.save && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+		else
+			redirect "/failure"
+		end
+	end
+
+  get "/login" do
+    erb :"users/login"
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect "/failure"
+    end
+  end
 
 end
