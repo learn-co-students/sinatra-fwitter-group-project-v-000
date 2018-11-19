3fc31abf72c1ae64f109f96e73ef96ec0856c8ca
Nadiafa <nadiafa581@gmail.com>
2018-11-19 17:27:26 +0000

    Move from ApplicationController the get signup and post users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/users' do 
+    if params[:name] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(params)
+      session[:user_id] = @user.id 
+      redirect '/tweets/index'
+    else 
+      redirect '/homepage'
+    end
+  end
+
+  
 
 end
