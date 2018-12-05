0c4fde0a59f3ff25bca7cca9ac60d0b31b4c4600
Marc Mills <mdmills1990@gmail.com>
2018-12-05 03:33:13 +0000

    builds some signup/login forms and controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
 
 get '/signup' do
@@ -6,6 +8,27 @@ end
 
 get '/login' do
   erb :"users/login"
+end
+
+post '/signup' do
+  @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  if @user.save && @user.username != ""
+    redirect '/login'
+  else
+#    flash[:message] = "Must include a username to signup." <-- not working, figure out flash messages later
+    redirect '/'
+  end
+end
+
+post '/login' do
+  @user= User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect '/tweets'
+  else
+#    flash[:message] = "Incorrect username or passord. Please try again." <-- not working, figure out flash messages later
+    redirect '/'
+  end
 end
 
 end
