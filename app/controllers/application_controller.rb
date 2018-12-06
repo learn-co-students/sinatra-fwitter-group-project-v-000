023779873198edff8fd04fdb6f597e24fbf13d6f
Naudria <rima.jean@gmail.com>
2018-12-06 12:58:40 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,133 +6,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
+    set :session_secret, 'something_secret'
   end
 
+#the homepage should be accessible withouth the tweets or user and is therefore put in the app controller
   get '/' do
     erb :index
   end
 
-  get '/signup' do
-    if session[:user_id] == nil
-      erb :'/users/create_user'
-    else
-      redirect to '/tweets'
-    end
-  end
+#The same goes for the helper methods which will be needed to make sure users are only creating tweets for their own accounts.
 
-  post '/signup' do
-    if params[:username] != "" && params[:email] != "" && params[:password] != ""
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      session[:user_id] = @user.id
-      redirect to '/tweets'
-    else
-      redirect to '/signup'
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
-  end
 
-  get '/tweets' do
-    #binding.pry
-    if session[:user_id] != nil
-      @user = User.find(session[:user_id])
-      erb :'/tweets/tweets'
-    else
-      redirect to '/login'
+    def logged_in?
+      !!current_user
     end
-  end
 
-  get '/login' do
-    if session[:user_id] == nil
-      erb :'/users/login'
-    else
-      redirect to '/tweets'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect to "/tweets"
-    else
-      redirect to '/login'
-    end
-  end
-
-  get '/users/:id' do
-    @user = User.find(params[:id])
-    erb :'/users/show'
-  end
-
-  get '/logout' do
-    if session[:user_id] != nil
-      session.clear
-      redirect to '/login'
-    else
-      redirect to '/tweets'
-    end
-  end
-
-  get '/tweets/new' do
-    if session[:user_id] != nil
-      erb :'/tweets/create_tweet'
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/tweets/new' do
-    if params[:content] != ""
-      @user = User.find(session[:user_id])
-      @tweet = Tweet.new(content: params[:content])
-      @user.tweets << @tweet
-      @user.save
-    else
-      redirect to '/tweets/new'
-    end
-  end
-
-  get '/tweets/:id' do
-    if session[:user_id] != nil
-      @tweet = Tweet.find(params[:id])
-      erb :'/tweets/show_tweet'
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/tweets/:id/delete' do
-    @tweet = Tweet.find(params[:id])
-    if @tweet.user.id == session[:user_id ]
-      @tweet.destroy
-    else
-      redirect to "/tweets/#{params[:id]}"
-    end
-  end
-
-  get '/tweets/:id/edit' do
-    if session[:user_id] != nil
-      @user = User.find(session[:user_id])
-      @tweet = Tweet.find(params[:id])
-      if @tweet.user = @user
-        erb :'/tweets/edit_tweet'
-      else
-        redirect to '/tweets'
-      end
-    else
-      redirect to '/login'
-    end
-  end
-
-  post '/tweets/:id/edit' do
-    @tweet = Tweet.find(params[:id])
-    if params[:content] != ""
-      @tweet.content = params[:content]
-      @tweet.save
-      redirect to "/tweets/#{@tweet.id}"
-    else
-      redirect to "/tweets/#{@tweet.id}/edit"
-    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ActiveRecord::Base
-	has_many :tweets
-	has_secure_password
+  has_many :tweets
+  validates :username, :email, :password, presence: true 
+  has_secure_password
 
-def slug
-    self.username.split(' ').join('-').downcase
+  def self.create_from_form(params)
+    User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+  end
+
+
+
+  def slug
+    username.downcase.gsub(" ","-")
   end
 
   def self.find_by_slug(slug)
-    User.all.select {|user| user.slug == slug}[0]
+    User.all.find{|user| user.slug == slug}
   end
+
 end
