0791aafe915b2912ec5f4108162f51dce6ddca4d
Naudria <rima.jean@gmail.com>
2018-12-06 09:50:56 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,32 @@ class ApplicationController < Sinatra::Base
  	 end
  	end
 
+ post '/signup' do 
+ 	if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
+ end
+
+ get '/login' do
+ 	if session[:user_id] == nil
+      erb :'/users/login'
+    else
+      redirect to '/tweets'
+    end
+ end
+
+ post '/login' do
+ 	@user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to '/login'
+    end
+ end
+
 end
