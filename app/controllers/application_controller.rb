c5d334ea979e035d8d52ef43e7441bd7a11d1e7e
Naudria <rima.jean@gmail.com>
2018-12-06 09:30:36 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,19 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+  	erb :index
+  end
+
+  get '/signup' do
+  	if session[:user_id] == nil
+  		erb :'/users/create_user'
+  	else
+  		redirect to '/tweets'
   end
 
 end
