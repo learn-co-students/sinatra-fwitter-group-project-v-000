8ddaac8073b898cb1e566c4429aab0100ac97064
Barbara Choy <barbchoy@gmail.com>
2018-12-13 20:54:24 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,10 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
 
+  get '/tweets' do
+    erb :'/tweets/index'
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
 
 
+    get '/signup' do
+      if !logged_in?
+        erb :'/users/create_user'
+      else
+        redirect to '/tweets'
+      end
+    end
+
+    post '/signup' do
+      if params[:username]=="" || params[:password]=="" || params[:email]==""
+        redirect to "/signup"
+      else
+        @user = User.create(:name => params[:username], :password => params[:password], :email => params[:email])
+        @user.save
+      end
+      binding.pry
+      session[:user_id] = @user.id
+      redirect to "/tweets/index"
+    end
+
+    get '/login' do
+      erb :'/users/login'
+    end
+
 end
