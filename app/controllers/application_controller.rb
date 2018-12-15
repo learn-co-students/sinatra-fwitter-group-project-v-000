32e8745722e43bec117e5112ccfbe2e0f85825af
Barbara Choy <barbchoy@gmail.com>
2018-12-15 03:01:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,16 @@ class ApplicationController < Sinatra::Base
     erb :'/users/login'
   end
 
+  post '/login' do
+    @user = User.find_by(:username => params[:username], :password => params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/login"
+    end
+  end
+
   get '/' do
     erb :index
   end
