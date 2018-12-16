4fa97494d0304d48fe0c8e7ced3af67daffd17cf
Edsel Caprice <caprice@mail.usf.edu>
2018-12-16 20:55:20 +0000

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,31 +8,43 @@ class UsersController < ApplicationController
     end
   end
 
-
   get '/login' do
-    erb :'/users/login'
+    if !logged_in?
+     erb :'/users/login'
+    else
+     redirect to '/tweets'
+   end
+  end
+
+  get '/logout' do
+    if logged_in?
+      reset_session
+      redirect to '/login'
+    else
+      redirect to '/login'
+    end
+
   end
 
   post '/signup' do
     if params[:password] == "" || params[:email] == "" || params[:username] == ""
       redirect to '/signup'
     else
-      @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      @user.save
-      session[:user_id] = @user.id
+      user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      user.save
+      session[:user_id] = user.id
       redirect to "/tweets"
     end
   end
 
   post '/login' do
-      @user = User.find_by(username: params[:username])
-      if @user.password == params[:password]
-        session[:user_id] = @user.id
+      user = User.find_by(:username => params[:username])
+       if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect to "/tweets"
       else
-        redirect to "/login"
+        redirect to "/signup"
       end
-
-  end
+    end
 
 end
