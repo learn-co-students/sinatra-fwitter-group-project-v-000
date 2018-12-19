2715c791237f26cdda7ee2ac24dd1444418dc8eb
Leslie Stevens <flylady@mail.utexas.edu>
2018-12-19 21:58:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,32 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user.save
+    #binding.pry
+    if params[:password] != "" && params[:username] != "" && params[:email] != ""
+      redirect '/tweets/tweets'
+    else
+      redirect '/signup'
+
+    end
+end
+
+
   helpers do
     def logged_in?
       !!session[:user_id]
     end
+
     def current_user
       User.find(session[:user_id])
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates_presence_of :username, :email, :password
   has_many :tweets
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    all.find{|item| item.slug == slug}
+  end
 end
