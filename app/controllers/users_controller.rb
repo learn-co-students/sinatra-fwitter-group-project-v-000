f022afa5f0c4646a11416e2db3129737a2ec91ec
Jonathan Luk <jluk87@gmail.com>
2018-12-22 03:03:38 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,37 +12,19 @@ class UsersController < ApplicationController
     if params.values.any? &:empty?
       redirect "/signup"
     else
-<<<<<<< HEAD
-      @user = User.find_by(username: params[:username], email: params[:email])
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect "/tweets"
-        else
-          @user = User.create(params)
-          session[:user_id] = @user.id
-          redirect "/tweets"
-        end
-=======
       @user = User.find_by(username: params[:username], email: params[:email], password_digest: params[:password]) || @user = User.create(params)
       session[:user_id] = @user.id
       redirect "/tweets"
->>>>>>> 0e9b6ef9bc5cfb864bbf5ddea00771d426dd4615
     end
 
   end
 
   get '/login' do
-<<<<<<< HEAD
-    if session[:user_id].empty?
-      erb :'/users/login'
-    else
-=======
     if Helpers.logged_in?(session)
       redirect "/tweets"
     else
       erb :'/users/login'
     end
->>>>>>> 0e9b6ef9bc5cfb864bbf5ddea00771d426dd4615
   end
 
   post '/login' do
