aba2d44ad491579787ceab6c3f2704b546412ec7
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-12-22 03:20:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,59 @@
 class TweetsController < ApplicationController
 
 
+  get '/tweets' do
+    if User.find_by_id(session[:user_id]) == nil
+      redirect '/login'
+    else
+      @user = User.find_by_id(session[:user_id])
+      erb :'/tweets/index'
+    end
+  end
+
+  get '/tweets/new' do
+    if User.find_by_id(session[:user_id]) == nil
+      redirect '/login'
+    end
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+
+    if params[:new_tweet].empty?
+      redirect :'/tweets/new'
+    end
+
+    user = User.find_by_id(session[:user_id])
+    @tweet = Tweet.new(content: params[:new_tweet])
+    @tweet.user_id = session[:user_id]
+    @tweet.save
+    erb '/tweets/show'
+  end
+
+  get '/tweets/:id' do
+    if User.find_by_id(session[:user_id]) == nil
+      redirect '/login'
+    end
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show'
+  end
+
+  delete '/tweets/:id/delete' do
+
+    if User.find_by_id(session[:user_id]) == nil
+      redirect '/login'
+    end
+
+    @user = User.find_by_id(session[:user_id])
+
+
+    if @user.tweets.include?(Tweet.find_by_id(params[:id]))
+
+      Tweet.find_by_id(params[:id]).delete
+      redirect '/tweets'
+    end
+    redirect '/tweets'
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,60 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
 
+    if User.find_by_id(session[:user_id]) != nil
+      redirect "/tweets"
+    else
+      erb :'/users/signup'
+    end
+
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  
+  end
+
+  get '/login' do
+    if User.find_by_id(session[:user_id]) != nil
+      redirect "/tweets/index"
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/signup' do
+
+    if !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
+      user = User.create(:username => params[:username], :password => params[:password], :email => params[:email])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/logout' do
+    if User.find_by_id(session[:user_id]) == nil
+      redirect '/login'
+    else
+      session.clear
+      if session.empty?
+        redirect '/login'
+      else
+        redirect '/tweets'
+      end
+    end
+  end
 end
