0e9b6ef9bc5cfb864bbf5ddea00771d426dd4615
aljimenez5 <arvida.jimenez@gmail.com>
2018-12-21 22:00:18 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,14 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params.each {|k,v| v.empty?}
+    if params.values.any? &:empty?
       redirect "/signup"
     else
-      @user = User.find_by(username: params[:username], email: params[:email])
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect "/tweets"
-        else
-          @user = User.create(params)
-        end
-        session[:user_id] = @user.id
-        redirect "/tweets"
+      @user = User.find_by(username: params[:username], email: params[:email], password_digest: params[:password]) || @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect "/tweets"
     end
+
   end
 
   get '/login' do
@@ -40,13 +35,19 @@ class UsersController < ApplicationController
     else
       redirect "/login"
     end
-  end 
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/"
+  end
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     @tweets = @user.tweets
     erb :"/users/show"
   end
+
 
 
 
