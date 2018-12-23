0467ed30a3788e8fca26e68e8ca23b0a36233263
Nathan  Visak <nathan.visak@gmail.com>
2018-12-23 21:21:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,27 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get "/" do
+    "Welcome to Fwitter"
+  end
+
+  get "/signup" do
+    erb :'/users/create_user'
+  end
+
+  post "/signup" do
+    if params[:username] == "" || params[:username] == nil
+      redirect to "/signup"
+    elsif params[:email] == "" || params[:email] == nil
+      redirect to "/signup"
+    elsif params[:password] == "" || params[:password] == nil
+      redirect to "/signup"
+    else 
+      @user = User.create(username: params["username"], password: params["password"], email: params["email"])
+      @user.save
+      redirect to "/tweets"
+    end
+    
+  end 
+
 end
