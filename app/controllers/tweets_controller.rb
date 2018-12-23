3d50af19d286d572f18ad2742d95d78fbc39613a
Jonathan Luk <jluk87@gmail.com>
2018-12-23 01:03:10 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,24 +39,36 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id/edit' do
-    if Helpers.logged_in?(session) && params[:id] == session[:user_id]
-      @tweet = Tweet.find_by(id: params[:id])
-      erb :'/tweets/edit_tweet'
+    if Helpers.logged_in?(session)
+      if params[:id].to_i == session[:user_id]
+        @tweet = Tweet.find_by(id: params[:id])
+        erb :'/tweets/edit_tweet'
+      else
+        redirect to "/tweets"
+      end
     else
-      redirect to "/tweets"
+      redirect to "/login"
     end
   end
 
   patch '/tweets/:id' do
     @tweet = Tweet.find_by(id: params[:id])
-    @tweet.update(content: params[:content])
-    redirect to '/tweets'
+    if !params[:content].empty?
+      @tweet.update(content: params[:content])
+      redirect to '/tweets'
+    else
+      redirect to "/tweets/#{@tweet.id}/edit"
+    end
   end
 
-  delete '/tweets/delete/:id' do
-    tweet = Tweet.find_by(id: params[:id])
-    tweet.destroy
-    redirect to '/tweets'
+  delete '/tweets/:id/delete' do
+    if params[:id].to_i == session[:user_id]
+      tweet = Tweet.find_by(id: params[:id])
+      tweet.destroy
+      redirect to '/tweets'
+    else
+      redirect to '/tweets'
+    end
   end
 
 
