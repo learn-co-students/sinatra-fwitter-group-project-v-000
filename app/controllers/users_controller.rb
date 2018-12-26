f082f86f17f8ed012b3aa7a5420468ca8e42ad92
Nathan  Visak <nathan.visak@gmail.com>
2018-12-26 22:25:27 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
 
   get "/signup" do
-    binding.pry
-      if is_logged_in?
-        redirect to "/tweets"
-      else 
-        erb :'/users/create_user'
-      end 
-  end
+    if is_logged_in?
+      redirect to "/tweets"
+    else
+      erb :'/users/create_user'
+    end
+  end 
 
   post "/signup" do
     if params[:username] == "" || params[:username] == nil
@@ -16,12 +15,16 @@ class UsersController < ApplicationController
       redirect to "/signup"
     elsif params[:password] == "" || params[:password] == nil
       redirect to "/signup"
-    else 
+    else
       @user = User.create(username: params["username"], password: params["password"], email: params["email"])
       @user.save
       session[:user_id] = @user.id
       redirect to "/tweets"
     end
-  end 
+  end
   
+  get "/login" do 
+    erb :'/users/login'
+  end 
+
 end
