9e8446920a24b073610314a412039fa825c0a50b
Nathan  Visak <nathan.visak@gmail.com>
2018-12-27 16:50:29 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if is_logged_in?
       @user = User.find_by_id(session[:user_id])
-      erb :'/tweets/index'
+      erb :'/tweets/tweets'
     else
       redirect to "/login"
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
-end 
+  has_many :tweets
+
+  def slug
+    self.username.downcase.strip.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug_version)
+  self.all.each do |user|
+    if user.slug == slug_version
+      return user
+      end
+    end
+  end
+
+end
