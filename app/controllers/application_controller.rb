e0189dceeda944c6eb303b1efe747a4ac440939f
Nathan  Visak <nathan.visak@gmail.com>
2018-12-28 18:05:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions 
+    set :session_secret, "blah-blah-blah-bitty"
   end
 
   get "/" do
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,38 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    if is_logged_in?
+    if current_user && is_logged_in?
       @user = User.find_by_id(session[:user_id])
+      @tweets = Tweet.all
       erb :'/tweets/tweets'
     else
       redirect to "/login"
     end
   end
 
+  get '/tweets/new' do
+    if is_logged_in?
+      erb :'/tweets/new'
+    else
+      redirect to '/login'
+    end
+  end
+  
+  get "/tweets/:id" do
+    if is_logged_in? 
+      @tweet = Tweet.find_by_id(params["id"])
+      erb :'/tweets/show_tweet'
+    else 
+      redirect to "/login"
+    end 
+  end 
+ 
+  post '/tweets' do
+    if !params["content"].empty? 
+      @tweet = Tweet.create(content: params["content"], user_id: session[:user_id])
+    else 
+      redirect to "/tweets/new"
+    end
+  end 
+  
 end
