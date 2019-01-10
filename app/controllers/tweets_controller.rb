ddd34ba5d3745624c635d366e8bad3848c7cdb7b
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-10 01:12:08 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,4 +43,13 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/edit_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
 end
