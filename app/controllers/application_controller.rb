8f884e57f099af3ba9512bbbe313903351cb1e6b
brandong1 <brandon@brandonleeproductions.com>
2019-01-11 17:23:16 -0700

    added index and signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,23 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/index'
+  end
+
+  get '/signup' do
+    
+    #if already signed in, redirect to user's tweets
+    #redirect....
+    erb :'/users/signup'
+  end
+
+  post /'signup' do
+
+  end
+
+  post '/signup' do
+
+  end
 end
+
