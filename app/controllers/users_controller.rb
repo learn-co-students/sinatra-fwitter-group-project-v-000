649a0d3709f0d360911338259332b48cac01d70a
Erin Green <erin.y.green@gmail.com>
2019-01-17 03:22:21 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,8 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-     
+    session[:user_id] = @user.id
+    
     if logged_in?
 			erb :'/users/show'
 		else
