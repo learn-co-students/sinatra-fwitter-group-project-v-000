ca405aaf382ac67b71913a73eacc0d7d3ec9b53d
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-17 21:01:01 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,12 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  get '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    
+  end
 
 end
