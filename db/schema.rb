7a36b453e5396138c007cb6c686a1850c644dac2
brandong1 <brandon@brandonleeproductions.com>
2019-01-17 19:57:57 -0700

    added migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190118021957) do
+ActiveRecord::Schema.define(version: 20190118025530) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "content"
@@ -21,7 +21,9 @@ ActiveRecord::Schema.define(version: 20190118021957) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
+    t.text   "email"
+    t.string "password_digest"
   end
 
 end
