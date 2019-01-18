e7997d49bcbe9cb8ad4adfc63c9654d66cc42677
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-18 16:50:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,18 +19,65 @@ class ApplicationController < Sinatra::Base
 
   post '/signup' do
     # can't get this to work: user = User.new(params[:user])
+    # password is showing
     user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-    if user.save && user.authenticate(params[:password])
-      session[:user_id] = user
+    if user.save
+      session[:user_id] = user.id
       redirect "/tweets"
+      binding.pry
     else
       redirect "/signup"
     end
+
+
   end
+
+  get '/login' do
+    erb :'login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets' do
+    "hello world"
+  end
+
+
+    get '/tweets/new' do
+      erb :'/tweets/new'
+    end
+
+    post '/tweets' do
+      @tweet = Tweet.create(params[:tweet])
+      # Need to assign user_id to tweet
+      redirect "/tweets/#{@tweet.id}"
+    end
+
+    get '/tweets/:id' do
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show'
+    end
+
 
 end
 
 helpers do
-  
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,12 +1,18 @@
 class TweetsController < ApplicationController
-
-  get '/tweets/new' do
-    erb :'/tweets/new'
-  end
-
-  get '/tweets' do
-    @tweet = Tweet.create(params[:tweet])
-    
-  end
+  #
+  # get '/tweets/new' do
+  #   erb :'/tweets/new'
+  # end
+  #
+  # post '/tweets' do
+  #   @tweet = Tweet.create(params[:tweet])
+  #   # Need to assign user_id to tweet
+  #   redirect "/tweets/#{@tweet.id}"
+  # end
+  #
+  # get '/tweets/:id' do
+  #   @tweet = Tweet.find_by_id(params[:id])
+  #   erb :'/tweets/show'
+  # end
 
 end
