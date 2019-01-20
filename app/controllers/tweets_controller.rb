a24b619f224e32a6b1b75f1b8ab56fffca7a7e35
Allyson Hotchkin <ahotchk1@yahoo.com>
2019-01-20 04:01:44 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -23,12 +23,15 @@ class TweetsController < ApplicationController
 
   post '/tweets' do
     # Is there a way to use helper methods?
-    if !!session[:user_id]
+
+    if !!session[:user_id] && params[:tweet][:content] != ""
       @tweet = Tweet.create(params[:tweet])
       # Need to assign user_id to tweet
       @tweet.user = User.find(session[:user_id])
       @tweet.save
       redirect "/tweets/#{@tweet.id}"
+    elsif !!session[:user_id] && params[:tweet][:content] == ""
+      redirect "/tweets/new"
     else
       redirect "/login"
     end
@@ -38,6 +41,25 @@ class TweetsController < ApplicationController
     # binding.pry
     @tweet = Tweet.find_by_id(params[:id])
     erb :'/tweets/show'
+  end
+
+  get '/tweets/:id/edit' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/edit'
+  end
+
+  patch '/tweets' do
+    @tweet = Tweet.find_by_id(params[:id])
+    # Need to assign user_id to tweet
+    @tweet.update(params[:tweet])
+    # @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id/delete' do
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect "/tweets"
   end
   # get '/tweets/new' do
   #   erb :'/tweets/new'
