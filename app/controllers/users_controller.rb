617c7ec42323e5a8f29e1247622cbfb15eef8f48
brandong1 <brandon@brandonleeproductions.com>
2019-01-21 18:06:41 -0700

    fixed authentication syntax.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
     post '/login' do
       @user = User.find_by(username: params[:username])
-      if @user @user.authenticate(params[:password])
+      if @user && @user.authenticate(params[:password])
         session[:user_id] = @user.id #user logged in
         redirect '/tweets'
       else
