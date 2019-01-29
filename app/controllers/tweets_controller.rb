7db8534b3ddb487e8d3482b5440754c1f85691a7
Emily S <emily.sugano@gmail.com>
2019-01-29 23:41:19 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
 
   get '/tweets' do
     if session[:user_id] == nil
-      redirect '/signup'
+      redirect '/login'
     else
       @user = User.find(session[:user_id])
       erb :'tweets/tweets'
