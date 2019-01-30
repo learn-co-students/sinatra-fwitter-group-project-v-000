d8931b4271bddcd78e8ddc5985433c21a1c5eae5
Emily S <emily.sugano@gmail.com>
2019-01-30 04:00:08 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,18 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if session[:user_id] == nil
+      redirect '/login'
+    else
+      @user = User.find(session[:user_id])
+      erb :'tweets/new'
+    end
+  end
+
+  post '/tweets/new' do
+    @user = User.find(session[:user_id])
+    Tweet.create(content: params[:content], user_id: session[:user_id]).save
+  end
+
 end
