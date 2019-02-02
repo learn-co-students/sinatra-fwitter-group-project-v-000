d61826d6dc2b9805f76f8adbd7a20a953eb12e33
Emily S <emily.sugano@gmail.com>
2019-02-02 05:24:13 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,16 @@ class UsersController < ApplicationController
     end
   end
 
-  post '/signup' do
-    if !params.has_value?("")
-      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      if login(@user.id)
-        redirect '/tweets'
-      else
-        redirect "/signup"
-      end
-    end
+  post "/login" do
+  @user = User.find_by(username: params[:username])
+  if @user && @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect to '/tweets'
+  else
+    erb :'users/login'
   end
+end
+
 
   get '/login' do
     if logged_in?
