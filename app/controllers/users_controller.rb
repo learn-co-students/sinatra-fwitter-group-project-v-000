0d8710afb060a8cd115245e428796ab565ed202c
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-03 14:55:31 -0500

    Stub out User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    @user = User.create(params)
+    # Log user in
+    # Add user_id to sessions hash
+    redirect ""
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find(params[:id])
+    # Log user in
+    # Add user_id to sessions hash
+    redirect ""
+  end
+
+  get '/logout' do
+    @user = User.find(params[:id])
+    # Clear session hash
+    redirect "/login"
+  end
 
 end
