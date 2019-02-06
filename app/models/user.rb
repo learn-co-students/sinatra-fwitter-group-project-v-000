669f228ac3a087e1cfc42489d8b8094a2a141483
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-06 12:11:08 -0500

    Create user slug method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
   # validates_presence_of :username, :email, :password
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
 end
