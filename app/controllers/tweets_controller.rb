ddabd4a23059aa2f8c738699f229811cd130f0e0
Meg Gutshall <meghan.gutshall@gmail.com>
2019-02-08 14:40:06 -0500

    Finish building Tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,39 +4,65 @@ class TweetsController < ApplicationController
     if !logged_in?
       redirect "/login"
     else
-      @current_user = current_user
+      @user = current_user
+      @tweets = Tweet.all
       erb :'tweets/tweets'
     end
   end
 
   get '/tweets/new' do
-    erb :'tweets/new'
+    if !logged_in?
+      redirect "/login"
+    else
+      erb :'tweets/new'
+    end
   end
 
   post '/tweets' do
-    @tweet = Tweet.create(params)
-    redirect "/tweets/#{@tweet.id}"
+    if params[:content].empty?
+      redirect "/tweets/new"
+    else
+      @tweet = Tweet.create(content: params[:content])
+      @tweet.user_id = current_user.id
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+    end
   end
 
   get '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
-    erb :'tweets/show_tweet'
+    if !logged_in?
+      redirect "/login"
+    else
+      @user = current_user
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'tweets/show_tweet'
+    end
   end
 
   get '/tweets/:id/edit' do
-    @tweet = Tweet.find(params[:id])
-    erb :'tweets/edit_tweet'
+    if !logged_in?
+      redirect "/login"
+    else
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'tweets/edit_tweet'
+    end
   end
 
   patch '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
-    @tweet.update(params)
-    redirect "/tweets/#{@tweet.id}"
+    @tweet = Tweet.find_by(id: params[:id])
+    if params[:content].empty?
+      redirect "/tweets/#{@tweet.id}/edit"
+    else
+      @tweet.update(content: params[:content])
+      redirect "/tweets/#{@tweet.id}"
+    end
   end
 
   delete '/tweets/:id/delete' do
-    @tweet = Tweet.find(params[:id])
-    @tweet.destroy
+    @tweet = Tweet.find_by(id: params[:id])
+    if @tweet.user_id == current_user.id
+      @tweet.delete
+    end
     redirect "/tweets"
   end
 
