a42f8ad3c77bd5d325971777e8fbb54a79f2b9bf
Jason Mei <jason.mei403@gmail.com>
2019-02-15 01:38:38 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,19 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
 
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
 
+  post '/tweets' do
+    @tweet = Tweet.create(params[:tweet])
+    redirect "/tweets/#{@tweet.id}"
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'/tweets/show_tweet'
+  end
 end
