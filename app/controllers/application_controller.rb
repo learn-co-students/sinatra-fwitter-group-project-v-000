fda12ef86d2990c49a6bf3545608cdb437af9657
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-02-25 17:09:08 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :sessions_secret, "my_application_secret"
+
+    #register Sinatra::Twitter::Bootstrap::Assets
+    enable :sessions
   end
 
+    get '/' do
+      erb :index
+    end
+
+    def logged_in?
+         session[:user_id]
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if logged_in?
+            redirect to '/tweets'
+        else
+        erb :'users/create_user'
+        end
+    end
+
+    post '/signup' do
+          if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+              @user = User.create(params)
+              @user.save
+              session[:user_id] = @user.id
+              redirect to '/tweets'
+          else
+              redirect to '/signup'
+          end
+      end
+
 
 end
