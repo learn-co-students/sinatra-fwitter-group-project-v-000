47497e30e849969dd7bf881d07a4154e857b817e
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-02 01:14:23 +0000

    update login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,26 +26,31 @@ class UsersController < ApplicationController
                 erb :'users/login'
             end
         end
-
-      post '/login' do
-          @user = User.find_by(:username => params[:username])
-          if @user
-              session[:user_id] = @user.id
-              redirect '/tweets'
-          else
-              redirect 'users/login'
+        #receive the login form, find the user, and log the user in(create a session)
+    post '/login' do
+          #find the user
+        @user = User.find_by(username: params[:username])
+          #authenticate the user-verfy the user is who they say they are. the right credentials
+        if @user.authenticate(params[:password])
+        # log the user in - create the user session
+        session[:user_id] = @user.slug #actually logging the user in.
+          #redirect to the users show page
+        puts session
+        redirect "users/#{@user.slug}"
+          #else
+            #  redirect 'users/login'
           end
-          erb :login
+          #erb :login
       end
-
 
       get '/logout' do
           session.destroy
           redirect '/login'
       end
 
-      get 'user/:slug' do
-          @user = User.find_by_slug(params[:slug])
-          erb :'/users/show'
+      get '/users/:slug' do
+          "This will be the user show route"
+          #@user = User.find_by_slug(params[:slug])
+          #erb :'/users/show'
       end
 end
