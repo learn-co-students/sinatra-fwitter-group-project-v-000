df23232a7d112518e3afcdb6eca200b5ea907549
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-03-03 02:24:05 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
 
-    get '/signup' do #renders the signup form
-            erb :'users/create_user'
-    end
-
     get '/login' do
         if logged_in?
             redirect to '/tweets'
@@ -12,13 +8,21 @@ class UsersController < ApplicationController
         end
     end
 
-    post '/users' do
+    get '/signup' do #renders the signup form
+        if logged_in?
+            redirect to '/tweets'
+        else
+            erb :'/users/create_user'
+        end
+    end
+
+    post '/signup' do
         if params[:username] != "" && params[:email] != "" && params[:password] != ""
             @user = User.create(params)
-            #session[:user_id] = @user.id
-             redirect to "/users/#{@user.id}"
+            session[:user_id] = @user.id
+             redirect to '/tweets'
          else
-             #redirect to '/signup'
+             redirect to '/signup'
           end
     end
 
@@ -27,21 +31,30 @@ class UsersController < ApplicationController
         @user = User.find_by(username: params[:username])
         #authenticate the user-verfy the user is who they say they are. the right credentials
         if @user.authenticate(params[:password])
+
             # log the user in - create the user session
-            #session[:user_id] = @user.slug #actually logging the user in.
+            session[:user_id] = @user.id #actually logging the user in.
             #redirect to the users show page
             #puts session
-            #redirect "users/#{@user.slug}"
-            #else
-            #  redirect 'users/login'
+            redirect to '/tweets'
+            else
+              erb  :'users/login'
         end
         #erb :login
     end
 
     get '/users/:slug' do
-        @user = User.find_by_slug(id: params[:slug])
-        binding.pry
+        @user = User.find_by_slug(params[:slug])
         erb :'/users/show'
     end
 
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect to 'login'
+        else
+            redirect to '/'
+        end
+
+    end
 end
