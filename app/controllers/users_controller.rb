046497b7baeb940177cff6202cf8c769a7ad24db
Ashley Wong <ashwong04@gmail.com>
2019-03-15 18:37:41 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
-  get '/' do
-    erb :'/users/index'
+  get '/signup' do
+    erb :"/users/create_user"
+  end
+
+  get '/signin' do
+    erb :"/users/login"
+  end
+
+  post '/users' do
+    user = User.new(params)
+      if User.find_by(:email => params[:email])
+        binding.pry
+        redirect to '/signin'
+      else
+        user.save
+        redirect to '/tweets'
+      end
   end
 
 end
