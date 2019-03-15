ecb25b6e09cee91220ce80ae87347b5371b24e02
Ashley Wong <ashwong04@gmail.com>
2019-03-15 20:05:48 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
-require './config/environment'
+require '../config/environment'
 
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
-    set :views, 'app/views'
     enable :sessions
     set :session_secret, "secret"
+    set :views, 'app/views'
   end
 
   get '/' do
     erb :index
   end
-
-  # helpers do
-	# 	# def logged_in?
-	# 	# 	# !!session[:user_id]
-	# 	# end
+  
+  # helper do
+	# 	def logged_in?
+	# 		!!session[:user_id]
+	# 	end
   #
-	# 	# def current_user
-	# 	# 	# User.find(session[:user_id])
-	# 	# end
+	# 	def current_user
+	# 		User.find(session[:user_id])
+	# 	end
 	# end 
 
 end
