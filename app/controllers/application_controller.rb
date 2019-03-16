33668fee08f4538bfeaa23ed0237e6d9139ba008
Ashley Wong <ashwong04@gmail.com>
2019-03-16 18:18:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,54 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-
   get '/' do
     erb :index
   end
 
+  get '/signup' do
+    if !logged_in?
+      erb :"/users/create_user"
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username].empty? || params[:email].empty? || params[:password].empty?
+      redirect to '/signup'
+    else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :"/users/login"
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username], :password => params[:password])
+    session[:id] = @user.id
+    redirect to '/tweets'
+  end
+
+
+
+
+
+
+
+  get '/logout' do
+    if !session[:session_id].empty?
+      session.clear
+      redirect to '/login'
+    end
+  end
 
   helpers do
      def logged_in?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,6 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-    if !logged_in?
-      erb :"/users/create_user"
-    else
-      redirect to '/tweets'
-    end
-  end
-
-  post '/signup' do
-    if params[:username].empty? || params[:email].empty? || params[:password].empty?
-      redirect to '/signup'
-    else
-      user = User.create(params)
-      session[:user_id] = user.id
-      redirect to "/tweets"
-    end
-  end
-
-  get '/login' do
-    if !logged_in?
-      erb :"/users/login"
-    else
-      redirect '/tweets'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(:username => params[:username], :password => params[:password])
-    session[:id] = @user.id
-    redirect to '/tweets'
-  end
+  
 
 
 
