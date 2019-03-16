c350c958df91c47ecccf244bf7d9970ef5f7cdd8
Ashley Wong <ashwong04@gmail.com>
2019-03-16 13:51:52 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,18 +18,23 @@ class UsersController < ApplicationController
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
       redirect to '/signup'
     else
-      binding.pry
       user = User.create(params)
       session[:user_id] = user.id
       redirect to "/tweets"
     end
   end
 
-  post '/users' do
+  post '/login' do
     @user = User.find_by(:username => params[:username], :password => params[:password])
     session[:id] = @user.id
     redirect to '/tweets'
   end
 
+
+
+  get '/logout' do
+    session[:id].clear
+    redirect to '/login'
+  end
 
 end
