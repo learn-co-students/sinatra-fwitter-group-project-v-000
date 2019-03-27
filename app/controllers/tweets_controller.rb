50a483af392d82f8e17941a6a651ee568c40067f
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-03-27 15:42:30 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,9 @@
-class TweetsController < ApplicationController
+class TweetsController < Sinatra::Base
+    set :session_secret, "my_application_secret"
+    set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/tweets' do
+        erb :'tweets/tweets'
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
-class UsersController < ApplicationController
+class UsersController < Sinatra::Base
+    set :session_secret, "my_application_secret"
+    set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/signup' do
+        erb :'users/signup'
+    end
+
+    post '/signup' do
+        @user = User.create(params['user'])
+        redirect to ("/tweets")
+    end
 
 end
