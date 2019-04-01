3ea5fa31abb22fc651b8a24a66d5e1c7e3a21388
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-04-01 11:32:35 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,9 @@ class UsersController < ApplicationController
     get '/logout' do
       if logged_in?
         session.destroy
-        redirect to '/login'
+        redirect to ('/login')
       else
-        redirect to '/'
+        redirect to ('/')
       end
     end
 
