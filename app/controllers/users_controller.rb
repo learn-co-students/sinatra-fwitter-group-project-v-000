4bca93dd4a3772e651f1968c10fd11d54611ad1b
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-01 21:37:49 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,9 @@ class UsersController < ApplicationController
       end
     end
 
-
-
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      @tweets = @user.tweets
+      erb :"/users/show"
+    end
 end
