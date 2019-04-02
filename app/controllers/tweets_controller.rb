1c5347d12e1445f9720dadfa7e4ec3d382f8f39e
Benito Lorenzo Santos <benitolsantos@gmail.com>
2019-04-02 02:16:05 +0000

    shows all a single users tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,39 +5,42 @@ class TweetsController < ApplicationController #ApplicationController inheritanc
 
     get '/tweets' do
         if logged_in?
-        @tweets = Tweet.all
-        erb :'tweets/tweets'
+         @tweets = Tweet.all
+         erb :'tweets/tweets'
         else
-            redirect to "/login"
+         redirect to "/login"
         end
     end
 
     get '/tweets/new' do
         if logged_in?
-        erb :'tweets/new'
+         erb :'tweets/new'
         else
-            redirect to "/login"
+          redirect to "/login"
         end
     end
 
     get '/tweets/:id' do
         if logged_in?
-            @tweet = Tweet.find_by_id(params[:id])
+          @tweet = Tweet.find_by_id(params[:id])
         erb :'tweets/show_tweet'
         else
-            redirect to "/login"
+          redirect to "/login"
         end
     end
 
     post '/tweets' do
-          if logged_in? && params[:content] == ""
-              redirect to '/tweets/new'
-          elsif logged_in?
-          @tweet = params[:content]
-          redirect to '/tweets/tweets'
-          else
-          redirect to "/login"
-          end
+      binding.pry
+      if logged_in? && params[:content] == ""
+        redirect to '/tweets/new'
+      elsif logged_in?
+        @tweet = Tweet.new(content: params[:content]) #@tweet = params[:content] is not enough to create a whole new instance
+        current_user.tweets << @tweet #@tweets not needed, changed tweets.erb form to current_user
+        #shows all a single users tweets
+        redirect to '/tweets/tweets'
+      else
+        redirect to "/login"
       end
+    end
 
 end
