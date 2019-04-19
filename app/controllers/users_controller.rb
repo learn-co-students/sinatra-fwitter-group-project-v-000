41b77279e4d0b4fc9a89ba02928f8812df6809df
Cheryl K <cherylkelson@icloud.com>
2019-04-19 11:44:07 +0000

    add code to post '/signup' that redirects user if signed up or not

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,40 @@ class UsersController < ApplicationController
         erb :'/users/create_user'
         # flash[:message] = "Welcome to Fwitter! Please sign up."
       else
-        redirect to '/tweets'
+        redirect '/tweets'
       end
     end
+
+    post '/signup' do
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
+        session[:user_id] = @user.id
+        redirect to '/tweets'
+    end
+    end
+
+    # get
+    # end
+    #
+    #
+    # get
+    # end
+    #
+    # put
+    # end
+    #
+    # delete
+    # end
+
+
+
+
+
+
+
 
 
 end
