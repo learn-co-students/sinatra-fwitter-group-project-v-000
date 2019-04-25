76ee3276c7248488cf53537b507e1ffa8955e1c2
sarahpaz <sarah.paz@gmail.com>
2019-04-24 22:50:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    if params[:username] != "" && params[:email] != "" && params[:password] != ""
-      @user = User.create(params)
-      session[:user_id] = @user.id 
-      redirect '/tweets'
-    else
-      redirect '/signup'
+    # if params[:username] != "" && params[:email] != "" && params[:password] != ""
+    #   @user = User.create(params)
+    #   session[:user_id] = @user.id 
+    #   redirect '/tweets'
+    # else
+    #   redirect '/signup'
+    redirect '/tweets'
     end
   end
 
