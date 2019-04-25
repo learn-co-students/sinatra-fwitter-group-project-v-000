7fce3a7c8ee80377df79938a787e95b6adf707ca
sarahpaz <sarah.paz@gmail.com>
2019-04-25 15:03:09 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,49 @@ class TweetsController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in? && params[:content] != ""
+      @tweet = Tweet.create(content: params[:content])
+      @tweet.user = User.find_by(params[:id])
+      @tweet.save
+
+      redirect "/tweets/#{@tweet.id}"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in? 
+      @tweet = Tweet.find_by(params[:id])
+      erb :'/tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+      @tweet = Tweet.find_by(params[:id])
+      erb :'/tweets/edit_tweet'
+    else
+      redirect '/login'
+    end  
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by(params[:id])
+    @tweet.update(content: params[:content]) unless params[:content] = ""
+    @tweet.save
+    redirect "/tweets/#{@tweet.id}"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,17 +21,19 @@ class UsersController < ApplicationController
     if !logged_in?
       erb :'/users/create_user'  
     else
-      redirect '/tweets' 
+      redirect '/tweets/new' 
     end
     erb :'/users/login'
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username])
-    @user.authenticate(params[:password])
-    session[:user_id] = @user.id
-    
-    redirect '/tweets'
+    @user = User.find_by(params[:id])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
   end  
 
   get '/logout' do
@@ -41,6 +43,12 @@ class UsersController < ApplicationController
     else 
       redirect '/'
     end
+  end
+
+  get '/users/:slug' do
+    @user = User.find_by(params[:id])
+    
+    erb :'/users/show'
   end
 
 end
