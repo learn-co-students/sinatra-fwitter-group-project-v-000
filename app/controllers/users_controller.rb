7f34473029ab3eca271e006838013cfc5973b60f
Dan Hintz <dhintz@live.com>
2019-05-25 02:28:29 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,12 @@ class UsersController < ApplicationController
   end
   
   get '/logout' do 
-    @session.clear
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
   end
   
   get '/failure' do
