8c12c9b4fa861145ed14cc9d02e79d5355acbbe1
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-26 10:42:30 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/users/login'
+  end
+
+  get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+  end
+
 end
