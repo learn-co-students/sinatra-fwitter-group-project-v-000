ece647bd3df13e0c288d651dca78444e9ebb4399
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-28 14:58:35 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+# use Rack::Flash
 
+  get '/users/:slug' do
+    @user = User.find_by_slub(params[:slug])
+    erb :'/users/show'
+  end
 
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,15 @@
 class Tweet < ActiveRecord::Base
 belongs_to :user
 
+# def slug
+#   name.downcase.gsub(" ", "-")
+# end
+#
+# def self.find_by_slug(slug)
+#   Tweet.all.find do |tweet|
+#     tweet.slug == slug
+#   end
+# end
 
 
 end
