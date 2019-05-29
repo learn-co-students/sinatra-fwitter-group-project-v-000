d90762ef272e1a873bc7fb6221c450949e2839d7
Kat Tochowicz <ktochow1@binghamton.edu>
2019-05-29 09:22:20 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
 # use Rack::Flash
 
-  get '/users/:slug' do
-    @user = User.find_by_slub(params[:slug])
-    erb :'/users/show'
-  end
+   # validates_presence_of :username, :email, :password
 
 end
