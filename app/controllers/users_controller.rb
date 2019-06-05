eab462237bf8ce8c1b7946c3dd2650f72712602b
Christiana Vandermale <christianavdm@gmail.com>
2019-06-05 23:12:51 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
 
  post '/signup' do
    @user = User.create(params)
+   session[:user_id] = @user.id
+   redirect '/tweets'
+ end
+
+ get '/login' do
+   erb :'/users/login'
+ end
+
+ post '/login' do
    binding.pry
+   @user = User.find_by_username(params[:username])
+   session[:user_id] = @user.id
+   redirect '/tweets'
  end
 end
