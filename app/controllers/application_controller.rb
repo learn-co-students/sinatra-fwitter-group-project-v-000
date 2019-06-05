4e11329be4228ad38a5ca37952dd50172279de7d
Christiana Vandermale <christianavdm@gmail.com>
2019-06-05 22:21:42 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
 
   configure do
     set :public_folder, 'public'
@@ -10,5 +13,4 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,9 @@
 class TweetsController < ApplicationController
 
+  get '/test' do
+    "how about this one?"
+  end
+
   get '/tweets/new' do
     erb :'tweets/new'
   end
