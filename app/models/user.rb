7216229fa34c15032efde29ca58da9a767166559
Christiana Vandermale <christianavdm@gmail.com>
2019-06-06 18:36:31 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
 
     def slug
-      self.name.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+      self.username.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
     end
 
     def self.find_by_slug(slug)
