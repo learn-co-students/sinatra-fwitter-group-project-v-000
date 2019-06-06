a7d3ae9935f204edfb9c5c0f032356a637aa557b
Christiana Vandermale <christianavdm@gmail.com>
2019-06-06 18:10:07 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_215104) do
+ActiveRecord::Schema.define(version: 2019_06_06_175254) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "content"
