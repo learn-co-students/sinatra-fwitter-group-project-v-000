b02830e26d49c740029decc7d52607e8d80850a7
BusterStatus <matthew.serre@gmail.com>
2019-06-23 15:25:22 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,7 @@ class TweetsController < ApplicationController
         @user = User.find_by(id: session[:user_id])
         # binding.pry
         if !@user.nil?
+            @tweets = Tweet.all
             erb :'/tweets/index'
         else
           redirect '/login'
