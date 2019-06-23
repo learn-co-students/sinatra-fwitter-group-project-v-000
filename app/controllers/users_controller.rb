a31f61ce219b060113d27b19be688eb72db23806
BusterStatus <matthew.serre@gmail.com>
2019-06-23 15:22:13 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,4 +71,10 @@ class UsersController < ApplicationController
         end
     end
 
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        @tweets = Tweet.all
+        erb :'/users/show'
+    end
+
 end
