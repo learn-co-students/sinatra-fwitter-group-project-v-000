f103b66574c88e6b66522e6b02802c4bd458d2ea
BusterStatus <matthew.serre@gmail.com>
2019-06-23 17:42:20 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,6 @@ class TweetsController < ApplicationController
 
     get '/tweets' do
         @user = User.find_by(id: session[:user_id])
-        # binding.pry
         if !@user.nil?
             @tweets = Tweet.all
             erb :'/tweets/index'
@@ -10,5 +9,58 @@ class TweetsController < ApplicationController
           redirect '/login'
         end
     end
+
+    get '/tweets/new' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            erb :'/tweets/new'
+        else
+          redirect '/login'
+        end
+    end
     
+    post '/tweets' do
+        if params[:content] != ""
+            @tweet = Tweet.create(content: params[:content], user_id: session[:user_id])
+            # binding.pry
+            redirect "/tweets/#{@tweet.id}"
+        else
+            redirect '/tweets/new'
+        end
+    end
+
+    get '/tweets/:id' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/show'
+        else
+          redirect '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            @tweet = Tweet.find(params[:id])
+            erb :'/tweets/edit'
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @user = User.find_by(id: session[:user_id])
+        if !@user.nil?
+            # binding.pry
+            if !params[:content] != ""
+                @tweet = Tweet.find(params[:id])
+                @tweet.update(content: params[:content])
+                redirect "/tweets/#{@tweet.id}"
+            end
+        else
+            redirect "/tweets/#{@tweet.id}/edit"
+        end
+    end
+
 end
