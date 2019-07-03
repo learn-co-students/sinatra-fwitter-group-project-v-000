ad57c74d32e910a5904a94120d09b1354523d0e0
Paul Fell <swellpf@gmail.com>
2019-07-02 21:12:03 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,60 @@
 class TweetsController < ApplicationController
 
 
+    get '/tweets' do
+        if Helpers.is_logged_in?(session)
+            @tweets = Tweet.all
+            erb :'/tweets/tweets'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/tweets/new' do
+        if Helpers.is_logged_in?(session)
+            erb :'/tweets/new'
+        else
+            redirect to '/login'
+        end
+    end
+
+    post '/tweets/' do
+    end
+
+    get '/tweets/:id' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            erb :'/tweets/show_tweet'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet && @tweet.user == current_user
+                erb :'/tweets/edit_tweet'
+            else
+                redirect to '/tweets'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+    end
+
+    delete '/tweets/:id/delete' do
+        if Helpers.is_logged_in?(session)
+            @tweet = Tweet.find_by_id(params[:id])
+            if @tweet && tweet.user == current_user
+                @tweet.delete
+            end
+            redirect to '/tweets'
+        else
+            redirect to '/login'
+        end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/signup' do
+        
+        @current_user = User.create(params)
+    end
+
+    get '/login' do
+        erb :'/users/login'
+    end
+
+    post '/login' do
+        @current_user = User.find_by(:username => params[:username])
+        if user && user.authenticate(params[:password])
+      session[:user_id] = @current_user.id
+      redirect to '/tweets/tweets'
+    end
+      erb :'/login'
+    end
+
+    get '/logout' do
+        session.clear
+        redirect to '/login'
+    end
 
 end
