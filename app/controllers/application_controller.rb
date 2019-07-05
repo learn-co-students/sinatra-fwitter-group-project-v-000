a74e009f099c7629efc19e310f3fcb57349e4b42
github username <github email address>
2019-07-05 16:01:49 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,49 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secretthatnooneknows"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session[:user_id].nil?
+      erb :'/users/create_user'
+    else
+      redirect to '/tweets'
+    end
+    
+  end
+
+  post '/signup' do
+    # binding.pry
+    if params[:username] === "" || params[:email] === "" || params[:password] === ""
+      redirect to "/signup"
+    end
+
+    user = User.create(params)
+    session[:user_id] = user.id
+    redirect to "/tweets/"
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+
+  post '/login' do
+    # binding.pry
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+    else
+      redirect to "/login"
+    end
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+   
+  
+
+ 
+
+#   post '/signup' do
+#     if params[:username].empty? || params[:email].empty? || params[:password].empty?
+#       redirect to "/signup"
+#     end
+#     # user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+#     # if !user.save
+#     #   redirect to "/signup"
+#     # end
+#     redirect to "/tweets"
+#   end  
+
+
 
 
 end
