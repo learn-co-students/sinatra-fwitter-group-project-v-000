5220eb8db029402b2f67c9cc161e24c9d8b2c18e
surpreet.mundra <surpreet.mundra@gmail.com>
2019-07-11 18:31:08 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,84 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'tweets/index'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+    erb :'/tweets/new'
+    else
+      redirect to "/login"
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/new'
+      else
+        @tweet = current_user.tweets.new(:content => params[:content])
+        if @tweet.save
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to "/tweets/new"
+        end
+      end
+    end
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+    @tweet = Tweet.find_by(params[:id])
+    erb :'tweets/show'
+    else
+    redirect to "/login"
+    end
+  end
+
+  delete '/tweets/:id/delete' do #destroy action
+    @tweet = Tweet.find_by_id(params[:id])
+    @tweet.delete
+    redirect to '/tweets'
+  end
+
+  get '/tweets/:id/edit' do
+    if logged_in?
+    @tweet = Tweet.find_by_id(params[:id])
+      if @tweet && @tweet.user_id == current_user.id
+        erb :'tweets/edit'
+        else
+          redirect to '/tweets'
+        end
+      else
+        redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to "/tweets/#{params[:id]}/edit"
+      else
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && @tweet.user == current_user
+          if @tweet.update(content: params[:content])
+            redirect to "/tweets/#{@tweet.id}"
+          else
+            redirect to "/tweets/#{@tweet.id}/edit"
+          end
+        else
+          redirect to '/tweets'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end
 
 end
