25f26c76de17fdbaae128cc0f7f59f2447b209ca
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-07-18 22:38:53 +0000

    saving progress on UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+      erb :"users/create_user"
+  end
 
+  post "/signup" do
+    if params[:username].blank? || params[:email].blank?
+      redirect "/signup" if session
+    elsif params[:password].blank?
+      redirect "/signup"
+    else
+      @user = User.create(params)
+      session[:user_id]
+      redirect "/tweets"
+    end
+  end
 end
