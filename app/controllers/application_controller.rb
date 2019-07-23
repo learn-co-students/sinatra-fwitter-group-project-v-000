f056365baca5192963b510eca49518d2cc283caf
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2019-07-23 18:12:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 require './config/environment'
 require 'sinatra/base'
-require 'rack-flash'
+#require 'rack-flash'
 
 
 class ApplicationController < Sinatra::Base
   enable :sessions
-  use Rack::Flash
+ # use Rack::Flash
   configure do
     set :session_secret, "secret"
     set :public_folder, 'public'
