a169bde2debe76b8b637511ffa2a8dc70ce77f68
Karly Mareka <github email address>
2019-08-14 14:10:05 -0700

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,36 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
+
+  get '/tweets/new' do
+
+    erb :'/tweets/new'
+  end
+
+  post '/tweets' do
+
+    redirect to '/tweets'
+  end
+
+  get '/tweets/:id' do
+
+    erb :'/tweets/show_tweet'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :'/tweets/edit_tweet'
+  end
+
+  patch '/tweets/:id' do
+
+    redirect to '/tweets/:id'
+  end
+
+  post '/tweets/:id/delete' do
+
+    redirect to '/tweets'
+  end
 
 end
