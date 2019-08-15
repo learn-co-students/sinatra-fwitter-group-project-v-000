2a8b7087700b3abf62d69f415ca3d0cb2ea4f5ad
Karly Mareka <github email address>
2019-08-14 18:43:19 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+
+    @user = User.create(:username => params[:username],
+      :email => params[:email], :password => params[:password])
+    session[:username] = @user.id
+    redirect to '/tweets'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+
+  end
+
+  get '/logout' do
+
+    redirect to '/login'
+  end
 
 end
--- config.ru
+++ config.ru
@@ -4,5 +4,11 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+require_relative 'app/controllers/tweets_controller'
+require_relative 'app/controllers/users_controller'
+
+use UsersController
+use TweetsController
+
 use Rack::MethodOverride
 run ApplicationController
