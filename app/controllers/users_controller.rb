3d13f0ad4f337138e9f74fd85335f3f9557e75e5
Karly Mareka <github email address>
2019-08-15 19:11:04 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,42 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'/users/create_user'
+    if session[:user_id] == nil
+      erb :'/users/create_user'
+    else
+      redirect to '/tweets'
+    end
   end
 
   post '/signup' do
-
     @user = User.create(:username => params[:username],
-      :email => params[:email], :password => params[:password])
-    session[:username] = @user.id
-    redirect to '/tweets'
+      :email => params[:email], :password_digest => params[:password])
+    session[:user_id] = @user.id
+    if @user.username.present? && @user.password_digest.present? &&
+      @user.email.present?
+      redirect to '/tweets'
+    else
+      redirect to '/signup'
+    end
   end
 
   get '/login' do
-    erb :'/users/login'
+    if session[:user_id] == nil
+      erb :'/users/login'
+    else
+      redirect to '/tweets'
+    end 
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    #binding.pry
+    if @user && @user.authenticate(params[:password_digest])
+      session[:user_id] = user.id
+      erb :'/tweets/tweets'
+    else
+      redirect to '/login'
+    end
   end
 
   get '/logout' do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates_presence_of :username, :email, :password
+  validates_presence_of :username, :email, :password_digest 
   has_many :tweets
+
+end
+
+class Helpers < ActiveRecord::Base
+
+  def self.current_user(user)
+    @user = User.find_by(id: user[:user_id])
+  end
+
+  def self.is_logged_in?(user)
+    if user[:user_id]
+      true
+    else
+      false
+    end
+  end
+
 end
