43b10ce368f051e311294212006522147bc55782
Azhar Shah <azharshahstl@yahoo.com>
2019-10-01 21:06:08 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -52,10 +52,37 @@ class TweetsController < ApplicationController
     end
   end 
   
+   delete "/tweets/:id/delete" do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && (@tweet.user == current_user)
+          @tweet.delete
+          redirect "/tweets"
+        else 
+          redirect "/tweets/#{@tweet.id}"
+        end
+    else
+      redirect '/login'
+    end
+  end
   
-  
- 
-  
-    
-
+  patch "/tweets/:id" do 
+    if logged_in? 
+      if params[:content] == ""
+        redirect "/tweets/#{params[:id]}/edit"
+      else
+        
+        @tweet = Tweet.find_by_id(params[:id])
+        if @tweet && (@tweet.user_id == current_user.id)
+           @tweet.update(content: params[:content])
+         
+           redirect "/tweets/#{@tweet.id}"
+        else 
+          redirect "/tweets"
+        end 
+      end
+    else
+       redirect "users/login"
+    end
+  end
 end
