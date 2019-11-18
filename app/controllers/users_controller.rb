1eba4bb3d242b7732a284595f4224e21d9ebfe64
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-18 23:22:48 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+     @user = User.find_by_slug(params[:slug])
+     erb :'/users/index'
+   end
   # Create route for users/slug here
   # Find the user by slug
   # render the users/show and show the tweets
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ActiveRecord::Base
   #Create an **instance** method slug to slugify the username
   #Create a **Class** method to find by slug
   #Hint
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    all.find do |username|
+    username.slug == slug
+    end
+  end
 end
