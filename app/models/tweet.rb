5f955bdd6746354170e9b6c011a5e3ab08809b83
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-11-27 21:11:47 +0000

    Done.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,15 +1,3 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 end
-
-# class CreateTweetsTable < ActiveRecord::Migration[4.2.5]
-#   def change
-#     create_table :tweets do |t|
-#       t.text :content
-#       t.integer :user_id
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
-#
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,14 @@
 class User < ActiveRecord::Base
-  has_secure_password
   has_many :tweets
+
+  has_secure_password
+
+  def slug
+    usernname.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 end
-
-
-# class CreateUsersTable < ActiveRecord::Migration[4.2.5]
-#   def change
-#     create_table :users do |t|
-#       t.string :username
-#       t.text :email
-#       t.string :password_digest
-#
-#       t.timestamps null: false
-#     end
-#   end
-# end
