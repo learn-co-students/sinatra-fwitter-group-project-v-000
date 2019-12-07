8cc8b5716ee6f95c2a094cfea7b8ba3bef1b193c
Toni-Ty <antonique2479@yahoo.com>
2019-12-07 04:41:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,125 +1,29 @@
-require './config/environment'
 #The ( :: ) symbol below is a unary operator that allows: constants, instance methods and class methods defined within a class or module, to be accessed from anywhere outside the class or module.
 # Base class inside the ActiveRecord.Module
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
-  enable :sessions
 
   configure do
-    set :session_secret, "secret"
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
-  get '/' do #home page
+  get '/' do
     erb :index
   end
 
-  get '/signup' do
-    if Helpers.is_logged_in?(session)
-      redirect to '/tweets'
-  end
+  helpers do
 
-    erb :"/users/create_user"
-  end
-
-  post '/signup' do
-    params.each do |label, user_input|
-      if user_input.empty?
-      flash[:new_error] = "Please enter a value for #{label}"
-      redirect to '/signup'
-    end
-  end
-
-    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
-    session[:userid] = user.id
-
-    redirect to '/tweets'
-  end
-
-  get '/login' do
-    if Helpers.is_logged_in(session)
-      redirect to '/tweets'
-  end
-
-    erb :"/users/login"
-  end
-
-    post '/login' do
-      user = User.find_by(:username => params["username"])
-
-      if user && user.authenticate(params[:password])
-        session[:userid] = user.id
-        redirect to '/tweets'
-
-      else
-        flash[:login_error] = "Incorrect login. Try again"
-        redirect to '/login'
-      end
+    def logged_in?
+      !!current_user
     end
 
-    get '/tweets' do
-      if !Helpers.is_logged_in?(session)
-        redirect to '/login'
-      end
-
-      @tweets = Tweet.all
-      @user = Helpers.current_user(session)
-      erb :"/tweets/tweets"
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
-    get '/tweets/new' do
-      if !Helpers.is_logged_in(session)
-        rediret to '/login'
-    end
-
-    erb :"/tweets/create_tweet"
   end
-
-    post '/tweets' do
-      user = Helperscurrent_user(session)
-      if params["content"].empty?
-        flash[:empty_tweet] = "Please enter your tweet content"
-        redirect to '/tweets/new'
-    end
-
-      tweet = Tweet.new(:content => params["content"], :user_id => user.id)
-
-      redirect to '/tweets'
-    end
-
-    get '/tweets/:id' do
-      if !Helpers.is_logged_in(session)
-        rediret to '/login'
-      end
-      @tweet = Tweet.find(params[:id])
-      erb :"/tweets/show_tweet"
-    end
-
-    get '/tweets/:id/edit' do
-      if !Helpers.is_logged_in(session)
-        rediret to '/login'
-      end
-
-        @tweet = Tweet.find(params[:id])
-        if Helpers.current_user(session).id != @tweet.user_id
-          flash[:wrong_user_edit] = "I'm sorry, but you can only edit your own tweets"
-          rediret to '/tweets'
-        end
-
-        erb :"/tweets/edit_tweet"
-      end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,13 +1,13 @@
-class Helpers #controls logic in views
-
-#singleton method or class method; a method added to a class Objectives
-#instance of the class Class
- def self.current_user(session_hash)
-   @user = User.find(session_hash[:user_id])
- end
-
-#logging in stores the user's ID in the session hash
- def self.is_logged_in?(session_hash)
-   !!session_hash[:user_id]
- end
-end
+# class Helpers #controls logic in views
+#
+# #singleton method or class method; a method added to a class Objectives
+# #instance of the class Class
+#  def self.current_user(session_hash)
+#    @user = User.find(session_hash[:user_id])
+#  end
+#
+# #logging in stores the user's ID in the session hash
+#  def self.is_logged_in?(session_hash)
+#    !!session_hash[:user_id]
+#  end
+# end
