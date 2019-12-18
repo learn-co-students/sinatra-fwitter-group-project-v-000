44795d6387b4813c64584d96522c24fe28e538fc
github username <github email address>
2019-12-18 09:00:35 +0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
 
-
+    get '/tweets' do
+        if Helpers.is_logged_in?(session)
+          @tweets = Tweet.all
+          erb :'/tweets/tweets'
+        else
+          redirect to '/users/login'
+        end
+      end
 end
