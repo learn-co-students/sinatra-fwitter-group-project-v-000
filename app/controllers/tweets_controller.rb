dca21171db35fb83766aff009ef83337c999a58d
github username <github email address>
2019-12-18 10:16:17 +0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,29 +5,74 @@ class TweetsController < ApplicationController
           @tweets = Tweet.all
           erb :'/tweets/tweets'
         else
-          redirect to '/users/login'
+          redirect '/users/login'
         end
       end
 
     get '/tweets/new' do 
       user = Helpers.current_user(session)
       if user.nil?
-        redirect to '/login'
+        redirect '/login'
       else
         erb :'/tweets/new'
+      end
     end
 
     post '/tweets' do 
-    user = Helpers.current_user(session)
+      user = Helpers.current_user(session)
       if user.nil?
         redirect to '/login'
       elsif params[:tweet][:content].empty?
-        redirect to '/tweets/new'
+        redirect '/tweets/new'
       else
         user.tweets.build({content: params[:tweet][:content]})
         user.save
       end
-        redirect to '/tweets'
+        redirect '/tweets'
     end
-      
+
+
+    get '/tweets/:id/edit' do
+      redirect '/login' unless Helpers.is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      if @tweet.user == Helpers.current_user(session)
+          erb :'/tweets/edit'
+      else
+        redirect '/login'
+      end
+    end
+
+    get '/tweets/:id' do 
+      redirect '/login' unless Helpers.is_logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show'
+    end
+
+    patch '/tweets/:id' do
+      @tweet = Tweet.find(params[:id])
+  
+      if params[:tweet][:content].empty?
+        redirect "/tweets/#{@tweet.id}/edit"
+      end
+  
+      @tweet.update(params[:tweet])
+      @tweet.save
+      redirect "/tweets/#{@tweet.id}"
+  
+    end
+  
+    delete '/tweets/:id/delete' do
+      if Helpers.is_logged_in?(session)
+        @tweet = Tweet.find(params[:id])
+        if @tweet.user == Helpers.current_user(session)
+          @tweet = Tweet.find_by_id(params[:id])
+          @tweet.delete
+          redirect '/tweets'
+        else
+          redirect '/tweets'
+        end
+      else
+        redirect '/login'
+      end
+    end
 end
