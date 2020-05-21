b591d6b4348cb7cecc58e2271547ae21f874727b
github username <github email address>
2020-05-20 20:44:16 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,27 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+enable :sessions
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    # enable :sessions
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    # @user = current_user if is_logged_in?
+    erb :index
+  end
+
+  helpers do
+    def current_user(session)
+      @user = User.find(session[:user_id])
+    end
+  
+    def is_logged_in?(session)
+      !!session[:user_id]
+    end
+  end  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        if is_logged_in?
+        redirect '/tweets'
+        else
+        erb :'/users/signup'
+        end
+    end
 
+    post '/signup' do
+        user = User.new(username:params[:username], email:params[:email], password:params[:password])
+        if user.save && user.username != " " && user.email != " "
+            session[:user_id] = user_id 
+        else
+           redirect to '/signup'
+        end
+      redirect to '/tweets'  
+    end 
 end
