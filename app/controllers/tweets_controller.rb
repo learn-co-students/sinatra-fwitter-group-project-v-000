f39ba08c512dc250a44ae358c5ffe3c90826059e
github username <github email address>
2020-05-22 22:29:06 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,23 +13,35 @@ class TweetsController < ApplicationController
     get '/tweets/new' do
         if is_logged_in?
             @user = current_user
-            erb :'/tweets/create_tweet'
+            erb :'tweets/create_tweet'
         else
             redirect '/login'
         end
     end
 
-    post '/tweets' do
-        # user = current_user
-        if !params[:content].empty?
-            @tweet = Tweet.create(:content=>params[:content], user:current_user)
-            current_user.tweets << @tweet
-            current_user.save
-            redirect '/tweets'
+     post '/tweets/new' do
+        @user = current_user
+        @tweet = Tweet.create(content: params[:content], user: @user)
+        binding.pry
+        if @tweet.save
+            
+            redirect to "/tweets"
         else
-            redirect '/tweets/new'
+            redirect to 'tweets/create_tweet'
         end
     end
+
+    # post '/tweets' do
+    #     # user = current_user
+    #     if !params[:content].empty?
+    #         @tweet = Tweet.create(:content=>params[:content], user:current_user)
+    #         current_user.tweets << @tweet
+    #         current_user.save
+    #         redirect '/tweets'
+    #     else
+    #         redirect '/tweets/new'
+    #     end
+    # end
 
     get '/tweets/:id' do
         if is_logged_in?
