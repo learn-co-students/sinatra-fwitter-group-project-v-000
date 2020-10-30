76c8a1e098a83301bbc9835d11cfb89d8e2c8a3d
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-10-29 22:34:31 -0700

    Added sign up form and session hash for log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get '/' do
@@ -12,12 +14,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    signed_in = nil
-    "You have reached the signup page"
-    if signed_in
-      redirect '/tweets'
+    "This is the signup page"
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect '/signup'
     else
-      "You are not signed in"
+      @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      #need helper method isLoggedIn?
+      redirect '/tweets'
     end
   end
 
