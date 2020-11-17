3bf32a092690c935f41ae120a7424bf63add38af
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-11-16 22:20:40 -0800

    Loads the tweets index after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,4 +38,28 @@ class ApplicationController < Sinatra::Base
     erb :'tweets/tweets'
   end
 
+  get '/login' do
+    if session[:user_id]
+      redirect '/tweets'
+    else
+      "This is the login page"
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/login'
+  end
+
 end
