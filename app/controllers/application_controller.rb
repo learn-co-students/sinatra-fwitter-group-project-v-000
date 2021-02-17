8544b7534d95d6b36f758e5d9d96c987cf7345e6
andy-aguilar <aaguil3@gmail.com>
2021-02-17 12:14:32 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  
   configure do
+    enable :sessions
     set :public_folder, 'public'
     set :views, 'app/views'
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
     get "/signup" do
-        erb :"users/signup"
+        if !session[:user_id]
+            erb :"users/signup"
+        else
+            erb :"users/tweets"
+        end
     end
 
     post "/signup" do
         @user = User.new(params)
         if @user.save
+            session[:user_id] = @user.id
             redirect "/tweets"
         else
             redirect "/signup"
