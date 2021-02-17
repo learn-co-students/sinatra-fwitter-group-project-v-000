bc2cb73b287e894156098aab90c08f54c3dc4a43
andy-aguilar <aaguil3@gmail.com>
2021-02-17 14:11:09 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,24 @@ class UsersController < ApplicationController
         end
     end
 
+    get "/login" do
+        if session[:user_id]
+            redirect "/tweets"
+        else
+            erb :"users/login"
+        end
+    end
+
+    post "/login" do
+        @user = User.find_by(username: params[:username])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect "/tweets"
+        else
+            redirect "/login"
+        end
+    end
+
     post "/signup" do
         @user = User.new(params)
         if @user.save
@@ -16,6 +34,20 @@ class UsersController < ApplicationController
         else
             redirect "/signup"
         end
+    end
+
+    get "/logout" do
+        if session[:user_id]
+            session.clear
+            redirect "/login"
+        else
+            redirect "/login"
+        end
+    end
+
+    get "/users/:slug" do
+        @user = User.find_by_slug(params[:slug])
+        erb :"users/show"
     end
 
 end
