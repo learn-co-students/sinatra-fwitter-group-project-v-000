484c319549e9a5ce9bec476b5b943ebed7ffa800
andy-aguilar <aaguil3@gmail.com>
2021-02-17 11:46:02 -0600

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
 
+    get "/signup" do
+        erb :"users/signup"
+    end
+
+    post "/signup" do
+        @user = User.create(params)
+        redirect "/tweets"
+    end
 
 end
