287b894b8095561a17cfbeccd0cd9fff7587e261
Laura Travin <laura.travin@gmail.com>
2021-03-05 17:07:22 -0500

    User Login route and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
   end
 
+  #home
+  get '/' do
+    erb :index
+  end
+
+  #helpers
+
+  helpers do
+    #find current user if the user logged in before
+    def current_user
+      @user_user || =  User.find_by_id(id: session[:user_id]) if session[:user_id]
+    end
+   
+    def logged_in?
+       !!current_user
+    end
+     
+
+    
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+    
+    get '/login' do
 
+        if !logged_in?
+          erb :"users/login" 
+        else
+          erb "/tweets"
+        end   
 
+    end    
+
+    post '/login' do
+        user = User.find_by(username => params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/tweets"
+        else
+            redirect "/signup"    
+        end    
+    end
+
+ 
 end
