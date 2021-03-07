3dc2dedb59d640b7df1b1e33609f1d2232a6ebe6
Laura Travin <laura.travin@gmail.com>
2021-03-07 14:55:30 -0500

    Tweet settins
    Controller, views done
    All 35 tests passed.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -45,4 +45,49 @@ class TweetsController < ApplicationController
 
     end
 
+    delete '/tweets/:id/delete' do
+        if is_logged_in? 
+            @tweet= Tweet.find_by_id(params[:id])
+            if @tweet && @tweet.user == current_user
+               @tweet.delete
+            end    
+            redirect "/tweets"
+        else
+            redirect "/login"
+        end
+    end
+
+    get '/tweets/:id/edit' do
+        if is_logged_in? 
+             @tweet=Tweet.find_by_id(params[:id])
+            if @tweet && @tweet.user == current_user
+              erb :'tweets/edit_tweet'
+            else 
+                redirect "/tweets"  
+            end  
+        else
+                redirect "/login"
+        end
+    end 
+
+    patch '/tweets/:id' do
+        if is_logged_in? 
+           
+            @tweet=Tweet.find_by_id(params[:id])
+            if params[:content].empty?
+                redirect "/tweets/#{params[:id]}/edit"
+            else   
+                if @tweet && @tweet.user == current_user  
+                      
+                        @tweet.update(:content => params[:content])
+                        redirect "/tweets/#{@tweet.id}" 
+                else
+                        redirect "/tweets/#{@tweet.id}/edit" 
+                end           
+            end
+        else
+            redirect "/login"
+        end        
+    end
+
 end
