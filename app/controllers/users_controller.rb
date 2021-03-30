70416272bb45890828fe840a2bfd2ef6c983f201
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-30 03:26:49 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
+
+  post '/signup' do
+    if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
+      user = User.create(params)
+      session[:user_id] = user.id
+
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id]= user.id
+      binding.pry
+      redirect '/tweets'
+    end
+  end
 
 end
